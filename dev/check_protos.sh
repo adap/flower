a778df14930e539afb393931943ae277f6084289
Taner Topal <tanertopal42@gmail.com>
2020-02-18 10:17:46 +0100

    Add script to detect changed proto files

--- dev/check_protos.sh
+++ dev/check_protos.sh
@@ -5,15 +5,13 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 # Purpose of this script is to evaluate if the user changed the proto definitions
 # but did not recompile or commit the new proto python files
 
-# Setup
-./bootstrap.sh
-
 # Recompile protos
-python -m flower_tools.grpc
+python -m flower_tools.grpc > /dev/null 2>&1
 
 # Fail if user forgot to recompile
 CHANGED=$(git diff-index --name-only HEAD --)
 
 if [ -n "$CHANGED" ]; then
+    echo "Proto files changed"
     exit 1
 fi
