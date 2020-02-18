52e4ba4db50a05344116762f8d3865d5e354182b
Taner Topal <tanertopal42@gmail.com>
2020-02-18 10:56:10 +0100

    Adjust test script

--- dev/test.sh
+++ dev/test.sh
@@ -2,8 +2,8 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
-isort --check-only -rc src && echo "- isort:  done" &&
-black --check src          && echo "- black:  done" &&
+isort -s src/flower/proto --check-only -rc src && echo "- isort:  done" &&
+black --exclude src/flower/proto --check src          && echo "- black:  done" &&
 mypy src                   && echo "- mypy:   done" &&
 pylint src/flower          && echo "- pylint: done" &&
 pytest src/flower          && echo "- pytest: done" &&
