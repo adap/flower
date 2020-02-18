3cac671e31bc7699fdcc376e7b80f6c4ccdf4b9e
Taner Topal <tanertopal42@gmail.com>
2020-02-18 11:03:11 +0100

    Adjust test.sh for compiled protos

--- dev/test.sh
+++ dev/test.sh
@@ -5,6 +5,6 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 isort -s src/flower/proto --check-only -rc src && echo "- isort:  done" &&
 black --exclude src/flower/proto --check src          && echo "- black:  done" &&
 mypy src                   && echo "- mypy:   done" &&
-pylint src/flower          && echo "- pylint: done" &&
+pylint --ignore=src/flower/proto src/flower          && echo "- pylint: done" &&
 pytest src/flower          && echo "- pytest: done" &&
 echo "- All Python checks passed"
