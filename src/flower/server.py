a3330e3b76e693351975b913440935741fd8776d
Daniel J. Beutel <daniel@adap.com>
2020-02-20 13:05:58 +0100

    Aggregate parameter updates using weighted average (for federated averaging) (#10)

--- src/flower/server.py
+++ src/flower/server.py
@@ -17,7 +17,10 @@
 
 import concurrent.futures
 import random
+from functools import reduce
 from typing import List, Tuple
+
+import numpy as np
 
 from flower.client import Client
 from flower.typing import Weights
@@ -88,8 +91,21 @@ def fit_client(client: Client, weights: Weights) -> Tuple[Weights, int]:
 
 
 def aggregate(results: List[Tuple[Weights, int]]) -> Weights:
-    """Stub for weight aggregation"""
-    return results[0][0]
+    """Compute weighted average"""
+    # Calculate the total number of examples used during training
+    num_examples_total = sum([num_examples for _, num_examples in results])
+
+    # Create a list of weights, each multiplied by the related number of examples
+    weighted_weights = [
+        [layer * num_examples for layer in weights] for weights, num_examples in results
+    ]
+
+    # Compute average weights of each layer
+    weights_prime: Weights = [
+        reduce(np.add, layer_updates) / num_examples_total
+        for layer_updates in zip(*weighted_weights)
+    ]
+    return weights_prime
 
 
 def eval_clients(clients: List[Client], weights: Weights) -> List[Tuple[int, float]]:
