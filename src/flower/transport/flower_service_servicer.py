d7997eac3ab94058ac76428179d26e02c02fc731
Taner Topal <taner@adap.com>
2020-02-21 19:43:39 +0100

    Fix typo

--- src/flower/transport/flower_service_servicer.py
+++ src/flower/transport/flower_service_servicer.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Servicer for for FlowerService."""
+"""Servicer for FlowerService."""
 from typing import Iterator
 
 import grpc
