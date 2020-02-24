8eb7b87aa0eda70ff39ddc09177c36586816bca3
Daniel <daniel@adap.com>
2020-02-24 22:36:44 +0100

    Test CI w/o isort

--- dev/test.sh
+++ dev/test.sh
@@ -2,7 +2,7 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
-isort -s src/flower/proto --check-only -rc src                  && echo "- isort:  done" &&
+#isort -s src/flower/proto --check-only -rc src                  && echo "- isort:  done" &&
 black --exclude src/flower/proto --check src                    && echo "- black:  done" &&
 mypy src                                                        && echo "- mypy:   done" &&
 pylint --ignore=src/flower/proto src/flower src/flower_tools    && echo "- pylint: done" &&
