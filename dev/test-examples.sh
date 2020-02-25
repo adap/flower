7f86d9f07f62ab55d807149ad929251799323c89
Daniel <daniel@adap.com>
2020-02-25 11:21:26 +0100

    Make checks in test.sh and test-examples.sh mutually exclusive

--- dev/test-examples.sh
+++ dev/test-examples.sh
@@ -2,9 +2,9 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
-isort -s src/flower/proto --check-only -rc src        && echo "- isort:  done" &&
-black --exclude src/flower/proto --check src          && echo "- black:  done" &&
-mypy src                                              && echo "- mypy:   done" &&
-pylint --ignore=src/flower/proto src/flower_examples  && echo "- pylint: done" &&
-pytest src/flower_examples                            && echo "- pytest: done" &&
+isort --check-only -rc src/flower_examples  && echo "- isort:  done" &&
+black --check src/flower_examples           && echo "- black:  done" &&
+# mypy is covered by test.sh
+pylint src/flower_examples                  && echo "- pylint: done" &&
+pytest src/flower_examples                  && echo "- pytest: done" &&
 echo "- All Python checks passed"
