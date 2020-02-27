6e6ec2a754696717d6d7e37316b615a4916679fd
Taner Topal <taner@adap.com>
2020-02-27 17:07:30 +0100

    Change module docstring to something more reasonable

--- src/flower/grpc_server/grpc_proxy_client_test.py
+++ src/flower/grpc_server/grpc_proxy_client_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Flower client (abstract base class)"""
+"""Networked Flower client implementation."""
 import unittest
 from unittest.mock import MagicMock
 
