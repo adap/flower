61d90618262e4e89aa73246504256625db880728
Taner Topal <taner@adap.com>
2020-03-05 15:03:01 +0100

    Upgrade Poetry and setuptools to the latest versions (#53)

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -4,8 +4,8 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
 # Upgrade/install spcific versions of `pip`, `setuptools`, and `poetry`
 python -m pip install -U pip==20.0.2
-python -m pip install -U setuptools==45.1.0
-python -m pip install -U poetry==1.0.3
+python -m pip install -U setuptools==45.2.0
+python -m pip install -U poetry==1.0.5
 
 # Use `poetry` to install project dependencies
 python -m poetry install --extras "examples-tensorflow"
