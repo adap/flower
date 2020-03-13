89f857a31ed998545f413e7e0b233be7664b7b8a
Daniel <daniel@adap.com>
2020-03-13 10:41:51 +0100

    Fix example module path in shell script

--- src/flower_examples/tf_cifar_10/run-server.sh
+++ src/flower_examples/tf_cifar_10/run-server.sh
@@ -19,4 +19,4 @@ set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../../../
 
 # Start five clients
-python -m flower_examples.tf_cifar_100.server
+python -m flower_examples.tf_cifar_10.server
