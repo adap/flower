a00675596ce629b63ec07c2c09a0fa8fae271e2d
Taner Topal <taner@adap.com>
2020-03-23 20:17:37 +0100

    Adjust cd to root path

--- src/flower_examples/tf_cifar/run-clients.sh
+++ src/flower_examples/tf_cifar/run-clients.sh
@@ -16,7 +16,7 @@
 # ==============================================================================
 
 set -e
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../../../
 
 GRPC_SERVER_ADDRESS="[::]"
 GRPC_SERVER_PORT=8080
