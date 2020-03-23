a0e251de9eb7881161c83dbca2e4faa335b02c97
Taner Topal <taner@adap.com>
2020-03-23 22:05:03 +0100

    Improves Fashion-MNIST strategy (#99)

--- src/flower_examples/tf_fashion_mnist/server.py
+++ src/flower_examples/tf_fashion_mnist/server.py
@@ -18,29 +18,57 @@
 import argparse
 from typing import Optional, Tuple
 
+import numpy as np
+
 import flower as flwr
 
 from . import DEFAULT_GRPC_SERVER_ADDRESS, DEFAULT_GRPC_SERVER_PORT
+from .client import load_data, load_model
 
 
 class FashionMnistStrategy(flwr.Strategy):
     """Strategy using at least three clients for training and evaluation."""
 
+    # pylint: disable-msg=too-many-arguments
+    def __init__(
+        self,
+        sample_fraction: float,
+        min_sample_size: int,
+        min_num_clients: int,
+        xy_test: Tuple[np.ndarray, np.ndarray],
+    ) -> None:
+        super().__init__()
+        self.sample_fraction = sample_fraction
+        self.min_sample_size = min_sample_size
+        self.min_num_clients = min_num_clients
+        self.x_test, self.y_test = xy_test
+
     def should_evaluate(self) -> bool:
         """Evaluate every round."""
-        return True
+        return False
 
     def num_fit_clients(self, num_available_clients: int) -> Tuple[int, int]:
-        """Use 10% of available clients for training (minimum: 3)."""
-        return int(max(num_available_clients * 0.1, 3)), 5
+        """Determine number of clients used for training."""
+        sample_size = int(
+            max(num_available_clients * self.sample_fraction, self.min_sample_size)
+        )
+        return sample_size, self.min_num_clients
 
     def num_evaluation_clients(self, num_available_clients: int) -> Tuple[int, int]:
-        """Use 5% of available clients for evaluation (minimum: 3)."""
-        return int(max(num_available_clients * 0.05, 3)), 5
+        """Determine number of clients used for evaluation."""
+        sample_size = int(
+            max(num_available_clients * self.sample_fraction, self.min_sample_size)
+        )
+        return sample_size, self.min_num_clients
 
     def evaluate(self, weights: flwr.Weights) -> Optional[Tuple[float, float]]:
-        """Do not evaluate using stretegy."""
-        return None
+        """Use entire Fashion-MNIST test set for evaluation."""
+        model = load_model()
+        model.set_weights(weights)
+        loss, acc = model.evaluate(
+            self.x_test, self.y_test, batch_size=len(self.x_test)
+        )
+        return float(loss), float(acc)
 
 
 def main() -> None:
@@ -85,8 +113,16 @@ def main() -> None:
     parser.add_argument("--cid", type=str, help="Client CID (no default)")
     args = parser.parse_args()
 
+    # Load evaluation data
+    _, xy_test = load_data(partition=0, num_clients=1)
+
     client_manager = flwr.SimpleClientManager()
-    strategy = FashionMnistStrategy()
+    strategy = FashionMnistStrategy(
+        sample_fraction=args.sample_fraction,
+        min_sample_size=args.min_sample_size,
+        min_num_clients=args.min_num_clients,
+        xy_test=xy_test,
+    )
     server = flwr.Server(client_manager=client_manager, strategy=strategy)
 
     # Run server
