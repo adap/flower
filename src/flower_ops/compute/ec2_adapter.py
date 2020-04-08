5f1113fe81b8b00558a974bb03a681e7b2513a20
Daniel J. Beutel <daniel@adap.com>
2020-04-08 19:19:35 +0200

    Update ec2_adapter.py

--- src/flower_ops/compute/ec2_adapter.py
+++ src/flower_ops/compute/ec2_adapter.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Provides standard interface for compute resources."""
+"""Provides an Adapter implementation for AWS EC2."""
+
 
 import itertools
 from typing import List, Optional, Tuple
