41fa425d1772efd3c0c7f06da348da991e9df2d3
Daniel J. Beutel <daniel@adap.com>
2020-04-09 17:21:28 +0200

    Break testing of flower_tool out of test.sh (#121)

--- dev/test.sh
+++ dev/test.sh
@@ -2,9 +2,9 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
-isort --skip src/flower/proto --skip src/flower_benchmark --skip src/flower_example --skip src/flower_ops --check-only -rc src && echo "- isort:  done" &&
-black --exclude "src\/(flower\/proto|flower_benchmark|flower_example|flower_ops)" --check src                                  && echo "- black:  done" &&
-mypy src                                                                                                                       && echo "- mypy:   done" &&
-pylint --ignore=src/flower/proto src/flower src/flower_tool                                                                    && echo "- pylint: done" &&
-pytest src/flower src/flower_tool                                                                                              && echo "- pytest: done" &&
+isort --skip src/flower/proto --check-only -rc src/flower  && echo "- isort:  done" &&
+black --exclude "src\/flower\/proto" --check src/flower    && echo "- black:  done" &&
+mypy src                                                   && echo "- mypy:   done" &&
+pylint --ignore=src/flower/proto src/flower                && echo "- pylint: done" &&
+pytest src/flower                                          && echo "- pytest: done" &&
 echo "- All Python checks passed"
