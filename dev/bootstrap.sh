6797ee9f189c8877f45949f27f47f49e9fdc0119
Daniel <daniel@adap.com>
2020-04-09 06:12:17 +0200

    Add benchmark dependencies to bootstrap.sh, clear caches

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -2,10 +2,13 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
+# Remove caches
+./dev/rm-caches.sh
+
 # Upgrade/install spcific versions of `pip`, `setuptools`, and `poetry`
 python -m pip install -U pip==20.0.2
 python -m pip install -U setuptools==45.2.0
 python -m pip install -U poetry==1.0.5
 
 # Use `poetry` to install project dependencies
-python -m poetry install --extras "examples-tensorflow ops"
+python -m poetry install --extras "benchmark examples-tensorflow ops"
