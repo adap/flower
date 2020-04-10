4421293bb413f30d88522cd03a109a6e96638f69
Daniel <daniel@adap.com>
2020-04-10 13:55:50 +0200

    Add Poetry/TensorFlow workaround to bootstrap script

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -12,3 +12,5 @@ python -m pip install -U poetry==1.0.5
 
 # Use `poetry` to install project dependencies
 python -m poetry install --extras "benchmark examples-tensorflow ops"
+# Temporary workaround (Poetry 1.0.5 cannot install TensorFlow 2.1.0)
+pip install -U tensorflow-cpu==2.1.0
