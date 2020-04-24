037c68a37659e4b1659a3b7a73173b80783bbd45
Daniel <daniel@adap.com>
2020-04-24 13:57:27 +0200

    Simplify Fashion-MNIST example

--- src/flower_example/tf_fashion_mnist/run-server.sh
+++ src/flower_example/tf_fashion_mnist/run-server.sh
@@ -23,7 +23,7 @@ GRPC_SERVER_PORT=8080
 
 # Start a Flower server
 python -m flower_example.tf_fashion_mnist.server \
-  --rounds=75 \
-  --sample_fraction=1.0 \
-  --min_sample_size=75 \
-  --min_num_clients=75
+  --rounds=5 \
+  --sample_fraction=0.5 \
+  --min_sample_size=5 \
+  --min_num_clients=5
