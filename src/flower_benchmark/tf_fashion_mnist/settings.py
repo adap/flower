d1053e2584357b395ed10e30d5d2204161ec3a57
Daniel J. Beutel <daniel@adap.com>
2020-05-19 17:24:01 +0200

    Create initial benchmark settings (#195)
    Co-authored-by: Taner Topal <taner@adap.com>

--- src/flower_benchmark/tf_fashion_mnist/settings.py
+++ src/flower_benchmark/tf_fashion_mnist/settings.py
@@ -22,21 +22,28 @@ from typing import List
 class ServerSetting:
     """Settings for the server."""
 
-    rounds: int = 100
-    min_num_clients: int = 100
-    sample_fraction: float = 1.0
-    min_sample_size: int = 100
-    training_round_timeout: int = 3600
-    lr_initial: float = 0.1
-    dry_run: bool = False
+    rounds: int
+    min_num_clients: int
+    sample_fraction: float
+    min_sample_size: int
+    training_round_timeout: int
+    lr_initial: float
+    dry_run: bool
 
 
 @dataclass
 class ClientSetting:
     """Settings for the client."""
 
-    num_clients: int = 100
-    dry_run: bool = False
+    # Individual per client
+    cid: str
+    partition: int
+    delay_factor: float
+
+    # Same across all clients
+    iid_fraction: float
+    num_clients: int
+    dry_run: bool
 
 
 @dataclass
@@ -65,9 +72,12 @@ SETTINGS = {
             sample_fraction=1.0,
             min_sample_size=1,
             training_round_timeout=600,
+            lr_initial=0.1,
             dry_run=True,
         ),
-        clients=[ClientSetting(num_clients=4, dry_run=True)],
+        clients=configure_uniform_clients(
+            iid_fraction=0.0, num_clients=4, dry_run=True
+        ),
     ),
     "minimal": Setting(
         server=ServerSetting(
@@ -76,7 +86,96 @@ SETTINGS = {
             sample_fraction=1.0,
             min_sample_size=3,
             training_round_timeout=3600,
+            lr_initial=0.1,
+            dry_run=False,
         ),
-        clients=[ClientSetting(num_clients=4)],
+        clients=configure_uniform_clients(
+            iid_fraction=0.0, num_clients=4, dry_run=False
+        ),
+    ),
+    "fedavg-sync": Setting(
+        server=ServerSetting(
+            rounds=10,
+            min_num_clients=80,
+            sample_fraction=1.0,
+            min_sample_size=80,
+            training_round_timeout=40,
+            lr_initial=0.1,
+            dry_run=False,
+        ),
+        clients=configure_clients(
+            iid_fraction=0.0,
+            num_clients=100,
+            dry_run=False,
+            delay_factor_fast=0.0,
+            delay_factor_slow=3.0,
+        ),
+    ),
+    "fedavg-async": Setting(
+        server=ServerSetting(
+            rounds=10,
+            min_num_clients=80,
+            sample_fraction=1.0,
+            min_sample_size=80,
+            training_round_timeout=20,
+            lr_initial=0.1,
+            dry_run=False,
+        ),
+        clients=configure_clients(
+            iid_fraction=0.0,
+            num_clients=100,
+            dry_run=False,
+            delay_factor_fast=0.0,
+            delay_factor_slow=3.0,
+        ),
     ),
 }
+
+
+def configure_uniform_clients(
+    iid_fraction: float, num_clients: int, dry_run: bool,
+) -> List[ClientSetting]:
+    """Configure `num_clients`, all using the same delay factor."""
+    clients = []
+    for i in range(num_clients):
+        client = ClientSetting(
+            # Individual
+            cid=str(i),
+            partition=i,
+            delay_factor=0.0,
+            # Shared
+            iid_fraction=iid_fraction,
+            num_clients=num_clients,
+            dry_run=dry_run,
+        )
+        clients.append(client)
+
+    return clients
+
+
+def configure_clients(
+    iid_fraction: float,
+    num_clients: int,
+    dry_run: bool,
+    delay_factor_fast: float,
+    delay_factor_slow: float,
+) -> List[ClientSetting]:
+    """Configure `num_clients` with different delay factors."""
+    clients = []
+    for i in range(num_clients):
+        client = ClientSetting(
+            # Individual
+            cid=str(i),
+            partition=i,
+            # Indices 0 to 49 fast, 50 to 99 slow
+            delay_factor=delay_factor_fast
+            if i < int(num_clients / 2)
+            else delay_factor_slow,
+            # Shared
+            iid_fraction=iid_fraction,
+            num_clients=num_clients,
+            dry_run=dry_run,
+        )
+        clients.append(client)
+
+    return clients
