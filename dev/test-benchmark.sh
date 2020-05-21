8cec59f406b42ab5e164ed03449993fe2f3981ab
Daniel <daniel@adap.com>
2020-05-21 08:05:22 +0200

    Make helper scripts less verbose

--- dev/test-benchmark.sh
+++ dev/test-benchmark.sh
@@ -2,9 +2,11 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
+echo "=== test-benchmark.sh ==="
+
 isort --check-only -rc src/flower_benchmark  && echo "- isort:  done" &&
-black --check src/flower_benchmark           && echo "- black:  done" &&
+black -q --check src/flower_benchmark           && echo "- black:  done" &&
 # mypy is covered by test.sh
 pylint src/flower_benchmark                  && echo "- pylint: done" &&
-pytest src/flower_benchmark                  && echo "- pytest: done" &&
+pytest -q src/flower_benchmark                  && echo "- pytest: done" &&
 echo "- All Python checks passed"
