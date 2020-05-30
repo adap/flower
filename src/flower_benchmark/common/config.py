5677b19a766624f1da21762ed83b8e79f2aca9d5
Taner Topal <taner@adap.com>
2020-05-30 22:31:17 +0200

    Add new function for delay factors

--- src/flower_benchmark/common/config.py
+++ src/flower_benchmark/common/config.py
@@ -21,6 +21,19 @@ import numpy as np
 
 from flower_ops.cluster import Instance
 
+SCORE_MISSING = 200
+
+DEVICE_DISTRIBUTION = [
+    ("10.0", "Note 10", 0.1612, 729),
+    ("Pie 9", "Samsung Galaxy Note 9", 0.374, 607),
+    ("Oreo 8.0/8.1", "Samsung Galaxy S8", 0.1129 + 0.0737, 359),
+    ("Nougat 7.0/7.1", "Samsung Galaxy S7", 0.0624 + 0.043, 343),
+    ("Marshmallow 6.0", "Samsung Galaxy Note 5", 0.0872, 226),
+    ("Lollipop 5.1", "Samsung Galaxy Note 4", 0.0484, SCORE_MISSING),
+    ("Kikkat 4.4", "Samsung Galaxy Note 4", 0.0187, SCORE_MISSING),
+    ("Other", "Samsung Galaxy S III", 0.0185, SCORE_MISSING),
+]
+
 
 def sample_delay_factors(
     num_clients: int, max_delay: float, seed: Optional[int]
@@ -32,6 +45,32 @@ def sample_delay_factors(
     step_size = max_delay / num_clients
     ds = [(i + 1) * step_size for i in range(num_clients)]
     return [p * d for p, d in zip(ps, ds)]
+
+
+def real_sample_delay_factors(
+    num_clients: int, max_delay, seed: Optional[int]
+) -> List[float]:
+    """Sample delay factors."""
+    np.random.seed(seed)
+
+    shares = [round(num_clients * dev[2]) for dev in DEVICE_DISTRIBUTION]
+    factors = []
+
+    for i, share in enumerate(shares):
+        for _ in range(share):
+            factors.append(DEVICE_DISTRIBUTION[i][3])
+
+    # Invert
+    max_value = max(factors)
+    factors = [max_value - f for f in factors]
+
+    # Normalize to max_delay
+    factors = [round(f / max_value * max_delay) for f in factors]
+
+    while len(factors) < num_clients:
+        factors.append(factors[-1])
+
+    return factors[:num_clients]
 
 
 def configure_client_instances(
---
+++ src/flower_benchmark/common/config_test.py
@@ -0,0 +1,40 @@
+# Copyright 2020 Adap GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Implments compute classes for EC2."""
+
+from .config import real_sample_delay_factors
+
+def test_real_sample_delay_factors_100():
+    """Test dealy factors."""
+    # Prepare
+    num_clients = 100
+
+    # Execute
+    factors = real_sample_delay_factors(num_clients=num_clients, max_delay=40, seed=2020)
+
+    # Assert
+    assert len(factors) == num_clients
+
+def test_real_sample_delay_factors_10():
+    """Test dealy factors."""
+    # Prepare
+    num_clients = 10
+
+    # Execute
+    factors = real_sample_delay_factors(num_clients=num_clients, max_delay=40, seed=2020)
+
+    # Assert
+    assert len(factors) == num_clients
+
