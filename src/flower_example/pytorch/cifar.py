6badf77254923c90b68bea90ce264a2a56064ca7
Daniel J. Beutel <daniel@adap.com>
2020-06-25 11:07:33 +0200

    Create Flower PyTorch example (#297)

--- src/flower_example/pytorch/cifar.py
+++ src/flower_example/pytorch/cifar.py
@@ -25,11 +25,13 @@ https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 
 
 from collections import OrderedDict
-from typing import cast
+from typing import Tuple
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
 from torch import Tensor
 
 import flower as fl
@@ -54,8 +56,8 @@ class Net(nn.Module):
     # pylint: disable-msg=arguments-differ,invalid-name
     def forward(self, x: Tensor) -> Tensor:
         """Compute forward pass."""
-        x = cast(Tensor, self.pool(F.relu(self.conv1(x))))
-        x = cast(Tensor, self.pool(F.relu(self.conv2(x))))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -77,3 +79,69 @@ class Net(nn.Module):
 def load_model() -> Net:
     """Load a simple CNN."""
     return Net()
+
+
+# pylint: disable-msg=unused-argument
+def load_data() -> Tuple[torchvision.datasets.CIFAR10, torchvision.datasets.CIFAR10]:
+    """Load CIFAR-10 (training and test set)."""
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+    trainset = torchvision.datasets.CIFAR10(
+        root=DATA_ROOT, train=True, download=True, transform=transform
+    )
+    testset = torchvision.datasets.CIFAR10(
+        root=DATA_ROOT, train=False, download=True, transform=transform
+    )
+    return trainset, testset
+
+
+def train(net: Net, trainloader: torch.utils.data.DataLoader, epochs: int) -> None:
+    """Train the network."""
+    # Define loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    print(f"Training {epochs} epoch(s) w/ {len(trainloader)} batches each")
+
+    # Train the network
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            # inputs, labels = data[0].to(device), data[1].to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+
+def test(net: Net, testloader: torch.utils.data.DataLoader) -> Tuple[float, float]:
+    """Validate the network on the entire test set."""
+    criterion = nn.CrossEntropyLoss()
+    correct = 0
+    total = 0
+    loss = 0.0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            # images, labels = data[0].to(device), data[1].to(device)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)  # pylint: disable-msg=no-member
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = correct / total
+    return loss, accuracy
--- src/flower_example/pytorch/client.py
+++ src/flower_example/pytorch/client.py
@@ -12,41 +12,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Flower client example using TensorFlow for Fashion-MNIST image classification."""
+"""Flower client example using PyTorch for CIFAR-10 image classification."""
 
 
 import argparse
 import timeit
-from typing import Tuple
 
-import numpy as np
-import tensorflow as tf
+import torch
+import torchvision
 
 import flower as fl
 
-from . import DEFAULT_SERVER_ADDRESS, fashion_mnist
+from . import DEFAULT_SERVER_ADDRESS, cifar
 
 
-class FashionMnistClient(fl.Client):
-    """Flower client implementing Fashion-MNIST image classification using TensorFlow/Keras."""
+class CifarClient(fl.Client):
+    """Flower client implementing CIFAR-10 image classification using PyTorch."""
 
     def __init__(
         self,
         cid: str,
-        model: tf.keras.Model,
-        xy_train: Tuple[np.ndarray, np.ndarray],
-        xy_test: Tuple[np.ndarray, np.ndarray],
-    ):
+        model: cifar.Net,
+        trainset: torchvision.datasets.CIFAR10,
+        testset: torchvision.datasets.CIFAR10,
+    ) -> None:
         super().__init__(cid)
         self.model = model
-        self.x_train, self.y_train = xy_train
-        self.x_test, self.y_test = xy_test
+        self.trainset = trainset
+        self.testset = testset
 
     def get_parameters(self) -> fl.ParametersRes:
-        parameters = fl.weights_to_parameters(self.model.get_weights())
+        print(f"Client {self.cid}: get_parameters")
+
+        weights: fl.Weights = self.model.get_weights()
+        parameters = fl.weights_to_parameters(weights)
         return fl.ParametersRes(parameters=parameters)
 
     def fit(self, ins: fl.FitIns) -> fl.FitRes:
+        print(f"Client {self.cid}: fit")
+
         weights: fl.Weights = fl.parameters_to_weights(ins[0])
         config = ins[1]
         fit_begin = timeit.default_timer()
@@ -55,37 +59,42 @@ class FashionMnistClient(fl.Client):
         epochs = int(config["epochs"])
         batch_size = int(config["batch_size"])
 
-        # Use provided weights to update the local model
+        # Set model parameters
         self.model.set_weights(weights)
 
-        # Train the local model using the local dataset
-        self.model.fit(
-            self.x_train, self.y_train, epochs=epochs, batch_size=batch_size, verbose=2
+        # Train model
+        trainloader = torch.utils.data.DataLoader(
+            self.trainset, batch_size=batch_size, shuffle=True
         )
+        cifar.train(self.model, trainloader, epochs=epochs)
 
         # Return the refined weights and the number of examples used for training
-        params_prime = fl.weights_to_parameters(self.model.get_weights())
-        num_examples = len(self.x_train)
+        weights_prime: fl.Weights = self.model.get_weights()
+        params_prime = fl.weights_to_parameters(weights_prime)
+        num_examples_train = len(self.trainset)
         fit_duration = timeit.default_timer() - fit_begin
-        return params_prime, num_examples, num_examples, fit_duration
+        return params_prime, num_examples_train, num_examples_train, fit_duration
 
     def evaluate(self, ins: fl.EvaluateIns) -> fl.EvaluateRes:
+        print(f"Client {self.cid}: evaluate")
+
         weights = fl.parameters_to_weights(ins[0])
 
         # Use provided weights to update the local model
         self.model.set_weights(weights)
 
         # Evaluate the updated model on the local dataset
-        loss, accuracy = self.model.evaluate(
-            self.x_test, self.y_test, batch_size=len(self.x_test), verbose=2
+        testloader = torch.utils.data.DataLoader(
+            self.testset, batch_size=32, shuffle=False
         )
+        loss, accuracy = cifar.test(self.model, testloader)
 
         # Return the number of evaluation examples and the evaluation result (loss)
-        return len(self.x_test), float(loss), float(accuracy)
+        return len(self.testset), float(loss), float(accuracy)
 
 
 def main() -> None:
-    """Load data, create and start FashionMnistClient."""
+    """Load data, create and start CifarClient."""
     parser = argparse.ArgumentParser(description="Flower")
     parser.add_argument(
         "--server_address",
@@ -97,12 +106,6 @@ def main() -> None:
         "--cid", type=str, required=True, help="Client CID (no default)"
     )
     parser.add_argument(
-        "--partition", type=int, required=True, help="Partition index (no default)"
-    )
-    parser.add_argument(
-        "--clients", type=int, required=True, help="Number of clients (no default)",
-    )
-    parser.add_argument(
         "--log_host", type=str, help="Logserver address (no default)",
     )
     args = parser.parse_args()
@@ -111,13 +114,11 @@ def main() -> None:
     fl.logger.configure(f"client_{args.cid}", host=args.log_host)
 
     # Load model and data
-    model = fashion_mnist.load_model()
-    xy_train, xy_test = fashion_mnist.load_data(
-        partition=args.partition, num_partitions=args.clients
-    )
+    model = cifar.load_model()
+    trainset, testset = cifar.load_data()
 
     # Start client
-    client = FashionMnistClient(args.cid, model, xy_train, xy_test)
+    client = CifarClient(args.cid, model, trainset, testset)
     fl.app.start_client(args.server_address, client)
 
 
