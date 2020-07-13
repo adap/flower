6393602475434fd856edcaf5ae2638762f79bccd
Taner Topal <taner@adap.com>
2020-07-13 17:39:06 +0200

    Rename flower to flwr and restructure packages

--- dev/test-ops.sh
+++ dev/test-ops.sh
@@ -4,9 +4,9 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
 echo "=== test-ops.sh ==="
 
-isort --check-only -rc src/flower_ops  && echo "- isort:  done" &&
-black -q --check src/flower_ops           && echo "- black:  done" &&
+isort --check-only -rc src/flwr_experimental/ops  && echo "- isort:  done" &&
+black -q --check src/flwr_experimental/ops           && echo "- black:  done" &&
 # mypy is covered by test.sh
-pylint src/flower_ops                  && echo "- pylint: done" &&
-pytest -q src/flower_ops                  && echo "- pytest: done" &&
+pylint src/flwr_experimental/ops                  && echo "- pylint: done" &&
+pytest -q src/flwr_experimental/ops                  && echo "- pytest: done" &&
 echo "- All Python checks passed"
