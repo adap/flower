f7d027395434377cc99a05db027d09a869bb4575
Taner Topal <taner@adap.com>
2020-07-16 17:57:31 +0200

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,7 +35,7 @@ extensions = [
     "sphinx.ext.autodoc",
 ]
 
-# Add any paths t   hat contain templates here, relative to this directory.
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
