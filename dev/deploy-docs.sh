5c3a0e7a37795279ec795b6d44e4d547cb09a75e
Taner Topal <taner@adap.com>
2020-07-17 13:32:37 +0200

    Fix the docs deployment script

--- dev/deploy-docs.sh
+++ dev/deploy-docs.sh
@@ -19,6 +19,6 @@ set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
 cd doc
-make html
+make docs
 cd build/html
 aws s3 sync --delete --exclude ".*" --acl public-read --cache-control "no-cache" ./ s3://flower.dev
