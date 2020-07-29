ff330b6a2b1e5f41ef04ec4cadf443368bc63e0c
Taner Topal <taner@adap.com>
2020-07-29 15:24:41 +0200

    Bump version for next release

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2020, Adap GmbH"
 author = "The Flower Authors"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.0"
+release = "0.4.0"
 
 
 # -- General configuration ---------------------------------------------------
