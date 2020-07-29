bcc97d33850f1aca055ade2343bb8ec9d7084028
Taner Topal <taner@adap.com>
2020-07-29 15:47:41 +0200

    Add GA to docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,18 +1,25 @@
+# Copyright 2020 Adap GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from sphinx.application import ConfigError
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -68,3 +75,39 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+
+# -- Analytics ---------------------------------------------------------------
+
+googleanalytics_id = "UA-173987939-1"
+
+
+def html_page_context(app, pagename, templatename, context, doctree):
+    metatags = context.get("metatags", "")
+    metatags += (
+        """<script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '%s']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>"""
+        % app.config.googleanalytics_id
+    )
+    context["metatags"] = metatags
+
+
+def check_config(app):
+    if not app.config.googleanalytics_id:
+        raise ConfigError(
+            "'googleanalytics_id' config value must be set for ga statistics to function properly."
+        )
+
+
+def setup(app):
+    app.add_config_value("googleanalytics_id", "", "html")
+    app.connect("builder-inited", check_config)
+    app.connect("html-page-context", html_page_context)
