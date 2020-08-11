d4a48d74d91f4a0c8e9dc28bb39a084a12b223f4
Can Tuerk <can.tuerkprivat@gmail.com>
2020-08-11 16:48:58 +0200

    Add Docformatter to format.sh and test.sh ( is missing --exclude flag )

--- dev/test.sh
+++ dev/test.sh
@@ -6,6 +6,8 @@ echo "=== test.sh ==="
 
 isort --skip src/flwr/proto --check-only -rc src/flwr     && echo "- isort:  done" &&
 black -q --exclude "src\/flwr\/proto" --check src/flwr  && echo "- black:  done" &&
+# docformatter is missing --exclude src/flwr/proto 
+docformatter -c -r src/flwr  && echo "- docformatter:  done" &&
 # mypy src                                                  && echo "- mypy:   done" &&
 pylint --ignore=src/flwr/proto src/flwr                   && echo "- pylint: done" &&
 pytest -q src/flwr                                        && echo "- pytest: done" &&
