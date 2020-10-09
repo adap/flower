cbd179c5372d342a41c3abc9cd2652f1ca010a31
Taner Topal <taner@adap.com>
2020-10-09 15:16:46 +0200

    Revert change - moved to different PR

--- src/py/flwr_experimental/ops/cluster.py
+++ src/py/flwr_experimental/ops/cluster.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Implements compute classes for EC2."""
+"""Implments compute classes for EC2."""
 import concurrent.futures
 from contextlib import contextmanager
 from itertools import groupby
