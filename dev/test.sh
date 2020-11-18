59e54466c80039a08f4e408089413dbf44a20871
Can Tuerk <can@adap.com>
2020-11-18 16:22:42 +0100

    Add docformatter to test and format.sh

--- dev/test.sh
+++ dev/test.sh
@@ -5,9 +5,8 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 echo "=== test.sh ==="
 
 isort --skip src/flwr/proto --check-only -rc src/flwr     && echo "- isort:  done" &&
-black -q --exclude "src\/flwr\/proto" --check src/flwr  && echo "- black:  done" &&
-# docformatter is missing --exclude src/flwr/proto 
-# docformatter -c -r src/flwr  && echo "- docformatter:  done" &&
+black -q --exclude "src\/flwr\/proto" --check src/flwr    && echo "- black:  done" &&
+docformatter -c -r src/flwr -e src/flwr/proto             && echo "- docformatter:  done" &&
 # mypy src                                                  && echo "- mypy:   done" &&
 pylint --ignore=src/flwr/proto src/flwr                   && echo "- pylint: done" &&
 pytest -q src/flwr                                        && echo "- pytest: done" &&
