f439ecd3f4ae18bee859badbc5d00ea5a6a42ba3
mariaboerner1987 <maria@adap.com>
2020-12-04 11:42:44 +0100

    adjust comment which batches are printed

--- src/py/flwr_example/pytorch_minimal/cifar.py
+++ src/py/flwr_example/pytorch_minimal/cifar.py
@@ -112,7 +112,7 @@ def train(
 
             # print statistics
             running_loss += loss.item()
-            if i % 500 == 99:  # print every 2000 mini-batches
+            if i % 500 == 99:  # print every 500 mini-batches
                 print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
 
