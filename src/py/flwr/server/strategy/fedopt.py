3c2daab2b83b01cbb78229a25aa10332f51372c3
Pedro Porto Buarque de Gusmão <pedropgusmao@gmail.com>
2021-01-05 13:34:32 +0000

    FedAdagrad including changes in all weights

---
+++ src/py/flwr/server/strategy/fedopt.py
@@ -0,0 +1,68 @@
+# Copyright 2020 Adap GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Adaptive Federated Optimization (FedOpt) [Reddi et al., 2020] abstract
+strategy.
+
+Paper: https://arxiv.org/abs/2003.00295
+"""
+
+
+from typing import Callable, Dict, Optional, Tuple
+
+from flwr.common import Weights
+
+from .fedavg import FedAvg
+
+
+class FedOpt(FedAvg):
+    """Configurable FedAdagrad strategy implementation."""
+
+    # pylint: disable-msg=too-many-arguments,too-many-instance-attributes
+    def __init__(
+        self,
+        *,
+        fraction_fit: float = 0.1,
+        fraction_eval: float = 0.1,
+        min_fit_clients: int = 2,
+        min_eval_clients: int = 2,
+        min_available_clients: int = 2,
+        eval_fn: Optional[Callable[[Weights], Optional[Tuple[float, float]]]] = None,
+        on_fit_config_fn: Optional[Callable[[int], Dict[str, str]]] = None,
+        on_evaluate_config_fn: Optional[Callable[[int], Dict[str, str]]] = None,
+        accept_failures: bool = True,
+        current_weights: Weights,
+        eta: float = 1e-1,
+        eta_l: float = 1e-1,
+        tau: float = 1e-9,
+    ) -> None:
+        super().__init__(
+            fraction_fit,
+            fraction_eval,
+            min_fit_clients,
+            min_eval_clients,
+            min_available_clients,
+            eval_fn,
+            on_fit_config_fn,
+            on_evaluate_config_fn,
+            accept_failures,
+        )
+        self.current_weights = current_weights
+        self.eta = eta
+        self.eta_l = eta_l
+        self.tau = tau
+
+    def __repr__(self) -> str:
+        rep = f"FedOpt(accept_failures={self.accept_failures})"
+        return rep
