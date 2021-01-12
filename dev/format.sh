a9cb685667d70b4f69a8bfe42f4d2aab0a121364
Can Türk <67055966+CanTuerk@users.noreply.github.com>
2021-01-12 16:17:58 +0100

    Proto Linter | Add clang-format to Flower (#584)

--- dev/format.sh
+++ dev/format.sh
@@ -8,6 +8,9 @@ python -m black -q --exclude src/py/flwr/proto src/py
 python -m docformatter -i -r src/py/flwr -e src/py/flwr/proto
 python -m docformatter -i -r src/py/flwr_experimental
 
+# Protos
+find src/proto/flwr/proto -name *.proto | grep "\.proto" | xargs clang-format-10 -i
+
 # Examples
 python -m isort -rc examples
 python -m black -q examples
