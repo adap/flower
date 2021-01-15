ff41e8e2116e4303f8973c6a541afe52b45e8d96
Taner Topal <taner@adap.com>
2021-01-15 12:19:39 +0100

    Run fnt

--- src/py/flwr_tool/init_py_check.py
+++ src/py/flwr_tool/init_py_check.py
@@ -38,8 +38,8 @@ def check_missing_init_files(absolute_path: str) -> None:
 if __name__ == "__main__":
     if len(sys.argv) == 0:
         raise Exception(
-    "Please provide at least one directory path relative to your current working directory."
-    )
+            "Please provide at least one directory path relative to your current working directory."
+        )
     for i in range(len(sys.argv)):
         abs_path: str = os.path.abspath(os.path.join(os.getcwd(), sys.argv[i]))
         check_missing_init_files(abs_path)
