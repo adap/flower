648970ee358f2fb8a014bfa04fbe8fbb612a70d0
Pedro Porto Buarque de Gusmão <pedropgusmao@gmail.com>
2021-01-18 16:00:18 +0000

    Includeing #noaqa to ignore flake8 error message

--- src/py/flwr/dataset/utils/common.py
+++ src/py/flwr/dataset/utils/common.py
@@ -263,9 +263,8 @@ def create_dla_partitions(
     boundaries: List[int] = np.append(
         [0], np.cumsum(num_samples_per_class, dtype=np.int)
     )
-    list_samples_per_class: List[List[np.ndarray]] = [
-        x_l[boundaries[idx] : boundaries[idx + 1]] # noqa: E203
-        for idx in range(num_classes)  # noqa: E203
+    list_samples_per_class: List[List[np.ndarray]] = [  # noqa: E203
+        x_l[boundaries[idx] : boundaries[idx + 1]] for idx in range(num_classes)
     ]
 
     if dirichlet_dist.size == 0:
