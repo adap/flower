a5cf55a60edcb52ddadc7150c95d384d8c17e62b
Can Tuerk <can@adap.com>
2021-01-19 12:56:04 +0100

    Update install-clang-format.sh

--- dev/install-clang-format.sh
+++ dev/install-clang-format.sh
@@ -2,12 +2,11 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
-# Install Clang-Format
-# sudo apt install clang-format
-
+# Check if Clang-Format is already installed
 if clang-format --version | grep -q 'clang-format version 10.0.0-4ubuntu1'; 
 then
    echo "Clang-Format is already installed on your Environment"
 else
+    # Install Clang-Format if it is not installed already
     sudo apt install clang-format
 fi
