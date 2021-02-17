2ee1fd9bb79718cc116e83d593f18429008be7b9
Daniel J. Beutel <daniel@adap.com>
2021-02-17 12:54:47 +0100

    Create advanced TensorFlow/Keras example (#627)

---
+++ examples/advanced_tensorflow/run.sh
@@ -0,0 +1,11 @@
+python server.py & sleep 2 \
+  & python client.py --partition=0 & sleep 0.1 \
+  & python client.py --partition=1 & sleep 0.1 \
+  & python client.py --partition=2 & sleep 0.1 \
+  & python client.py --partition=3 & sleep 0.1 \
+  & python client.py --partition=4 & sleep 0.1 \
+  & python client.py --partition=5 & sleep 0.1 \
+  & python client.py --partition=6 & sleep 0.1 \
+  & python client.py --partition=7 & sleep 0.1 \
+  & python client.py --partition=8 & sleep 0.1 \
+  & python client.py --partition=9
