1676acbd4096f2c417cc957e0ebb115918c85e94
Daniel J. Beutel <daniel@adap.com>
2021-02-18 16:38:33 +0100

    Update examples to 0.14 return types (#638)

--- examples/quickstart_tensorflow/client.py
+++ examples/quickstart_tensorflow/client.py
@@ -22,13 +22,13 @@ if __name__ == "__main__":
 
         def fit(self, parameters, config):  # type: ignore
             model.set_weights(parameters)
-            model.fit(x_train, y_train, epochs=1, batch_size=32, steps_per_epoch=3)
-            return model.get_weights(), len(x_train)
+            model.fit(x_train, y_train, epochs=1, batch_size=32)
+            return model.get_weights(), len(x_train), {}
 
         def evaluate(self, parameters, config):  # type: ignore
             model.set_weights(parameters)
             loss, accuracy = model.evaluate(x_test, y_test)
-            return len(x_test), loss, accuracy
+            return loss, len(x_test), {"accuracy": accuracy}
 
     # Start Flower client
     fl.client.start_numpy_client("0.0.0.0:8080", client=CifarClient())
