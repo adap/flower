correct loss calculation

examples/mxnet_from_centralized_to_federated/client.py
@@ -46,7 +46,7 @@ class MNISTClient(fl.client.NumPyClient):
     ) -> Tuple[List[np.ndarray], int, Dict]:
         # Set model parameters, train model, return updated model parameters
         self.set_parameters(parameters)
-        mxnet_mnist.train(self.model, self.train_data, epoch=1, device=self.device)
+        mxnet_mnist.train(self.model, self.train_data, epoch=2, device=self.device)
         return self.get_parameters(), self.train_data.batch_size, {}
 
     def evaluate(
@@ -55,6 +55,8 @@ class MNISTClient(fl.client.NumPyClient):
         # Set model parameters, evaluate model on local test dataset, return result
         self.set_parameters(parameters)
         loss, accuracy = mxnet_mnist.test(self.model, self.val_data, device=self.device)
+        print("Evaluation Loss", loss)
+        print("Evaluation Accuracy", accuracy)
         return float(loss), self.val_data.batch_size, {"accuracy": float(accuracy)}
 
 

examples/mxnet_from_centralized_to_federated/mxnet_mnist.py
@@ -34,6 +34,7 @@ def model():
     # Define simple Sequential model
     net = nn.Sequential()
     net.add(nn.Dense(256, activation="relu"))
+    net.add(nn.Dense(64, activation='relu'))
     net.add(nn.Dense(10))
     net.collect_params().initialize()
     return net
@@ -42,13 +43,15 @@ def model():
 def train(
     net: mx.gluon.nn, train_data: mx.io.NDArrayIter, epoch: int, device: mx.context
 ) -> None:
-    trainer = gluon.Trainer(net.collect_params(), "sgd", {"learning_rate": 0.03})
+    trainer = gluon.Trainer(net.collect_params(), "sgd", {"learning_rate": 0.01})
     # Use Accuracy as the evaluation metric.
     metric = mx.metric.Accuracy()
+    loss_metric = mx.metric.CrossEntropy()
     softmax_cross_entropy_loss = gluon.loss.SoftmaxCrossEntropyLoss()
     for i in range(epoch):
         # Reset the train data iterator.
         train_data.reset()
+        
         # Loop over the train data iterator.
         for batch in train_data:
             # Splits train data into multiple slices along batch_axis
@@ -70,18 +73,21 @@ def train(
                     loss = softmax_cross_entropy_loss(z, y)
                     # Backpropogate the error for one iteration.
                     loss.backward()
-                    outputs.append(z)
+                    outputs.append(z.softmax())
             # Updates internal evaluation
             metric.update(label, outputs)
+            
             # Make one step of parameter update. Trainer needs to know the
             # batch size of data to normalize the gradient by 1/batch_size.
             trainer.step(batch.data[0].shape[0])
         # Gets the evaluation result.
         name, acc = metric.get()
-        # name_loss, running_loss = loss_metric.get()
+        loss_metric.update(label, outputs)
+        name_loss, running_loss = loss_metric.get()
         # Reset evaluation result to initial state.
         metric.reset()
         print("training acc at epoch %d: %s=%f" % (i, name, acc))
+        # print("training loss", loss_metric )
 
 
 def test(
@@ -89,7 +95,7 @@ def test(
 ) -> Tuple[float, float]:
     # Use Accuracy as the evaluation metric.
     metric = mx.metric.Accuracy()
-    loss_metric = mx.metric.Loss()
+    loss_metric = mx.metric.CrossEntropy()
     loss = 0.0
     # Reset the validation data iterator.
     val_data.reset()
@@ -105,11 +111,11 @@ def test(
         )
         outputs = []
         for x in data:
-            outputs.append(net(x))
-            loss_metric.update(label, outputs)
-            loss += loss_metric.get()[1]
+            outputs.append(net(x).softmax())
         # Updates internal evaluation
         metric.update(label, outputs)
+    loss_metric.update(label, outputs)
+    loss += loss_metric.get()[1]
     accuracy = metric.get()[1]
     return loss, accuracy
 