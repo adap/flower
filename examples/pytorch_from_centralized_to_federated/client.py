0b93b35676845176520befa59e5cef32988bc9a5
Meirui Jiang <31134669+MeiruiJiang@users.noreply.github.com>
2021-04-13 11:26:19 +0800

    Update client.py
    Changing back to default settings to be consistent with other examples.

--- examples/pytorch_from_centralized_to_federated/client.py
+++ examples/pytorch_from_centralized_to_federated/client.py
@@ -70,7 +70,7 @@ def main() -> None:
 
     # Start client
     client = CifarClient(model, trainloader, testloader)
-    fl.client.start_numpy_client("localhost:8080", client)
+    fl.client.start_numpy_client("[::]:8080", client)
 
 
 if __name__ == "__main__":
