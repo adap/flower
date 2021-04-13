bb1fe27cda152fc9728aa7a29674a81d0cfb4b61
Meirui Jiang <31134669+MeiruiJiang@users.noreply.github.com>
2021-04-13 11:14:58 +0800

    Update examples/pytorch_from_centralized_to_federated/client.py
    Co-authored-by: Maria Boerner <69673117+mariaboerner1987@users.noreply.github.com>

--- examples/pytorch_from_centralized_to_federated/client.py
+++ examples/pytorch_from_centralized_to_federated/client.py
@@ -70,7 +70,6 @@ def main() -> None:
 
     # Start client
     client = CifarClient(model, trainloader, testloader)
-    # fl.client.start_numpy_client("[::]:9080", client)
     fl.client.start_numpy_client("localhost:8080", client)
     
 
