e972ee6f76a0f67835bfdd8d57f56933177f0387
Meirui Jiang <31134669+MeiruiJiang@users.noreply.github.com>
2021-04-13 11:15:11 +0800

    Update examples/pytorch_from_centralized_to_federated/server.py
    Co-authored-by: Maria Boerner <69673117+mariaboerner1987@users.noreply.github.com>

--- examples/pytorch_from_centralized_to_federated/server.py
+++ examples/pytorch_from_centralized_to_federated/server.py
@@ -4,4 +4,4 @@
 import flwr as fl
 
 if __name__ == "__main__":
-    fl.server.start_server("localhost:9080", config={"num_rounds": 30})
+    fl.server.start_server("[::]:8080", config={"num_rounds": 3})
