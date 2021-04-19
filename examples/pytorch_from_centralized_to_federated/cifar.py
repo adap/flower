812627e58bb501392214b6b1e99d3a5c97eb2386
Meirui Jiang <31134669+MeiruiJiang@users.noreply.github.com>
2021-04-19 20:21:00 +0800

    Implementation of fedbn on client side (#696)
    Co-authored-by: Maria Boerner <maria@adap.com>
    Co-authored-by: Daniel J. Beutel <daniel@adap.com>

--- examples/pytorch_from_centralized_to_federated/cifar.py
+++ examples/pytorch_from_centralized_to_federated/cifar.py
@@ -30,20 +30,24 @@ class Net(nn.Module):
     def __init__(self) -> None:
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
+        self.bn1 = nn.BatchNorm2d(6)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
+        self.bn2 = nn.BatchNorm2d(16)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.bn3 = nn.BatchNorm1d(120)
         self.fc2 = nn.Linear(120, 84)
+        self.bn4 = nn.BatchNorm1d(84)
         self.fc3 = nn.Linear(84, 10)
 
     # pylint: disable=arguments-differ,invalid-name
     def forward(self, x: Tensor) -> Tensor:
         """Compute forward pass."""
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
         x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.bn3(self.fc1(x)))
+        x = F.relu(self.bn4(self.fc2(x)))
         x = self.fc3(x)
         return x
 
