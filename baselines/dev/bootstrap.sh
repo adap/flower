969ba03ce62faf26a21752d17ef4fc287d4c0f37
Taner Topal <taner@adap.com>
2021-05-11 11:21:58 +0200

    Update poetry and setuptools

--- baselines/dev/bootstrap.sh
+++ baselines/dev/bootstrap.sh
@@ -14,7 +14,7 @@ rm -f poetry.lock
 
 # Upgrade/install spcific versions of `pip`, `setuptools`, and `poetry`
 python -m pip install -U pip==21.0.1
-python -m pip install -U setuptools==49.3.1
+python -m pip install -U  setuptools==56.2.0
 python -m pip install -U poetry
 
 # Use `poetry` to install project dependencies
