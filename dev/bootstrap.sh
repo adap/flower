24daac105b6f99d565d8ab6014702780e17b026a
Taner Topal <taner@adap.com>
2021-05-11 11:28:34 +0200

    fix space

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -7,7 +7,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
 # Upgrade/install spcific versions of `pip`, `setuptools`, and `poetry`
 python -m pip install -U pip==21.1.1
-python -m pip install -U  setuptools==56.2.0
+python -m pip install -U setuptools==56.2.0
 python -m pip install -U poetry==1.1.6
 
 # Use `poetry` to install project dependencies
