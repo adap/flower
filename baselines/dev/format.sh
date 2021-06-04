0ed627c2b099274ba7aa9c88c9f157ab654dbdf1
Daniel <daniel@adap.com>
2021-06-04 11:39:37 +0200

    Configure formatters (and testing) for baselines

--- baselines/dev/format.sh
+++ baselines/dev/format.sh
@@ -2,6 +2,6 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
-python -m isort flwr_baselines
-python -m black -q flwr_baselines
+python -m isort .
+python -m black -q .
 python -m docformatter -i -r flwr_baselines
