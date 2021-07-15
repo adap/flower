f772993df9212b3e96b8fa916fdc1ecbe96500c2
Mrinaal Dogra <azadmrinaal@gmail.com>
2021-07-15 15:16:01 +0530

    Replace `sleep` with `wait` command in run.sh (#771)

--- examples/advanced_tensorflow/run.sh
+++ examples/advanced_tensorflow/run.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-python server.py & 
+python server.py &
 sleep 2 # Sleep for 2s to give the server enough time to start
-python client.py --partition=0 &
-python client.py --partition=1 &
-python client.py --partition=2 &
-python client.py --partition=3 &
-python client.py --partition=4 &
-python client.py --partition=5 &
-python client.py --partition=6 &
-python client.py --partition=7 &
-python client.py --partition=8 &
-python client.py --partition=9 &
+
+for i in `seq 0 9`; do
+    echo "Starting client $i"
+    python client.py --partition=${i} &
+done
 
 # This will allow you to use CTRL+C to stop all background processes
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
-sleep 86400
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
+# Wait for all background processes to complete
+wait
