9fb3b403bb16b28e4658ebec19811cee11224220
TheDaniel97 <gajbhiye.saurabh123@gmail.com>
2021-07-20 23:29:45 +0530

    Creating a pytorch example based on MNIST dataset which utilized FedAvgGrad as aggregation Strategy

--- examples/pytorch_mnist_FedAvgGrad/server.py
+++ examples/pytorch_mnist_FedAvgGrad/server.py
@@ -1,7 +1,9 @@
+
 import flwr as fl
 
-from src.py.flwr.server.strategy import FedAvgGrad
+from flwr.server.strategy.FedAvgGrad import FedAvgGrad
+
 
 if __name__ == "__main__":
-    fl.server.start_server("[::]:8080",config={"num_rounds": 4},strategy= FedAvgGrad() )
+    fl.server.start_server("[::]:8080",config={"num_rounds": 4},strategy=FedAvgGrad())
 
