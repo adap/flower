ccf9208812d44b09f2d18b161e2842cb51615fef
Daniel <daniel@adap.com>
2021-07-22 23:25:03 +0200

    Remove duplicate code

--- src/py/flwr/simulation/app.py
+++ src/py/flwr/simulation/app.py
@@ -18,6 +18,7 @@
 from logging import ERROR, INFO
 from typing import Dict, Optional, Tuple
 
+import flwr
 from flwr.client.client import Client
 from flwr.common.logger import log
 from flwr.server.client_manager import SimpleClientManager
@@ -72,7 +73,9 @@ def start_ray_simulation(
     )
 
     # Initialize server and server config
-    initialized_server, initialized_config = _init_defaults(None, config, strategy)
+    initialized_server, initialized_config = flwr.server.app._init_defaults(
+        None, config, strategy
+    )
     log(
         INFO,
         f"Starting Flower Ray simulation running: {initialized_config}",
@@ -89,54 +92,4 @@ def start_ray_simulation(
         initialized_server.client_manager().register(client=client_proxy)
 
     # Start training
-    _fl(server=initialized_server, config=initialized_config)
-
-
-def _init_defaults(
-    server: Optional[Server],
-    config: Optional[Dict[str, int]],
-    strategy: Optional[Strategy],
-) -> Tuple[Server, Dict[str, int]]:
-    # Create server instance if none was given
-    if server is None:
-        client_manager = SimpleClientManager()
-        if strategy is None:
-            strategy = FedAvg()
-        server = Server(client_manager=client_manager, strategy=strategy)
-
-    # Set default config values
-    if config is None:
-        config = {}
-    if "num_rounds" not in config:
-        config["num_rounds"] = 1
-
-    return server, config
-
-
-def _fl(server: Server, config: Dict[str, int]) -> None:
-    # Fit model
-    hist = server.fit(num_rounds=config["num_rounds"])
-    log(INFO, "app_fit: losses_distributed %s", str(hist.losses_distributed))
-    log(INFO, "app_fit: metrics_distributed %s", str(hist.metrics_distributed))
-    log(INFO, "app_fit: losses_centralized %s", str(hist.losses_centralized))
-    log(INFO, "app_fit: metrics_centralized %s", str(hist.metrics_centralized))
-
-    # Temporary workaround to force distributed evaluation
-    server.strategy.eval_fn = None  # type: ignore
-
-    # Evaluate the final trained model
-    res = server.evaluate_round(rnd=-1)
-    if res is not None:
-        loss, _, (results, failures) = res
-        log(INFO, "app_evaluate: federated loss: %s", str(loss))
-        log(
-            INFO,
-            "app_evaluate: results %s",
-            str([(res[0].cid, res[1]) for res in results]),
-        )
-        log(INFO, "app_evaluate: failures %s", str(failures))
-    else:
-        log(INFO, "app_evaluate: no evaluation result")
-
-    # Graceful shutdown
-    server.disconnect_all_clients()
+    flwr.server.app._fl(server=initialized_server, config=initialized_config)
