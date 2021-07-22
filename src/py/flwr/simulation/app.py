ff847b395985a72745ef69322bde7284d413d0c7
Daniel <daniel@adap.com>
2021-07-22 22:42:28 +0200

    Test if ray is importable

--- src/py/flwr/simulation/app.py
+++ src/py/flwr/simulation/app.py
@@ -15,18 +15,22 @@
 """Flower Simulation app."""
 
 
-from logging import INFO
+from logging import ERROR, INFO
 from typing import Dict, Optional, Tuple
 
+from flwr.client.client import Client
 from flwr.common.logger import log
 from flwr.server.client_manager import SimpleClientManager
-from flwr.simulation.ray_simulation.ray_client_proxy import RayClientProxy
 from flwr.server.server import Server
 from flwr.server.strategy import FedAvg, Strategy
-from flwr.client.client import Client
+from flwr.simulation.ray_simulation.ray_client_proxy import RayClientProxy
 
-# TODO: dynamically imported when user wants to use start_ray_simulation()
-import ray
+RAY_IMPORT_ERROR: str = """Unable to import module `ray`.
+
+To install the necessary dependencies, install `flwr` with the `simulation` extra:
+
+    pip install -U flwr["simulation"]
+"""
 
 
 def start_ray_simulation(
@@ -51,16 +55,24 @@ def start_ray_simulation(
         no strategy is provided, then `start_server` will use
         `flwr.server.strategy.FedAvg`.
     """
-    initialized_server, initialized_config = _init_defaults(None, config, strategy)
+    # Try to import ray
+    try:
+        import ray
+    except:
+        ray = None
+    if not ray:
+        log(ERROR, RAY_IMPORT_ERROR)
+        return None
 
-    # initialize Ray
+    # Initialize Ray
     ray.init(**ray_init_config)
-
     log(
         INFO,
         f"Ray initialized with resources: {ray.cluster_resources()}",
     )
 
+    # Initialize server and server config
+    initialized_server, initialized_config = _init_defaults(None, config, strategy)
     log(
         INFO,
         f"Starting Flower Ray simulation running: {initialized_config}",
@@ -68,9 +80,12 @@ def start_ray_simulation(
 
     # Ask Ray to create and register RayClientProxy objects with the ClientManager
     for i in range(pool_size):
-        client_proxy = RayClientProxy(cid=str(i), client_type=client_type,
-                                      resources=client_resources,
-                                      fed_dir=data_partitions_dir)
+        client_proxy = RayClientProxy(
+            cid=str(i),
+            client_type=client_type,
+            resources=client_resources,
+            fed_dir=data_partitions_dir,
+        )
         initialized_server.client_manager().register(client=client_proxy)
 
     # Start training
