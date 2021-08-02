write functions for masking functions. TODO: check whether functions are correct

src/py/flwr/client/secagg_client.py
@@ -149,16 +149,33 @@ class SecAggClient(Client):
         # temporary code
         weights = [np.array([[-0.2, -0.5, 1.9], [0.0, 2.4, -1.9]]),
                    np.array([[0.2, 0.5, -1.9], [0.0, -2.4, 1.9]])]
+        dimensions_list = [a.shape for a in weights]
         quantized_weights = secagg_utils.quantize(
             weights, self.clipping_range, self.target_range)
         for client in available_clients:
             if client == self.secagg_id:
                 # add private mask
-                pass
+                private_mask = secagg_utils.pseudo_rand_gen(
+                    self.b, self.mod_range, dimensions_list)
+                '''for idx in range(len(quantized_weights)):
+                    quantized_weights[idx] += private_mask[idx]'''
+                for idx in range(len(quantized_weights)):
+                    quantized_weights[idx] %= self.mod_range
 
             else:
                 # add pairwise mask
-                pass
+                shared_key = secagg_utils.generate_shared_key(
+                    self.sk1, secagg_utils.bytes_to_public_key(self.public_keys_dict[client].pk1))
+                pairwise_mask = secagg_utils.pseudo_rand_gen(
+                    shared_key, self.mod_range, dimensions_list)
+                if self.secagg_id > client:
+                    for idx in range(len(quantized_weights)):
+                        quantized_weights[idx] += pairwise_mask[idx]
+                else:
+                    for idx in range(len(quantized_weights)):
+                        quantized_weights[idx] -= pairwise_mask[idx]
+                for idx in range(len(quantized_weights)):
+                    quantized_weights[idx] %= self.mod_range
 
         log(INFO, "Sent vectors")
         return AskVectorsRes(parameters=weights_to_parameters(quantized_weights))

src/py/flwr/common/secagg_utils.py
@@ -144,9 +144,15 @@ def rand_bytes(num: int = 32) -> bytes:
 # Pseudo Bytes Generator
 
 
-def pseudo_rand_gen(seed: bytes, num_range: int, l: int):
+def pseudo_rand_gen(seed: bytes, num_range: int, dimensions_list: List[Tuple]):
     random.seed(seed)
-    return [random.randrange(0, num_range) for i in range(l)]
+    output = []
+    for dimension in dimensions_list:
+        flat_arr = np.array([random.randrange(0, num_range)
+                            for i in range(np.prod(dimension))])
+        modified_arr = np.reshape(flat_arr, dimension)
+        output.append(modified_arr)
+    return output
 
 
 # String Concatenation