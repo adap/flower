ccd524186eac75c9ddb0c9efbd252acb4a414038
catprogrammer <hei411@gmail.com>
2021-08-12 16:31:38 +0100

    comments for secagg strategy base class

--- src/py/flwr/server/strategy/secagg_strategy.py
+++ src/py/flwr/server/strategy/secagg_strategy.py
@@ -9,10 +9,24 @@ from flwr.server.client_proxy import ClientProxy
 class SecAggStrategy(ABC):
     @abstractmethod
     def get_sec_agg_param(self) -> Dict[str, int]:
-        pass
+        '''Produce a dictionary storing parameters for the secure aggregation protocol
+        sample_num: Maximum number of clients to be sampled
+        min_num: Minimum number of clients to be available at the end of protocol
+        share_num: Number of shares to be generated for secret
+        threshold: Number of shares needed to reconstruct secret
+        clipping_range: Range of weight vector initially
+        target_range: Range of weight vector after quantization
+        mod_range: Field of cryptographic primitives
+        max_weights_factor: maximum weights factor mulitplied on weights vector
+        timeout: not used, but timeout for gRPC in the future
+
+        Note: do not use secagg_id, as it will be used overwritten on server side'''
 
     @abstractmethod
     def sec_agg_configure_fit(
         self, rnd: int, parameters: Parameters, client_manager: ClientManager, sample_num: int, min_num: int
     ) -> List[Tuple[ClientProxy, FitIns]]:
+        '''Sample min_num to sample_num of clients and produce clients instruction
+        Ideally, should only sample extractly sample_num of clients. 
+        Note if it is a range, sample_num needs to be updated after sampled a smaller number of people'''
         pass
