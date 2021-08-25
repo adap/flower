b961c55c5b0485e42307f1299bf45139436950c0
Hei Li <hei411@gmail.com>
2021-08-25 15:02:17 +0100

    add license files

--- src/py/flwr/client/secagg_client.py
+++ src/py/flwr/client/secagg_client.py
@@ -1,5 +1,17 @@
-from typing import Dict, List, Tuple
-import numpy as np
+# Copyright 2020 Adap GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 from flwr.common import (
     AskKeysRes,
     EvaluateIns,
@@ -11,10 +23,8 @@ from flwr.common import (
 )
 from flwr.common.parameter import parameters_to_weights, weights_to_parameters
 from flwr.common.typing import AskKeysIns, AskVectorsIns, AskVectorsRes, SetupParamIns, SetupParamRes, ShareKeysIns, ShareKeysPacket, ShareKeysRes, UnmaskVectorsIns, UnmaskVectorsRes, Weights
-from flwr.common.secagg import secagg_primitives, secagg_client_logic
+from flwr.common.secagg import secagg_client_logic
 from .client import Client
-from flwr.common.logger import log
-from logging import DEBUG, INFO, WARNING
 
 
 class SecAggClient(Client):
--- src/py/flwr/common/secagg/secagg_client_logic.py
+++ src/py/flwr/common/secagg/secagg_client_logic.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Adap GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 import numpy as np
 from flwr.common.parameter import parameters_to_weights, weights_to_parameters
 from flwr.common.typing import AskKeysIns, AskVectorsIns, AskVectorsRes, SetupParamIns, SetupParamRes, ShareKeysIns, ShareKeysPacket, ShareKeysRes, UnmaskVectorsIns, UnmaskVectorsRes, Weights
@@ -7,11 +21,7 @@ from logging import DEBUG, INFO, WARNING
 from typing import Dict, List, Tuple
 from flwr.common import (
     AskKeysRes,
-    EvaluateIns,
-    EvaluateRes,
-    FitIns,
-    FitRes,
-    ParametersRes,
+
 
 )
 
