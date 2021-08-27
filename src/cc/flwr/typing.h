d1c9ac18cafe6ad2c8b2687e0253c31499ef2c9c
Pedro Porto Buarque de Gusmão <pedropgusmao@gmail.com>
2021-08-27 08:53:15 +0100

    Reorganizing stuff

--- src/cc/flwr/typing.h
+++ src/cc/flwr/typing.h
@@ -3,6 +3,7 @@
 * Typing
 *
 * There is no "bytes" type in C++, so "string" is used instead
+* as defined in https://developers.google.com/protocol-buffers/docs/proto#scalar
 *
 */
 
@@ -11,39 +12,50 @@
 * The Scalar defined in "transport" uses "double" and "int64", I am not sure if there should be changed
 *
 */
-class LocalScalar {
+class LocalScalar
+{
 public:
 	// getters
-	std::optional<bool> getBool() {
+	std::optional<bool> getBool()
+	{
 		return b;
 	}
-	std::optional<std::string> getBytes() {
+	std::optional<std::string> getBytes()
+	{
 		return bytes;
 	}
-	std::optional<float> getFloat() {
+	std::optional<float> getFloat()
+	{
 		return f;
 	}
-	std::optional<int> getInt() {
+	std::optional<int> getInt()
+	{
 		return i;
 	}
-	std::optional<std::string> getString() {
+	std::optional<std::string> getString()
+	{
 		return string;
 	}
 
 	// setters
-	void setBool(bool b) {
+	void setBool(bool b)
+	{
 		this->b = b;
 	}
-	void setBytes(std::string bytes) {
+	void setBytes(std::string bytes)
+	{
 		this->bytes = bytes;
 	}
-	void setFloat(float f) {
+	void setFloat(float f)
+	{
 		this->f = f;
 	}
-	void setInt(int i) {
+	void setInt(int i)
+	{
 		this->i = i;
 	}
-	void setString(std::string string) {
+	void setString(std::string string)
+	{
 		this->string = string;
 	}
 
@@ -57,108 +69,135 @@ private:
 
 typedef std::map<std::string, LocalScalar> Metrics;
 
-class Parameters {
+class Parameters
+{
 public:
-	Parameters() {};
+	Parameters(){};
 	Parameters(std::list<std::string> tensors, std::string tensor_type)
-		:tensors(tensors), tensor_type(tensor_type) {};
+		: tensors(tensors), tensor_type(tensor_type){};
 
 	// getters
-	std::list<std::string> getTensors() {
+	std::list<std::string> getTensors()
+	{
 		return tensors;
 	}
-	std::string getTensor_type() {
+	std::string getTensor_type()
+	{
 		return tensor_type;
 	}
 
 	// setters
-	void setTensors(std::list<std::string> tensors) {
+	void setTensors(std::list<std::string> tensors)
+	{
 		this->tensors = tensors;
 	}
-	void setTensor_type(std::string tensor_type) {
+	void setTensor_type(std::string tensor_type)
+	{
 		this->tensor_type = tensor_type;
 	}
+
 private:
 	std::list<std::string> tensors;
 	std::string tensor_type;
 };
 
-class ParametersRes {
+class ParametersRes
+{
 public:
 	ParametersRes(Parameters parameters)
-		:parameters(parameters) {};
+		: parameters(parameters){};
 
-	Parameters getParameters() {
+	Parameters getParameters()
+	{
 		return parameters;
 	}
-	void setParameters(Parameters p) {
+	void setParameters(Parameters p)
+	{
 		parameters = p;
 	}
+
 private:
 	// Response when asked to return parameters
 	Parameters parameters;
 };
 
-class FitIns {
+class FitIns
+{
 public:
 	FitIns(Parameters parameters, std::map<std::string, LocalScalar> config)
-		: parameters(parameters), config(config) {};
+		: parameters(parameters), config(config){};
 
-	Parameters getParameters() {
+	Parameters getParameters()
+	{
 		return parameters;
 	}
-	std::map<std::string, LocalScalar> getConfig() {
+	std::map<std::string, LocalScalar> getConfig()
+	{
 		return config;
 	}
 
-	void setParameters(Parameters p) {
+	void setParameters(Parameters p)
+	{
 		parameters = p;
 	}
-	void setConfig(std::map<std::string, LocalScalar> config) {
+	void setConfig(std::map<std::string, LocalScalar> config)
+	{
 		this->config = config;
 	}
+
 private:
 	// Fit instructions for a client
 	Parameters parameters;
 	std::map<std::string, LocalScalar> config;
 };
 
-class FitRes {
+class FitRes
+{
 public:
-	FitRes() {};
+	FitRes(){};
 	FitRes(Parameters parameters, int num_examples, int num_examples_ceil, float fit_duration, Metrics metrics)
 		: parameters(parameters), num_examples(num_examples), num_examples_ceil(num_examples_ceil),
-		fit_duration(fit_duration), metrics(metrics) {};
+		  fit_duration(fit_duration), metrics(metrics){};
 
-	Parameters getParameters() {
+	Parameters getParameters()
+	{
 		return parameters;
 	}
-	int getNum_example() {
+	int getNum_example()
+	{
 		return num_examples;
 	}
-	std::optional<int> getNum_examples_ceil() {
+	std::optional<int> getNum_examples_ceil()
+	{
 		return num_examples_ceil;
 	}
-	std::optional<float> getFit_duration() {
+	std::optional<float> getFit_duration()
+	{
 		return fit_duration;
 	}
-	std::optional<Metrics> getMetrics() {
+	std::optional<Metrics> getMetrics()
+	{
 		return metrics;
 	}
 
-	void setParameters(Parameters p) {
+	void setParameters(Parameters p)
+	{
 		parameters = p;
 	}
-	void setNum_example(int n) {
+	void setNum_example(int n)
+	{
 		num_examples = n;
 	}
-	void setNum_examples_ceil(int n) {
+	void setNum_examples_ceil(int n)
+	{
 		num_examples_ceil = n;
 	}
-	void setFit_duration(float f) {
+	void setFit_duration(float f)
+	{
 		fit_duration = f;
 	}
-	void setMetrics(Metrics m) {
+	void setMetrics(Metrics m)
+	{
 		metrics = m;
 	}
 
@@ -166,27 +205,32 @@ private:
 	// Fit response from a client
 	Parameters parameters;
 	int num_examples;
-	std::optional<int> num_examples_ceil = std::nullopt;	// Deprecated
-	std::optional<float> fit_duration = std::nullopt;		// Deprecated
+	std::optional<int> num_examples_ceil = std::nullopt; // Deprecated
+	std::optional<float> fit_duration = std::nullopt;	 // Deprecated
 	std::optional<Metrics> metrics = std::nullopt;
 };
 
-class EvaluateIns {
+class EvaluateIns
+{
 public:
 	EvaluateIns(Parameters parameters, std::map<std::string, LocalScalar> config)
-		: parameters(parameters), config(config) {};
+		: parameters(parameters), config(config){};
 
-	Parameters getParameters() {
+	Parameters getParameters()
+	{
 		return parameters;
 	}
-	std::map<std::string, LocalScalar> getConfig() {
+	std::map<std::string, LocalScalar> getConfig()
+	{
 		return config;
 	}
 
-	void setParameters(Parameters p) {
+	void setParameters(Parameters p)
+	{
 		parameters = p;
 	}
-	void setConfig(std::map<std::string, LocalScalar> config) {
+	void setConfig(std::map<std::string, LocalScalar> config)
+	{
 		this->config = config;
 	}
 
@@ -196,41 +240,51 @@ private:
 	std::map<std::string, LocalScalar> config;
 };
 
-class EvaluateRes {
+class EvaluateRes
+{
 public:
-	EvaluateRes() {};
+	EvaluateRes(){};
 	EvaluateRes(float loss, int num_examples, float accuracy, Metrics metrics)
-		: loss(loss), num_examples(num_examples), accuracy(accuracy), metrics(metrics) {};
+		: loss(loss), num_examples(num_examples), accuracy(accuracy), metrics(metrics){};
 
-	float getLoss() {
+	float getLoss()
+	{
 		return loss;
 	}
-	int getNum_example() {
+	int getNum_example()
+	{
 		return num_examples;
 	}
-	std::optional<float> getAccuracy() {
+	std::optional<float> getAccuracy()
+	{
 		return accuracy;
 	}
-	std::optional<Metrics> getMetrics() {
+	std::optional<Metrics> getMetrics()
+	{
 		return metrics;
 	}
 
-	void setLoss(float f) {
+	void setLoss(float f)
+	{
 		loss = f;
 	}
-	void setNum_example(int n) {
+	void setNum_example(int n)
+	{
 		num_examples = n;
 	}
-	void setAccuracy(float f) {
+	void setAccuracy(float f)
+	{
 		accuracy = f;
 	}
-	void setMetrics(Metrics m) {
+	void setMetrics(Metrics m)
+	{
 		metrics = m;
 	}
+
 private:
 	// Evaluate response from a client
 	float loss;
 	int num_examples;
-	std::optional<float> accuracy = std::nullopt;		// Deprecated
+	std::optional<float> accuracy = std::nullopt; // Deprecated
 	std::optional<Metrics> metrics = std::nullopt;
 };
