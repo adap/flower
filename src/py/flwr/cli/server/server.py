becac1f4291d9a9296977f1cf631b6a6e7326884
Kushal Garg <kushalgarg2000@gmail.com>
2021-08-31 01:55:28 +0530

    fix: help message

--- src/py/flwr/cli/server/server.py
+++ src/py/flwr/cli/server/server.py
@@ -31,7 +31,7 @@ def server():
 @click.option("-a","--server-address",type=str,default="[::]:8080",help="string (default: “[::]:8080”). The IPv6 address of the server.")
 @click.option("-l","--grpc-max-message-length",type=int,default=GRPC_MAX_MESSAGE_LENGTH,help="int (default: 536_870_912, this equals 512MB). The maximum length of gRPC messages that can be exchanged with the Flower clients. The default should be sufficient for most models. Users who train very large models might need to increase this value. Note that the Flower clients need to be started with the same value (see flwr.client.start_client), otherwise clients will not know about the increased limit and block larger messages.")
 @click.option("-f","--force-final-distributed-eval",type=bool,default=False,help="bool (default: False). Forces a distributed evaulation to occur after the last training epoch when enabled.")
-@click.option("-s","--strategy",type=str,default="fedavg",help="fault_tolerant_fedavg | fedadagrad | aggregate | default | fast_and_slow | fault_tolerant_fedavg | fedavg | fedfs_v0 | fedfs_v1 | fedopt | qfedavg . Sets the strategy for Federated Learning.")
+@click.option("-s","--strategy",type=str,default="fedavg",help=f"{' | '.join(strategy_mapping.keys())} . Sets the strategy for Federated Learning.")
 @click.option("-n",'--num-rounds',type=int,default=3,help="int (default: 3). Number of Rounds during Federated Learning")
 @click.option("-p","--strategy-params",type=str,help="JSON. Sets the parameters for selected strategy. Parameters available are: fraction_fit: float, fraction_eval: float, min_fit_clients: int, min_eval_clients: int, min_available_clients:int, accept_failures: bool")
 
