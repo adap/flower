95c8f4d3f66d4824fd3cbd4c431ba38b7412dc79
TheDaniel97 <gajbhiye.saurabh123@gmail.com>
2021-08-31 12:20:27 +0530

    Modified client.py to accomodate DEPRECATION changes made in flwr

--- examples/pytorch_mnist_FedAvgGrad/server.py
+++ examples/pytorch_mnist_FedAvgGrad/server.py
@@ -1,5 +1,7 @@
-from flwr import server
+import flwr as fl
+
 from flwr.server.strategy.FedAvgGrad import FedAvgGrad
 
+# Start Flower server for three rounds of federated learning
 if __name__ == "__main__":
-    server.start_server("[::]:8080", config={"num_rounds": 4}, strategy= FedAvgGrad())
+    fl.server.start_server("[::]:8080", config={"num_rounds": 5},strategy=FedAvgGrad(),force_final_distributed_eval = True)
