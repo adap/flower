402573ab2020cfd024e0bd3c1d407c34bc7dbe8f
Hei Li <hei411@gmail.com>
2021-08-31 12:30:20 +0100

    add num_rounds

--- src/py/flwr/common/sec_agg/sec_agg_test.py
+++ src/py/flwr/common/sec_agg/sec_agg_test.py
@@ -30,9 +30,9 @@ class CifarClient(fl.client.NumPyClient):
         return loss, len(x_test), {"accuracy": accuracy}
 
 
-def test_start_server(sample_num=2, min_num=2, vector_dimension=100000, dropout_value=0):
+def test_start_server(sample_num=2, min_num=2, vector_dimension=100000, dropout_value=0, num_rounds=1):
     fl.server.start_server("localhost:8080", config={
-                           "num_rounds": 1, "sec_agg": 1},
+                           "num_rounds": num_rounds, "sec_agg": 1},
                            strategy=SecAggFedAvg(fraction_fit=1, min_fit_clients=sample_num, min_available_clients=sample_num,
                                                  sec_agg_param_dict={"min_num": min_num,
                                                                      "test": 1,
@@ -46,7 +46,7 @@ def test_start_client(server_address: str,
     fl.client.start_numpy_client(server_address, client, grpc_max_message_length)
 
 
-def test_start_simulation(sample_num=2, min_num=2, vector_dimension=100000, dropout_value=0):
+def test_start_simulation(sample_num=2, min_num=2, vector_dimension=100000, dropout_value=0, num_rounds=1):
     """Start a FL simulation."""
     # This will hold all the processes which we are going to create
     processes = []
@@ -54,7 +54,7 @@ def test_start_simulation(sample_num=2, min_num=2, vector_dimension=100000, drop
     # Start the server
     server_process = Process(
         target=test_start_server, args=(
-            sample_num, min_num, vector_dimension, dropout_value)
+            sample_num, min_num, vector_dimension, dropout_value, num_rounds)
     )
     server_process.start()
     processes.append(server_process)
--- src/py/flwr/server/server.py
+++ src/py/flwr/server/server.py
@@ -176,7 +176,8 @@ class Server:
                 history.add_loss_centralized(rnd=current_round, loss=loss_cen)
                 history.add_metrics_centralized(rnd=current_round, metrics=metrics_cen)
 
-            # Evaluate model on a sample of available clients
+            # REMOVE TEMPORARILY FOR TEST
+            '''# Evaluate model on a sample of available clients
             res_fed = self.evaluate_round(rnd=current_round)
             if res_fed:
                 loss_fed, evaluate_metrics_fed, _ = res_fed
@@ -184,7 +185,7 @@ class Server:
                     history.add_loss_distributed(rnd=current_round, loss=loss_fed)
                     history.add_metrics_distributed(
                         rnd=current_round, metrics=evaluate_metrics_fed
-                    )
+                    )'''
 
         # Bookkeeping
         end_time = timeit.default_timer()
