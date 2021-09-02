d7d8ed8b59c5ac3ac5f454d6275353b7e247d11b
Pedro Porto Buarque de Gusmão <pedropgusmao@gmail.com>
2021-09-02 14:11:12 +0100

    setting some reasonable initial values

--- src/py/flwr/server/strategy/fedyogi.py
+++ src/py/flwr/server/strategy/fedyogi.py
@@ -59,11 +59,11 @@ class FedYogi(FedOpt):
         on_evaluate_config_fn: Optional[Callable[[int], Dict[str, Scalar]]] = None,
         accept_failures: bool = True,
         initial_parameters: Parameters,
-        eta: float = 1e-1,
-        eta_l: float = 1e-1,
+        eta: float = 1e-2,
+        eta_l: float = 0.0316,
         beta_1: float = 0.9,
         beta_2: float = 0.99,
-        tau: float = 1e-9,
+        tau: float = 1e-3,
     ) -> None:
         """Federated learning strategy using Yogi on server-side.
 
