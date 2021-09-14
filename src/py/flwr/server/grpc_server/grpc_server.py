9c5d9cf9ed28d66297428410a03c34b06db1b1dc
Taner Topal <taner@adap.com>
2021-09-14 21:28:41 +0200

    Improve docstring

--- src/py/flwr/server/grpc_server/grpc_server.py
+++ src/py/flwr/server/grpc_server/grpc_server.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Implements utility function to create a grpc server."""
+"""Implements utility function to create a gRPC server."""
 
 
 import concurrent.futures
