9935b6b18c0c4d7ac7793fb40b7161c62260db72
Chris-george-anil <66996004+Chris-george-anil@users.noreply.github.com>
2021-10-17 13:07:31 +0530

    Support for Object type numpy arrays

--- src/py/flwr/common/parameter.py
+++ src/py/flwr/common/parameter.py
@@ -37,12 +37,12 @@ def parameters_to_weights(parameters: Parameters) -> Weights:
 def ndarray_to_bytes(ndarray: np.ndarray) -> bytes:
     """Serialize NumPy ndarray to bytes."""
     bytes_io = BytesIO()
-    np.save(bytes_io, ndarray, allow_pickle=False)
+    np.save(bytes_io, ndarray, allow_pickle=True)
     return bytes_io.getvalue()
 
 
 def bytes_to_ndarray(tensor: bytes) -> np.ndarray:
     """Deserialize NumPy ndarray from bytes."""
     bytes_io = BytesIO(tensor)
-    ndarray_deserialized = np.load(bytes_io, allow_pickle=False)
+    ndarray_deserialized = np.load(bytes_io, allow_pickle=True)
     return cast(np.ndarray, ndarray_deserialized)
