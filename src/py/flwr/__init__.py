ef9280afbc8e72aeabcd4460d40fe4766332cc33
Francisco J. Solis <siscomagma@gmail.com>
2021-12-26 11:17:06 -0600

    Added PyLint variations using sections

--- src/py/flwr/__init__.py
+++ src/py/flwr/__init__.py
@@ -24,9 +24,11 @@ __all__ = [
     "simulation",
 ]
 
+# pylint: disable=import-error, no-name-in-module
 if sys.version_info < (3, 8):
-    import importlib_metadata  # pylint: disable=import-error, no-name-in-module
+    import importlib_metadata
 else:
-    import importlib.metadata as importlib_metadata  # pylint: disable=import-error, no-name-in-module
+    import importlib.metadata as importlib_metadata
+# pylint: enable=import-error, no-name-in-module
 
 __version__ = importlib_metadata.version(__name__)
