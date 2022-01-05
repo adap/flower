dcf8a7a328b814bb093991e1eb18d764646ef1b8
Taner Topal <taner@adap.com>
2022-01-05 13:41:52 +0100

    Fix

--- src/py/flwr/server/grpc_server/grpc_server.py
+++ src/py/flwr/server/grpc_server/grpc_server.py
@@ -28,7 +28,7 @@ from flwr.proto import transport_pb2_grpc
 from flwr.server.client_manager import ClientManager
 from flwr.server.grpc_server import flower_service_servicer as fss
 
-SSLFILES = Tuple[ByteString, ByteString, ByteString]
+SSL_FILES = Tuple[ByteString, ByteString, ByteString]
 
 INVALID_SSL_FILES_ERR_MSG = """
     When setting any of root_certificate, certificate, or private_key,
@@ -36,8 +36,8 @@ INVALID_SSL_FILES_ERR_MSG = """
 """
 
 
-def valid_ssl_files(ssl_files: SSLFILES) -> bool:
-    """Validate type SSLFILES."""
+def valid_ssl_files(ssl_files: SSL_FILES) -> bool:
+    """Validate type SSL_FILES."""
     is_valid = (
         all([isinstance(ssl_file, bytes) for ssl_file in ssl_files])
         and len(ssl_files) == 3
@@ -54,7 +54,7 @@ def start_grpc_server(
     server_address: str,
     max_concurrent_workers: int = 1000,
     max_message_length: int = GRPC_MAX_MESSAGE_LENGTH,
-    ssl_files: Optional[SSLFILES] = None,
+    ssl_files: Optional[SSL_FILES] = None,
 ) -> grpc.Server:
     """Create gRPC server and return instance of grpc.Server.
 
