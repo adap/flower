86c4126e39be96897d57524a499e0b93e7e22a21
mariaboerner1987 <maria@adap.com>
2022-01-06 13:12:45 +0100

    add linter to format

--- dev/format.sh
+++ dev/format.sh
@@ -7,6 +7,9 @@ python -m isort --skip src/py/flwr/proto src/py
 python -m black -q --exclude src/py/flwr/proto src/py
 python -m docformatter -i -r src/py/flwr -e src/py/flwr/proto
 python -m docformatter -i -r src/py/flwr_experimental
+python -m black -q baselines/flwr_baselines/experiments/fedbn_convergence_rate
+python -m docformatter -i -r baselines/flwr_baselines/experiments/fedbn_convergence_rate
+python -m isort baselines/flwr_baselines/experiments/fedbn_convergence_rate
 
 # Protos
 find src/proto/flwr/proto -name *.proto | grep "\.proto" | xargs clang-format-10 -i
