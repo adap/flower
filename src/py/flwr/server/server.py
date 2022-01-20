490c59508a94d90970de698dda1dc604a46c3621
Daniel J. Beutel <daniel@adap.com>
2022-01-20 13:46:38 +0100

    Refactor client shutdown

--- src/py/flwr/server/server.py
+++ src/py/flwr/server/server.py
@@ -305,8 +305,10 @@ class Server:
         """Send shutdown signal to all clients."""
         all_clients = self._client_manager.all()
         clients = [all_clients[k] for k in all_clients.keys()]
-        _ = shutdown(
-            clients=clients,
+        reconnect = Reconnect(seconds=None)
+        client_instructions = [(cp, reconnect) for cp in clients]
+        _ = reconnect_clients(
+            client_instructions=client_instructions,
             max_workers=self.max_workers,
         )
 
@@ -329,19 +331,22 @@ class Server:
         return parameters_res.parameters
 
 
-def shutdown(
-    clients: List[ClientProxy],
+def reconnect_clients(
+    client_instructions: List[Tuple[ClientProxy, Reconnect]],
     max_workers: Optional[int],
 ) -> ReconnectResultsAndFailures:
     """Instruct clients to disconnect and never reconnect."""
-    reconnect = Reconnect(seconds=None)
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = [executor.submit(reconnect_client, c, reconnect) for c in clients]
-        concurrent.futures.wait(futures)
+        submitted_fs = [
+            executor.submit(reconnect_client, cp, ins)
+            for cp, ins in client_instructions
+        ]
+        concurrent.futures.wait(submitted_fs)
+
     # Gather results
     results: List[Tuple[ClientProxy, Disconnect]] = []
     failures: List[BaseException] = []
-    for future in futures:
+    for future in submitted_fs:
         failure = future.exception()
         if failure is not None:
             failures.append(failure)
@@ -366,15 +371,15 @@ def fit_clients(
 ) -> FitResultsAndFailures:
     """Refine parameters concurrently on all selected clients."""
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = [
-            executor.submit(fit_client, c, ins) for c, ins in client_instructions
+        submitted_fs = [
+            executor.submit(fit_client, cp, ins) for cp, ins in client_instructions
         ]
-        concurrent.futures.wait(futures)
+        concurrent.futures.wait(submitted_fs)
 
     # Gather results
     results: List[Tuple[ClientProxy, FitRes]] = []
     failures: List[BaseException] = []
-    for future in futures:
+    for future in submitted_fs:
         failure = future.exception()
         if failure is not None:
             failures.append(failure)
@@ -397,14 +402,15 @@ def evaluate_clients(
 ) -> EvaluateResultsAndFailures:
     """Evaluate parameters concurrently on all selected clients."""
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        futures = [
-            executor.submit(evaluate_client, c, ins) for c, ins in client_instructions
+        submitted_fs = [
+            executor.submit(evaluate_client, cp, ins) for cp, ins in client_instructions
         ]
-        concurrent.futures.wait(futures)
+        concurrent.futures.wait(submitted_fs)
+
     # Gather results
     results: List[Tuple[ClientProxy, EvaluateRes]] = []
     failures: List[BaseException] = []
-    for future in futures:
+    for future in submitted_fs:
         failure = future.exception()
         if failure is not None:
             failures.append(failure)
