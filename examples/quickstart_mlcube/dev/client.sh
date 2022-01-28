86af2da1435da5826a5bfc8e9236b46d41687c29
Taner Topal <taner@adap.com>
2022-01-28 16:06:20 +0100

    MLcube workspaces (#1034)
    * Use workspaces for MLCube runs

--- examples/quickstart_mlcube/dev/client.sh
+++ examples/quickstart_mlcube/dev/client.sh
@@ -2,22 +2,10 @@
 
 set -e
 
-echo "Prepare client $1"
-
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied"
+    echo "Please provide the name of the client workspace e.g. 'python client.py one'"
     exit 1
 fi
 
-# Create client directory
-mkdir -p clients/client_$1
-
-# Copy mlcube and client
-cp -r mlcube clients/client_$1/
-cp -r client.py clients/client_$1/
-cp -r mlcube_utils.py clients/client_$1/
-
-echo "Start client $1"
-# Start client Python process in background
-poetry run python clients/client_$1/client.py
+poetry run python client.py $1
--- examples/quickstart_mlcube/mlcube_utils.py
+++ examples/quickstart_mlcube/mlcube_utils.py
@@ -10,32 +10,32 @@ from flwr.common import weights_to_parameters
 MODULE_PATH = os.path.abspath(__file__)
 MODULE_DIR = os.path.dirname(MODULE_PATH)
 MLCUBE_DIR = os.path.join(MODULE_DIR, "mlcube")
-MLCUBE_WORKSPACE_DIR = os.path.join(MLCUBE_DIR, "workspace")
 
 
 def create_directory(path: str) -> None:
+    print(f"Creating directory: {path}")
     os.makedirs(path, exist_ok=True)
 
 
-def workspace_path(path: str, is_file=True) -> str:
+def workspace_path(workspace_path: str, item_path: str, is_file=True) -> str:
     """Return filepath and create directories if required."""
-    full_path = os.path.join(MLCUBE_WORKSPACE_DIR, path)
+    full_path = os.path.join(workspace_path, item_path)
     dir_path = os.path.dirname(full_path) if is_file else full_path
     create_directory(dir_path)
     return full_path
 
 
-def run_task(task_name: str):
+def run_task(workspace: str, task_name: str):
     """Run mlcube task and return if successful."""
     command = [
-        "mlcube_docker",
+        "mlcube",
         "run",
-        "--mlcube=.",
-        f"--task={task_name}"
+        f"--task={task_name}",
+        f"--workspace={workspace}",
     ]
 
     print()
-    print('\n\033[32m' + " ".join(command) + '\033[39m\n')
+    print("\n\033[32m" + " ".join(command) + "\033[39m\n")
     process = subprocess.Popen(
         command, cwd=MLCUBE_DIR, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
     )
@@ -48,9 +48,9 @@ def run_task(task_name: str):
         raise Exception("MLCube task %s failed" % task_name)
 
 
-def save_parameteras_as_model(parameters):
+def save_parameteras_as_model(workspace: str, parameters):
     """Write model to $WORKSPACE/model_in/mnist_model from parameters."""
-    filepath = workspace_path("model_in/mnist_model")
+    filepath = workspace_path(workspace, "model_in/mnist_model", False)
     model = get_model()
     model.compile(
         optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
@@ -59,45 +59,44 @@ def save_parameteras_as_model(parameters):
     model.save(filepath)
 
 
-def load_model_parameters():
+def load_model_parameters(workspace: str):
     """Load and return model parameters."""
-    filepath = workspace_path("model/mnist_model")
+    filepath = workspace_path(workspace, "model/mnist_model", False)
     model = tf.keras.models.load_model(filepath)
     parameters = model.get_weights()
     return parameters
 
 
-def load_train_metrics():
+def load_train_metrics(workspace: str):
     """Load and return metrics."""
-    filepath = workspace_path("metrics/train_metrics.json")
+    filepath = workspace_path(workspace, "metrics/train_metrics.json")
     with open(filepath, "r") as f:
         data = json.load(f)
 
     data["loss"] = float(data["loss"])
     data["accuracy"] = float(data["accuracy"])
-    data["num_examples"] = 1 #int(data["num_examples"])
+    data["num_examples"] = 1  # int(data["num_examples"])
 
     return data
 
 
-def load_evaluate_metrics():
+def load_evaluate_metrics(workspace: str):
     """Load and return metrics."""
-    filepath = workspace_path("metrics/evaluate_metrics.json")
+    filepath = workspace_path(workspace, "metrics/evaluate_metrics.json")
     with open(filepath, "r") as f:
         data = json.load(f)
 
     data["loss"] = float(data["loss"])
     data["accuracy"] = float(data["accuracy"])
-    data["num_examples"] = 1 # int(data["num_examples"])
+    data["num_examples"] = 1  # int(data["num_examples"])
 
     return data
 
 
-def write_hyperparameters(optimizer, epochs, batch_size):
+def write_hyperparameters(workspace: str, optimizer, epochs, batch_size):
     """Write hyperparameters to mlcube."""
-    filepath = workspace_path("parameters/default.parameters.yaml")
-    os.remove(filepath)
-    with open(filepath, "w") as f:
+    filepath = workspace_path(workspace, "parameters/default.parameters.yaml")
+    with open(filepath, "w+") as f:
         params = [
             f'optimizer: "{optimizer}"',
             f"epochs: {epochs}",
@@ -106,15 +105,20 @@ def write_hyperparameters(optimizer, epochs, batch_size):
         for param in params:
             f.write(f"{param}\n")
 
+
 def get_model():
     """Create example model"""
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(10, activation='softmax')
-    ])
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(10, activation="softmax"),
+        ]
+    )
     return model
+
+
 def initial_parameters():
     """Return initial checkpoint parameters."""
     model = get_model()
