bc972b9d33abe2f14d5728bc8720ea34e381dc7f
Taner Topal <taner@adap.com>
2022-02-14 12:59:02 +0100

    Set a keepalive time which increases stability
    Some cloud providers are terminating idle TCP connections
    more aggresively than  others. Setting a keepalive time of
    210 seconds will increase the stability of connections, for
    example, on Azure. The lower keepalive might impact battery
    lifetime on mobile devices so the keepalive should be
    increased when serving those.

--- src/py/flwr/server/grpc_server/grpc_server.py
+++ src/py/flwr/server/grpc_server/grpc_server.py
@@ -52,6 +52,7 @@ def start_grpc_server(
     server_address: str,
     max_concurrent_workers: int = 1000,
     max_message_length: int = GRPC_MAX_MESSAGE_LENGTH,
+    keepalive_time_ms: int = 7200000,
     certificates: Optional[Tuple[bytes, bytes, bytes]] = None,
 ) -> grpc.Server:
     """Create gRPC server and return instance of grpc.Server.
@@ -75,6 +76,25 @@ def start_grpc_server(
     max_message_length : int
         Maximum message length that the server can send or receive.
         Int valued in bytes. -1 means unlimited. (default: GRPC_MAX_MESSAGE_LENGTH)
+    keepalive_time_ms : int
+        The primary reason we are customizing this setting is that some cloud
+        providers, for example, Azure clean up idle TCP connections aggressively
+        and terminate them after some time (in case of Azure 4 minutes). As of
+        2022-02-13 Flower has no application level keepalive signals and relies on
+        the assumption that the transport layer will fail in case the connection is
+        no longer active. Configuring the gRPC keepalive to 210000 seconds (3 minutes
+        30 seconds) will ensure that on most cloud providers we can keep the long
+        running streaming connection which is as of 2022-02-13 the default in Flower.
+        The actual gRPC default of this setting is 7200000 (2 hours).
+
+        These settings are related to the issue described here:
+        - https://github.com/grpc/proposal/blob/master/A8-client-side-keepalive.md
+        - https://github.com/grpc/grpc/blob/master/doc/keepalive.md
+        - https://grpc.io/docs/guides/performance/
+
+        Users using mobile Flower clients may increase it if their server environment
+        allows them to have long running idle TCP connections.
+        (default: 210000)
     certificates : Tuple[bytes, bytes, bytes] (default: None)
         Tuple containing root certificate, server certificate, and private key to
         start a secure SSL-enabled server. The tuple is expected to have three bytes
@@ -104,22 +124,34 @@ def start_grpc_server(
     >>>     ),
     >>> )
     """
+    # Possible options:
+    # https://github.com/grpc/grpc/blob/v1.43.x/include/grpc/impl/codegen/grpc_types.h
+    options = [
+        # Maximum number of concurrent incoming streams to allow on a http2
+        # connection. Int valued.
+        ("grpc.max_concurrent_streams", max(100, max_concurrent_workers)),
+        # Maximum message length that the channel can send.
+        # Int valued, bytes. -1 means unlimited.
+        ("grpc.max_send_message_length", max_message_length),
+        # Maximum message length that the channel can receive.
+        # Int valued, bytes. -1 means unlimited.
+        ("grpc.max_receive_message_length", max_message_length),
+        # The actual gRPC default of this setting is 7200000 (2 hours). We are going
+        # to set it to 210000 (3 minutes and 30 seconds to cover Azure and others).
+        # Users using mobile Flower clients may increase it if their server environment
+        # allows them to have long running idle TCP connections.
+        ("grpc.keepalive_time_ms", 210000),
+        # Setting this to zero will allow sending unlimited keepalive's between sending
+        # a data frame. This is especially important
+        ("grpc.http2.max_pings_without_data", 0),
+    ]
+
     server = grpc.server(
         concurrent.futures.ThreadPoolExecutor(max_workers=max_concurrent_workers),
         # Set the maximum number of concurrent RPCs this server will service before
         # returning RESOURCE_EXHAUSTED status, or None to indicate no limit.
         maximum_concurrent_rpcs=max_concurrent_workers,
-        options=[
-            # Maximum number of concurrent incoming streams to allow on a http2
-            # connection. Int valued.
-            ("grpc.max_concurrent_streams", max(100, max_concurrent_workers)),
-            # Maximum message length that the channel can send.
-            # Int valued, bytes. -1 means unlimited.
-            ("grpc.max_send_message_length", max_message_length),
-            # Maximum message length that the channel can receive.
-            # Int valued, bytes. -1 means unlimited.
-            ("grpc.max_receive_message_length", max_message_length),
-        ],
+        options=options,
     )
 
     servicer = fss.FlowerServiceServicer(client_manager)
