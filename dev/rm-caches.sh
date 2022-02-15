4b01154f7d68b66e6449bb45373f4033509e1780
Daniel J. Beutel <daniel@adap.com>
2022-02-15 12:22:59 +0100

    Include .cache in rm-caches.sh (#1063)

--- dev/rm-caches.sh
+++ dev/rm-caches.sh
@@ -5,3 +5,4 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 find src -type d -name __pycache__ -exec rm -r {} \+
 rm -rf .mypy_cache
 rm -rf .pytest_cache
+rm -rf .cache
