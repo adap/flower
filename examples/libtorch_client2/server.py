8258127c0af2b816435fc05dece934b1a13c4141
Pedro Porto Buarque de Gusmão <pedropgusmao@gmail.com>
2022-03-04 07:56:52 +0000

    removing libtorch examples

--- examples/libtorch_client2/server.py
+++
@@ -1,5 +0,0 @@
-import flwr as fl
-from fedavg_libtorch import FedAvgLibTorch
-# Start Flower server for three rounds of federated learning
-if __name__ == "__main__":
-    fl.server.start_server("[::]:8080", config={"num_rounds": 3}, strategy=FedAvgLibTorch())
