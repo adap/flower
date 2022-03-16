3045c898d44c48999099fc961080b81b82364592
Daniel J. Beutel <daniel@adap.com>
2022-03-16 21:51:57 +0100

    Remove unused type ignore comment

--- src/py/flwr/__init__.py
+++ src/py/flwr/__init__.py
@@ -31,4 +31,4 @@ else:
     import importlib.metadata as importlib_metadata
 # pylint: enable=import-error, no-name-in-module
 
-__version__: str = importlib_metadata.version(__name__)  # type: ignore
+__version__: str = importlib_metadata.version(__name__)
