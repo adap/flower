01f6c655edc3f919450c9ac2a999c1395e4031f4
Daniel J. Beutel <daniel@adap.com>
2022-03-19 19:25:03 +0100

    Revert changes in dev/format.sh

--- dev/format.sh
+++ dev/format.sh
@@ -6,10 +6,6 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 python -m isort --skip src/py/flwr/proto src/py
 python -m black -q --exclude src/py/flwr/proto src/py
 python -m docformatter -i -r src/py/flwr -e src/py/flwr/proto
-python -m docformatter -i -r src/py/flwr_experimental
-python -m black -q baselines/flwr_baselines/experiments/fedbn_convergence_rate
-python -m docformatter -i -r baselines/flwr_baselines/experiments/fedbn_convergence_rate
-python -m isort baselines/flwr_baselines/experiments/fedbn_convergence_rate
 python -m docformatter -i -r src/py/flwr_tool
 
 # Protos
