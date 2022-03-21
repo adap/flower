a56f318f01498776d0421fa0d6efed38d5f13a05
Daniel J. Beutel <daniel@adap.com>
2022-03-21 09:51:41 +0100

    Shorten quickstart PyTorch example (#1117)

--- examples/quickstart_pytorch/client.py
+++ examples/quickstart_pytorch/client.py
@@ -3,67 +3,52 @@ import warnings
 
 import flwr as fl
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.transforms as transforms
+from torchvision.transforms import Compose, ToTensor, Normalize
 from torch.nn import GroupNorm
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 from torchvision.models import resnet18
+from tqdm import tqdm
 
+
+# #############################################################################
+# 1. Regular PyTorch pipeline: nn.Module, train, test, and DataLoader
+# #############################################################################
 
 warnings.filterwarnings("ignore", category=UserWarning)
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-# #############################################################################
-# 1. PyTorch pipeline: model/train/test/dataloader
-# #############################################################################
-
-
 def train(net, trainloader, epochs):
-    """Train the network on the training set."""
+    """Train the model on the training set."""
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    net.train()
     for _ in range(epochs):
-        for images, labels in trainloader:
-            images, labels = images.to(DEVICE), labels.to(DEVICE)
+        for images, labels in tqdm(trainloader):
             optimizer.zero_grad()
-            loss = criterion(net(images), labels)
-            loss.backward()
+            criterion(net(images.to(DEVICE)), labels.to(DEVICE)).backward()
             optimizer.step()
 
 
 def test(net, testloader):
-    """Validate the network on the entire test set."""
+    """Validate the model on the test set."""
     criterion = torch.nn.CrossEntropyLoss()
     correct, total, loss = 0, 0, 0.0
-    net.eval()
     with torch.no_grad():
-        for images, labels in testloader:
-            images, labels = images.to(DEVICE), labels.to(DEVICE)
-            outputs = net(images)
-            loss += criterion(outputs, labels).item()
-            _, predicted = torch.max(outputs.data, 1)
+        for images, labels in tqdm(testloader):
+            outputs = net(images.to(DEVICE))
+            loss += criterion(outputs, labels.to(DEVICE)).item()
             total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-    loss /= len(testloader.dataset)
-    accuracy = correct / total
-    return loss, accuracy
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+    return loss / len(testloader.dataset), correct / total
 
 
 def load_data():
     """Load CIFAR-10 (training and test set)."""
-    transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-    )
-    trainset = CIFAR10("./dataset", train=True, download=True, transform=transform)
-    testset = CIFAR10("./dataset", train=False, download=True, transform=transform)
-    trainloader = DataLoader(trainset, batch_size=32, shuffle=True)
-    testloader = DataLoader(testset, batch_size=32)
-    num_examples = {"trainset": len(trainset), "testset": len(testset)}
-    return trainloader, testloader, num_examples
+    trf = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trainset = CIFAR10("./data", train=True, download=True, transform=trf)
+    testset = CIFAR10("./data", train=False, download=True, transform=trf)
+    return DataLoader(trainset, batch_size=32, shuffle=True), DataLoader(testset)
 
 
 # #############################################################################
@@ -71,39 +56,30 @@ def load_data():
 # #############################################################################
 
 
-def main():
-    """Create model, load data, define Flower client, start Flower client."""
+# Load model and data (ResNet-18, CIFAR-10)
+net = resnet18(norm_layer=lambda x: GroupNorm(2, x), num_classes=10).to(DEVICE)
+trainloader, testloader = load_data()
 
-    # Load model
-    net = resnet18(norm_layer=lambda x: GroupNorm(2, x), num_classes=10)
-    net = net.to(DEVICE)
+# Define Flower client
+class FlowerClient(fl.client.NumPyClient):
+    def get_parameters(self):
+        return [val.cpu().numpy() for _, val in net.state_dict().items()]
 
-    # Load data (CIFAR-10)
-    trainloader, testloader, num_examples = load_data()
+    def set_parameters(self, parameters):
+        params_dict = zip(net.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        net.load_state_dict(state_dict, strict=True)
 
-    # Flower client
-    class CifarClient(fl.client.NumPyClient):
-        def get_parameters(self):
-            return [val.cpu().numpy() for _, val in net.state_dict().items()]
+    def fit(self, parameters, config):
+        self.set_parameters(parameters)
+        train(net, trainloader, epochs=1)
+        return self.get_parameters(), len(trainloader.dataset), {}
 
-        def set_parameters(self, parameters):
-            params_dict = zip(net.state_dict().keys(), parameters)
-            state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
-            net.load_state_dict(state_dict, strict=True)
-
-        def fit(self, parameters, config):
-            self.set_parameters(parameters)
-            train(net, trainloader, epochs=1)
-            return self.get_parameters(), num_examples["trainset"], {}
-
-        def evaluate(self, parameters, config):
-            self.set_parameters(parameters)
-            loss, accuracy = test(net, testloader)
-            return float(loss), num_examples["testset"], {"accuracy": float(accuracy)}
-
-    # Start client
-    fl.client.start_numpy_client("[::]:8080", client=CifarClient())
+    def evaluate(self, parameters, config):
+        self.set_parameters(parameters)
+        loss, accuracy = test(net, testloader)
+        return float(loss), len(testloader.dataset), {"accuracy": float(accuracy)}
 
 
-if __name__ == "__main__":
-    main()
+# Start Flower client
+fl.client.start_numpy_client("[::]:8080", client=FlowerClient())
