2786715e055d9070f2147704b1335af0d0dc60b8
Daniel J. Beutel <daniel@adap.com>
2022-03-23 19:15:46 +0100

    Use metrics aggregation in PyTorch quickstart example

--- examples/quickstart_pytorch/server.py
+++ examples/quickstart_pytorch/server.py
@@ -1,8 +1,21 @@
 import flwr as fl
 
+# Define metric aggregation function
+def agg(metrics):
+    # Weigh accuracy of each client by number of examples used
+    accuracies = [m["accuracy"] * n for m, n in metrics]
+    examples = [n for _, n in metrics]
+
+    # Aggregate and return custom metric
+    return {"accuracy": sum(accuracies) / sum(examples)}
+
+
+# Define strategy
+strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=agg)
 
 # Start Flower server
 fl.server.start_server(
     server_address="[::]:8080",
     config={"num_rounds": 3},
+    strategy=strategy,
 )
