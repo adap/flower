15c82db2aec93b0ed1295f2686219b43eeadb772
Daniel J. Beutel <daniel@adap.com>
2022-04-28 18:17:47 -0600

    Round timeout code example

---
+++ examples/timeout/client_fast.py
@@ -0,0 +1,24 @@
+import time
+import flwr as fl
+import numpy as np
+
+
+weights = [np.ones((3, 3))]
+
+# Define Flower client
+class TimeoutClient(fl.client.NumPyClient):
+    def get_parameters(self):
+        time.sleep(10)
+        return weights
+
+    def fit(self, parameters, config):
+        time.sleep(20)
+        return weights, 1, {}
+
+    def evaluate(self, parameters, config):
+        time.sleep(10)
+        return 0.1, 1, {"accuracy": 1}
+
+
+# Start Flower client
+fl.client.start_numpy_client("[::]:8080", client=TimeoutClient())
