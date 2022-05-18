afeb7313c28e3f91ac8b751ffac1e667d3257317
Daniel J. Beutel <daniel@adap.com>
2022-05-18 17:03:57 +0200

    Fix example

--- examples/quickstart_pytorch/server.py
+++ examples/quickstart_pytorch/server.py
@@ -1,18 +1,21 @@
+from typing import List, Tuple
+
 import flwr as fl
+from flwr.common import Metrics
 
 
 # Define metric aggregation function
-def agg(metrics):
-    # Weigh accuracy of each client by number of examples used
-    accuracies = [m["accuracy"] * n for m, n in metrics]
-    examples = [n for _, n in metrics]
+def weighted_average(metrics: List[Tuple[Metrics, int]]) -> Metrics:
+    # Multiply accuracy of each client by number of examples used
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
 
-    # Aggregate and return custom metric
+    # Aggregate and return custom metric (weighted average)
     return {"accuracy": sum(accuracies) / sum(examples)}
 
 
 # Define strategy
-strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=agg)
+strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_average)
 
 # Start Flower server
 fl.server.start_server(
