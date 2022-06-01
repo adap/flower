442d215900061ebc3fb6d77459f4bd8da008f1de
Alex Kyllo <jekyllo@microsoft.com>
2022-06-01 00:05:07 -0700

    main.py bugfixes

--- examples/dp_flower_opacus_mnist/main.py
+++ examples/dp_flower_opacus_mnist/main.py
@@ -1,18 +1,27 @@
-import sys
-import flwr as fl
-import multiprocessing as mp
 import argparse
-sys.path.insert(0,"..")
-from src.py.flwr.server.strategy.fedAvgDP import test
-from src.py.flwr.client.dp_client import DPClient
-from src.py.flwr.server.strategy.fedAvgDP import FedAvgDp
-from opacus import PrivacyEngine
+import multiprocessing as mp
+import sys
+from functools import partial
+
+from loguru import logger
+
+sys.path.insert(0, "../../src/py")
+
+from typing import List, Tuple
+
+import numpy as np
 import torch
 import torch.nn as nn
-from torch.nn import BCELoss, Linear, Module
-from tensorflow.keras.datasets import mnist
-from typing import List, Tuple
-import numpy as np
+from opacus import PrivacyEngine
+from torch.nn import CrossEntropyLoss
+from torch.optim import SGD
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+from torchvision.transforms import Compose, Normalize, ToTensor
+
+import flwr as fl
+from flwr.client.dp_client import DPClient, test
+from flwr.server.strategy import FedAvgDp
 
 
 # Training Model that is being used by the client
@@ -38,19 +47,33 @@ class Net(nn.Module):
         x = torch.tanh(self.fc2(x))
         x = self.fc3(x)
         return x
-    
-#function to load the MNIST dataset
+
+
+# function to load the MNIST dataset
 XY = Tuple[np.ndarray, np.ndarray]
 XYList = List[XY]
 PartitionedDataset = List[Tuple[XY, XY]]
-def load(
-    num_partitions: int,
-) -> PartitionedDataset:
-    """Create partitioned version of MNIST."""
-    xy_train, xy_test = tf.keras.datasets.mnist.load_data()
-    xy_train_partitions = create_partitions(xy_train, num_partitions)
-    xy_test_partitions = create_partitions(xy_test, num_partitions)
-    return list(zip(xy_train_partitions, xy_test_partitions))
+
+
+# def load(
+#     num_partitions: int,
+# ) -> PartitionedDataset:
+#     """Create partitioned version of MNIST."""
+#     xy_train, xy_test = tf.keras.datasets.mnist.load_data()
+#     xy_train_partitions = create_partitions(xy_train, num_partitions)
+#     xy_test_partitions = create_partitions(xy_test, num_partitions)
+#     return list(zip(xy_train_partitions, xy_test_partitions))
+
+
+def load_data(batch_size: int):
+    """Load CIFAR-10 (training and test set)."""
+    trf = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trainset = CIFAR10("./data", train=True, download=True, transform=trf)
+    testset = CIFAR10("./data", train=False, download=True, transform=trf)
+    return DataLoader(trainset, batch_size=batch_size, shuffle=True, drop_last=True), DataLoader(
+        testset
+    )
+
 
 # Function to get the weights of the model
 def get_weights(model):
@@ -58,35 +81,20 @@ def get_weights(model):
 
 
 # Main function to create DP client
-def main(args) -> None:
-    # Load model
-    model = Net()
-    
-    # Load a subset of MNIST to simulate the local data partition
-    (x_train, y_train), (x_test, y_test) = load(args.num_clients)[args.partition]
+def start_client(batch_size, epochs, cid) -> None:
+    """Start a client."""
+    module = Net()
 
-    # drop samples to form exact batches for dpsgd
-    # this is necessary since dpsgd is sensitive to uneven batches
-    # due to microbatching
-    if args.dpsgd and x_train.shape[0] % args.batch_size != 0:
-        drop_num = x_train.shape[0] % args.batch_size
-        x_train = x_train[:-drop_num]
-        y_train = y_train[:-drop_num]
+    train_loader, test_loader = load_data(batch_size)
 
-
-    # Start Flower client
-    # client = DPClient(model, x_train, y_train, x_test, y_test, args)
-    
-    
     optimizer = SGD(module.parameters(), lr=0.001)
-    criterion = BCELoss()
+    criterion = CrossEntropyLoss()
     target_epsilon = 1.0
     target_delta = 0.1
     privacy_engine = PrivacyEngine()
-    train_loader, test_loader = get_dataloaders(batch_size)
-    
+
     client = DPClient(
-        model,
+        module,
         optimizer,
         criterion,
         privacy_engine,
@@ -94,27 +102,16 @@ def main(args) -> None:
         test_loader,
         target_epsilon,
         target_delta,
-        epochs=10,
+        epochs=epochs,
         max_grad_norm=1.0,
     )
+    logger.info("Starting client # {}", cid)
     fl.client.start_numpy_client("[::]:8080", client=client)
-    if args.dpsgd:
-        print("Privacy Loss: ", PRIVACY_LOSS)
-        
-        
 
-def get_dataloaders(batch_size):
-    """Get dataloaders for unit testing."""
-    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
-    X = torch.from_numpy(numpy.array(X_train))
-    y = torch.from_numpy(numpy.array(Y_train))
-    train_loader = DataLoader(TensorDataset(X, y), batch_size=batch_size)
-    test_loader = DataLoader(TensorDataset(X, y), batch_size=batch_size)
-    return train_loader, test_loader
 
 def get_eval_fn():
     """Get the evaluation function for server side.
-    
+
     Returns
     -------
     evaluate
@@ -136,7 +133,6 @@ def get_eval_fn():
         accuracy
             Accuracy on the test set.
         """
-        global batch_size
         # Prepare multiprocess
         manager = mp.Manager()
         # We receive the results through a shared dictionary
@@ -163,6 +159,7 @@ def get_eval_fn():
 
     return evaluate
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flower Client")
     parser.add_argument(
@@ -174,7 +171,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--partition",
         type=int,
-        required=True,
+        default=0,
         help="Data Partion to train on. Must be less than number of clients",
     )
     parser.add_argument(
@@ -208,51 +205,47 @@ if __name__ == "__main__":
         help="Number of microbatches " "(must evenly divide batch_size)",
     )
     args = parser.parse_args()
+    epochs = args.local_epochs
+    num_clients = args.num_clients
+    # ----------------------------------------------------------------
 
-    main(args)
-    
-    #----------------------------------------------------------------
-    
     # NOW THAT WE HAVE DONE CLIENT LETS START THE SERVER
-    
-    
-    
+
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument(
         "-r", type=int, default=3, help="Number of rounds for the federated training"
     )
-    
+
     parser.add_argument(
         "-nbc",
         type=int,
         default=2,
         help="Number of clients to keep track of dataset share",
     )
-    
+
     parser.add_argument("-b", type=int, default=256, help="Batch size")
-    
+
     parser.add_argument(
         "-fc",
         type=int,
         default=2,
         help="Min fit clients, min number of clients to be sampled next round",
     )
-    
+
     parser.add_argument(
         "-ac",
         type=int,
         default=2,
         help="Min available clients, min number of clients that need to connect to the server before training round can start",
     )
-    
+
     args = parser.parse_args()
     rounds = int(args.r)
     fc = int(args.fc)
     ac = int(args.ac)
-    global batch_size
     batch_size = int(args.b)
-    
+
     # Set the start method for multiprocessing in case Python version is under 3.8.1
     mp.set_start_method("spawn")
     # Create a new fresh model to initialize parameters
@@ -260,19 +253,25 @@ if __name__ == "__main__":
     init_weights = get_weights(net)
     # Convert the weights (np.ndarray) to parameters
     init_param = fl.common.weights_to_parameters(init_weights)
-    
+
     # del the net as we don't need it anymore
     del net
-    
+
     # Define the strategy
     strategy = FedAvgDp(
         fraction_fit=float(fc / ac),
         min_fit_clients=fc,
         min_available_clients=ac,
-        eval_fn=get_eval_fn(),
+        # eval_fn=get_eval_fn(), # TODO: Pickle error because of the inner function
         initial_parameters=init_param,
     )
-    fl.server.start_server(
-        "[::]:8080", config={"num_rounds": rounds}, strategy=strategy
+    server_process = mp.Process(
+        target=fl.server.start_server,
+        args=["[::]:8080"],
+        kwargs=dict(config={"num_rounds": rounds}, strategy=strategy),
     )
-    
+    server_process.start()
+    client_fn = partial(start_client, batch_size, epochs)
+    with mp.Pool(num_clients) as pool:
+        pool.map(client_fn, range(num_clients))
+    server_process.kill()
