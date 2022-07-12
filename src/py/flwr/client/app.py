cf6cc0c64ca82cfc6c1a3d92e9751075c416f957
Daniel J. Beutel <daniel@adap.com>
2022-07-12 13:16:07 +0200

    Move wrapper class methods into different scope

--- src/py/flwr/client/app.py
+++ src/py/flwr/client/app.py
@@ -223,96 +223,98 @@ def to_client(client_like: ClientLike) -> Client:
     return client_like
 
 
+def _constructor(self: Client, numpy_client: NumPyClient) -> None:
+    self.numpy_client = numpy_client  # type: ignore
+
+
+def _get_properties(self: Client, ins: GetPropertiesIns) -> GetPropertiesRes:
+    """Return the current client properties."""
+    properties = self.numpy_client.get_properties(config=ins.config)  # type: ignore
+    return GetPropertiesRes(
+        status=Status(code=Code.OK, message="Success"),
+        properties=properties,
+    )
+
+
+def _get_parameters(self: Client, ins: GetParametersIns) -> GetParametersRes:
+    """Return the current local model parameters."""
+    parameters = self.numpy_client.get_parameters(config=ins.config)  # type: ignore
+    parameters_proto = weights_to_parameters(parameters)
+    return GetParametersRes(
+        status=Status(code=Code.OK, message="Success"), parameters=parameters_proto
+    )
+
+
+def _fit(self: Client, ins: FitIns) -> FitRes:
+    """Refine the provided weights using the locally held dataset."""
+    # Deconstruct FitIns
+    parameters: List[np.ndarray] = parameters_to_weights(ins.parameters)
+
+    # Train
+    results = self.numpy_client.fit(parameters, ins.config)  # type: ignore
+    if not (
+        len(results) == 3
+        and isinstance(results[0], list)
+        and isinstance(results[1], int)
+        and isinstance(results[2], dict)
+    ):
+        raise Exception(EXCEPTION_MESSAGE_WRONG_RETURN_TYPE_FIT)
+
+    # Return FitRes
+    parameters_prime, num_examples, metrics = results
+    parameters_prime_proto = weights_to_parameters(parameters_prime)
+    return FitRes(
+        status=Status(code=Code.OK, message="Success"),
+        parameters=parameters_prime_proto,
+        num_examples=num_examples,
+        metrics=metrics,
+    )
+
+
+def _evaluate(self: Client, ins: EvaluateIns) -> EvaluateRes:
+    """Evaluate the provided parameters using the locally held dataset."""
+    parameters: List[np.ndarray] = parameters_to_weights(ins.parameters)
+
+    results = self.numpy_client.evaluate(parameters, ins.config)  # type: ignore
+    if not (
+        len(results) == 3
+        and isinstance(results[0], float)
+        and isinstance(results[1], int)
+        and isinstance(results[2], dict)
+    ):
+        raise Exception(EXCEPTION_MESSAGE_WRONG_RETURN_TYPE_EVALUATE)
+
+    # Return EvaluateRes
+    loss, num_examples, metrics = results
+    return EvaluateRes(
+        status=Status(code=Code.OK, message="Success"),
+        loss=loss,
+        num_examples=num_examples,
+        metrics=metrics,
+    )
+
+
 def _wrap_numpy_client(client: NumPyClient) -> Client:
-    # Define wrapper class methods
-
-    def constructor(self: Client, numpy_client: NumPyClient) -> None:
-        self.numpy_client = numpy_client  # type: ignore
-
-    def get_properties(self: Client, ins: GetPropertiesIns) -> GetPropertiesRes:
-        """Return the current client properties."""
-        properties = self.numpy_client.get_properties(config=ins.config)  # type: ignore
-        return GetPropertiesRes(
-            status=Status(code=Code.OK, message="Success"),
-            properties=properties,
-        )
-
-    def get_parameters(self: Client, ins: GetParametersIns) -> GetParametersRes:
-        """Return the current local model parameters."""
-        parameters = self.numpy_client.get_parameters(config=ins.config)  # type: ignore
-        parameters_proto = weights_to_parameters(parameters)
-        return GetParametersRes(
-            status=Status(code=Code.OK, message="Success"), parameters=parameters_proto
-        )
-
-    def fit(self: Client, ins: FitIns) -> FitRes:
-        """Refine the provided weights using the locally held dataset."""
-        # Deconstruct FitIns
-        parameters: List[np.ndarray] = parameters_to_weights(ins.parameters)
-
-        # Train
-        results = self.numpy_client.fit(parameters, ins.config)  # type: ignore
-        if not (
-            len(results) == 3
-            and isinstance(results[0], list)
-            and isinstance(results[1], int)
-            and isinstance(results[2], dict)
-        ):
-            raise Exception(EXCEPTION_MESSAGE_WRONG_RETURN_TYPE_FIT)
-
-        # Return FitRes
-        parameters_prime, num_examples, metrics = results
-        parameters_prime_proto = weights_to_parameters(parameters_prime)
-        return FitRes(
-            status=Status(code=Code.OK, message="Success"),
-            parameters=parameters_prime_proto,
-            num_examples=num_examples,
-            metrics=metrics,
-        )
-
-    def evaluate(self: Client, ins: EvaluateIns) -> EvaluateRes:
-        """Evaluate the provided parameters using the locally held dataset."""
-        parameters: List[np.ndarray] = parameters_to_weights(ins.parameters)
-
-        results = self.numpy_client.evaluate(parameters, ins.config)  # type: ignore
-        if not (
-            len(results) == 3
-            and isinstance(results[0], float)
-            and isinstance(results[1], int)
-            and isinstance(results[2], dict)
-        ):
-            raise Exception(EXCEPTION_MESSAGE_WRONG_RETURN_TYPE_EVALUATE)
-
-        # Return EvaluateRes
-        loss, num_examples, metrics = results
-        return EvaluateRes(
-            status=Status(code=Code.OK, message="Success"),
-            loss=loss,
-            num_examples=num_examples,
-            metrics=metrics,
-        )
-
-    # Create wrapper class dynamically
-
     member_dict: Dict[str, Callable] = {  # type: ignore
-        "__init__": constructor,
+        "__init__": _constructor,
     }
 
-    # Dynamically add wrapper type methods (if overridden by )
+    # Add wrapper type methods (if overridden)
 
     if numpyclient_has_get_properties(client=client):
-        member_dict["get_properties"] = get_properties
+        member_dict["get_properties"] = _get_properties
 
     if numpyclient_has_get_parameters(client=client):
-        member_dict["get_parameters"] = get_parameters
+        member_dict["get_parameters"] = _get_parameters
 
     if numpyclient_has_fit(client=client):
-        member_dict["fit"] = fit
+        member_dict["fit"] = _fit
 
     if numpyclient_has_evaluate(client=client):
-        member_dict["evaluate"] = evaluate
+        member_dict["evaluate"] = _evaluate
 
-    DynNumPyClientWrapper = type("DynNumPyClientWrapper", (Client,), member_dict)
+    # Create wrapper class
+    wrapper_class = type("NumPyClientWrapper", (Client,), member_dict)
 
-    # Return instance of newly created class
-    return DynNumPyClientWrapper(numpy_client=client)  # type: ignore
+    # Create and return an instance of the newly created class
+    return wrapper_class(numpy_client=client)  # type: ignore
