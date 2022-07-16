75da98942a30a0b91654dd4f08b21aaf50049ebe
Daniel J. Beutel <daniel@adap.com>
2022-07-16 14:53:47 +0200

    Update quickstart_pytorch

--- examples/quickstart_pytorch/run.sh
+++ examples/quickstart_pytorch/run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
+
+# Download the CIFAR-10 dataset
+python -c "from torchvision.datasets import CIFAR10; CIFAR10('./data', download=True)"
 
 echo "Starting server"
 python server.py &
@@ -9,7 +14,7 @@ for i in `seq 0 1`; do
     python client.py &
 done
 
-# This will allow you to use CTRL+C to stop all background processes
+# Enable CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
 # Wait for all background processes to complete
 wait
