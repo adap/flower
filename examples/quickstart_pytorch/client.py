56ce62a76063be3a088d55ccd1b6aa852f600826
Daniel J. Beutel <daniel@adap.com>
2022-07-24 13:37:54 +0200

    Add missing config parameter

--- examples/quickstart_pytorch/client.py
+++ examples/quickstart_pytorch/client.py
@@ -94,7 +94,7 @@ class FlowerClient(fl.client.NumPyClient):
     def fit(self, parameters, config):
         self.set_parameters(parameters)
         train(net, trainloader, epochs=1)
-        return self.get_parameters(), len(trainloader.dataset), {}
+        return self.get_parameters(config={}), len(trainloader.dataset), {}
 
     def evaluate(self, parameters, config):
         self.set_parameters(parameters)
