0aead3744a461f108705e168cc304fed5af07883
Taner Topal <taner@adap.com>
2022-07-27 16:14:41 +0200

    Fix

--- examples/quickstart_tensorflow/server.py
+++ examples/quickstart_tensorflow/server.py
@@ -4,5 +4,5 @@ import flwr as fl
 # Start Flower server
 fl.server.start_server(
     server_address="0.0.0.0:8080",
-    config={"num_rounds": 3},
+    config=fl.server.ServerConfig(num_rounds=3),
 )
