9427d70ff5737b64b5e16a9cbd3440ccab58cbcc
Akis Linardos <linardos.akis@gmail.com>
2022-09-06 17:05:45 +0300

    Checkpointing Pytorch Example
    The code runs properly and stores the checkpoint, but the checkpoint doesn't maintain the performance once I load it.

---
+++ examples/checkpointing_pytorch/client_checkpointing.py
@@ -0,0 +1,71 @@
+from collections import OrderedDict
+from typing import Dict, List, Tuple
+
+import numpy as np
+import torch
+
+import cifar
+import flwr as fl
+
+DEVICE: str = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class CifarClient(fl.client.NumPyClient):
+    """Flower client implementing CIFAR-10 image classification using
+    PyTorch."""
+
+    def __init__(
+        self,
+        model: cifar.Net,
+        trainloader: torch.utils.data.DataLoader,
+        testloader: torch.utils.data.DataLoader,
+        num_examples: Dict,
+    ) -> None:
+        self.model = model
+        self.trainloader = trainloader
+        self.testloader = testloader
+        self.num_examples = num_examples
+
+    def get_parameters(self) -> List[np.ndarray]:
+        # Return model parameters as a list of NumPy ndarrays
+        return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
+
+    def set_parameters(self, parameters: List[np.ndarray]) -> None:
+        # Set model parameters from a list of NumPy ndarrays
+        params_dict = zip(self.model.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        self.model.load_state_dict(state_dict, strict=True)
+
+    def fit(
+        self, parameters: List[np.ndarray], config: Dict[str, str]
+    ) -> Tuple[List[np.ndarray], int, Dict]:
+        # Set model parameters, train model, return updated model parameters
+        self.set_parameters(parameters)
+        cifar.train(self.model, self.trainloader, epochs=1, device=DEVICE)
+        return self.get_parameters(), self.num_examples["trainset"], {}
+
+    def evaluate(
+        self, parameters: List[np.ndarray], config: Dict[str, str]
+    ) -> Tuple[float, int, Dict]:
+        # Set model parameters, evaluate model on local test dataset, return result
+        self.set_parameters(parameters)
+        loss, accuracy = cifar.test(self.model, self.testloader, device=DEVICE)
+        return float(loss), self.num_examples["testset"], {"accuracy": float(accuracy)}
+
+
+        
+def main() -> None:
+    """Load data, start CifarClient."""
+
+    # Load model and data
+    model = cifar.Net()
+    model.to(DEVICE)
+    trainloader, testloader, num_examples = cifar.load_data()
+
+    # Start client
+    client = CifarClient(model, trainloader, testloader, num_examples)
+    fl.client.start_numpy_client("0.0.0.0:8080", client)
+
+
+if __name__ == "__main__":
+    main()        
---
+++ examples/checkpointing_pytorch/server_checkpointing.py
@@ -0,0 +1,53 @@
+from typing import List, Optional, Tuple
+from collections import OrderedDict
+import glob
+import os
+
+import torch
+import flwr as fl
+
+import cifar
+
+DEVICE: str = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Load PyTorch model
+net = cifar.Net().to(DEVICE)
+continue_from_checkpoint = True 
+class SaveModelStrategy(fl.server.strategy.FedAvg):
+    def aggregate_fit(
+        self,
+        rnd: int,
+        results: List[Tuple[fl.server.client_proxy.ClientProxy, fl.common.FitRes]],
+        failures: List[BaseException],
+    ) -> Optional[fl.common.Weights]:
+
+        """Aggregate model weights using weighted average and store checkpoint"""
+        aggregated_parameters_tuple = super().aggregate_fit(rnd, results, failures)
+        aggregated_parameters, _ = aggregated_parameters_tuple
+        # log_dict['aggregated_parameters']=aggregated_parameters
+    
+        if aggregated_parameters is not None:
+            print(f"Saving round {rnd} aggregated_parameters...")
+            # Convert `Parameters` to `List[np.ndarray]`
+            aggregated_weights: List[np.ndarray] = fl.common.parameters_to_weights(aggregated_parameters)
+            
+            # Convert `List[np.ndarray]` to PyTorch`state_dict`
+            params_dict = zip(net.state_dict().keys(), aggregated_weights)
+            state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+            net.load_state_dict(state_dict, strict=True)
+            # Save the model
+            torch.save(net.state_dict(), f"model_round_{rnd}.pth")
+
+        return aggregated_parameters_tuple
+
+if continue_from_checkpoint:
+    # import Net
+    list_of_files = [fname for fname in glob.glob("./model_round_*")]
+    latest_round_file = max(list_of_files, key=os.path.getctime)
+    print("Loading pre-trained model from: ", latest_round_file)
+    state_dict = torch.load(latest_round_file)
+    net.load_state_dict(state_dict)
+
+if __name__ == "__main__":
+    fl.server.start_server("0.0.0.0:8080", 
+    strategy=SaveModelStrategy(),
+    config={"num_rounds":3})
