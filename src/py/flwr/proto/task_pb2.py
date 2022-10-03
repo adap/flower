39bf92a6f0ef3b0984aec2bafd6656bc65f8cb30
Daniel J. Beutel <daniel@adap.com>
2022-10-03 12:30:57 +0200

    Add task assignment to CreateTasksRequest (#1436)
    * Add task assignment to CreateTasksRequest
    * Recompile task.proto

--- src/py/flwr/proto/task_pb2.py
+++ src/py/flwr/proto/task_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x15\x66lwr/proto/task.proto\x12\nflwr.proto\x1a\x1a\x66lwr/proto/transport.proto\"Q\n\x04Task\x12\x0f\n\x07task_id\x18\x01 \x01(\x03\x12\x38\n\x15legacy_server_message\x18\x65 \x01(\x0b\x32\x19.flwr.proto.ServerMessage\"S\n\x06Result\x12\x0f\n\x07task_id\x18\x01 \x01(\x03\x12\x38\n\x15legacy_client_message\x18\x65 \x01(\x0b\x32\x19.flwr.proto.ClientMessageb\x06proto3'
+  serialized_pb=b'\n\x15\x66lwr/proto/task.proto\x12\nflwr.proto\x1a\x1a\x66lwr/proto/transport.proto\"U\n\x04Task\x12\x0f\n\x07task_id\x18\x01 \x01(\x03\x12<\n\x15legacy_server_message\x18\x65 \x01(\x0b\x32\x19.flwr.proto.ServerMessageB\x02\x18\x01\"D\n\x0eTaskAssignment\x12\x1e\n\x04task\x18\x01 \x01(\x0b\x32\x10.flwr.proto.Task\x12\x12\n\nclient_ids\x18\x02 \x03(\x03\"W\n\x06Result\x12\x0f\n\x07task_id\x18\x01 \x01(\x03\x12<\n\x15legacy_client_message\x18\x65 \x01(\x0b\x32\x19.flwr.proto.ClientMessageB\x02\x18\x01\x62\x06proto3'
   ,
   dependencies=[flwr_dot_proto_dot_transport__pb2.DESCRIPTOR,])
 
@@ -48,7 +48,7 @@ _TASK = _descriptor.Descriptor(
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+      serialized_options=b'\030\001', file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -62,7 +62,46 @@ _TASK = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=65,
-  serialized_end=146,
+  serialized_end=150,
+)
+
+
+_TASKASSIGNMENT = _descriptor.Descriptor(
+  name='TaskAssignment',
+  full_name='flwr.proto.TaskAssignment',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='task', full_name='flwr.proto.TaskAssignment.task', index=0,
+      number=1, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='client_ids', full_name='flwr.proto.TaskAssignment.client_ids', index=1,
+      number=2, type=3, cpp_type=2, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=152,
+  serialized_end=220,
 )
 
 
@@ -87,7 +126,7 @@ _RESULT = _descriptor.Descriptor(
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+      serialized_options=b'\030\001', file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -100,13 +139,15 @@ _RESULT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=148,
-  serialized_end=231,
+  serialized_start=222,
+  serialized_end=309,
 )
 
 _TASK.fields_by_name['legacy_server_message'].message_type = flwr_dot_proto_dot_transport__pb2._SERVERMESSAGE
+_TASKASSIGNMENT.fields_by_name['task'].message_type = _TASK
 _RESULT.fields_by_name['legacy_client_message'].message_type = flwr_dot_proto_dot_transport__pb2._CLIENTMESSAGE
 DESCRIPTOR.message_types_by_name['Task'] = _TASK
+DESCRIPTOR.message_types_by_name['TaskAssignment'] = _TASKASSIGNMENT
 DESCRIPTOR.message_types_by_name['Result'] = _RESULT
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -117,6 +158,13 @@ Task = _reflection.GeneratedProtocolMessageType('Task', (_message.Message,), {
   })
 _sym_db.RegisterMessage(Task)
 
+TaskAssignment = _reflection.GeneratedProtocolMessageType('TaskAssignment', (_message.Message,), {
+  'DESCRIPTOR' : _TASKASSIGNMENT,
+  '__module__' : 'flwr.proto.task_pb2'
+  # @@protoc_insertion_point(class_scope:flwr.proto.TaskAssignment)
+  })
+_sym_db.RegisterMessage(TaskAssignment)
+
 Result = _reflection.GeneratedProtocolMessageType('Result', (_message.Message,), {
   'DESCRIPTOR' : _RESULT,
   '__module__' : 'flwr.proto.task_pb2'
@@ -125,4 +173,6 @@ Result = _reflection.GeneratedProtocolMessageType('Result', (_message.Message,),
 _sym_db.RegisterMessage(Result)
 
 
+_TASK.fields_by_name['legacy_server_message']._options = None
+_RESULT.fields_by_name['legacy_client_message']._options = None
 # @@protoc_insertion_point(module_scope)
