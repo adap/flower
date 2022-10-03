1df031a22963ec335c57854b7a507d526422c919
Daniel J. Beutel <daniel@adap.com>
2022-10-03 11:12:35 +0100

    Recompile task.proto

--- src/py/flwr/proto/task_pb2.pyi
+++ src/py/flwr/proto/task_pb2.pyi
@@ -28,22 +28,6 @@ class Task(google.protobuf.message.Message):
     def ClearField(self, field_name: typing_extensions.Literal["legacy_server_message",b"legacy_server_message","task_id",b"task_id"]) -> None: ...
 global___Task = Task
 
-class Result(google.protobuf.message.Message):
-    DESCRIPTOR: google.protobuf.descriptor.Descriptor
-    TASK_ID_FIELD_NUMBER: builtins.int
-    LEGACY_CLIENT_MESSAGE_FIELD_NUMBER: builtins.int
-    task_id: builtins.int
-    @property
-    def legacy_client_message(self) -> flwr.proto.transport_pb2.ClientMessage: ...
-    def __init__(self,
-        *,
-        task_id: builtins.int = ...,
-        legacy_client_message: typing.Optional[flwr.proto.transport_pb2.ClientMessage] = ...,
-        ) -> None: ...
-    def HasField(self, field_name: typing_extensions.Literal["legacy_client_message",b"legacy_client_message"]) -> builtins.bool: ...
-    def ClearField(self, field_name: typing_extensions.Literal["legacy_client_message",b"legacy_client_message","task_id",b"task_id"]) -> None: ...
-global___Result = Result
-
 class TaskAssignment(google.protobuf.message.Message):
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
     TASK_FIELD_NUMBER: builtins.int
@@ -60,3 +44,19 @@ class TaskAssignment(google.protobuf.message.Message):
     def HasField(self, field_name: typing_extensions.Literal["task",b"task"]) -> builtins.bool: ...
     def ClearField(self, field_name: typing_extensions.Literal["client_ids",b"client_ids","task",b"task"]) -> None: ...
 global___TaskAssignment = TaskAssignment
+
+class Result(google.protobuf.message.Message):
+    DESCRIPTOR: google.protobuf.descriptor.Descriptor
+    TASK_ID_FIELD_NUMBER: builtins.int
+    LEGACY_CLIENT_MESSAGE_FIELD_NUMBER: builtins.int
+    task_id: builtins.int
+    @property
+    def legacy_client_message(self) -> flwr.proto.transport_pb2.ClientMessage: ...
+    def __init__(self,
+        *,
+        task_id: builtins.int = ...,
+        legacy_client_message: typing.Optional[flwr.proto.transport_pb2.ClientMessage] = ...,
+        ) -> None: ...
+    def HasField(self, field_name: typing_extensions.Literal["legacy_client_message",b"legacy_client_message"]) -> builtins.bool: ...
+    def ClearField(self, field_name: typing_extensions.Literal["legacy_client_message",b"legacy_client_message","task_id",b"task_id"]) -> None: ...
+global___Result = Result
