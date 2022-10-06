f07fa26793992bddd60f83b203afb2b2f80b449f
Taner Topal <taner@adap.com>
2022-10-06 15:27:02 +0200

    Update fedavg.py

--- src/py/flwr/server/strategy/fedavg.py
+++ src/py/flwr/server/strategy/fedavg.py
@@ -84,7 +84,9 @@ class FedAvg(Strategy):
             is larger `fraction_fit * available_clients` than `min_fit_clients`
             will still be sampled. Defaults to 1.0.
         fraction_evaluate : float, optional
-            Fraction of clients used during validation. Defaults to 1.0.
+            Fraction of clients used during validation. In case `min_evaluate_clients`
+            is larger `fraction_evaluate * available_clients` than `min_evaluate_clients`
+            will still be sampled. Defaults to 1.0.
         min_fit_clients : int, optional
             Minimum number of clients used during training. Defaults to 2.
         min_evaluate_clients : int, optional
