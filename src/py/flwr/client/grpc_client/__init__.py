2670f1d58cbacb02ac5823b9ecfe7a66559b63aa
Daniel J. Beutel <daniel@adap.com>
2022-10-23 20:40:05 +0100

    Update package docstring

--- src/py/flwr/client/grpc_client/__init__.py
+++ src/py/flwr/client/grpc_client/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Client-side gRPC transport layer."""
+"""Client-side part of the gRPC transport layer."""
