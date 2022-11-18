c7f22759cb9380cb1603efda1bbbbd5efba6da52
Pedro Porto Buarque de Gusmão <pedropgusmao@gmail.com>
2022-11-18 11:52:24 +0000

    Update run_server.sh
    Make sure both client and server use the same port `8000`.

--- examples/quickstart_rest_api/run_server.sh
+++ examples/quickstart_rest_api/run_server.sh
@@ -1,2 +1,2 @@
 # Launch Flower REST Server
-uvicorn flwr.server.rest_server.rest_api:app --reload --host 0.0.0.0 --port 8080
+uvicorn flwr.server.rest_server.rest_api:app --reload --host 0.0.0.0 --port 8000
