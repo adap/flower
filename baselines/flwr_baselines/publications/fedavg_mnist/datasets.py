6efb6fbc90ef187271f2476195ba24a3d65f0596
charlesbvll <charles@adap.com>
2022-12-06 11:18:56 +0100

    Rename dataset.py to fix mypy error

--- baselines/flwr_baselines/publications/fedavg_mnist/datasets.py
+++ baselines/flwr_baselines/publications/fedavg_mnist/datasets.py
@@ -8,6 +8,51 @@ from torch.utils.data import ConcatDataset, DataLoader, Dataset, Subset, random_
 from torchvision.datasets import MNIST
 
 
+def load_datasets(
+    num_clients: int = 10,
+    idd: Optional[bool] = True,
+    val_ratio: float = 0.1,
+    batch_size: Optional[int] = 32,
+    seed: Optional[int] = 42,
+) -> Tuple[DataLoader, DataLoader, DataLoader]:
+    """Creates the dataloaders to be fed into the model.
+
+    Parameters
+    ----------
+    num_clients : int, optional
+        The number of clients that hold a part of the data, by default 10
+    idd : bool, optional
+        Whether the data should be independent and identically distributed between the clients or if the data should first be sorted
+        by labels and distributed by chunks to each client (used to test the convergence in a worst case scenario), by default True
+    val_ratio : float, optional
+        The ratio of training data that will be used for validation (between 0 and 1), by default 0.1
+    batch_size : int, optional
+        The size of the batches to be fed into the model, by default 32
+    seed : int, optional
+        Used to set a fix seed to replicate experiments, by default 42
+
+    Returns
+    -------
+    Tuple[DataLoader, DataLoader, DataLoader]
+        The DataLoader for training, the DataLoader for validation, the DataLoader for testing.
+    """
+    datasets, testset = _partition_data(num_clients, idd, seed)
+    # Split each partition into train/val and create DataLoader
+    trainloaders = []
+    valloaders = []
+    for ds in datasets:
+        len_val = len(ds) // (1 / val_ratio)
+        len_train = len(ds) - len_val
+        lengths = [len_train, len_val]
+        ds_train, ds_val = random_split(
+            ds, lengths, torch.Generator().manual_seed(seed)
+        )
+        trainloaders.append(DataLoader(ds_train, batch_size=batch_size, shuffle=True))
+        valloaders.append(DataLoader(ds_val, batch_size=batch_size))
+    testloader = DataLoader(testset, batch_size=batch_size)
+    return trainloaders, valloaders, testloader
+
+
 def _download_data() -> Tuple[Dataset, Dataset]:
     """Downloads (if necessary) and returns the MNIST dataset.
 
@@ -70,48 +115,3 @@ def _partition_data(
         ]
 
     return datasets, testset
-
-
-def load_datasets(
-    num_clients: int = 10,
-    idd: Optional[bool] = True,
-    val_ratio: float = 0.1,
-    batch_size: Optional[int] = 32,
-    seed: Optional[int] = 42,
-) -> Tuple[DataLoader, DataLoader, DataLoader]:
-    """Creates the dataloaders to be fed into the model.
-
-    Parameters
-    ----------
-    num_clients : int, optional
-        The number of clients that hold a part of the data, by default 10
-    idd : bool, optional
-        Whether the data should be independent and identically distributed between the clients or if the data should first be sorted
-        by labels and distributed by chunks to each client (used to test the convergence in a worst case scenario), by default True
-    val_ratio : float, optional
-        The ratio of training data that will be used for validation (between 0 and 1), by default 0.1
-    batch_size : int, optional
-        The size of the batches to be fed into the model, by default 32
-    seed : int, optional
-        Used to set a fix seed to replicate experiments, by default 42
-
-    Returns
-    -------
-    Tuple[DataLoader, DataLoader, DataLoader]
-        The DataLoader for training, the DataLoader for validation, the DataLoader for testing.
-    """
-    datasets, testset = _partition_data(num_clients, idd, seed)
-    # Split each partition into train/val and create DataLoader
-    trainloaders = []
-    valloaders = []
-    for ds in datasets:
-        len_val = len(ds) // (1 / val_ratio)
-        len_train = len(ds) - len_val
-        lengths = [len_train, len_val]
-        ds_train, ds_val = random_split(
-            ds, lengths, torch.Generator().manual_seed(seed)
-        )
-        trainloaders.append(DataLoader(ds_train, batch_size=batch_size, shuffle=True))
-        valloaders.append(DataLoader(ds_val, batch_size=batch_size))
-    testloader = DataLoader(testset, batch_size=batch_size)
-    return trainloaders, valloaders, testloader
