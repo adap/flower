fe4f9525c51461f3d9174d67384a0cce0de84df7
charlesbvll <charles@adap.com>
2022-12-11 19:12:39 +0100

    Better comment

--- dev/venv-create.sh
+++ dev/venv-create.sh
@@ -4,7 +4,8 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
 version=${1:-3.7.12}
 
-# Check if the directory does not exist and if so, install python
+# Check if the directory for the Python version does not exist and if so, 
+# install the right Python version through pyenv
 [[ ! -d $PYENV_ROOT/versions/$version ]] && pyenv install $version
 
 pyenv virtualenv $version flower-$version
