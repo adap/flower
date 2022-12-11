541fed9dfeda9f3205d5cfab83475dcb27ba5a59
Charles Beauville <charlesbeauville@gmail.com>
2022-12-11 19:04:23 +0100

    Add comment to dev/venv-create.sh
    Co-authored-by: Taner Topal <taner@adap.com>

--- dev/venv-create.sh
+++ dev/venv-create.sh
@@ -4,6 +4,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
 version=${1:-3.7.12}
 
+# Check if the directory does not exist and if so, install python
 [[ ! -d $PYENV_ROOT/versions/$version ]] && pyenv install $version
 
 pyenv virtualenv $version flower-$version
