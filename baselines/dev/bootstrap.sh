ad1a71b72d2066acb239f64bf852cd698cf2d135
Charles Beauville <charles@adap.com>
2022-12-16 19:58:06 +0100

    Align scripts from baselines to scripts on root (#1514)

--- baselines/dev/bootstrap.sh
+++ baselines/dev/bootstrap.sh
@@ -2,17 +2,8 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
-version=${1:-3.7.12}
-
-# Destroy and recreate the venv
-./dev/venv-delete.sh $version
-./dev/venv-create.sh $version
-
 # Remove caches
 ./dev/rm-caches.sh
-
-# Remove poetry.lock file
-rm -f poetry.lock
 
 # Upgrade/install spcific versions of `pip`, `setuptools`, and `poetry`
 python -m pip install -U pip==22.2
