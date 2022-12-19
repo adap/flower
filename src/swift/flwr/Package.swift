8651ee59ac828602f61ba754967a3f3d8e5f3752
Christoph Weinhuber <christoph.weinhuber@tum.de>
2022-12-19 09:24:30 +0100

    Lowered iOS version requirement

--- src/swift/flwr/Package.swift
+++ src/swift/flwr/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "flwr",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v13),
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
