ee21fcf2cd81577bd61bce525f04a4629a404a69
Taner Topal <taner@adap.com>
2023-01-11 20:58:14 -0800

    Update telemetry code

--- src/py/flwr/common/telemetry.py
+++ src/py/flwr/common/telemetry.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Flower Telemetry."""
+"""Flower telemetry."""
 
 import datetime
 import json
