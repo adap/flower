d2654004049e760cd77f76c8da73de85dcf9984e
Taner Topal <taner@adap.com>
2023-01-13 15:43:50 -0800

    Update version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,7 +34,7 @@ copyright = "2022 Adap GmbH"
 author = "The Flower Authors"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.0"
+release = "1.3.0"
 
 
 # -- General configuration ---------------------------------------------------
