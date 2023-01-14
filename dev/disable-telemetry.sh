94856aa2973ed316acee1b087ac449174a42eea6
Taner Topal <taner@adap.com>
2023-01-14 22:52:34 +0100

    Update dev/disable-telemetry.sh
    Co-authored-by: Daniel J. Beutel <daniel@adap.com>

--- dev/disable-telemetry.sh
+++ dev/disable-telemetry.sh
@@ -9,6 +9,6 @@ if  ! grep -q "FLWR_TELEMETRY_ENABLED" $BASH_FILE ; then
   echo "Use new terminal for it to be loaded."
 
   echo "" >> $BASH_FILE
-  echo "# Flwr config" >> $BASH_FILE
+  echo "# Flower config" >> $BASH_FILE
   echo "export FLWR_TELEMETRY_ENABLED=0" >> $BASH_FILE
 fi
