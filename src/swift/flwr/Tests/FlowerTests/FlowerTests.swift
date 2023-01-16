441611c0630fce7ae087a1055259865311ea413a
Maximilian Kapsecker <max.kapsecker@tum.de>
2023-01-16 16:46:53 +0100

    included parameter converter

--- src/swift/flwr/Tests/FlowerTests/FlowerTests.swift
+++ src/swift/flwr/Tests/FlowerTests/FlowerTests.swift
@@ -1,11 +1,25 @@
 import XCTest
 @testable import flwr
 
+@available(iOS 14.0, *)
 final class FlowerTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        //XCTAssertEqual(Flower().text, "Hello, World!")
+    func testParameterConverter() throws {
+        let pConv = ParameterConverter.shared
+        let input1: [Float] = [1, 2, 3, 4]
+        let input2: [Float] = [1.1435, 2, 3, 4]
+        let output1 = pConv.dataToArray(
+            data: pConv.arrayToData(
+                array: input1,
+                shape: [2, 2]
+            )!
+        )
+        let output2 = pConv.dataToArray(
+            data: pConv.arrayToData(
+                array: input2,
+                shape: [2, 2]
+            )!
+        )
+        XCTAssertEqual(input1, output1)
+        XCTAssertEqual(input2, output2)
     }
 }
