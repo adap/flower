02c2308a07a69ca533e6462dd1ae9f0231420579
charlesbvll <charles@adap.com>
2023-01-19 07:18:01 +0000

    Add better text

--- dev/setup-envs.sh
+++ dev/setup-envs.sh
@@ -12,10 +12,12 @@ case "$SHELL" in
 esac
 
 if  ! grep -q "FLWR_TELEMETRY_ENABLED" $CONFIG_FILE ; then
-  echo "Writing FLWR_TELEMETRY_ENABLED=0 into .bash_profile."
+  echo "Writing FLWR_TELEMETRY_ENABLED=0 into $CONFIG_FILE"
   echo "Use new terminal for it to be loaded."
 
   echo "" >> $CONFIG_FILE
   echo "# Flower config" >> $CONFIG_FILE
   echo "export FLWR_TELEMETRY_ENABLED=0" >> $CONFIG_FILE
+else
+  echo "Telemetry is already disabled."
 fi
