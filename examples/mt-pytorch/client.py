56969d92e6a7eba21bd94980b3a695284973dbed
Daniel J. Beutel <daniel@adap.com>
2023-01-19 21:38:18 -0800

    Update mt-pytorch example to use REST

--- examples/mt-pytorch/client.py
+++ examples/mt-pytorch/client.py
@@ -33,6 +33,7 @@ class FlowerClient(fl.client.NumPyClient):
 
 # Start Flower client
 fl.client.start_numpy_client(
-    server_address="[::]:9092",
+    server_address="0.0.0.0:8000",
     client=FlowerClient(),
+    use_rest=True,
 )
