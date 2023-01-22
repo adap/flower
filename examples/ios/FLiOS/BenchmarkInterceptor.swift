e629f5206b736f611b196e4089b0195005e6885f
Christoph Weinhuber <christoph.weinhuber@tum.de>
2023-01-22 10:31:38 +0100

    add BenchmarkInterceptor

---
+++ examples/ios/FLiOS/BenchmarkInterceptor.swift
@@ -0,0 +1,18 @@
+//
+//  BenchmarkInterceptor.swift
+//  FLiOS
+//
+//  Created by Christoph Weinhuber on 22.01.23.
+//
+
+import Foundation
+import flwr
+
+
+class BenchmarkInterceptor: FlwrGRPCInterceptor {
+    func receive() {
+    }
+    
+    func send() {
+    }
+}
---
+++ src/swift/flwr/Sources/Flower/GRPC/FlwrGRPCInterceptor.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Christoph Weinhuber on 20.01.23.
+//
+
+import Foundation
