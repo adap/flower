5b2566672bb33cb7cc74d888d27b17ce6fdd5620
Christoph Weinhuber <christoph.weinhuber@tum.de>
2023-01-22 15:29:41 +0100

    implement custom client interceptor to benchmark application

--- examples/ios/FLiOS/Benchmark/BenchmarkInterceptor.swift
+++ examples/ios/FLiOS/Benchmark/BenchmarkInterceptor.swift
@@ -8,11 +8,40 @@
 import Foundation
 import flwr
 
-
 class BenchmarkInterceptor: FlwrGRPCInterceptor {
-    func receive() {
+    let benchmarkSuite = BenchmarkSuite.shared
+    
+    func receive(part: GRPCPartWrapper) {
+        switch part {
+        
+        case .metadata(_):
+            benchmarkSuite.takeNetworkSnaptshot(snapshot: NetworkSnapshot(type: NetworkAction.received(size: "0 Bytes"), description: "Received headers"))
+            
+        case let .message(content):
+            let size = NetworkSnapshot.calcSize(message: content)
+            benchmarkSuite.takeNetworkSnaptshot(snapshot: NetworkSnapshot(type: NetworkAction.received(size: size), description: "Received response"))
+            
+        case .end(_, _):
+            benchmarkSuite.takeActionSnapshot(snapshot: ActionSnaptshot(action: "Response stream closed"))
+            benchmarkSuite.takeNetworkSnaptshot(snapshot: NetworkSnapshot(type: NetworkAction.received(size: "0 Bytes"), description: "Response stream closed"))
+        }
+        
     }
     
-    func send() {
+    
+    func send(part: GRPCPartWrapper) {
+        switch part {
+        
+        case .metadata(_):
+            benchmarkSuite.takeNetworkSnaptshot(snapshot: NetworkSnapshot(type: NetworkAction.sent(size: "0 Bytes"), description: "Sent headers"))
+            
+        case let .message(content):
+            let size = NetworkSnapshot.calcSize(message: content)
+            benchmarkSuite.takeNetworkSnaptshot(snapshot: NetworkSnapshot(type: NetworkAction.sent(size: size), description: "Sent request"))
+            
+        case .end(_, _):
+            benchmarkSuite.takeNetworkSnaptshot(snapshot: NetworkSnapshot(type: NetworkAction.sent(size: "0 Bytes"), description: "Closing request stream"))
+            
+        }
     }
 }
