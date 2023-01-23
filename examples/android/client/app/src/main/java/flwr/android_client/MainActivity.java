5aeae29219092e7a7e5156e0e73cf67a477583a0
charlesbvll <charles@adap.com>
2023-01-23 15:53:26 +0100

    Modernize app UI

--- examples/android/client/app/src/main/java/flwr/android_client/MainActivity.java
+++ examples/android/client/app/src/main/java/flwr/android_client/MainActivity.java
@@ -35,6 +35,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
