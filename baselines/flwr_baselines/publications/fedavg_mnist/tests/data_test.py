e291acb5e4102d9ac8dce664eae72e07a1681a70
Charles Beauville <charlesbeauville@gmail.com>
2023-02-16 12:46:19 +0100

    Add class balancing

--- baselines/flwr_baselines/publications/fedavg_mnist/tests/data_test.py
+++ baselines/flwr_baselines/publications/fedavg_mnist/tests/data_test.py
@@ -1,20 +1,7 @@
 """Used to test the model and the data partitionning."""
 
 
-from flwr_baselines.publications.fedavg_mnist import dataset, model
-
-
-def test_cnn_size_mnist() -> None:
-    """Test number of parameters with MNIST-sized inputs."""
-    # Prepare
-    net = model.Net()
-    expected = 1_663_370
-
-    # Execute
-    actual = sum([p.numel() for p in net.parameters()])
-
-    # Assert
-    assert actual == expected
+from flwr_baselines.publications.fedavg_mnist import dataset
 
 
 def test_non_iid_partitionning(num_clients: int = 100) -> None:
@@ -26,7 +13,7 @@ def test_non_iid_partitionning(num_clients: int = 100) -> None:
         The number of clients to distribute the data to, by default 100
     """
     trainloaders, _, _ = dataset.load_datasets(
-        batch_size=1, num_clients=num_clients, iid=False
+        batch_size=1, num_clients=num_clients, iid=False, balance=True
     )
     for trainloader in trainloaders:
         labels = []
