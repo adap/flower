ce59cac09de7cc6dfdd7f2c882e0fe13940b8c45
Adam Narozniak <adam@adap.com>
2023-02-17 17:37:08 +0100

    Add NIST SD19 downloader

---
+++ baselines/flwr_baselines/publications/leaf/femnist/nist_downloader.py
@@ -0,0 +1,44 @@
+import os
+import pathlib
+import shutil
+from typing import Union
+
+import wget
+
+
+class NISTDownloader:
+    def __init__(self, name: str, url: str, save_path: Union[str, pathlib.Path] = None) -> None:
+        self._name = name
+        self._url = url
+        self._save_path = save_path if save_path is not None else pathlib.Path(f"./{name}" + ".zip")
+
+    def download(self) -> None:
+        if self._save_path.with_suffix("").exists() and len(list(self._save_path.with_suffix("").glob("*"))) != 0:
+            print("Files are already downloaded and extracted from the zip file")
+            return None
+        self._create_dir_structure()
+        if self._save_path.exists():
+            print("Zip file already downloaded. Skipping downloading.")
+        else:
+            wget.download(self._url, out=str(self._save_path))
+        self._unzip()
+
+    def _create_dir_structure(self):
+        self._save_path.parent.mkdir(parents=True, exist_ok=True)
+
+    def _unzip(self):
+        print(f"Unzipping of {self._save_path} started")
+        shutil.unpack_archive(self._save_path, self._save_path.with_suffix(""))
+        print(f"Unzipping of {self._save_path} done")
+        print("Removing zip file started")
+        os.remove(self._save_path)
+        print("Removing zip file done")
+
+
+if __name__ == "__main__":
+    nist_by_class_url = "https://s3.amazonaws.com/nist-srd/SD19/by_class.zip"
+    nist_by_writer_url = "https://s3.amazonaws.com/nist-srd/SD19/by_write.zip"
+    nist_by_class_downloader = NISTDownloader("data/raw", nist_by_class_url)
+    nist_by_writer_downloader = NISTDownloader("data/raw", nist_by_writer_url)
+    nist_by_class_downloader.download()
+    nist_by_writer_downloader.download()
