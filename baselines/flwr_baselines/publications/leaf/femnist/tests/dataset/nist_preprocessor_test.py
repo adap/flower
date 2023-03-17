97648187082b36b96918b2b6bf295c8d75fb42ef
Adam Narozniak <adam@adap.com>
2023-03-17 18:45:36 +0100

    Add test for preprocessing

---
+++ baselines/flwr_baselines/publications/leaf/femnist/tests/dataset/nist_preprocessor_test.py
@@ -0,0 +1,35 @@
+import pathlib
+import unittest
+import random
+import tempfile
+from hamcrest import assert_that, equal_to
+from pathlib import Path
+from PIL import Image
+from ..utils import recreate_nist
+from femnist.dataset.nist_preprocessor import NISTPreprocessor
+
+
+class NistPreprocessorTest(unittest.TestCase):
+
+    def test_preprocessing(self):
+        """Test if the number of created images examples match expected."""
+        temp_dir = recreate_nist()
+        print(temp_dir.name)
+        print(list(Path(temp_dir.name).glob("*/*/*")))
+        nist_preprocessor = NISTPreprocessor(temp_dir.name)
+        nist_preprocessor.preprocess()
+        print("writer df")
+        print(nist_preprocessor._writer_df)
+        print("class df")
+        print(nist_preprocessor._class_df)
+        print("df")
+        print(nist_preprocessor._df)
+        print("preprocessed df")
+        print(nist_preprocessor._preprocessed_df)
+        preprocessed_df = nist_preprocessor._preprocessed_df
+        assert_that(preprocessed_df.shape, equal_to((13, 3)))
+        temp_dir.cleanup()
+
+
+if __name__ == "__main__":
+    unittest.main()
