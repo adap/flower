66631b217fa159a552a6a610dc4ae71839a0f7ba
Adam Narozniak <adam@adap.com>
2023-03-21 08:26:31 -0700

    Remove prints from the preprocessor test

--- baselines/flwr_baselines/publications/leaf/femnist/tests/dataset/nist_preprocessor_test.py
+++ baselines/flwr_baselines/publications/leaf/femnist/tests/dataset/nist_preprocessor_test.py
@@ -1,14 +1,9 @@
-import pathlib
-import random
-import tempfile
 import unittest
-from pathlib import Path
+
+from hamcrest import assert_that, equal_to
 
 from femnist.dataset.nist_preprocessor import NISTPreprocessor
-from hamcrest import assert_that, equal_to
-from PIL import Image
-
-from ..utils import recreate_nist
+from tests.utils import recreate_nist
 
 
 class NistPreprocessorTest(unittest.TestCase):
@@ -16,17 +11,8 @@ class NistPreprocessorTest(unittest.TestCase):
         """Test if the number of created images examples match expected."""
         temp_dir = recreate_nist()
         print(temp_dir.name)
-        print(list(Path(temp_dir.name).glob("*/*/*")))
         nist_preprocessor = NISTPreprocessor(temp_dir.name)
         nist_preprocessor.preprocess()
-        print("writer df")
-        print(nist_preprocessor._writer_df)
-        print("class df")
-        print(nist_preprocessor._class_df)
-        print("df")
-        print(nist_preprocessor._df)
-        print("preprocessed df")
-        print(nist_preprocessor._preprocessed_df)
         preprocessed_df = nist_preprocessor._preprocessed_df
         assert_that(preprocessed_df.shape, equal_to((13, 3)))
         temp_dir.cleanup()
