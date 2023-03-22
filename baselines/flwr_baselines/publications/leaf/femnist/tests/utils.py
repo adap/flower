ba10d92c2ea7be9d59ac260c5c7ebdf1bf9b5da9
Adam Narozniak <adam@adap.com>
2023-03-22 15:38:37 -0700

    Fix random images creation

--- baselines/flwr_baselines/publications/leaf/femnist/tests/utils.py
+++ baselines/flwr_baselines/publications/leaf/femnist/tests/utils.py
@@ -1,7 +1,8 @@
-import random
 import tempfile
 from pathlib import Path
 
+import PIL
+import numpy as np
 from PIL import Image
 
 
@@ -15,24 +16,30 @@ def recreate_nist():
 
     # create 5 PNG files for directory "a" in "by_write"
     for i in range(3):
-        img = Image.new("L", (128, 128), random.randint(0, 255))
+        img = create_random_image()
         img_path = raw_dir / "by_write" / "hsf_0" / "a" / "c000_sth" / f"{i}.png"
         img.save(img_path)
         (raw_dir / "by_class" / "4a" / "hsf_0" / f"{i}.png").symlink_to(img_path)
     for i in range(3, 5):
-        img = Image.new("L", (128, 128), random.randint(0, 255))
+        img = create_random_image()
         img_path = raw_dir / "by_write" / "hsf_0" / "a" / "c000_sth" / f"{i}.png"
         img.save(img_path)
         (raw_dir / "by_class" / "4b" / "hsf_0" / f"{i}.png").symlink_to(img_path)
     # create 8 PNG files for directory "b" in "by_write"
     for i in range(4):
-        img = Image.new("L", (128, 128), random.randint(0, 255))
+        img = create_random_image()
         img_path = raw_dir / "by_write" / "hsf_0" / "b" / "c000_sth" / f"{i}.png"
         img.save(img_path)
         (raw_dir / "by_class" / "4a" / "hsf_0" / f"{i + 3}.png").symlink_to(img_path)
     for i in range(4, 8):
-        img = Image.new("L", (128, 128), random.randint(0, 255))
+        img = create_random_image()
         img_path = raw_dir / "by_write" / "hsf_0" / "b" / "c000_sth" / f"{i}.png"
         img.save(img_path)
         (raw_dir / "by_class" / "4b" / "hsf_0" / f"{i + 1}.png").symlink_to(img_path)
     return temp_dir
+
+
+def create_random_image() -> PIL.Image:
+    arr = np.random.randint(0, 255, size=(128, 128), dtype=np.uint8)
+    img = Image.fromarray(arr)
+    return img
