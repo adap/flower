4a8db478ff8cfedf67b9a99203d5ae2aa84e8944
m-danilow <mdanilowski@student.agh.edu.pl>
2023-03-31 01:50:38 +0200

    ci fix

--- src/py/flwr/server/strategy/__init__.py
+++ src/py/flwr/server/strategy/__init__.py
@@ -24,11 +24,11 @@ from .fedavgm import FedAvgM as FedAvgM
 from .fedmedian import FedMedian as FedMedian
 from .fedopt import FedOpt as FedOpt
 from .fedprox import FedProx as FedProx
+from .fedtrimmedavg import FedTrimmedAvg as FedTrimmedAvg
 from .fedxgb_nn_avg import FedXgbNnAvg as FedXgbNnAvg
 from .fedyogi import FedYogi as FedYogi
 from .qfedavg import QFedAvg as QFedAvg
 from .strategy import Strategy as Strategy
-from .fedtrimmedavg import FedTrimmedAvg as FedTrimmedAvg
 
 __all__ = [
     "FaultTolerantFedAvg",
