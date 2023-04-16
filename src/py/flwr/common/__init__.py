5c358e5b59a61aa458868bab0bcf244816d273dc
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-04-16 21:43:55 +0800

    do not export flwr.common.logger

--- src/py/flwr/common/__init__.py
+++ src/py/flwr/common/__init__.py
@@ -15,7 +15,6 @@
 """Flower utilities shared between server and client."""
 
 
-from . import logger
 from .date import now as now
 from .grpc import GRPC_MAX_MESSAGE_LENGTH
 from .logger import log as log
@@ -66,7 +65,6 @@ __all__ = [
     "GetPropertiesRes",
     "GRPC_MAX_MESSAGE_LENGTH",
     "log",
-    "logger",
     "Metrics",
     "MetricsAggregationFn",
     "ndarray_to_bytes",
