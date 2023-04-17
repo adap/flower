584e9d0d8a2fc2fa2a16233c6d80753ab1878dd0
charlesbvll <charles@adap.com>
2023-04-17 10:48:34 +0000

    Rename dataset to preprocessing

--- baselines/flwr_baselines/publications/fl_wav2vec2/model/model.py
+++ baselines/flwr_baselines/publications/fl_wav2vec2/model/model.py
@@ -6,7 +6,7 @@ import torch
 from flwr.common import ndarrays_to_parameters
 from hyperpyyaml import load_hyperpyyaml
 
-from flwr_baselines.publications.fl_wav2vec2.dataset.data_loader import dataio_prepare
+from flwr_baselines.publications.fl_wav2vec2.preprocessing.data_loader import dataio_prepare
 from flwr_baselines.publications.fl_wav2vec2.model.sb_w2v2 import ASR, get_weights
 
 
