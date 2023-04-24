902e800e53f9cd06d3f190fb59afdcb2760555a4
RashidDHera <rash0882@yahoo.co.uk>
2023-04-24 20:55:49 +0300

    adding fedavg_cifar10

---
+++ baselines/flwr_baselines/publications/fedavg_cifar10/model.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+
+"""CNN model architecutre, training and testing functions for CIFAR10."""
+
+
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+
+class Net(nn.Module):
+    """Convolutional Neural Network architecture as described in McMahan 2017
+    paper :
+
+    [Communication-Efficient Learning of Deep Networks from
+    Decentralized Data] (https://arxiv.org/pdf/1602.05629.pdf)
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+         # Convolutional Layers
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+
+        # Fully Connected Layers
+        #self.dropout1 = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(in_features=64*8*8, out_features=512)
+        self.dropout2 = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(in_features=512, out_features=10)
+
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the CNN.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input Tensor that will pass through the network
+
+        Returns
+        -------
+        torch.Tensor
+            The resulting Tensor after it has passed through the network
+        """
+        # Convolutional Layers
+        output_tensor = self.conv1(input_tensor)
+        output_tensor = nn.functional.relu(output_tensor)
+        output_tensor = nn.functional.max_pool2d(output_tensor, kernel_size=2)
+        output_tensor = self.conv2(output_tensor)
+        output_tensor = nn.functional.relu(output_tensor)
+        output_tensor = nn.functional.max_pool2d(output_tensor, kernel_size=2)
+
+        # Flatten for Fully Connected Layers
+        output_tensor = output_tensor.view(-1, 64*8*8)
+
+        # Fully Connected Layers with Dropout
+        #output_tensor = self.dropout1(output_tensor)
+        output_tensor = self.fc1(output_tensor)
+        output_tensor = nn.functional.relu(output_tensor)
+        output_tensor = self.dropout2(output_tensor)
+        output_tensor = self.fc2(output_tensor)
+        
+        return output_tensor
+
+
+def train(
+    net: nn.Module,
+    trainloader: DataLoader,
+    device: torch.device,
+    epochs: int,
+    learning_rate: float,
+) -> None:
+    """Train the network on the training set.
+
+    Parameters
+    ----------
+    net : nn.Module
+        The neural network to train.
+    trainloader : DataLoader
+        The DataLoader containing the data to train the network on.
+    device : torch.device
+        The device on which the model should be trained, either 'cpu' or 'cuda'.
+    epochs : int
+        The number of epochs the model should be trained for.
+    learning_rate : float
+        The learning rate for the SGD optimizer.
+    """
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate)
+    net.train()
+    for _ in range(epochs):
+        net = _training_loop(net, trainloader, device, criterion, optimizer)
+
+
+def _training_loop(
+    net: nn.Module,
+    trainloader: DataLoader,
+    device: torch.device,
+    criterion: torch.nn.CrossEntropyLoss,
+    optimizer: torch.optim.Adam,
+) -> nn.Module:
+    """Train for one epoch.
+
+    Parameters
+    ----------
+    net : nn.Module
+        The neural network to train.
+    trainloader : DataLoader
+        The DataLoader containing the data to train the network on.
+    device : torch.device
+        The device on which the model should be trained, either 'cpu' or 'cuda'.
+    criterion : torch.nn.CrossEntropyLoss
+        The loss function to use for training
+    optimizer : torch.optim.Adam
+        The optimizer to use for training
+
+    Returns
+    -------
+    nn.Module
+        The model that has been trained for one epoch.
+    """
+    for images, labels in trainloader:
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        loss = criterion(net(images), labels)
+        loss.backward()
+        optimizer.step()
+    return net
+
+
+def test(
+    net: nn.Module, testloader: DataLoader, device: torch.device
+) -> Tuple[float, float]:
+    """Evaluate the network on the entire test set.
+
+    Parameters
+    ----------
+    net : nn.Module
+        The neural network to test.
+    testloader : DataLoader
+        The DataLoader containing the data to test the network on.
+    device : torch.device
+        The device on which the model should be tested, either 'cpu' or 'cuda'.
+
+    Returns
+    -------
+    Tuple[float, float]
+        The loss and the accuracy of the input model on the given data.
+    """
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, total, loss = 0, 0, 0.0
+    net.eval()
+    with torch.no_grad():
+        for images, labels in testloader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    if len(testloader.dataset) == 0:
+        raise ValueError("Testloader can't be 0, exiting...")
+    loss /= len(testloader.dataset)
+    accuracy = correct / total
+    return loss, accuracy
