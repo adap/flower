3d93e71fc4460a34b7e63461a81b1bbcdb1dea59
RashidDHera <rash0882@yahoo.co.uk>
2023-04-30 15:08:54 +0300

    create new fedavg_cifar10

---
+++ baselines/flwr_baselines/publications/fedavg_cifar10/tests/model_test.py
@@ -0,0 +1,16 @@
+"""Used to test the model and the data partitionning."""
+
+from flwr_baselines.publications.fedavg_cifar10 import model
+
+
+def test_cnn_size_mnist() -> None:
+    """Test number of parameters with CIFAR10-sized inputs."""
+    # Prepare
+    net = model.Net()
+    expected = 2_122_186
+
+    # Execute
+    actual = sum([p.numel() for p in net.parameters()])
+
+    # Assert
+    assert actual == expected
