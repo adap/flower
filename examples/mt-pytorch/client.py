f1ebd56260b90d45e56a5aa0f8cfae66dacc7735
Adam Narozniak <adam@adap.com>
2023-05-02 09:44:33 +0200

    Add http:// before the server address

--- examples/mt-pytorch/client.py
+++ examples/mt-pytorch/client.py
@@ -31,7 +31,7 @@ class FlowerClient(fl.client.NumPyClient):
 
 # Start Flower client
 fl.client.start_numpy_client(
-    server_address="0.0.0.0:9093",
+    server_address="http://0.0.0.0:9093",
     client=FlowerClient(),
     rest=True,
 )
