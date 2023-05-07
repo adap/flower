497735404a9c9225197edd5ece72a79e87daf03a
charlesbvll <charles@adap.com>
2023-05-07 18:06:25 +0200

    Add check for tf

--- tests/tensorflow/server.py
+++ tests/tensorflow/server.py
@@ -1,8 +1,22 @@
+import sys
+
 import flwr as fl
 
 
 # Start Flower server
-fl.server.start_server(
+hist = fl.server.start_server(
     server_address="0.0.0.0:8080",
     config=fl.server.ServerConfig(num_rounds=3),
 )
+
+prev_loss = 1_000_000
+for _, loss in hist.losses_distributed:
+    if loss > prev_loss:
+        sys.exit("Loss did not decrease.")
+    else:
+        prev_loss = loss
+
+with open("result", "w") as file:
+    file.write("SUCCESS")
+
+sys.exit()
