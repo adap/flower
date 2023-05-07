fc362ca313e089bb0f5949897c8641fbf35d581d
charlesbvll <charles@adap.com>
2023-05-07 18:44:57 +0200

    Improve failing

--- tests/tensorflow/server.py
+++ tests/tensorflow/server.py
@@ -12,9 +12,12 @@ hist = fl.server.start_server(
 prev_loss = 1_000_000
 for _, loss in hist.losses_distributed:
     if loss > prev_loss:
+        with open("result", "w") as file:
+            file.write("FAIL")
         sys.exit("Loss did not decrease.")
     else:
         prev_loss = loss
+
 
 with open("result", "w") as file:
     file.write("SUCCESS")
