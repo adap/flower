38418b2bc1bc0316f94423b736ea2487d6c718ef
danielnugraha <daniel.nata@flower.dev>
2023-05-10 11:21:31 +0200

    change source directory to src/swift/flwr

--- src/swift/flwr/Sources/Flower/Client/Client.swift
+++ src/swift/flwr/Sources/Flower/Client/Client.swift
@@ -17,7 +17,7 @@ public protocol Client {
     func evaluate(ins: EvaluateIns) -> EvaluateRes
 }
 
-/// Extension to Client since per default GetPropertiesIns is not needed.
+/// Extension to Client since per default GetPropertiesIns is not implemented.
 public extension Client {
     func getProperties(ins: GetPropertiesIns) -> GetPropertiesRes {
         return GetPropertiesRes(properties: [:], status: Status(code: .getPropertiesNotImplemented, message: String()))
