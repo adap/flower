772bbb504a30d79c2a521dce2ccdec7bece2eaa9
charlesbvll <charles@adap.com>
2023-05-10 13:56:05 +0200

    Fix import order

--- src/py/flwr/simulation/app.py
+++ src/py/flwr/simulation/app.py
@@ -25,7 +25,7 @@ from flwr.client import ClientLike
 from flwr.common import EventType, event
 from flwr.common.logger import log
 from flwr.server import Server
-from flwr.server.app import ServerConfig, run_fl, init_defaults
+from flwr.server.app import ServerConfig, init_defaults, run_fl
 from flwr.server.client_manager import ClientManager
 from flwr.server.history import History
 from flwr.server.strategy import Strategy
