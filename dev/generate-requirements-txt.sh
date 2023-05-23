c12eee224c8762086d896a5fc17717413ed4c29b
Taner Topal <taner@adap.com>
2023-05-23 22:12:23 +0200

    Add code for debugging

--- dev/generate-requirements-txt.sh
+++ dev/generate-requirements-txt.sh
@@ -21,5 +21,7 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 # Purpose of this script is to regenerate requirements.txt
 for path in $(find ./examples -type f -name 'pyproject.toml' | sed -E 's|/[^/]+$||' |sort -u)
 do
+    echo -e "\nRunning pipreqs for example in ${path}"
+    ls $path 
     pipreqs --mode 'compat' --force --ignore .venv,poetry.lock $path
 done
