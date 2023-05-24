eb82d2b53e62200954cd40e0bf6a483a15ba2253
danielnugraha <daniel.nata@flower.dev>
2023-05-24 09:12:41 +0200

    remove redundant output in script

--- dev/check-requirements-txt.sh
+++ dev/check-requirements-txt.sh
@@ -24,7 +24,6 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 pwd
 
 # Fail if user forgot to sync requirements.txt and pyproject.toml
-git diff HEAD examples
 CHANGED=$(git diff --name-only HEAD examples)
 
 if [ -n "$CHANGED" ]; then
