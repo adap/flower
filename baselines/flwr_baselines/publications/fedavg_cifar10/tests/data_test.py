7d0d24e6679b454fe501f17ce41b713bbacbe276
RashidDHera <rash0882@yahoo.co.uk>
2023-06-22 19:04:24 +0300

    fedavg_cifar10 updated 2

---
+++ baselines/flwr_baselines/publications/fedavg_cifar10/tests/data_test.py
@@ -0,0 +1,25 @@
+"""Used to test the model and the data partitionning."""
+# pylint: disable=E0401
+
+import unittest
+
+from flwr_baselines.publications.fedavg_cifar10 import dataset
+
+
+@unittest.skip("no reason")
+def test_non_iid_partitionning(num_clients: int = 100) -> None:
+    """Test the non iid partitionning of the CIFAR10 dataset.
+
+    Parameters
+    ----------
+    num_clients : int, optional
+        The number of clients to distribute the data to, by default 100
+    """
+    trainloaders, _, _ = dataset.load_datasets(
+        batch_size=1, num_clients=num_clients, iid=False, balance=True
+    )
+    for trainloader in trainloaders:
+        labels = []
+        for _, label in trainloader:
+            labels.append(label.item())
+        assert len(set(labels)) <= 2
---
+++ baselines/flwr_baselines/publications/fedavg_cifar10/utils.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+
+"""Contains utility functions for CNN FL on CIFAR10."""
+# pylint: disable=E0401 disable=W0612
+
+from collections import OrderedDict
+from pathlib import Path
+from typing import Callable, Dict, List, Optional, Tuple
+
+import numpy as np
+import torch
+from flwr.common import Metrics
+from flwr.common.typing import NDArrays, Scalar
+from flwr.server.history import History
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
+
+from flwr_baselines.publications.fedavg_cifar10 import model
+
+
+def plot_metric_from_history(
+    hist: History,
+    save_plot_path: Path,
+    suffix: Optional[str] = "",
+) -> None:
+    """Function to plot from Flower server History.
+
+    Parameters
+    ----------
+    hist : History
+        Object containing evaluation for all rounds.
+    save_plot_path : Path
+        Folder to save the plot to.
+
+    suffix: Optional[str]
+        Optional string to add at the end of the filename for the plot.
+    """
+    metric_type = "centralized"
+    metric_dict = (
+        hist.metrics_centralized
+        if metric_type == "centralized"
+        else hist.metrics_distributed
+    )
+
+    rounds, values = zip(*metric_dict["accuracy"])
+
+    fig, axis = plt.subplots()
+    plt.plot(np.asarray(rounds), np.asarray(values), c="b", label="FedAvg")
+
+    # Set paper's results
+    plt.axhline(
+        y=max(values),
+        color="r",
+        linestyle="dashed",
+        label="Paper's baseline accuracy ",
+    )
+
+    plt.title(f"{metric_type.capitalize()} Validation - CIFAR10")
+    plt.xlabel("Rounds")
+    plt.ylabel("Accuracy")
+    plt.legend(loc="lower right")
+
+    # Set the apect ratio to 1.0
+    xleft, xright = axis.get_xlim()
+    ybottom, ytop = axis.get_ylim()
+    axis.set_aspect(abs((xright - xleft) / (ybottom - ytop)) * 1.0)
+
+    plt.savefig(Path(save_plot_path) / Path(f"{metric_type}_metrics{suffix}.png"))
+    plt.close()
+
+
+def plot_eval_loss_from_history(
+    hist: History,
+    save_plot_path: Path,
+    suffix: Optional[str] = "",
+) -> None:
+    """Function to plot from Flower server History.
+
+    Parameters
+    ----------
+    hist : History
+        Object containing evaluation for all rounds.
+    save_plot_path : Path
+        Folder to save the plot to.
+
+    suffix: Optional[str]
+        Optional string to add at the end of the filename for the plot.
+    """
+    loss_type = "centralized"
+    loss_dict = (
+        hist.losses_centralized
+        if loss_type == "centralized"
+        else hist.losses_distributed
+    )
+    x = zip(*loss_dict)
+    loss_rounds, eval_loss = list(x)
+
+    fig, axis = plt.subplots()
+    plt.plot(np.asarray(loss_rounds), np.asarray(eval_loss), c="r", label="FedAvg")
+    # Set paper's results
+    plt.axhline(
+        y=min(eval_loss),
+        color="b",
+        linestyle="dashed",
+        label="Paper's baseline loss ",
+    )
+
+    plt.title(f"{loss_type.capitalize()} Validation - CIFAR 10")
+    plt.xlabel("Rounds")
+    plt.ylabel("Loss")
+    plt.legend(loc="upper right")
+
+    xleft, xright = axis.get_xlim()
+    ybottom, ytop = axis.get_ylim()
+    axis.set_aspect(abs((xright - xleft) / (ybottom - ytop)) * 1.0)
+
+    plt.savefig(Path(save_plot_path) / Path(f"{loss_type}_loss{suffix}.png"))
+    plt.close()
+
+
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    """Aggregation function for weighted average during evaluation.
+
+    Parameters
+    ----------
+    metrics : List[Tuple[int, Metrics]]
+        The list of metrics to aggregate.
+
+    Returns
+    -------
+    Metrics
+        The weighted average metric.
+    """
+    # Multiply accuracy of each client by number of examples used
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+
+    # Aggregate and return custom metric (weighted average)
+    return {
+        "accuracy": int(sum(list(map(bool, accuracies))))
+        / int(sum(list(map(bool, examples))))
+    }
+
+
+def gen_evaluate_fn(
+    testloader: DataLoader, device: torch.device
+) -> Callable[
+    [int, NDArrays, Dict[str, Scalar]], Optional[Tuple[float, Dict[str, Scalar]]]
+]:
+    """Generates the function for centralized evaluation.
+
+    Parameters
+    ----------
+    testloader : DataLoader
+        The dataloader to test the model with.
+    device : torch.device
+        The device to test the model on.
+
+    Returns
+    -------
+    Callable[ [int, NDArrays, Dict[str, Scalar]], Optional[Tuple[float, Dict[str, Scalar]]] ]
+        The centralized evaluation function.
+    """
+
+    def evaluate(
+        server_round: int, parameters_ndarrays: NDArrays, config: Dict[str, Scalar]
+    ) -> Optional[Tuple[float, Dict[str, Scalar]]]:
+        # pylint: disable=unused-argument
+        """Use the entire MNIST test set for evaluation."""
+        # determine device
+        net = model.Net()
+        params_dict = zip(net.state_dict().keys(), parameters_ndarrays)
+        state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+        net.load_state_dict(state_dict, strict=True)
+        net.to(device)
+
+        loss, accuracy = model.test(net, testloader, device=device)
+        # return statistics
+        return loss, {"accuracy": accuracy}
+
+    return evaluate
