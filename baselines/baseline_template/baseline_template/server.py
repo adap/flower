78feea5ea20d422e339b618394cdc4f3ea86d87b
Javier <jafermarq@users.noreply.github.com>
2023-06-23 11:41:20 +0100

    Update baselines/baseline_template/baseline_template/server.py
    Co-authored-by: Daniel J. Beutel <daniel@adap.com>

--- baselines/baseline_template/baseline_template/server.py
+++ baselines/baseline_template/baseline_template/server.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Please use this script to define the function called by your strategy in   #
+# Please use this module to define the function called by your strategy in   #
 # order to evaluate the state of the global model. You might use this script #
 # also to define a new Server class (please note this is not needed in most  #
 # settings).                                                                 #
