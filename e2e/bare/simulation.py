ffdbc93dbf833d2cbdd541ec4a0310bfabc1dc05
Charles Beauville <charlesbeauville@gmail.com>
2023-06-28 13:13:37 +0200

    Fix errors

--- e2e/bare/simulation.py
+++ e2e/bare/simulation.py
@@ -11,4 +11,4 @@ hist = fl.simulation.start_simulation(
     num_clients=2,
     config=fl.server.ServerConfig(num_rounds=3),
 )
-assert (hist.losses_distributed[0][1] / hist.losses_distributed[-1][1]) > 0.98
+assert hist.losses_distributed[-1][1] == 0
