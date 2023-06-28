3ddc383297716d8e9848f6f165e402d291339dfb
Charles Beauville <charlesbeauville@gmail.com>
2023-06-28 13:49:28 +0200

    Debugging MXNet

--- e2e/mxnet/test.sh
+++ e2e/mxnet/test.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 set -e
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 
-bash ../test.sh
+# run the first command in background and save output to a temporary file:
+timeout 3m python server.py &
+pid=$!
+sleep 3
+
+python client.py &
+sleep 3
+
+python client.py &
+sleep 3
+
+wait $pid
+res=$?
+
+if [[ "$res" = "0" ]];
+  then echo "Training worked correctly";
+  else echo "Training had an issue" && exit 1;
+fi
