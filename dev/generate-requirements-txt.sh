743164f7e8141d3ef35112539bea49554df1c3f8
danielnugraha <daniel.nata@flower.dev>
2023-06-30 11:32:53 +0200

    resolve all examples dependencies

--- dev/generate-requirements-txt.sh
+++ dev/generate-requirements-txt.sh
@@ -21,6 +21,10 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 # Purpose of this script is to regenerate requirements.txt
 for path in $(find ./examples -type f -name 'pyproject.toml' | sed -E 's|/[^/]+$||' |sort -u)
 do
-    echo -e "\nRunning pipreqs for example in ${path}"
-    pipreqs --mode 'compat' --force --ignore .venv,poetry.lock $path
+    echo -e "\nChanging directory to example in ${path}"
+    cd $path
+    echo -e "\nRunning poeareq for example in ${path}"
+    poeareq "requirements.txt"
+    echo -e "\nGoing back to root directory"
+    cd ../../
 done
