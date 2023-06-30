3184aa001a5bbb73f98f5948bf9a1cb9e3950393
Adam Narozniak <adam@flower.dev>
2023-06-30 15:16:30 +0200

    Add format and test scripts

---
+++ baselines/dev/format-baseline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <baseline-name=directory-of-the-baseline>"
+    exit 1
+fi
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../$1
+
+python -m isort .
+python -m black -q .
+python -m docformatter -i -r .
+python -m ruff check --fix .
---
+++ baselines/dev/test-baseline.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <baseline-name=directory-of-the-baseline>"
+    exit 1
+fi
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../$1
+
+echo "=== test.sh ==="
+
+echo "- Start Python checks"
+
+echo "- isort: start"
+python -m isort --check-only .
+echo "- isort: done"
+
+echo "- black: start"
+python -m black --check .
+echo "- black: done"
+
+echo "- docformatter: start"
+python -m docformatter -c -r .
+echo "- docformatter:  done"
+
+#echo "- ruff: start"
+#python -m ruff check .
+#echo "- ruff: done"
+
+echo "- mypy: start"
+python -m mypy .
+echo "- mypy: done"
+
+echo "- pylint: start"
+python -m pylint --recursive y .
+echo "- pylint: done"
+
+echo "- All Python checks passed"
