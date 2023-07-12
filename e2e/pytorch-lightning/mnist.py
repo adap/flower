b9862fd04fcc665d5ca492dfc872bf655dbd5514
Charles Beauville <charles@flower.dev>
2023-07-12 16:34:12 +0200

    Add PL E2E test to workflow

--- e2e/pytorch-lightning/mnist.py
+++ e2e/pytorch-lightning/mnist.py
@@ -62,13 +62,13 @@ class LitAutoEncoder(pl.LightningModule):
 
 def load_data():
     # Training / validation set
-    trainset = MNIST("", train=True, download=True, transform=transforms.ToTensor())
+    trainset = MNIST("./data", train=True, download=True, transform=transforms.ToTensor())
     mnist_train, mnist_val = random_split(trainset, [55000, 5000])
     train_loader = DataLoader(mnist_train, batch_size=32, shuffle=True, num_workers=16)
     val_loader = DataLoader(mnist_val, batch_size=32, shuffle=False, num_workers=16)
 
     # Test set
-    testset = MNIST("", train=False, download=True, transform=transforms.ToTensor())
+    testset = MNIST("./data", train=False, download=True, transform=transforms.ToTensor())
     test_loader = DataLoader(testset, batch_size=32, shuffle=False, num_workers=16)
 
     return train_loader, val_loader, test_loader
