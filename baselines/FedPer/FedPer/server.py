2a7cc8efb259f465b184406f8353dd1f309a180e
WilliamLindskog <williamlindskog@protonmail.com>
2023-07-14 13:44:28 +0200

    New code server and client

--- baselines/FedPer/FedPer/server.py
+++ baselines/FedPer/FedPer/server.py
@@ -3,3 +3,83 @@
 Optionally, also define a new Server class (please note this is not needed in most
 settings).
 """
+
+import random
+from flcore.clients.clientper import clientPer
+from flcore.servers.serverbase import Server
+from threading import Thread
+
+
+class FedPer(Server):
+    def __init__(self, args, times):
+        super().__init__(args, times)
+
+        # select slow clients
+        self.set_slow_clients()
+        self.set_clients(clientPer)
+
+        print(f"\nJoin ratio / total clients: {self.join_ratio} / {self.num_clients}")
+        print("Finished creating server and clients.")
+
+        # self.load_model()
+
+
+    def train(self):
+        for i in range(self.global_rounds+1):
+            self.selected_clients = self.select_clients()
+            self.send_models()
+
+            if i%self.eval_gap == 0:
+                print(f"\n-------------Round number: {i}-------------")
+                print("\nEvaluate personalized models")
+                self.evaluate()
+
+            for client in self.selected_clients:
+                client.train()
+
+            # threads = [Thread(target=client.train)
+            #            for client in self.selected_clients]
+            # [t.start() for t in threads]
+            # [t.join() for t in threads]
+
+            self.receive_models()
+            if self.dlg_eval and i%self.dlg_gap == 0:
+                self.call_dlg(i)
+            self.aggregate_parameters()
+
+            if self.auto_break and self.check_done(acc_lss=[self.rs_test_acc], top_cnt=self.top_cnt):
+                break
+
+        print("\nBest accuracy.")
+        # self.print_(max(self.rs_test_acc), max(
+        #     self.rs_train_acc), min(self.rs_train_loss))
+        print(max(self.rs_test_acc))
+
+        self.save_results()
+
+        if self.num_new_clients > 0:
+            self.eval_new_clients = True
+            self.set_new_clients(clientPer)
+            print(f"\n-------------Fine tuning round-------------")
+            print("\nEvaluate new clients")
+            self.evaluate()
+
+
+    def receive_models(self):
+        assert (len(self.selected_clients) > 0)
+
+        active_clients = random.sample(
+            self.selected_clients, int((1-self.client_drop_rate) * self.current_num_join_clients))
+
+        self.uploaded_weights = []
+        self.uploaded_models = []
+        tot_samples = 0
+        for client in active_clients:
+            client_time_cost = client.train_time_cost['total_cost'] / client.train_time_cost['num_rounds'] + \
+                    client.send_time_cost['total_cost'] / client.send_time_cost['num_rounds']
+            if client_time_cost <= self.time_threthold:
+                tot_samples += client.train_samples
+                self.uploaded_weights.append(client.train_samples)
+                self.uploaded_models.append(client.model.base)
+        for i, w in enumerate(self.uploaded_weights):
+            self.uploaded_weights[i] = w / tot_samples
