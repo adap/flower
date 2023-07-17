321ecfd42a247d212805cf085a9f35df563fa1f5
Adam Narozniak <adam@flower.dev>
2023-07-17 14:46:33 +0200

    Set up flwr_datasets

---
+++ datasets/dev/format.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
+
+# Python
+python -m isort flwr_datasets/
+python -m black -q flwr_datasets/
+python -m docformatter -i -r flwr_datasets/
+python -m ruff check --fix flwr_datasets/
