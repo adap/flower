d069682f42fb6f9d7af92112b30204ac7ca2dc03
Adam Narozniak <adam@flower.dev>
2023-07-17 15:08:40 +0200

    Add echo to format.sh

--- datasets/dev/format.sh
+++ datasets/dev/format.sh
@@ -3,7 +3,9 @@ set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
 # Python
+echo "Formatting started"
 python -m isort flwr_datasets/
 python -m black -q flwr_datasets/
 python -m docformatter -i -r flwr_datasets/
 python -m ruff check --fix flwr_datasets/
+echo "Formatting done"
