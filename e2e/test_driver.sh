f5626d5a371beb267079ff527a764175f63b4f82
Charles Beauville <charles@flower.dev>
2023-07-18 16:04:19 +0200

    Add a Driver test to E2E tests

---
+++ e2e/test_driver.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+timeout 2m flower-server --grpc-bidi --grpc-bidi-fleet-api-address 0.0.0.0:8080 &
+sleep 3
+
+python client.py &
+sleep 3
+
+python client.py &
+sleep 3
+
+timeout 2m python driver.py &
+pid=$!
+
+wait $pid
+res=$?
+
+if [[ "$res" = "0" ]];
+  then echo "Training worked correctly" && pkill python;
+  else echo "Training had an issue" && exit 1;
+fi
+
