944175af0bb84708615d2aeb8e4b500e7b6d3400
Charles Beauville <charles@flower.dev>
2023-07-20 17:45:12 +0200

    Fix stupid mistake

--- e2e/test_driver_grpc_rere.sh
+++ e2e/test_driver_grpc_rere.sh
@@ -6,9 +6,11 @@ server_pid=$!
 sleep 3
 
 python client.py grpc-rere &
+client1_pid=$!
 sleep 3
 
 python client.py grpc-rere &
+client2_pid=$!
 sleep 3
 
 timeout 2m python driver.py &
@@ -18,7 +20,7 @@ wait $pid
 res=$?
 
 if [[ "$res" = "0" ]];
-  then echo "Training worked correctly" && kill server_pid;
+  then echo "Training worked correctly" && kill $client1_pid && kill $client2_pid && kill $server_pid;
   else echo "Training had an issue" && exit 1;
 fi
 
--- e2e/test_driver_rest.sh
+++ e2e/test_driver_rest.sh
@@ -6,9 +6,11 @@ server_pid=$!
 sleep 3
 
 python client.py rest &
+client1_pid=$!
 sleep 3
 
 python client.py rest &
+client2_pid=$!
 sleep 3
 
 timeout 2m python driver.py &
@@ -18,7 +20,7 @@ wait $pid
 res=$?
 
 if [[ "$res" = "0" ]];
-  then echo "Training worked correctly" && kill server_pid;
+  then echo "Training worked correctly" && kill $client1_pid && kill $client2_pid && kill $server_pid;
   else echo "Training had an issue" && exit 1;
 fi
 
