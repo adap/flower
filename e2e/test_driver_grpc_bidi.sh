6a63a69a655146739d113a81d58614b662a62f4e
Charles Beauville <charles@flower.dev>
2023-07-20 17:39:41 +0200

    Kill process correctly

--- e2e/test_driver_grpc_bidi.sh
+++ e2e/test_driver_grpc_bidi.sh
@@ -2,6 +2,7 @@
 set -e
 
 timeout 2m flower-server --grpc-bidi --grpc-bidi-fleet-api-address 0.0.0.0:8080 &
+server_pid=$!
 sleep 3
 
 python client.py grpc-bidi &
@@ -17,7 +18,7 @@ wait $pid
 res=$?
 
 if [[ "$res" = "0" ]];
-  then echo "Training worked correctly" && pkill python;
+  then echo "Training worked correctly" && kill server_pid;
   else echo "Training had an issue" && exit 1;
 fi
 
--- e2e/test_driver_rest.sh
+++ e2e/test_driver_rest.sh
@@ -2,6 +2,7 @@
 set -e
 
 timeout 2m flower-server --rest --rest-fleet-api-address 0.0.0.0:8080 &
+server_pid=$!
 sleep 3
 
 python client.py rest &
@@ -17,7 +18,7 @@ wait $pid
 res=$?
 
 if [[ "$res" = "0" ]];
-  then echo "Training worked correctly" && pkill python;
+  then echo "Training worked correctly" && kill server_pid;
   else echo "Training had an issue" && exit 1;
 fi
 
