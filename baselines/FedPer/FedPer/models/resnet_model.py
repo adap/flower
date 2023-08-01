d0b270ce356d030fa4b31580e5f4acb9b2bccc16
WilliamLindskog <williamlindskog@protonmail.com>
2023-08-01 16:53:49 +0200

    Working on ResNet implementation

--- baselines/FedPer/FedPer/models/resnet_model.py
+++ baselines/FedPer/FedPer/models/resnet_model.py
@@ -11,6 +11,8 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 from FedPer.utils.model_split import ModelSplit
 from FedPer.utils.model_manager import ModelManager
 
+from torchvision.models.resnet import resnet34
+
 class ResNet(nn.Module):
     """Model adapted from simple MobileNet-v1 (PyTorch) \
         (https://github.com/wjc852456/pytorch-mobilenet-v1)."""
@@ -23,147 +25,36 @@ class ResNet(nn.Module):
             name : str = 'mobile'
         ) -> None:
         super(ResNet, self).__init__()
+        assert num_head_layers > 0 and num_head_layers < 16, "num_head_layers must be greater than 0 and less than 16"
+        self.body = None
+        self.head = None
 
-        ARCHITECTURE = {
-            'layer_1' : {'conv_bn' : [3, 32, 2]},
-            'layer_2' : {'conv_dw' : [32, 64, 1]},
-            'layer_3' : {'conv_dw' : [64, 128, 2]},
-            'layer_4' : {'conv_dw' : [128, 128, 1]},
-            'layer_5' : {'conv_dw' : [128, 256, 2]},
-            'layer_6' : {'conv_dw' : [256, 256, 1]},
-            'layer_7' : {'conv_dw' : [256, 512, 2]},
-            'layer_8' : {'conv_dw' : [512, 512, 1]},
-            'layer_9' : {'conv_dw' : [512, 512, 1]},
-            'layer_10' : {'conv_dw' : [512, 512, 1]},
-            'layer_11' : {'conv_dw' : [512, 512, 1]},
-            'layer_12' : {'conv_dw' : [512, 512, 1]},
-            'layer_13' : {'conv_dw' : [512, 1024, 2]},
-            'layer_14' : {'conv_dw' : [1024, 1024, 1]},
-            'layer_15' : {'avg_pool' : [7]},
-            'layer_16' : {'fc' : [1024, num_classes]}
-        }
+        # Create resnet34 model
+        self.resnet = resnet34()
 
-        self.body = ResNetBody(num_head_layers=num_head_layers, architecture=ARCHITECTURE)
-        self.head = ResNetHead(num_head_layers=num_head_layers, architecture=ARCHITECTURE)
+        # Self head is classifier and avgpool (initially)
+        # body is the rest
+        if num_head_layers == 1:
+            self.head = nn.Sequential(
+                nn.AdaptiveAvgPool2d((1, 1)),
+                nn.Flatten(),
+                nn.Linear(512, num_classes)
+            )
+            self.body = nn.Sequential(*list(self.resnet.children())[:-2])
+        else:
+            add_to_head = nn.Sequential(
+                nn.AdaptiveAvgPool2d((1, 1)),
+                nn.Flatten(),
+                nn.Linear(512, num_classes)
+            )
+            # Check how many basic blocks are in last layer
+            num_blocks = len(list(self.resnet.children())[-1])
+            print("num_blocks", num_blocks)
+            quit()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.body(x)
         return self.head(x)
-    
-class ResNetBody(nn.Module):
-    """ 
-    Body of the MobileNet_v1 model, for which n layers at the end are removed. 
-    
-    Args:
-        num_head_layers: number of layers in the head.
-        architecture: architecture of the model.
-    """
-    def __init__(self, num_head_layers : int = 1, architecture : dict = None) -> None: 
-        super(ResNetBody, self).__init__()
-        assert num_head_layers >= 1, "Number of head layers must be at least 1."
-        def conv_bn(inp, oup, stride):
-            return nn.Sequential(
-                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
-                nn.BatchNorm2d(oup),
-                nn.ReLU(inplace=True)
-            )
-
-        def conv_dw(inp, oup, stride):
-            return nn.Sequential(
-                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
-                nn.BatchNorm2d(inp),
-                nn.ReLU(inplace=True),
-    
-                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(oup),
-                nn.ReLU(inplace=True),
-            )
-        
-        def avg_pool(value : int):
-            return nn.AvgPool2d(value)
-        
-        def fc(inp, oup):
-            return nn.Linear(inp, oup)
-        
-        self.model = nn.Sequential()
-        for i in range(1, len(architecture) - num_head_layers + 1):
-            for key, value in architecture[f'layer_{i}'].items():
-                if key == 'conv_bn':
-                    self.model.add_module(f'conv_bn_{i}', conv_bn(*value))
-                elif key == 'conv_dw':
-                    self.model.add_module(f'conv_dw_{i}', conv_dw(*value))
-                elif key == 'avg_pool':
-                    self.model.add_module(f'avg_pool_{i}', avg_pool(*value))
-                elif key == 'fc':
-                    self.model.add_module(f'fc_{i}', fc(*value))
-                else:
-                    raise NotImplementedError("Layer type not implemented.")
-                
-    def forward(self, x : Tensor) -> Tensor:
-        for i in range(len(self.model)):
-            x = self.model[i](x)
-            if isinstance(self.model[i], nn.AvgPool2d):
-                x = x.view(-1, 1024)
-        return x
-
-class ResNetHead(nn.Module):
-    """ 
-    MobileNet_v1 head, consists out of n layers that will be added to body of model. 
-    
-    Args:
-        num_head_layers: number of layers in the head.
-        architecture: architecture of the model.
-
-    """
-
-    def __init__(self, num_head_layers : int = 1, architecture : dict = None) -> None:
-        super(ResNetHead, self).__init__()
-        assert num_head_layers >= 1, "Number of head layers must be at least 1."
-        self.num_head_layers = num_head_layers
-        def conv_bn(inp, oup, stride):
-            return nn.Sequential(
-                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
-                nn.BatchNorm2d(oup),
-                nn.ReLU(inplace=True)
-            )
-        
-        def conv_dw(inp, oup, stride):
-            return nn.Sequential(
-                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
-                nn.BatchNorm2d(inp),
-                nn.ReLU(inplace=True),
-    
-                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(oup),
-                nn.ReLU(inplace=True),
-            )
-        
-        def avg_pool(value : int):
-            return nn.AvgPool2d(value)
-        
-        def fc(inp, oup):
-            return nn.Linear(inp, oup)
-        
-        self.model = nn.Sequential()
-        for i in range(len(architecture) - num_head_layers + 1, len(architecture) + 1):
-            for key, value in architecture[f'layer_{i}'].items():
-                if key == 'conv_bn':
-                    self.model.add_module(f'conv_bn_{i}', conv_bn(*value))
-                elif key == 'conv_dw':
-                    self.model.add_module(f'conv_dw_{i}', conv_dw(*value))
-                elif key == 'avg_pool':
-                    self.model.add_module(f'avg_pool_{i}', avg_pool(*value))
-                elif key == 'fc':
-                    self.model.add_module(f'fc_{i}', fc(*value))
-                else:
-                    raise NotImplementedError("Layer type not implemented.")
-    
-    def forward(self, x : Tensor) -> Tensor:
-        for i in range(len(self.model)):
-            x = self.model[i](x)
-            if isinstance(self.model[i], nn.AvgPool2d):
-                x = x.view(-1, 1024)
-        return x
 
 class ResNetModelSplit(ModelSplit):
     """Concrete implementation of ModelSplit for models for node kind prediction in action flows \
