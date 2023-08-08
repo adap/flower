9f487fe747e93b93f4618f7dbbb371ba7c10e31c
Adam Narozniak <adam@flower.dev>
2023-08-08 12:13:43 +0200

    Parametrize tests

--- datasets/flwr_datasets/federated_dataset_test.py
+++ datasets/flwr_datasets/federated_dataset_test.py
@@ -1,73 +1,70 @@
+"""Federated Dataset tests."""
 import unittest
+
+import pytest
+from federated_dataset import FederatedDataset
+from parameterized import parameterized, parameterized_class
 
 import datasets
 
-from federated_dataset import FederatedDataset
 
+@parameterized_class(
+    [
+        {"dataset_name": "mnist"},
+        {"dataset_name": "cifar10"},
+    ]
+)
+class RealDatasetsFederatedDatasets(unittest.TestCase):
+    """Test Real Dataset (MNIST, CIFAR10) in FederatedDatasets."""
 
-class MNISTFederatedDatasets(unittest.TestCase):
-    """Test MNIST dataset used in FederatedDatasets."""
+    dataset_name = ""
 
-    def test_load_partition_size(self):
-        dataset_fds = FederatedDataset(dataset="mnist", partitioners={"train": 100})
+    @parameterized.expand(
+        [
+            (
+                "10",
+                10,
+            ),
+            (
+                "100",
+                100,
+            ),
+        ]
+    )
+    def test_load_partition_size(self, _: str, train_num_partitions: int):
+        """Test if the partition size is correct based on the number of partitions."""
+        dataset_fds = FederatedDataset(
+            dataset=self.dataset_name, partitioners={"train": train_num_partitions}
+        )
         dataset_partition0 = dataset_fds.load_partition(0, "train")
-        dataset = datasets.load_dataset("mnist")
-        self.assertEqual(len(dataset_partition0), len(dataset["train"]) // 100)
+        dataset = datasets.load_dataset(self.dataset_name)
+        self.assertEqual(
+            len(dataset_partition0), len(dataset["train"]) // train_num_partitions
+        )
 
     def test_load_full(self):
-        dataset_fds = FederatedDataset(dataset="mnist", partitioners={"train": 100})
+        """Test if the load_full works on the correct split name."""
+        dataset_fds = FederatedDataset(
+            dataset=self.dataset_name, partitioners={"train": 100}
+        )
         dataset_fds_test = dataset_fds.load_full("test")
-        dataset_test = datasets.load_dataset("mnist")["test"]
+        dataset_test = datasets.load_dataset(self.dataset_name)["test"]
         self.assertEqual(len(dataset_fds_test), len(dataset_test))
 
     def test_multiple_partitioners(self):
+        """Test if the dataset works when multiple partitioners are specified."""
         num_train_partitions = 100
         num_test_partitions = 100
-        dataset_fds = FederatedDataset(dataset="mnist",
-                                       partitioners={"train": num_train_partitions,
-                                                     "test":
-                                                         num_test_partitions})
+        dataset_fds = FederatedDataset(
+            dataset=self.dataset_name,
+            partitioners={"train": num_train_partitions, "test": num_test_partitions},
+        )
         dataset_test_partition0 = dataset_fds.load_partition(0, "test")
 
-        dataset = datasets.load_dataset("mnist")
-        self.assertEqual(len(dataset_test_partition0),
-                         len(dataset["test"]) // num_test_partitions)
+        dataset = datasets.load_dataset(self.dataset_name)
+        self.assertEqual(
+            len(dataset_test_partition0), len(dataset["test"]) // num_test_partitions
+        )
 
-
-class CIFAR10FederatedDatasets(unittest.TestCase):
-    """Test CIFAR10 dataset used in FederatedDatasets."""
-
-    def test_load_partition_size(self):
-        dataset_fds = FederatedDataset(dataset="cifar10", partitioners={"train": 100})
-        dataset_partition0 = dataset_fds.load_partition(0, "train")
-        dataset = datasets.load_dataset("cifar10")
-        self.assertEqual(len(dataset_partition0), len(dataset["train"]) // 100)
-
-    def test_load_full(self):
-        dataset_fds = FederatedDataset(dataset="cifar10", partitioners={"train": 100})
-        dataset_fds_test = dataset_fds.load_full("test")
-        dataset_test = datasets.load_dataset("cifar10")["test"]
-        self.assertEqual(len(dataset_fds_test), len(dataset_test))
-
-    def test_multiple_partitioners(self):
-        num_train_partitions = 100
-        num_test_partitions = 10
-        dataset_fds = FederatedDataset(dataset="cifar10",
-                                       partitioners={"train": num_train_partitions,
-                                                     "test":
-                                                         num_test_partitions})
-        dataset_test_partition0 = dataset_fds.load_partition(0, "test")
-
-        dataset = datasets.load_dataset("cifar10")
-        self.assertEqual(len(dataset_test_partition0),
-                         len(dataset["test"]) // num_test_partitions)
-
-
-# class IncorrectUsageFederatedDatasets(unittest.TestCase):
-#     """Test incorrect usages in FederatedDatasets."""
-#
-#     def test_unsupported_dataset_exception(self):
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
