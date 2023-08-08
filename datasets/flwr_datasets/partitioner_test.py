5d0f7c8d7616d97f1751dd9ac030718bfa36302e
Adam Narozniak <adam@flower.dev>
2023-08-08 12:24:02 +0200

    Add missing hint types

--- datasets/flwr_datasets/partitioner_test.py
+++ datasets/flwr_datasets/partitioner_test.py
@@ -1,3 +1,4 @@
+"""Partitioner tests."""
 import unittest
 
 from partitioner import IidPartitioner
@@ -8,10 +9,10 @@ from datasets import Dataset
 class TestIidPartitioner(unittest.TestCase):
     """Test IidPartitioner."""
 
-    def setUp(self):
+    def setUp(self) -> None:
         """Create a dummy dataset with 100 rows, numerical features, and labels."""
         data = {
-            "features": [i for i in range(100)],
+            "features": list(range(100)),
             "labels": [i % 2 for i in range(100)],
         }
         self.dataset = Dataset.from_dict(data)
@@ -19,12 +20,14 @@ class TestIidPartitioner(unittest.TestCase):
         self.partition_size = self.dataset.num_rows // self.num_partitions
         self.sampler = IidPartitioner(num_partitions=self.num_partitions)
 
-    def test_load_partition_size(self):
+    def test_load_partition_size(self) -> None:
+        """Test if the partition size matches the manually computed size."""
         partition_index = 2
         partition = self.sampler.load_partition(self.dataset, partition_index)
-        self.assertEqual(partition.num_rows, self.partition_size)
+        self.assertEqual(len(partition), self.partition_size)
 
-    def test_load_partition_correct_data(self):
+    def test_load_partition_correct_data(self) -> None:
+        """Test if the data in partition is equal to the expected."""
         partition_index = 2
         partition = self.sampler.load_partition(self.dataset, partition_index)
         self.assertEqual(
