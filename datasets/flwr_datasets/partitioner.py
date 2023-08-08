26cf2f51481555440d359020821a16520ed3d8b5
Adam Narozniak <adam@flower.dev>
2023-08-08 12:22:41 +0200

    Add missing hint types

--- datasets/flwr_datasets/partitioner.py
+++ datasets/flwr_datasets/partitioner.py
@@ -1,6 +1,5 @@
 """Partitioner class that works with HuggingFace Dataset."""
 from abc import ABC, abstractmethod
-from typing import Union
 
 import datasets
 from datasets import Dataset
@@ -14,16 +13,14 @@ class Partitioner(ABC):
     """
 
     @abstractmethod
-    def load_partition(
-        self, dataset: Dataset, partition_index: Union[int, str]
-    ) -> Dataset:
+    def load_partition(self, dataset: Dataset, partition_index: int) -> Dataset:
         """Load a single partition based on the partition index.
 
         Parameters
         ----------
         dataset: Dataset
             dataset that will be partitioned
-        partition_index: Union[int, str]
+        partition_index: int
             the index that corresponds to the requested partition
 
         Returns
@@ -43,11 +40,13 @@ class IidPartitioner(Partitioner):
         The total number of partitions that the data will be divided into.
     """
 
-    def __init__(self, num_partitions: int):
+    def __init__(self, num_partitions: int) -> None:
         super().__init__()
         self._num_partitions = num_partitions
 
-    def load_partition(self, dataset, partition_index) -> datasets.Dataset:
+    def load_partition(
+        self, dataset: Dataset, partition_index: int
+    ) -> datasets.Dataset:
         """Load a single iid partition based on the partition index."""
         return dataset.shard(
             num_shards=self._num_partitions, index=partition_index, contiguous=True
