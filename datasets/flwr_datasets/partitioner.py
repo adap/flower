d73b435611f954887c918f1052c93b7d1d6b3983
Adam Narozniak <adam@flower.dev>
2023-08-08 12:17:10 +0200

    Add missing docstrings

--- datasets/flwr_datasets/partitioner.py
+++ datasets/flwr_datasets/partitioner.py
@@ -9,15 +9,15 @@ from datasets import Dataset
 class Partitioner(ABC):
     """The base partitioner class that enables obtaining federated partitions.
 
-    The initialization is intended to take all necessary arguments such that the call
-    to the `load_partition` method can be use the same for all partitioners.
+    The initialization is intended to take all necessary arguments such that the call to
+    the `load_partition` method can be use the same for all partitioners.
     """
 
     @abstractmethod
     def load_partition(
         self, dataset: Dataset, partition_index: Union[int, str]
     ) -> Dataset:
-        """Get a single partition based on the partition index.
+        """Load a single partition based on the partition index.
 
         Parameters
         ----------
@@ -35,20 +35,20 @@ class Partitioner(ABC):
 
 
 class IidPartitioner(Partitioner):
-    """Partitioner creates each partition sampled randomly from the dataset."""
+    """Partitioner creates each partition sampled randomly from the dataset.
+
+    Parameters
+    ----------
+    num_partitions: int
+        The total number of partitions that the data will be divided into.
+    """
 
     def __init__(self, num_partitions: int):
-        """
-
-        Parameters
-        ----------
-        num_partitions: int
-            The total number of partitions that the data will be divided into.
-        """
         super().__init__()
         self._num_partitions = num_partitions
 
     def load_partition(self, dataset, partition_index) -> datasets.Dataset:
+        """Load a single iid partition based on the partition index."""
         return dataset.shard(
             num_shards=self._num_partitions, index=partition_index, contiguous=True
         )
