7a2e73284bb282d5c585338efd2449b2886733c4
Pranjal Gulati <pranjal.carmel@gmail.com>
2023-08-14 05:43:25 +0530

    add dataset details: partitioning and mashing

--- baselines/fedmix/fedmix/dataset_preparation.py
+++ baselines/fedmix/fedmix/dataset_preparation.py
@@ -1,34 +1,83 @@
-"""Handle the dataset partitioning and (optionally) complex downloads.
+import numpy as np
+import torch
+from torchvision.datasets import CIFAR10, CIFAR100
+from torch.utils.data import Subset
+import torchvision.transforms as transforms
+import torch.nn.functional as F
 
-Please add here all the necessary logic to either download, uncompress, pre/post-process
-your dataset (or all of the above). If the desired way of running your baseline is to
-first download the dataset and partition it and then run the experiments, please
-uncomment the lines below and tell us in the README.md (see the "Running the Experiment"
-block) that this file should be executed first.
-"""
-# import hydra
-# from hydra.core.hydra_config import HydraConfig
-# from hydra.utils import call, instantiate
-# from omegaconf import DictConfig, OmegaConf
+def _download_cifar10():
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010))
+        ]
+    )
 
+    trainset = CIFAR10("./dataset", train=True, download=True, transform=transform)
+    testset = CIFAR10("./dataset", train=False, download=True, transform=transform)
+    return trainset, testset
 
-# @hydra.main(config_path="conf", config_name="base", version_base=None)
-# def download_and_preprocess(cfg: DictConfig) -> None:
-#     """Does everything needed to get the dataset.
+def _download_cifar100():
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5071, 0.4867, 0.4408),
+                                 (0.2675, 0.2565, 0.2761))
+        ]
+    )
 
-#     Parameters
-#     ----------
-#     cfg : DictConfig
-#         An omegaconf object that stores the hydra config.
-#     """
+    trainset = CIFAR100("./dataset", train=True, download=True, transform=transform)
+    testset = CIFAR100("./dataset", train=False, download=True, transform=transform)
+    return trainset, testset
 
-#     ## 1. print parsed config
-#     print(OmegaConf.to_yaml(cfg))
+def _partition_trainset(trainset, num_classes, num_clients, num_classes_per_client, seed):
+    partition_size = int(len(trainset) / num_clients)
+    np.random.seed(seed)
 
-#     # Please include here all the logic
-#     # Please use the Hydra config style as much as possible specially
-#     # for parts that can be customised (e.g. how data is partitioned)
+    class_indices = {i: [] for i in range(num_classes)}
+    for idx, (_, label) in enumerate(trainset):
+        class_indices[label].append(idx)
 
-# if __name__ == "__main__":
+    client_datasets = []
 
-#     download_and_preprocess()
+    for client_id in range(num_clients):
+        selected_classes = np.random.choice(
+            num_classes, num_classes_per_client, replace=False)
+        selected_indices = []
+
+        for class_idx in selected_classes:
+            selected_indices.extend(class_indices[class_idx])
+
+        np.random.shuffle(selected_indices)
+        client_indices = selected_indices[:partition_size]
+
+        client_dataset = Subset(trainset, client_indices)
+        client_datasets.append(client_dataset)
+
+    return client_datasets
+
+def _mash_data(client_datasets, mash_batch_size, num_classes):
+    mashed_data = []
+    for client_dataset in client_datasets:
+        mashed_image, mashed_label = [], []
+        for i, (image, label) in enumerate(client_dataset):
+            mashed_image.append(image)
+            mashed_label.append(torch.tensor([label]))
+            if (i + 1) % mash_batch_size == 0:
+                mashed_data.append(
+                    (
+                        torch.mean(torch.stack(mashed_image), dim=0),
+                        torch.mean(
+                            F.one_hot(
+                                torch.squeeze(torch.stack(mashed_label)),
+                                num_classes
+                            ).to(dtype=torch.float32),
+                            dim=0
+                        )
+                    )
+                )
+                # print(mashed_data[0][1])
+                mashed_image, mashed_label = [], []
+
+    return mashed_data
