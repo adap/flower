49479daa6c1f86559fab6aae569817d640824ab0
Pranjal Gulati <pranjal.carmel@gmail.com>
2023-08-14 05:48:41 +0530

    put everything together in main and create config file for cifar10

--- baselines/fedmix/fedmix/main.py
+++ baselines/fedmix/fedmix/main.py
@@ -1,53 +1,53 @@
-"""Create and connect the building blocks for your experiments; start the simulation.
-
-It includes processioning the dataset, instantiate strategy, specify how the global
-model is going to be evaluated, etc. At the end, this script saves the results.
-"""
-# these are the basic packages you'll need here
-# feel free to remove some if aren't needed
 import hydra
+from hydra.utils import instantiate
+from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
+import flwr as fl
 
+from fedmix.dataset import load_datasets
+from fedmix import client, server
+from fedmix.utils import save_results_as_pickle
 
-@hydra.main(config_path="conf", config_name="base", version_base=None)
-def main(cfg: DictConfig) -> None:
-    """Run the baseline.
-
-    Parameters
-    ----------
-    cfg : DictConfig
-        An omegaconf object that stores the hydra config.
-    """
-    # 1. Print parsed config
+@hydra.main(config_path="conf", config_name="cifar10", version_base=None)
+def main(cfg):
     print(OmegaConf.to_yaml(cfg))
 
-    # 2. Prepare your dataset
-    # here you should call a function in datasets.py that returns whatever is needed to:
-    # (1) ensure the server can access the dataset used to evaluate your model after
-    # aggregation
-    # (2) tell each client what dataset partitions they should use (e.g. a this could
-    # be a location in the file system, a list of dataloader, a list of ids to extract
-    # from a dataset, it's up to you)
+    seed = cfg.seed
+    device = cfg.device
+    num_gpus = cfg.num_gpus
+    num_clients = cfg.num_clients
+    clients_per_round = cfg.clients_per_round
+    num_rounds = cfg.num_rounds
 
-    # 3. Define your clients
-    # Define a function that returns another function that will be used during
-    # simulation to instantiate each individual client
-    # client_fn = client.<my_function_that_returns_a_function>()
+    dataset_config = cfg.dataset
+    model_config = cfg.model
+    strategy_config = cfg.strategy
+    client_config = cfg.client
 
-    # 4. Define your strategy
-    # pass all relevant argument (including the global dataset used after aggregation,
-    # if needed by your method.)
-    # strategy = instantiate(cfg.strategy, <additional arguments if desired>)
+    trainloaders, testloader, mashed_data = load_datasets(
+        dataset_config, num_clients, seed)
 
-    # 5. Start Simulation
-    # history = fl.simulation.start_simulation(<arguments for simulation>)
+    client_fn = client.gen_client_fn(client_config, trainloaders, model_config)
+    evaluate_fn = server.gen_evaluate_fn(testloader, device, model_config)
 
-    # 6. Save your results
-    # Here you can save the `history` returned by the simulation and include
-    # also other buffers, statistics, info needed to be saved in order to later
-    # on generate the plots you provide in the README.md. You can for instance
-    # access elements that belong to the strategy for example:
-    # data = strategy.get_my_custom_data() -- assuming you have such method defined.
-    # Hydra will generate for you a directory each time you run the code. You
-    # can retrieve the path to that directory with this:
-    # save_path = HydraConfig.get().runtime.output_dir
+    strategy = instantiate(
+        strategy_config, mashed_data=mashed_data, evaluate_fn=evaluate_fn)
+
+    history = fl.simulation.start_simulation(
+        client_fn=client_fn,
+        num_clients=num_clients,
+        config=fl.server.ServerConfig(num_rounds=num_rounds),
+        client_resources={'num_gpus': num_gpus},
+        strategy=strategy
+    )
+
+    print('----------------------')
+    print(history)
+    print('----------------------')
+
+    save_path = HydraConfig.get().runtime.output_dir
+    save_results_as_pickle(history, file_path=save_path, extra_results={})
+
+
+if __name__ == "__main__":
+    main()
--- baselines/fedmix/fedmix/utils.py
+++ baselines/fedmix/fedmix/utils.py
@@ -1,6 +1,34 @@
-"""Define any utility function.
+from pathlib import Path
+import pickle
+from secrets import token_hex
 
-They are not directly relevant to  the other (more FL specific) python modules. For
-example, you may define here things like: loading a model from a checkpoint, saving
-results, plotting.
-"""
+def save_results_as_pickle(history, file_path, extra_results, default_filename="results.pkl"):
+    path = Path(file_path)
+
+    # ensure path exists
+    path.mkdir(exist_ok=True, parents=True)
+
+    def _add_random_suffix(path_):
+        print(f"File `{path_}` exists! ")
+        suffix = token_hex(4)
+        print(f"New results to be saved with suffix: {suffix}")
+        return path_.parent / (path_.stem + "_" + suffix + ".pkl")
+
+    def _complete_path_with_default_name(path_):
+        print("Using default filename")
+        return path_ / default_filename
+
+    if path.is_dir():
+        path = _complete_path_with_default_name(path)
+
+    if path.is_file():
+        # file exists already
+        path = _add_random_suffix(path)
+
+    print(f"Results will be saved into: {path}")
+
+    data = {"history": history, **extra_results}
+
+    # save results to pickle
+    with open(str(path), "wb") as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
