847dbc64c074af6e5ae29a21e634f2505b7ce8fa
Pranjal Gulati <pranjal.carmel@gmail.com>
2023-08-14 05:41:05 +0530

    add custom vgg model as described in paper

--- baselines/fedmix/fedmix/models.py
+++ baselines/fedmix/fedmix/models.py
@@ -1,7 +1,62 @@
-"""Define our models, and training and eval functions.
+import torch
+import torch.nn as nn
 
-If your model is 100% off-the-shelf (e.g. directly from torchvision without requiring
-modifications) you might be better off instantiating your  model directly from the Hydra
-config. In this way, swapping your model for  another one can be done without changing
-the python code at all
-"""
+class CustomVGG(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CustomVGG, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU())
+        
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU())
+        
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.Dropout(0.1))
+        
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU())
+        
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.Dropout(0.1))
+        
+        self.fc = nn.Sequential(
+            nn.Dropout(0.1),
+            nn.Linear(4*4*256, 512),
+            nn.ReLU())
+        
+        self.fc1 = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.ReLU())
+        
+        self.fc2= nn.Sequential(
+            nn.Dropout(0.1),
+            nn.Linear(512, num_classes))
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
