bc71008183f4cda9ca1d1a81367c3a8f70ee7bf0
jafermarq <javier@flower.dev>
2023-08-22 15:09:02 +0100

    w/ previous

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -195,7 +195,6 @@ html_theme_options = {
     # },
 }
 
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
