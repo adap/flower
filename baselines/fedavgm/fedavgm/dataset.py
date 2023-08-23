8bf5d05512f067819aedaf4545719ab36fb97a77
Gustavo Bertoli <gubertoli@gmail.com>
2023-08-23 18:20:33 +0200

    Update dataset.py

--- baselines/fedavgm/fedavgm/dataset.py
+++ baselines/fedavgm/fedavgm/dataset.py
@@ -1,10 +1,22 @@
-"""Handle basic dataset creation.
+from tensorflow import keras
 
-In case of PyTorch it should return dataloaders for your dataset (for both the clients
-and the server). If you are using a custom dataset class, this module is the place to
-define it. If your dataset requires to be downloaded (and this is not done
-automatically -- e.g. as it is the case for many dataset in TorchVision) and
-partitioned, please include all those functions and logic in the
-`dataset_preparation.py` module. You can use all those functions from functions/methods
-defined here of course.
-"""
+def prepare_dataset(FEMNIST):
+    if FEMNIST == True:
+    (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+    input_shape = x_train.shape[1:]
+    num_classes = len(np.unique(y_train))
+  else:
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+    input_shape = x_train.shape[1:]
+    num_classes = len(np.unique(y_train))
+
+  return x_train, y_train, x_test, y_test, input_shape, num_classes
+
+def partition(x_train, y_train, num_clients, concentration, num_classes):
+  dataset = [x_train, y_train]
+  partitions, b = create_lda_partitions(dataset, num_partitions=num_clients, concentration=concentration * num_classes, seed=1234)
+  return partitions
