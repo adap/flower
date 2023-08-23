0aefb81bfda03bad373300a279e6404d7d542696
Gustavo Bertoli <gubertoli@gmail.com>
2023-08-23 18:35:40 +0200

    Update client.py

--- baselines/fedavgm/fedavgm/client.py
+++ baselines/fedavgm/fedavgm/client.py
@@ -7,13 +7,14 @@ from tensorflow.keras.utils import to_categorical
 
 
 class FlowerClient(fl.client.NumPyClient):
-    def __init__(self, model, x_train, y_train, x_val, y_val, input_shape, num_classes, local_epochs) -> None:
+    def __init__(self, model, x_train, y_train, x_val, y_val, input_shape, num_classes, local_epochs, batch_size) -> None:
         self.model = model()
         self.x_train, self.y_train = x_train, y_train
         self.x_val, self.y_val = x_val, y_val
         self.input_shape = input_shape
         self.num_classes = num_classes
         self.local_epochs = local_epochs
+        self.batch_size = batch_size
     
     def model(self):
         # CNN Model from (McMahan et. al., 2017) Communication-efficient learning of deep networks from decentralized data
@@ -34,7 +35,7 @@ class FlowerClient(fl.client.NumPyClient):
 
     def fit(self, parameters, config):
         self.model.set_weights(parameters)
-        self.model.fit(self.x_train, self.y_train, epochs=self.local_epochs)
+        self.model.fit(self.x_train, self.y_train, epochs=self.local_epochs, batch_size=self.batch_size)
         return self.model.get_weights(), len(self.x_train), {}
 
     def evaluate(self, parameters, config):
@@ -43,7 +44,7 @@ class FlowerClient(fl.client.NumPyClient):
         return loss, len(self.x_val), {"accuracy": acc}
 
 
-def generate_client_fn(partitions, input_shape, num_classes, local_epochs):
+def generate_client_fn(partitions, input_shape, num_classes, local_epochs, batch_size):
 
     def client_fn(cid: str) -> fl.client.Client:
       full_x_train_cid, full_y_train_cid = partitions[int(cid)]
@@ -57,6 +58,6 @@ def generate_client_fn(partitions, input_shape, num_classes, local_epochs):
       )
       x_val_cid, y_val_cid = full_x_train_cid[split_idx:], full_y_train_cid[split_idx:]
         
-      return FlowerClient(model, x_train_cid, y_train_cid, x_val_cid, y_val_cid, input_shape, num_classes, local_epochs)
+      return FlowerClient(model, x_train_cid, y_train_cid, x_val_cid, y_val_cid, input_shape, num_classes, local_epochs, batch_size)
 
     return client_fn
