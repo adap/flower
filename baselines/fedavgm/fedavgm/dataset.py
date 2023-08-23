6ceaa77c95d85c4bc2226550f553e4c263869b78
Gustavo Bertoli <gubertoli@gmail.com>
2023-08-23 18:32:37 +0200

    Update dataset.py

--- baselines/fedavgm/fedavgm/dataset.py
+++ baselines/fedavgm/fedavgm/dataset.py
@@ -1,22 +1,22 @@
 from tensorflow import keras
+from common import create_lda_partitions
 
 def prepare_dataset(FEMNIST):
     if FEMNIST == True:
-    (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
-    x_train = x_train.astype('float32') / 255
-    x_test = x_test.astype('float32') / 255
-    input_shape = x_train.shape[1:]
-    num_classes = len(np.unique(y_train))
-  else:
-    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-    x_train = x_train.astype('float32') / 255
-    x_test = x_test.astype('float32') / 255
-    input_shape = x_train.shape[1:]
-    num_classes = len(np.unique(y_train))
-
-  return x_train, y_train, x_test, y_test, input_shape, num_classes
+        (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+        x_train = x_train.astype('float32') / 255
+        x_test = x_test.astype('float32') / 255
+        input_shape = x_train.shape[1:]
+        num_classes = len(np.unique(y_train))
+    else:
+        (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+        x_train = x_train.astype('float32') / 255
+        x_test = x_test.astype('float32') / 255
+        input_shape = x_train.shape[1:]
+        num_classes = len(np.unique(y_train))
+    return x_train, y_train, x_test, y_test, input_shape, num_classes
 
 def partition(x_train, y_train, num_clients, concentration, num_classes):
-  dataset = [x_train, y_train]
-  partitions, b = create_lda_partitions(dataset, num_partitions=num_clients, concentration=concentration * num_classes, seed=1234)
-  return partitions
+    dataset = [x_train, y_train]
+    partitions, b = create_lda_partitions(dataset, num_partitions=num_clients, concentration=concentration * num_classes, seed=1234)
+    return partitions
