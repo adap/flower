badadb43774a7757ae4616d1aff6e701f4991d8f
William Lindskog <w.lindskog@CRD-ITS3.dnde.emea.denso>
2023-08-25 15:21:17 +0200

    Modification FL System

--- baselines/FedPer/FedPer/run_figures.sh
+++ baselines/FedPer/FedPer/run_figures.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# CIFAR10 MobileNet-v1
-python -m FedPer.main --config-path conf/cifar/mobile --config-name fedper_10 &&
-python -m FedPer.main --config-path conf/cifar/mobile --config-name fedper_8 
+# CIFAR10 ResNet
+# python -m rest2.main --config-path conf/cifar/resnet --config-name fedper_10 &&
+python -m rest2.main --config-path conf/cifar/resnet --config-name fedper_8 &&
+python -m rest2.main --config-path conf/cifar/resnet --config-name fedper_4 &&
+python -m rest2.main --config-path conf/cifar/resnet --config-name fedavg_10 &&
+python -m rest2.main --config-path conf/cifar/resnet --config-name fedavg_8 &&
+python -m rest2.main --config-path conf/cifar/resnet --config-name fedavg_4 
