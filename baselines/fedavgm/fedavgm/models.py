3796d05d4e929974ba1c9c0045d320a840166e97
Gustavo Bertoli <gubertoli@gmail.com>
2023-08-29 15:25:03 +0200

    fix CNN architecture according to McMahan 2017

--- baselines/fedavgm/fedavgm/models.py
+++ baselines/fedavgm/fedavgm/models.py
@@ -16,20 +16,15 @@ def cnn(input_shape, num_classes):
     weight_decay = 0.004
     model = keras.Sequential(
         [
-            keras.layers.Conv2D(
-                32,
-                (5, 5),
-                activation="relu",
-                input_shape=input_shape,
-                kernel_regularizer=l2(weight_decay),
-            ),
-            keras.layers.MaxPooling2D(2, 2),
-            keras.layers.Conv2D(
-                64, (5, 5), activation="relu", kernel_regularizer=l2(weight_decay)
-            ),
-            keras.layers.MaxPooling2D(2, 2),
+            keras.layers.Conv2D(64, (5, 5), padding='same', activation='relu', input_shape=input_shape, kernel_regularizer=l2(weight_decay)),
+            keras.layers.MaxPooling2D((3, 3), strides=(2, 2)),
+            keras.layers.BatchNormalization(),
+            keras.layers.Conv2D(64, (5, 5), padding='same', activation='relu', kernel_regularizer=l2(weight_decay)),
+            keras.layers.BatchNormalization(),
+            keras.layers.MaxPooling2D((3, 3), strides=(2, 2)),
             keras.layers.Flatten(),
-            keras.layers.Dense(512, activation="relu"),
+            keras.layers.Dense(384, activation="relu"),
+            keras.layers.Dense(192, activation="relu"),
             keras.layers.Dense(num_classes, activation="softmax"),
         ]
     )
