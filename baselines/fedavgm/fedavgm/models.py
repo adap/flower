afe0fa106646385643f74e699440e4178596b61f
Gustavo Bertoli <gubertoli@gmail.com>
2023-08-29 15:28:35 +0200

    passing test-baseline

--- baselines/fedavgm/fedavgm/models.py
+++ baselines/fedavgm/fedavgm/models.py
@@ -16,10 +16,23 @@ def cnn(input_shape, num_classes):
     weight_decay = 0.004
     model = keras.Sequential(
         [
-            keras.layers.Conv2D(64, (5, 5), padding='same', activation='relu', input_shape=input_shape, kernel_regularizer=l2(weight_decay)),
+            keras.layers.Conv2D(
+                64,
+                (5, 5),
+                padding="same",
+                activation="relu",
+                input_shape=input_shape,
+                kernel_regularizer=l2(weight_decay),
+            ),
             keras.layers.MaxPooling2D((3, 3), strides=(2, 2)),
             keras.layers.BatchNormalization(),
-            keras.layers.Conv2D(64, (5, 5), padding='same', activation='relu', kernel_regularizer=l2(weight_decay)),
+            keras.layers.Conv2D(
+                64,
+                (5, 5),
+                padding="same",
+                activation="relu",
+                kernel_regularizer=l2(weight_decay),
+            ),
             keras.layers.BatchNormalization(),
             keras.layers.MaxPooling2D((3, 3), strides=(2, 2)),
             keras.layers.Flatten(),
