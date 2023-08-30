9434e5b2f54160ec23c8088a88e0c635c3acfcbb
WilliamLindskog <w.lindskog@CRD-ITS3.dnde.emea.denso>
2023-08-30 23:25:19 +0200

    Only FedPer

--- baselines/FedPer/FedPer_new/run_figures.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# CIFAR10 MobileNet-v1
-python -m FedPer.main --config-path conf/cifar/mobile --config-name fedper_10 &&
-python -m FedPer.main --config-path conf/cifar/mobile --config-name fedper_8 
