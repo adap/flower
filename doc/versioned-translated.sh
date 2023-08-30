679b522a9099fa27e99377fa57ff96e46ce00b97
Charles Beauville <charles@flower.dev>
2023-08-30 19:47:50 +0200

    Refactor script

--- doc/versioned-translated.sh
+++ doc/versioned-translated.sh
@@ -1,32 +1,30 @@
 #!/bin/sh
 
+# Store the current branch in a variable
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
+# Move to the top level
 cd $(git rev-parse --show-toplevel)
 
-##############
-# BUILD DOCS #
-##############
-  
-# first, cleanup any old builds' static assets
-# make -C doc clean
-rm -rf doc/build/html
-rm -rf doc/build/gettext
+# Clean up previous builds
+rm -rf doc/build
 
+# Create a temporary directory and store locales and _templates files in it
 tmp_dir=`mktemp -d`
-
 cp -r doc/locales ${tmp_dir}/locales
 cp -r doc/source/_templates ${tmp_dir}/_templates
 cp doc/source/conf.py ${tmp_dir}/conf.py
 
 cd doc
 
-# get a list of branches, excluding 'HEAD' and 'gh-pages'
+# Get a list of languages based on the folders in locales
 languages="en `find locales/ -mindepth 1 -maxdepth 1 -type d -exec basename '{}' \;`"
+# Get a list of tags, excluding those before v1.0.0
 versions="`git for-each-ref '--format=%(refname:lstrip=-1)' refs/tags/ | grep -iE '^v(([1-9]|[0-9]{2,}).*)$'`"
+
 for current_version in ${versions}; do
   
-   # make the current language available to conf.py
+   # Make the current language available to conf.py
    export current_version
 
    changed=false
@@ -35,7 +33,7 @@ for current_version in ${versions}; do
   
    echo "INFO: Building sites for ${current_version}"
   
-   # skip this branch if it doesn't have our docs dir & sphinx config
+   # Skip this branch if it doesn't have our docs dir & sphinx config
    if [ ! -e 'source/conf.py' ]; then
       echo "INFO: Couldn't find 'doc/source/conf.py' (skipped)"
       continue
@@ -43,12 +41,14 @@ for current_version in ${versions}; do
   
    for current_language in ${languages}; do
   
-      # make the current language available to conf.py
+      # Make the current language available to conf.py
       export current_language
   
       echo "INFO: Building for ${current_language}"
 
       if [[ ! -f "locales/$current_language/LC_MESSAGES/framework-docs.po" ]]; then
+
+        # Adding translation to versions that didn't contain one
         if [[ $current_language != "en" ]]; then
           echo "No translation, using default one"
           rm -rf locales/$current_language
@@ -59,39 +59,41 @@ for current_version in ${versions}; do
           make gettext
           sphinx-intl update -p build/gettext -l ${current_language}
         fi
+
+        # Adding version switcher to versions that didn't contain it
         cp -r ${tmp_dir}/_templates source
-
-        echo "html_context = dict()" >> source/conf.py
-        echo "html_context['current_language'] = '$current_language'" >> source/conf.py
-        echo "from git import Repo" >> source/conf.py
-        echo "repo = Repo( search_parent_directories=True )" >> source/conf.py
-        echo "current_version = '$current_version'" >> source/conf.py
-        echo "html_context['current_version'] = {}" >> source/conf.py
-        echo "html_context['current_version']['name'] = 'Main' if current_version=='main' else current_version" >> source/conf.py
-        echo "html_context['current_version']['url'] = current_version" >> source/conf.py
-        echo "html_context['current_version']['full_name'] = 'Main' if current_version=='main' else f'Flower Framework {current_version}'" >> source/conf.py
-        echo "html_context['versions'] = list()" >> source/conf.py
-        echo "versions = [tag.name for tag in repo.tags if int(tag.name[1]) != 0]" >> source/conf.py
-        echo "versions.append('main')" >> source/conf.py
-        echo "for version in versions:" >> source/conf.py
-        echo "    html_context['versions'].append({'name': version})" >> source/conf.py
-        echo "html_sidebars = {" >> source/conf.py
-        echo "    '**': [" >> source/conf.py
-        echo "        'sidebar/brand.html'," >> source/conf.py
-        echo "        'sidebar/search.html'," >> source/conf.py
-        echo "        'sidebar/scroll-start.html'," >> source/conf.py
-        echo "        'sidebar/navigation.html'," >> source/conf.py
-        echo "        'sidebar/scroll-end.html'," >> source/conf.py
-        echo "        'sidebar/versioning.html'," >> source/conf.py
-        echo "        'sidebar/lang.html'," >> source/conf.py
-        echo "    ]" >> source/conf.py
-        echo "}" >> source/conf.py
-
+        tee -a source/conf.py << END
+                html_context = dict()
+                html_context['current_language'] = '$current_language'
+                from git import Repo
+                repo = Repo( search_parent_directories=True )
+                current_version = '$current_version'
+                html_context['current_version'] = {}
+                html_context['current_version']['url'] = current_version
+                html_context['current_version']['full_name'] = 'main' if current_version=='main' else f'Flower Framework {current_version}'
+                html_context['versions'] = list()
+                versions = [tag.name for tag in repo.tags if int(tag.name[1]) != 0]
+                versions.append('main')
+                for version in versions:
+                    html_context['versions'].append({'name': version})
+                html_sidebars = {
+                    '**': [
+                        'sidebar/brand.html',
+                        'sidebar/search.html',
+                        'sidebar/scroll-start.html',
+                        'sidebar/navigation.html',
+                        'sidebar/scroll-end.html',
+                        'sidebar/versioning.html',
+                        'sidebar/lang.html',
+                    ]
+                }
+        END
         changed=true
       fi
   
       sphinx-build -b html source/ build/html/${current_version}/${current_language} -A lang=True -D language=${current_language}
 
+      # Restore branch as it was to avoid conflicts
       if [[ changed ]]; then
         git restore source/conf.py
         rm -rf locales/${current_language}
@@ -103,6 +105,7 @@ for current_version in ${versions}; do
   
 done
   
+# Build the main version (main for GH CI, local branch for local) 
 if [[ $GITHUB_ACTIONS ]]
 then
   git switch main
@@ -117,4 +120,5 @@ for current_language in ${languages}; do
     sphinx-build -b html source/ build/html/${current_version}/${current_language} -A lang=True -D language=${current_language}
 done
 
+# Copy main version to the root of the built docs
 cp -r build/html/main/en/* build/html/
