3b3ffab39b2ec5fe3d04b1af751c8acb623a9aa6
William Lindskog <w.lindskog@CRD-ITS3.dnde.emea.denso>
2023-08-30 22:56:29 +0200

    Almost done reproducing figure 2

--- baselines/FedPer/FedPer/run_figures.sh
+++ baselines/FedPer/FedPer/run_figures.sh
@@ -11,7 +11,7 @@
 # CIFAR10 Resnet
 #python -m FedPer.main --config-path conf/cifar/resnet --config-name fedper_10 &&
 #python -m FedPer.main --config-path conf/cifar/resnet --config-name fedper_8 &&
-python -m FedPer.main --config-path conf/cifar/resnet --config-name fedper_4 &&
+# python -m FedPer.main --config-path conf/cifar/resnet --config-name fedper_4 &&
 python -m FedPer.main --config-path conf/cifar/resnet --config-name fedavg_10 &&
 python -m FedPer.main --config-path conf/cifar/resnet --config-name fedavg_8 &&
 python -m FedPer.main --config-path conf/cifar/resnet --config-name fedavg_4
