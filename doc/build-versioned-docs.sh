4cd57a0140c78f8f414b3c2e40314c42d8b5cff9
Charles Beauville <charles@flower.dev>
2023-08-31 10:18:18 +0200

    Fix bracket error

--- doc/build-versioned-docs.sh
+++ doc/build-versioned-docs.sh
@@ -96,7 +96,7 @@ for current_version in ${versions}; do
           echo "$language_config" >> source/conf.py
 
           # Update the text and the translation to match the source files if no_translate is not provided
-          if [ ! $1 == "no_translate" || ! $1 ]; then
+          if [ ! $1 ] || [ ! $1 == "no_translate" ]; then
             make gettext
             sphinx-intl update -p build/gettext -l ${current_language}
           fi
