87ae25f162ad684998f0c822ce0a04d0c8fb450a
Adam Narozniak <adam@flower.dev>
2023-09-07 16:17:01 +0200

    Migrate TensorFlow quickstart to Flower Datasets

--- examples/quickstart-tensorflow/client.py
+++ examples/quickstart-tensorflow/client.py
@@ -1,16 +1,39 @@
+import argparse
 import os
+import sys
 
 import flwr as fl
 import tensorflow as tf
-
+from flwr_datasets import FederatedDataset
 
 # Make TensorFlow log less verbose
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+# Parse arguments
+parser = argparse.ArgumentParser(description="Flower")
+parser.add_argument(
+    "--partition",
+    type=int,
+    choices=[0, 1, 2],
+    required=True,
+    help="Partition of the dataset (0,1 or 2). "
+    "The dataset is divided into 3 partitions created artificially.",
+)
+args = parser.parse_args()
+
 # Load model and data (MobileNetV2, CIFAR-10)
 model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
 model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+# Download and partition dataset
+fds = FederatedDataset(dataset="cifar10", partitioners={"train": 3})
+partition = fds.load_partition(args.partition, "train")
+partition.set_format("numpy")
+
+# Divide data on each node: 80% train, 20% test
+partition = partition.train_test_split(test_size=0.2)
+x_train, y_train = partition["train"]["img"] / 255.0, partition["train"]["label"]
+x_test, y_test = partition["test"]["img"] / 255.0, partition["test"]["label"]
 
 
 # Define Flower client
