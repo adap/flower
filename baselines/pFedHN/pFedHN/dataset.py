19a382138afdc0dafed84bd39bc59d0a4cb70162
achiverram28 <ramsamarth21bcs24@iiitkottayam.ac.in>
2023-09-15 17:14:02 +0530

    Revert "Code formatting"
    This reverts commit 7372b8d36da6985deb0231f16b84c727475b7ea9.

--- baselines/pFedHN/pFedHN/dataset.py
+++ baselines/pFedHN/pFedHN/dataset.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 import numpy as np
 import torch.utils.data
 import torchvision.transforms as transforms
-from torchvision.datasets import CIFAR10, CIFAR100, MNIST
+from torchvision.datasets import CIFAR10, CIFAR100 , MNIST
 
 
 def get_datasets(data_name, dataroot, normalize=True, val_size=10000):
@@ -20,16 +20,12 @@ def get_datasets(data_name, dataroot, normalize=True, val_size=10000):
     :return: train_set, val_set, test_set (tuple of pytorch dataset/subset)
     """
 
-    if data_name == "cifar10":
-        normalization = transforms.Normalize(
-            (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
-        )
+    if data_name =='cifar10':
+        normalization = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
         data_obj = CIFAR10
 
-    elif data_name == "cifar100":
-        normalization = transforms.Normalize(
-            (0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762)
-        )
+    elif data_name == 'cifar100':
+        normalization = transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762))
         data_obj = CIFAR100
 
     elif data_name == "mnist":
@@ -46,9 +42,19 @@ def get_datasets(data_name, dataroot, normalize=True, val_size=10000):
 
     transform = transforms.Compose(trans)
 
-    dataset = data_obj(dataroot, train=True, download=True, transform=transform)
+    dataset = data_obj(
+        dataroot,
+        train=True,
+        download=True,
+        transform=transform
+    )
 
-    test_set = data_obj(dataroot, train=False, download=True, transform=transform)
+    test_set = data_obj(
+        dataroot,
+        train=False,
+        download=True,
+        transform=transform
+    )
 
     train_size = len(dataset) - val_size
     train_set, val_set = torch.utils.data.random_split(dataset, [train_size, val_size])
@@ -80,9 +86,7 @@ def get_num_classes_samples(dataset):
     return num_classes, num_samples, data_labels_list
 
 
-def gen_classes_per_node(
-    dataset, num_users, classes_per_user=2, high_prob=0.6, low_prob=0.4
-):
+def gen_classes_per_node(dataset, num_users, classes_per_user=2, high_prob=0.6, low_prob=0.4):
     """
     creates the data distribution of each client
     :param dataset: pytorch dataset object
@@ -105,7 +109,7 @@ def gen_classes_per_node(
         probs = np.random.uniform(low_prob, high_prob, size=count_per_class)
         # normalizing
         probs_norm = (probs / probs.sum()).tolist()
-        class_dict[i] = {"count": count_per_class, "prob": probs_norm}
+        class_dict[i] = {'count': count_per_class, 'prob': probs_norm}
 
     # -------------------------------------#
     # Assign each client with data indexes #
@@ -114,12 +118,12 @@ def gen_classes_per_node(
     for i in range(num_users):
         c = []
         for _ in range(classes_per_user):
-            class_counts = [class_dict[i]["count"] for i in range(num_classes)]
+            class_counts = [class_dict[i]['count'] for i in range(num_classes)]
             max_class_counts = np.where(np.array(class_counts) == max(class_counts))[0]
             c.append(np.random.choice(max_class_counts))
-            class_dict[c[-1]]["count"] -= 1
-        class_partitions["class"].append(c)
-        class_partitions["prob"].append([class_dict[i]["prob"].pop() for i in c])
+            class_dict[c[-1]]['count'] -= 1
+        class_partitions['class'].append(c)
+        class_partitions['prob'].append([class_dict[i]['prob'].pop() for i in c])
     return class_partitions
 
 
@@ -149,9 +153,7 @@ def gen_data_split(dataset, num_users, class_partitions):
     # ------------------------------ #
     user_data_idx = [[] for i in range(num_users)]
     for usr_i in range(num_users):
-        for c, p in zip(
-            class_partitions["class"][usr_i], class_partitions["prob"][usr_i]
-        ):
+        for c, p in zip(class_partitions['class'][usr_i], class_partitions['prob'][usr_i]):
             end_idx = int(num_samples[c] * p)
             user_data_idx[usr_i].extend(data_class_idx[c][:end_idx])
             data_class_idx[c] = data_class_idx[c][end_idx:]
@@ -169,27 +171,18 @@ def gen_random_loaders(data_name, data_path, num_users, bz, classes_per_user):
     :param classes_per_user: number of classes assigned to each client
     :return: train/val/test loaders of each client, list of pytorch dataloaders
     """
-    loader_params = {
-        "batch_size": bz,
-        "shuffle": False,
-        "pin_memory": True,
-        "num_workers": 0,
-    }
+    loader_params = {"batch_size": bz, "shuffle": False, "pin_memory": True, "num_workers": 0}
     dataloaders = []
     datasets = get_datasets(data_name, data_path, normalize=True)
     for i, d in enumerate(datasets):
         # ensure same partition for train/test/val
         if i == 0:
             cls_partitions = gen_classes_per_node(d, num_users, classes_per_user)
-            loader_params["shuffle"] = True
+            loader_params['shuffle'] = True
         usr_subset_idx = gen_data_split(d, num_users, cls_partitions)
         # create subsets for each client
         subsets = list(map(lambda x: torch.utils.data.Subset(d, x), usr_subset_idx))
         # create dataloaders from subsets
-        dataloaders.append(
-            list(
-                map(lambda x: torch.utils.data.DataLoader(x, **loader_params), subsets)
-            )
-        )
+        dataloaders.append(list(map(lambda x: torch.utils.data.DataLoader(x, **loader_params), subsets)))
 
     return dataloaders
