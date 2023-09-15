b8301e3a6f3ccc3cdd778f8ba159f0bdf6d5df70
WilliamLindskog <williamlindskog@protonmail.com>
2023-09-15 10:54:31 +0200

    Updated mobile plot, figure 2

--- baselines/fedper/fedper/run_figures.sh
+++ baselines/fedper/fedper/run_figures.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # CIFAR10 Mobile and Resnet (non-iid n classes (FIGURE 2a&b))
-#for model in mobile resnet
-#do 
-#    for num_classes in 4 8 10
-#    do
-#        python -m fedper.main --config-path conf/cifar10/${model} --config-name fedper dataset.num_classes=${num_classes} &&
-#        python -m fedper.main --config-path conf/cifar10/${model} --config-name fedavg dataset.num_classes=${num_classes} 
-#    done
-#done  
+for model in resnet
+do 
+    for num_classes in 4 8 10
+    do
+        python -m fedper.main --config-path conf/cifar10/${model} --config-name fedper dataset.num_classes=${num_classes} num_rounds=50 &&
+        python -m fedper.main --config-path conf/cifar10/${model} --config-name fedavg dataset.num_classes=${num_classes} num_rounds=50
+    done
+done  
 
 # CIFAR10 Mobile (n head layers (FIGURE 4a))
 #for num_head_layers in 2 3 4
@@ -18,13 +18,11 @@
 #done
 
 # CIFAR10 Resnet (n head layers (FIGURE 4b))
-for num_head_layers in 2 3
-do
-    python -m fedper.main --config-path conf/cifar10/resnet --config-name fedper model.num_head_layers=${num_head_layers} num_rounds=25 &&
-    python -m fedper.main --config-path conf/cifar10/resnet --config-name fedavg
-done
-
-python -m fedper.main --config-path conf/cifar10/resnet --config-name fedper dataset.num_classes=10 # &&
+#for num_head_layers in 2 3
+#do
+#    python -m fedper.main --config-path conf/cifar10/resnet --config-name fedper model.num_head_layers=${num_head_layers} num_rounds=25 &&
+#    python -m fedper.main --config-path conf/cifar10/resnet --config-name fedavg
+#done
 
 # FLICKR
 #for model in mobile resnet
