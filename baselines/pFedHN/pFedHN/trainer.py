7372b8d36da6985deb0231f16b84c727475b7ea9
achiverram28 <ramsamarth21bcs24@iiitkottayam.ac.in>
2023-09-15 17:12:38 +0530

    Code formatting
    Signed-off-by: achiverram28 <ramsamarth21bcs24@iiitkottayam.ac.in>

--- baselines/pFedHN/pFedHN/trainer.py
+++ baselines/pFedHN/pFedHN/trainer.py
@@ -1,9 +1,10 @@
 import torch
 import torch.utils.data
 
-def train(netw,trainloader,testloader,
-          epochs: int, lr: float,
-          wd: float,device,cid) -> None:
+
+def train(
+    netw, trainloader, testloader, epochs: int, lr: float, wd: float, device, cid
+) -> None:
     """Train the network on the training set."""
 
     net = netw
@@ -12,14 +13,10 @@ def train(netw,trainloader,testloader,
     criteria = torch.nn.CrossEntropyLoss()
 
     # init optimizer
-    optim = torch.optim.SGD(
-            net.parameters(), lr=lr, momentum=.9, weight_decay=wd
-        )
-    
-    
+    optim = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=wd)
+
     # inner updates -> obtaining theta_tilda
     for i in range(epochs):
-
         net.train()
         optim.zero_grad()
         batch = next(iter(trainloader[int(cid)]))
@@ -29,15 +26,15 @@ def train(netw,trainloader,testloader,
         loss.backward()
         torch.nn.utils.clip_grad_norm_(net.parameters(), 50)
         optim.step()
-    
+
     # calculating loss and accuracy on test set for the target model
     with torch.no_grad():
-            net.eval()
-            batch = next(iter(testloader[int(cid)]))
-            img, label = tuple(t.to(device) for t in batch)
-            pred = net(img)
-            loss = criteria(pred, label)
-            acc = pred.argmax(1).eq(label).sum().item() / len(label)
-            net.train()
+        net.eval()
+        batch = next(iter(testloader[int(cid)]))
+        img, label = tuple(t.to(device) for t in batch)
+        pred = net(img)
+        loss = criteria(pred, label)
+        acc = pred.argmax(1).eq(label).sum().item() / len(label)
+        net.train()
 
     print(f"Client: {cid}, TargetModelLoss: {loss:.4f},  TargetModelAcc: {acc:.4f}")
