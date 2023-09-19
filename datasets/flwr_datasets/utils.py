293fb71fca9881b6a413f5dd6fecbcb30260d5ad
Adam Narozniak <adam@flower.dev>
2023-09-19 15:57:45 +0200

    Change the ValueError to the warning when the dataset was not tested

--- datasets/flwr_datasets/utils.py
+++ datasets/flwr_datasets/utils.py
@@ -13,11 +13,25 @@
 # limitations under the License.
 # ==============================================================================
 """Utils for FederatedDataset."""
-
-
+import warnings
 from typing import Dict
 
 from flwr_datasets.partitioner import IidPartitioner, Partitioner
+
+tested_vision_image_classification_datasets = [
+    "mnist",
+    "cifar10",
+    "fashion_mnist",
+    "sasha/dog-food",
+    "zh-plus/tiny-imagenet",
+]
+tested_tabular_classification_datasets = ["hitorilabs/iris"]
+tested_uncategorized_datasets = ["scikit-learn/iris"]
+tested_datasets = [
+    *tested_vision_image_classification_datasets,
+    *tested_tabular_classification_datasets,
+    *tested_uncategorized_datasets,
+]
 
 
 def _instantiate_partitioners(partitioners: Dict[str, int]) -> Dict[str, Partitioner]:
@@ -41,17 +55,11 @@ def _instantiate_partitioners(partitioners: Dict[str, int]) -> Dict[str, Partiti
     return instantiated_partitioners
 
 
-def _check_if_dataset_supported(dataset: str) -> None:
+def _check_if_dataset_tested(dataset: str) -> None:
     """Check if the dataset is in the narrowed down list of the tested datasets."""
-    supported_datasets = [
-        "mnist",
-        "cifar10",
-        "fashion_mnist",
-        "sasha/dog-food",
-        "zh-plus/tiny-imagenet",
-    ]
-    if dataset not in supported_datasets:
-        raise ValueError(
-            f"The currently tested and supported dataset are {supported_datasets}. "
-            f"Given: {dataset}"
+    if dataset not in tested_datasets:
+        warnings.warn(
+            f"The currently tested dataset are {tested_datasets}. Given: {dataset}. "
+            f"Note that unsupported datasets might cause errors.",
+            stacklevel=1,
         )
