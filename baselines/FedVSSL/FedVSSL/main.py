efaa6d490f505e95fea96c139c5d7d82575317ea
yangao <yangao@user-117-12.svr-vpn-1.vpn.cl.cam.ac.uk>
2023-09-21 18:18:11 +0100

    update

--- baselines/FedVSSL/FedVSSL/main.py
+++ baselines/FedVSSL/FedVSSL/main.py
@@ -39,15 +39,15 @@ from flwr.common import (
     parameters_to_ndarrays,   # weights_to_parameters in the original FedVSSL repository
 )
 # import CtP.tools._init_paths
-from FedVSSL.strategy import FedVSSL
-from FedVSSL.client import SslClient
+from strategy import FedVSSL
+from client import SslClient
 
 
 def initial_setup(cid, base_work_dir, rounds, data_dir, num_gpus, partition_dir):
-    import FedVSSL.utils as utils
+    import utils
     cid_plus_one = str(int(cid) + 1)
     args = Namespace(
-        cfg='FedVSSL/conf/mmcv_conf/r3d_18_ucf101/pretraining.py', # Path to the pretraining configuration file
+        cfg='conf/mmcv_conf/r3d_18_ucf101/pretraining.py', # Path to the pretraining configuration file
         checkpoint=None, cid=int(cid), data_dir=data_dir, gpus=num_gpus,
         launcher='none',
         local_rank=0, progress=False, resume_from=None, rounds=rounds, seed=7, validate=False,
@@ -117,148 +117,148 @@ def parse_args():
     return args
 
 
-#if __name__ == "__main__":
-args = parse_args()
+if __name__ == "__main__":
+    args = parse_args()
 
-print('#######', args.pre_train)
-import sys
-sys.exit(0)
+    print('#######', args.pre_train)
+    import sys
+    sys.exit(0)
 
-if args.pre_train:
+    if args.pre_train:
 
-    # first the paths needs to be defined otherwise the program may not be able to locate the files of the ctp
-    from FedVSSL.utils import init_p_paths
-    init_p_paths("FedVSSL")
+        # first the paths needs to be defined otherwise the program may not be able to locate the files of the ctp
+        from utils import init_p_paths
+        init_p_paths("FedVSSL")
 
-    client_resources = {"num_cpus": args.cpus_per_client, "num_gpus": args.gpus_per_client}
-    base_work_dir = 'ucf_' + args.exp_name
-    rounds = args.rounds
-    data_dir = args.data_dir
-    partition_dir = args.partition_dir
-    num_gpus = args.gpus_per_client
+        client_resources = {"num_cpus": args.cpus_per_client, "num_gpus": args.gpus_per_client}
+        base_work_dir = 'ucf_' + args.exp_name
+        rounds = args.rounds
+        data_dir = args.data_dir
+        partition_dir = args.partition_dir
+        num_gpus = args.gpus_per_client
 
-    def client_fn(cid: str):
-        args, cfg, distributed, logger, model, train_dataset, test_dataset, videossl = initial_setup(cid,
-                                                                                                     base_work_dir,
-                                                                                                     rounds,
-                                                                                                     data_dir,
-                                                                                                     num_gpus,
-                                                                                                     partition_dir)
-        return SslClient(model, train_dataset, test_dataset, cfg, args, distributed, logger, videossl)
+        def client_fn(cid: str):
+            args, cfg, distributed, logger, model, train_dataset, test_dataset, videossl = initial_setup(cid,
+                                                                                                         base_work_dir,
+                                                                                                         rounds,
+                                                                                                         data_dir,
+                                                                                                         num_gpus,
+                                                                                                         partition_dir)
+            return SslClient(model, train_dataset, test_dataset, cfg, args, distributed, logger, videossl)
 
-    # configure the strategy
-    strategy = FedVSSL(
-        mix_coeff=args.mix_coeff,
-        swbeta=args.swbeta,
-        base_work_dir=base_work_dir,
-        fraction_fit=(float(args.num_clients_per_round) / args.pool_size),
-        min_fit_clients=args.num_clients_per_round,
-        min_available_clients=args.pool_size,
-        on_fit_config_fn=fit_config,
-        fedavg=args.fedavg,
-    )
-     # (optional) specify ray config
-    ray_config = {"include_dashboard": args.include_dashboard}
+        # configure the strategy
+        strategy = FedVSSL(
+            mix_coeff=args.mix_coeff,
+            swbeta=args.swbeta,
+            base_work_dir=base_work_dir,
+            fraction_fit=(float(args.num_clients_per_round) / args.pool_size),
+            min_fit_clients=args.num_clients_per_round,
+            min_available_clients=args.pool_size,
+            on_fit_config_fn=fit_config,
+            fedavg=args.fedavg,
+        )
+         # (optional) specify ray config
+        ray_config = {"include_dashboard": args.include_dashboard}
 
-    # start simulation
-    hist = fl.simulation.start_simulation(
-        client_fn=client_fn,
-        num_clients=args.pool_size,
-        client_resources=client_resources,
-        config=fl.server.ServerConfig(num_rounds=args.rounds),
-        strategy=strategy,
-        ray_init_args=ray_config,
-    )
-else:
-    import subprocess
-    import os
-    import textwrap
-    from mmcv.runner import load_state_dict
-    import textwrap
-    from mmcv.runner import load_state_dict
-    # import CtP
-    # from CtP.configs.ctp.r3d_18_kinetics import finetune_ucf101
-    from FedVSSL.CtP.pyvrl.builder import build_model, build_dataset
+        # start simulation
+        hist = fl.simulation.start_simulation(
+            client_fn=client_fn,
+            num_clients=args.pool_size,
+            client_resources=client_resources,
+            config=fl.server.ServerConfig(num_rounds=args.rounds),
+            strategy=strategy,
+            ray_init_args=ray_config,
+        )
+    else:
+        import subprocess
+        import os
+        import textwrap
+        from mmcv.runner import load_state_dict
+        import textwrap
+        from mmcv.runner import load_state_dict
+        # import CtP
+        # from CtP.configs.ctp.r3d_18_kinetics import finetune_ucf101
+        from CtP.pyvrl.builder import build_model, build_dataset
 
-    # we give an example on how one can perform fine-tuning uisng UCF-101 dataset.
-    cfg_path = "FedVSSL/CtP/configs/ctp/r3d_18_kinetics/finetune_ucf101.py"
-    cfg = Config.fromfile(cfg_path)
-    cfg.model.backbone['pretrained'] = None
+        # we give an example on how one can perform fine-tuning uisng UCF-101 dataset.
+        cfg_path = "CtP/configs/ctp/r3d_18_kinetics/finetune_ucf101.py"
+        cfg = Config.fromfile(cfg_path)
+        cfg.model.backbone['pretrained'] = None
 
-    # build a model using the configuration file from Ctp repository
-    model = build_model(cfg.model)
+        # build a model using the configuration file from Ctp repository
+        model = build_model(cfg.model)
 
-    # path to the pretrained model. We provide certain federated pretrained model that can be easily downloaded
-    # from the following link: https://github.com/yasar-rehman/FEDVSSL
-    # here we gave an example with FedVSSL (alpha=0, beta=0) checkpoint file
-    # The files after federated pretraining are usually saved in .npz format.
+        # path to the pretrained model. We provide certain federated pretrained model that can be easily downloaded
+        # from the following link: https://github.com/yasar-rehman/FEDVSSL
+        # here we gave an example with FedVSSL (alpha=0, beta=0) checkpoint file
+        # The files after federated pretraining are usually saved in .npz format.
 
-    pretrained = args.pretrained_model_path
+        pretrained = args.pretrained_model_path
 
-    # conversion of the .npz files to the .pth format. If the files are saved in .npz format
-    if pretrained.endswith('.npz'):
-        # following changes are made here
-        params = np.load(pretrained, allow_pickle=True)
-        params = params['arr_0'].item()
-        params = parameters_to_ndarrays(params)
-        params_dict = zip(model.state_dict().keys(), params)
-        state_dict = {
-            'state_dict':OrderedDict({k: torch.from_numpy(v) for k, v in params_dict})
-        }
-        torch.save(state_dict, './model_pretrained.pth')
+        # conversion of the .npz files to the .pth format. If the files are saved in .npz format
+        if pretrained.endswith('.npz'):
+            # following changes are made here
+            params = np.load(pretrained, allow_pickle=True)
+            params = params['arr_0'].item()
+            params = parameters_to_ndarrays(params)
+            params_dict = zip(model.state_dict().keys(), params)
+            state_dict = {
+                'state_dict':OrderedDict({k: torch.from_numpy(v) for k, v in params_dict})
+            }
+            torch.save(state_dict, './model_pretrained.pth')
 
 
-#-----------------------------------------------------------------------------------------------------------------------
-# The cfg_path need to be updated with the following updated configuration contents to be able to load the pretrained model.
-# Instead of executing the blow mentioned code, one can also directly modify the "pretrained" variable by opening the path represented
-# by the config_path variable
-#
-    config_content = textwrap.dedent('''\
-    _base_ = ['../../recognizers/_base_/model_r3d18.py',
-    '../../recognizers/_base_/runtime_ucf101.py']
-    work_dir = './output/ctp/r3d_18_kinetics/finetune_ucf101/'
-    model = dict(
-        backbone=dict(
-            pretrained='./model_pretrained.pth', 
+    #-----------------------------------------------------------------------------------------------------------------------
+    # The cfg_path need to be updated with the following updated configuration contents to be able to load the pretrained model.
+    # Instead of executing the blow mentioned code, one can also directly modify the "pretrained" variable by opening the path represented
+    # by the config_path variable
+    #
+        config_content = textwrap.dedent('''\
+        _base_ = ['../../recognizers/_base_/model_r3d18.py',
+        '../../recognizers/_base_/runtime_ucf101.py']
+        work_dir = './output/ctp/r3d_18_kinetics/finetune_ucf101/'
+        model = dict(
+            backbone=dict(
+                pretrained='./model_pretrained.pth', 
+            ),
+        )
+       ''').strip("\n")
+
+        with open(cfg_path, 'w') as f:
+            f.write(config_content)
+
+
+        process_obj = subprocess.run(["bash", "CtP/tools/dist_train.sh",\
+        f"{cfg_path}", "4",\
+        f"--work_dir {args.exp_name_finetune}",
+        f"--data_dir {args.data_dir}"])
+
+
+    #-----------------------------------------------------------------------------------------------------------------------
+    # The cfg_path need to be updated with the following updated configuration contents to be able to load the pretrained model.
+    # Instead of executing the blow mentioned code, one can also directly modify the "pretrained" variable by opening the file represented
+    # by the cfg_path_test variable
+    #
+        config_content_test = textwrap.dedent('''\
+        _base_ = ['../../recognizers/_base_/model_r3d18.py',
+        '../../recognizers/_base_/runtime_ucf101.py']
+        work_dir = './output/ctp/r3d_18_ucf101/finetune_ucf101/'
+        model = dict(
+            backbone=dict(
+            pretrained='/finetune/ucf101/epoch_150.pth',
         ),
-    )
-   ''').strip("\n")
+        )
+       ''').strip("\n")
 
-    with open(cfg_path, 'w') as f:
-        f.write(config_content)
+        cfg_path_test= "CtP/configs/ctp/r3d_18_ucf101/finetune_ucf101.py"
+        with open(cfg_path_test, 'w') as f:
+            f.write(config_content_test)
 
-
-    process_obj = subprocess.run(["bash", "FedVSSL/CtP/tools/dist_train.sh",\
-    f"{cfg_path}", "4",\
-    f"--work_dir {args.exp_name_finetune}",
-    f"--data_dir {args.data_dir}"])
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# The cfg_path need to be updated with the following updated configuration contents to be able to load the pretrained model.
-# Instead of executing the blow mentioned code, one can also directly modify the "pretrained" variable by opening the file represented
-# by the cfg_path_test variable
-#
-    config_content_test = textwrap.dedent('''\
-    _base_ = ['../../recognizers/_base_/model_r3d18.py',
-    '../../recognizers/_base_/runtime_ucf101.py']
-    work_dir = './output/ctp/r3d_18_ucf101/finetune_ucf101/'
-    model = dict(
-        backbone=dict(
-        pretrained='/finetune/ucf101/epoch_150.pth',
-    ),
-    )
-   ''').strip("\n")
-
-    cfg_path_test= "FedVSSL/CtP/configs/ctp/r3d_18_ucf101/finetune_ucf101.py"
-    with open(cfg_path_test, 'w') as f:
-        f.write(config_content_test)
-
-    # Evaluating the finetuned model
-    process_obj = subprocess.run(["bash", "FedVSSL/CtP/tools/dist_test.sh",\
-    f"{cfg_path_test}", "4",\
-    f"--work_dir {args.exp_name_finetune}",
-    f"--data_dir {args.data_dir}",\
-    f"--progress"])
+        # Evaluating the finetuned model
+        process_obj = subprocess.run(["bash", "CtP/tools/dist_test.sh",\
+        f"{cfg_path_test}", "4",\
+        f"--work_dir {args.exp_name_finetune}",
+        f"--data_dir {args.data_dir}",\
+        f"--progress"])
 
 
