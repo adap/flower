13d7f0e8e820cbdd0125b0e599fff82a120e11b2
Charles Beauville <charles@flower.dev>
2023-09-29 12:43:41 +0200

    Update examples/federated-diffusion/config.py
    Co-authored-by: Javier <jafermarq@users.noreply.github.com>

--- examples/federated-diffusion/config.py
+++ examples/federated-diffusion/config.py
@@ -7,7 +7,7 @@ class Params:
     num_clients: int = 2
     num_epochs: int = 1
     num_rounds: int = 2
-    device: str = "cpu"
+    server_device: str = "cpu"
     nclass_cifar: int = 2
     nsamples_cifar: int = int(50000 / 20)
     rate_unbalance_cifar: float = 1.0
