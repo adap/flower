f8eb3920f804affc1c0afb35eba28c42aeacb900
apzl <apsalshbk550@gmail.com>
2023-10-01 17:30:51 +0530

    add emnist data preparation

--- baselines/fedpac/fedpac/dataset_preparation.py
+++ baselines/fedpac/fedpac/dataset_preparation.py
@@ -89,14 +89,6 @@ def partition_cifar_data(
     if balance:
         trainset = _balance_classes(trainset, seed)
 
-    # label_counter = {}
-    # for label  in trainset.targets:
-    #     if int(label) not in label_counter.keys():
-    #         label_counter[int(label)]=1
-    #     else:
-    #         label_counter[int(label)] += 1
-    # print(label_counter)
-
 
     partition_size = int(len(trainset) / num_clients) 
     lengths = [partition_size] * num_clients
@@ -120,7 +112,6 @@ def partition_cifar_data(
             label_index[int(i)] = int(i)*num_idxs_per_class
 
         tmp = []
-        # print(label_index)
         for idx in range(num_clients):
             for i in labels:
                 tmp.append(
@@ -146,10 +137,6 @@ def partition_cifar_data(
             for c in range(num_clients)
         ]
 
-        # for i in datasets:
-        #     # d = tmp[i]
-        #     dist = get_label_dist(i)
-        #     print(dist)
     return datasets, testset
 
 
@@ -194,132 +181,13 @@ def _balance_classes(
 
     return shuffled
 
-
-def _sort_by_class(
-    trainset: Dataset,
-) -> Dataset:
-    """Sort dataset by class/label.
-
-    Parameters
-    ----------
-    trainset : Dataset
-        The training dataset that needs to be sorted.
-
-    Returns
-    -------
-    Dataset
-        The sorted training dataset.
-    """
-    class_counts = np.bincount(trainset.targets)
-    idxs = trainset.targets.argsort()  # sort targets in ascending order
-
-    tmp = []  # create subset of smallest class
-    tmp_targets = []  # same for targets
-
-    start = 0
-    for count in np.cumsum(class_counts):
-        tmp.append(
-            Subset(trainset, idxs[start : int(count + start)])
-        )  # add rest of classes
-        tmp_targets.append(trainset.targets[idxs[start : int(count + start)]])
-        start += count
-    sorted_dataset = ConcatDataset(tmp)  # concat dataset
-    sorted_dataset.targets = torch.cat(tmp_targets)  # concat targets
-    return sorted_dataset
-
-
-def _power_law_split(
-    sorted_trainset: Dataset,
-    num_partitions: int,
-    num_labels_per_partition: int = 2,
-    min_data_per_partition: int = 10,
-    mean: float = 0.0,
-    sigma: float = 2.0,
-) -> Dataset:
-    """Partitions the dataset following a power-law distribution. It follows
-    the implementation of Li et al 2020: https://arxiv.org/abs/1812.06127 with
-    default values set accordingly.
-
-    Parameters
-    ----------
-    sorted_trainset : Dataset
-        The training dataset sorted by label/class.
-    num_partitions: int
-        Number of partitions to create
-    num_labels_per_partition: int
-        Number of labels to have in each dataset partition. For
-        example if set to two, this means all training examples in
-        a given partition will be long to the same two classes. default 2
-    min_data_per_partition: int
-        Minimum number of datapoints included in each partition, default 10
-    mean: float
-        Mean value for LogNormal distribution to construct power-law, default 0.0
-    sigma: float
-        Sigma value for LogNormal distribution to construct power-law, default 2.0
-
-    Returns
-    -------
-    Dataset
-        The partitioned training dataset.
-    """
-
-    targets = sorted_trainset.targets
-    full_idx = range(len(targets))
-
-    class_counts = np.bincount(sorted_trainset.targets)
-    labels_cs = np.cumsum(class_counts)
-    labels_cs = [0] + labels_cs[:-1].tolist()
-
-    partitions_idx = []
-    num_classes = len(np.bincount(targets))
-    hist = np.zeros(num_classes, dtype=np.int32)
-
-    # assign min_data_per_partition
-    min_data_per_class = int(min_data_per_partition / num_labels_per_partition)
-    for u_id in range(num_partitions):
-        partitions_idx.append([])
-        for cls_idx in range(num_labels_per_partition):
-            # label for the u_id-th client
-            cls = (u_id + cls_idx) % num_classes
-            # record minimum data
-            indices = list(
-                full_idx[
-                    labels_cs[cls]
-                    + hist[cls] : labels_cs[cls]
-                    + hist[cls]
-                    + min_data_per_class
-                ]
-            )
-            partitions_idx[-1].extend(indices)
-            hist[cls] += min_data_per_class
-
-    # add remaining images following power-law
-    probs = np.random.lognormal(
-        mean,
-        sigma,
-        (num_classes, int(num_partitions / num_classes), num_labels_per_partition),
-    )
-    remaining_per_class = class_counts - hist
-    # obtain how many samples each partition should be assigned for each of the labels it contains
-    probs = (
-        remaining_per_class.reshape(-1, 1, 1)
-        * probs
-        / np.sum(probs, (1, 2), keepdims=True)
-    )
-
-    for u_id in range(num_partitions):
-        for cls_idx in range(num_labels_per_partition):
-            cls = (u_id + cls_idx) % num_classes
-            count = int(probs[cls, u_id // num_classes, cls_idx])
-
-            # add count of specific class to partition
-            indices = full_idx[
-                labels_cs[cls] + hist[cls] : labels_cs[cls] + hist[cls] + count
-            ]
-            partitions_idx[u_id].extend(indices)
-            hist[cls] += count
-
-    # construct subsets
-    partitions = [Subset(sorted_trainset, p) for p in partitions_idx]
-    return partitions
-
+def partition_emnist_data(
+    dataset,
+    num_clients,
+    iid: Optional[bool] = False,
+    balance: Optional[bool] = True,
+    s: Optional[float] = 0.2,
+    seed: Optional[int] = 42,
+) -> Tuple[List[Dataset], Dataset]:
+    
+    pass
