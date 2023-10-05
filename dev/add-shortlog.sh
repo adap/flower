a62573dc3866648df182b4617587bf8fb9d6d5da
Charles Beauville <charles@flower.dev>
2023-10-05 16:09:49 +0200

    Move token

--- dev/add-shortlog.sh
+++ dev/add-shortlog.sh
@@ -6,10 +6,17 @@ tags=$(git tag --sort=-creatordate)
 new_version=$(echo "$tags" | sed -n '1p')
 old_version=$(echo "$tags" | sed -n '2p')
 
-shortlog=$(git shortlog "$old_version".."$new_version" -s | grep -vEi '(\(|\[)bot(\)|\])' | awk '{name = substr($0, index($0, $2)); printf "%s`%s`", sep, name; sep=", "} END {print ""}'
-thanks="\n### Thanks to our contributors\n\nWe would like to give our special thanks to all the contributors who made the new version of Flower possible (in \`git shortlog\` order):\n<!---TOKEN_$new_version-->\n$shortlog"
+shortlog=$(git shortlog "$old_version".."$new_version" -s | grep -vEi '(\(|\[)bot(\)|\])' | awk '{name = substr($0, index($0, $2)); printf "%s`%s`", sep, name; sep=", "} END {print ""}')
 
-if ! grep "TOKEN_$new_version" doc/source/ref-changelog.md; then
-  awk -v version="$new_version" -v text="$thanks" \
-    '{print} $0 ~ "## " version {print text}' doc/source/ref-changelog.md > temp.md && mv temp.md doc/source/ref-changelog.md
+token="<!---TOKEN_$new_version-->"
+thanks="\n### Thanks to our contributors\n\nWe would like to give our special thanks to all the contributors who made the new version of Flower possible (in \`git shortlog\` order):\n$token $shortlog"
+
+# Check if the token exists in the markdown file
+if ! grep -q "$token" doc/source/ref-changelog.md; then
+    # If the token does not exist in the markdown file, append the new content after the version
+    awk -v version="$new_version" -v text="$thanks" \
+        '{print} $0 ~ "## " version {print text}' doc/source/ref-changelog.md > temp.md && mv temp.md doc/source/ref-changelog.md
+else
+    # If the token exists, replace the line containing the token with the new shortlog
+    awk -v token="$token" -v newlog="$token $shortlog" '{ if ($0 ~ token) print newlog; else print $0 }' doc/source/ref-changelog.md > temp.md && mv temp.md doc/source/ref-changelog.md
 fi
