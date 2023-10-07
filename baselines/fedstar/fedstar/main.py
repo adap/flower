76da3b1ca48332836068ba4254186bb7de44eec2
Raj Parekh <rajparekh144@gmail.com>
2023-10-06 23:31:20 -0700

    Comment in main.py file

---
+++ baselines/fedstar/fedstar/main.py
@@ -0,0 +1,6 @@
+"""
+"Instead of using a main.py file, 
+the project's instructions direct you 
+to run the server.py and client.py files separately. 
+Detailed guidance on how to do this can be found in the README.md file."
+"""
