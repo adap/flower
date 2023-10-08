5d9694d227136c59683c0b2045dce40491d1b205
Steve Laskaridis <stevelaskaridis@gmail.com>
2023-10-08 21:35:20 +0100

    Fix redefinition of type

--- baselines/FjORD/fjord/strategy.py
+++ baselines/FjORD/fjord/strategy.py
@@ -16,9 +16,8 @@ from flwr.common import (
 from flwr.server.client_proxy import ClientProxy
 from flwr.server.strategy import FedAvg
 
+from .client import FJORD_CONFIG_TYPE
 from .utils.logger import Logger
-
-FJORD_CONFIG_TYPE = Dict[Union[str, float], List[Union[Dict[str, int], str]]]
 
 
 # Define metric aggregation function
