c8dc8f423b171afe41c9c61bf51006ce84ddf2f6
Adam Narozniak <adam@flower.dev>
2023-10-10 15:37:47 +0200

    Remove separate Flower Datasets docs scripts

--- datasets/dev/build-flwr-datasets-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
-
-echo "Build Flower Datasets docs: Start"
-cd doc
-make docs
-echo "Build Flower Datasets docs: Done"
