0ab07fd059b2a675f8dd643617b8179353a31dea
Aml-Hassan-Abd-El-hamid <aml.hassan.esmil@gmail.com>
2023-10-12 07:18:00 +0300

    updates to log the results from wandb

--- baselines/hfedxgboost/hfedxgboost/main.py
+++ baselines/hfedxgboost/hfedxgboost/main.py
@@ -50,112 +50,113 @@ def main(cfg: DictConfig) -> None:
             writer.create_res_csv("results_centralized.csv")
             writer.write_res("results_centralized.csv", result_train, result_test)
     else:
-        print("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",cfg.clients.n_estimators_client)
-        dataset_name = cfg.dataset.dataset_name
-        task_type = cfg.dataset.task.task_type
-        early_stopper = Early_Stop(cfg)
-        X_train, y_train, X_test, y_test = load_single_dataset(
-            task_type, dataset_name, train_ratio=cfg.dataset.train_ratio
-        )
-        print("Feature dimension of the dataset:", X_train.shape[1])
-        print("Size of the trainset:", X_train.shape[0])
-        print("Size of the testset:", X_test.shape[0])
-        if task_type == "BINARY":
-            print(
-                "First class ratio in train data",
-                y_train[y_train == 0.0].size / X_train.shape[0],
-            )
-            print(
-                "Second class ratio in train data",
-                y_train[y_train != 0.0].size / X_train.shape[0],
-            )
-            print(
-                "First class ratio in test data",
-                y_test[y_test == 0.0].size / X_test.shape[0],
-            )
-            print(
-                "Second class ratio in test data",
-                y_test[y_test != 0.0].size / X_test.shape[0],
-            )
-        trainset = TensorDataset(torch.from_numpy(X_train), torch.from_numpy(y_train))
-        testset = TensorDataset(torch.from_numpy(X_test), torch.from_numpy(y_test))
+        with wandb.init(**cfg.wandb.setup) as run:
+                print("Dataset Name", cfg.dataset.dataset_name)
+                dataset_name = cfg.dataset.dataset_name
+                task_type = cfg.dataset.task.task_type
+                early_stopper = Early_Stop(cfg)
+                X_train, y_train, X_test, y_test = load_single_dataset(
+                    task_type, dataset_name, train_ratio=cfg.dataset.train_ratio
+                )
+                print("Feature dimension of the dataset:", X_train.shape[1])
+                print("Size of the trainset:", X_train.shape[0])
+                print("Size of the testset:", X_test.shape[0])
+                if task_type == "BINARY":
+                    print(
+                        "First class ratio in train data",
+                        y_train[y_train == 0.0].size / X_train.shape[0],
+                    )
+                    print(
+                        "Second class ratio in train data",
+                        y_train[y_train != 0.0].size / X_train.shape[0],
+                    )
+                    print(
+                        "First class ratio in test data",
+                        y_test[y_test == 0.0].size / X_test.shape[0],
+                    )
+                    print(
+                        "Second class ratio in test data",
+                        y_test[y_test != 0.0].size / X_test.shape[0],
+                    )
+                trainset = TensorDataset(torch.from_numpy(X_train), torch.from_numpy(y_train))
+                testset = TensorDataset(torch.from_numpy(X_test), torch.from_numpy(y_test))
 
-        trainloaders, valloaders, testloader = divide_dataset_between_clients(
-            trainset,
-            testset,
-            batch_size=cfg.batch_size,
-            pool_size=cfg.client_num,
-            val_ratio=cfg.val_ratio,
-        )
-        print(
-            f"Data partitioned across {cfg.client_num} clients"
-            f" and {cfg.val_ratio} of local dataset reserved for validation."
-        )
-        # clients_preformance_on_local_data(cfg,trainloaders,X_test,y_test,task_type)
+                trainloaders, valloaders, testloader = divide_dataset_between_clients(
+                    trainset,
+                    testset,
+                    batch_size=cfg.batch_size,
+                    pool_size=cfg.client_num,
+                    val_ratio=cfg.val_ratio,
+                )
+                print(
+                    f"Data partitioned across {cfg.client_num} clients"
+                    f" and {cfg.val_ratio} of local dataset reserved for validation."
+                )
+                # clients_preformance_on_local_data(cfg,trainloaders,X_test,y_test,task_type)
 
-        num_rounds = cfg.run_experiment.num_rounds
-        client_pool_size = cfg.client_num
-        batch_size = cfg.run_experiment.batch_size
-        val_ratio = cfg.val_ratio
+                num_rounds = cfg.run_experiment.num_rounds
+                client_pool_size = cfg.client_num
+                batch_size = cfg.run_experiment.batch_size
+                val_ratio = cfg.val_ratio
 
-        # Configure the strategy
-        def fit_config(server_round: int) -> Dict[str, Scalar]:
-            print(f"Configuring round {server_round}")
-            return {
-                "num_iterations": cfg.run_experiment.fit_config.num_iterations,
-                "batch_size": batch_size,
-            }
+                # Configure the strategy
+                def fit_config(server_round: int) -> Dict[str, Scalar]:
+                    print(f"Configuring round {server_round}")
+                    return {
+                        "num_iterations": cfg.run_experiment.fit_config.num_iterations,
+                        "batch_size": batch_size,
+                    }
 
-        # FedXgbNnAvg
-        strategy = instantiate(
-            cfg.strategy,
-            on_fit_config_fn=fit_config,
-            on_evaluate_config_fn=(lambda r: {"batch_size": batch_size}),
-            evaluate_fn=functools.partial(
-                serverside_eval,
-                cfg=cfg,
-                testloader=testloader,
-                batch_size=batch_size,
-            ),
-        )
+                # FedXgbNnAvg
+                strategy = instantiate(
+                    cfg.strategy,
+                    on_fit_config_fn=fit_config,
+                    on_evaluate_config_fn=(lambda r: {"batch_size": batch_size}),
+                    evaluate_fn=functools.partial(
+                        serverside_eval,
+                        cfg=cfg,
+                        testloader=testloader,
+                        batch_size=batch_size,
+                    ),
+                )
 
-        print(
-            f"FL experiment configured for {num_rounds} rounds with",
-            f"{client_pool_size} client in the pool.",
-        )
+                print(
+                    f"FL experiment configured for {num_rounds} rounds with",
+                    f"{client_pool_size} client in the pool.",
+                )
 
-        def client_fn(cid: str) -> fl.client.Client:
-            """Create a federated learning client."""
-            return FL_Client(
-                cfg,
-                trainloaders[int(cid)],
-                valloaders[int(cid)],
-                client_pool_size,
-                cid,
-                log_progress=False,
-            )
-            
-        # Start the simulation
-        history = fl.simulation.start_simulation(
-            client_fn=client_fn,
-            server=FL_Server(
-                client_manager=SimpleClientManager(),
-                early_stopper=early_stopper,
-                strategy=strategy,
-            ),
-            num_clients=client_pool_size,
-            client_resources=cfg.client_resources,
-            config=ServerConfig(num_rounds=num_rounds),
-            strategy=strategy,
-        )
+                def client_fn(cid: str) -> fl.client.Client:
+                    """Create a federated learning client."""
+                    return FL_Client(
+                        cfg,
+                        trainloaders[int(cid)],
+                        valloaders[int(cid)],
+                        client_pool_size,
+                        cid,
+                        log_progress=False,
+                    )
+                    
+                # Start the simulation
+                history = fl.simulation.start_simulation(
+                    client_fn=client_fn,
+                    server=FL_Server(
+                        client_manager=SimpleClientManager(),
+                        early_stopper=early_stopper,
+                        strategy=strategy,
+                    ),
+                    num_clients=client_pool_size,
+                    client_resources=cfg.client_resources,
+                    config=ServerConfig(num_rounds=num_rounds),
+                    strategy=strategy,
+                )
 
-        # print(type(history.metrics_centralized),history.metrics_centralized)
-        print(history)
-        writer = results_writer(cfg)
-        best_res, best_res_round = writer.extract_best_res(history)
-        print("Best Result", best_res, "best_res_round", best_res_round)
-        writer.create_res_csv("results.csv")
-        writer.write_res("results.csv")
+                # print(type(history.metrics_centralized),history.metrics_centralized)
+                print(history)
+                writer = results_writer(cfg)
+                best_res, best_res_round = writer.extract_best_res(history)
+                print("Best Result", best_res, "best_res_round", best_res_round)
+                writer.create_res_csv("results.csv")
+                writer.write_res("results.csv")
         # return history
 
 if __name__ == "__main__":
