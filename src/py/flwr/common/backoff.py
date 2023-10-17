06c8e5ce4762ecc20edfa69827b19dace7d9776c
Heng Pan <ac.panh99@gmail.com>
2023-10-17 12:16:21 +0100

    add backoff.py

--- src/py/flwr/common/backoff.py
+++ src/py/flwr/common/backoff.py
@@ -12,12 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Functions to enchance other functions with error handling and retries."""
+"""`SafeInvoker` to argument other functions with error handling and retries."""
 
 
-import random
-from typing import Generator, Iterable, Union, Optional, Callable, Any
 import itertools
+import random
+import time
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import Any, Callable, Dict, Generator, Iterable, Optional, Tuple, Union
 
 
 def exponential(
@@ -36,7 +39,7 @@ def exponential(
     max_delay: Optional[float] (default: None)
         The maximum delay duration between two consecutive retries.
     """
-    delay = min(base_delay, max_delay)
+    delay = base_delay if max_delay is None else min(base_delay, max_delay)
     while True:
         yield delay
         delay *= multiplier
@@ -44,7 +47,7 @@ def exponential(
             delay = min(delay, max_delay)
 
 
-def interval(
+def constant(
     interval: Union[float, Iterable[float]] = 1,
 ) -> Generator[float, None, None]:
     """Wait time generator for specified intervals.
@@ -54,14 +57,11 @@ def interval(
     interval: Union[float, Iterable[float]] (default: 1)
         A constant value to yield or an iterable of such values.
     """
-    try:
-        iter(interval)
-    except TypeError:
+    if not isinstance(interval, Iterable):
         interval = itertools.repeat(interval)
-    for wait_time in interval:
-        yield wait_time
-        
-        
+    yield from interval
+
+
 def random_jitter(value: float) -> float:
     """Jitter the value by a random number of milliseconds.
 
@@ -73,7 +73,6 @@ def random_jitter(value: float) -> float:
     value : float
         The original, unjittered backoff value.
     """
-
     return value + random.random()
 
 
@@ -89,72 +88,180 @@ def full_jitter(value: float) -> float:
     value : float
         The original, unjittered backoff value.
     """
-
     return random.uniform(0, value)
 
 
-class SafeInvoker
+@dataclass
+class Details:
+    """Details for event handlers in SafeInvoker."""
 
-
-def safe_invoke(
-    wait_gen: Generator[float, None, None],
-    exception: Union[Exception, Iterable[Exception]],
     func: Callable[[Any], Any]
-    *,
-    max_tries: Optional[int] = None,
-    max_time: Optional[float] = None,
-    jitter: Optional[Callable[[float], float]] = full_jitter,
-    giveup_condition: Callable[[Exception], bool] = lambda e: False,
-    on_success: Optional[Callable[Exception]] = None,
-    on_backoff: Union[_Handler, Iterable[_Handler], None] = None,
-    on_giveup: Union[_Handler, Iterable[_Handler], None] = None,
-    logger: _MaybeLogger = 'backoff',
-    backoff_log_level: int = logging.INFO,
-    giveup_log_level: int = logging.ERROR,
-) -> Callable[[_CallableT], _CallableT]:
-    """Returns decorator for backoff and retry triggered by exception.
+    args: Tuple[Any, ...]
+    kwargs: Dict[str, Any]
+    tries: int
+    elapsed: float
+    exception: Optional[Exception] = None
+    wait: Optional[float] = None
 
-    Args:
-        wait_gen: A generator yielding successive wait times in
-            seconds.
-        exception: An exception type (or tuple of types) which triggers
-            backoff.
-        max_tries: The maximum number of attempts to make before giving
-            up. Once exhausted, the exception will be allowed to escape.
-            The default value of None means there is no limit to the
-            number of tries. If a callable is passed, it will be
-            evaluated at runtime and its return value used.
-        max_time: The maximum total amount of time to try for before
-            giving up. Once expired, the exception will be allowed to
-            escape. If a callable is passed, it will be
-            evaluated at runtime and its return value used.
-        jitter: A function of the value yielded by wait_gen returning
-            the actual time to wait. This distributes wait times
-            stochastically in order to avoid timing collisions across
-            concurrent clients. Wait times are jittered by default
-            using the full_jitter function. Jittering may be disabled
-            altogether by passing jitter=None.
-        giveup: Function accepting an exception instance and
-            returning whether or not to give up. Optional. The default
-            is to always continue.
-        on_success: Callable (or iterable of callables) with a unary
-            signature to be called in the event of success. The
-            parameter is a dict containing details about the invocation.
-        on_backoff: Callable (or iterable of callables) with a unary
-            signature to be called in the event of a backoff. The
-            parameter is a dict containing details about the invocation.
-        on_giveup: Callable (or iterable of callables) with a unary
-            signature to be called in the event that max_tries
-            is exceeded.  The parameter is a dict containing details
-            about the invocation.
-        raise_on_giveup: Boolean indicating whether the registered exceptions
-            should be raised on giveup. Defaults to `True`
-        logger: Name or Logger object to log to. Defaults to 'backoff'.
-        backoff_log_level: log level for the backoff event. Defaults to "INFO"
-        giveup_log_level: log level for the give up event. Defaults to "ERROR"
-        **wait_gen_kwargs: Any additional keyword args specified will be
-            passed to wait_gen when it is initialized.  Any callable
-            args will first be evaluated and their return values passed.
-            This is useful for runtime configuration.
+
+# pylint: disable-next=too-many-instance-attributes
+class SafeInvoker:
+    """Wrapper class for backoff and retry triggered by exceptions.
+
+    Parameters
+    ----------
+    wait: Generator[float, None, None]
+        A generator yielding successive wait times in seconds. If the generator
+        is finite, the giveup event will be triggered when the generator raises
+        `StopIteration`.
+    exception: Union[Exception, Iterable[Exception]]
+        An exception type (or iterable of types) that triggers backoff.
+    max_tries: Optional[int] (default: None)
+        The maximum number of attempts to make before giving up. Once exhausted,
+        the exception will be allowed to escape. If set to None, there is no limit
+        to the number of tries.
+    max_time: Optional[float] (default: None)
+        The maximum total amount of time to try before giving up. Once this time
+        has expired, the exception will be allowed to escape.
+    jitter: Optional[Callable[[float], float]] (default: full_jitter)
+        A function of the value yielded by `wait_gen` returning the actual time
+        to wait. This function helps distribute wait times stochastically to avoid
+        timing collisions across concurrent clients. Wait times are jittered by
+        default using the `full_jitter` function. To disable jittering, pass
+        `jitter=None`.
+    giveup_condition: Optional[Callable[[Exception], bool]] (default: None)
+        A function accepting an exception instance, returning whether or not
+        to give up. If set to None, the strategy is to always continue.
+    on_success: Optional[Callable[[Details], None]] (default: None)
+        A callable to be executed in the event of success. The parameter is a
+        data class object detailing the invocation.
+    on_backoff: Optional[Callable[[Details], None]] (default: None)
+        A callable to be executed in the event of a backoff. The parameter is a
+        data class object detailing the invocation.
+    on_giveup: Optional[Callable[[Details], None]] (default: None)
+        A callable to be executed in the event that `max_tries` or `max_time` is
+        exceeded. The parameter is a data class object detailing the invocation.
     """
-    ...
+
+    def __init__(
+        self,
+        wait: Generator[float, None, None],
+        exception: Union[Exception, Iterable[Exception]],
+        *,
+        max_tries: Optional[int] = None,
+        max_time: Optional[float] = None,
+        jitter: Optional[Callable[[float], float]] = full_jitter,
+        giveup_condition: Optional[Callable[[Exception], bool]] = None,
+        on_success: Optional[Callable[[Details], None]] = None,
+        on_backoff: Optional[Callable[[Details], None]] = None,
+        on_giveup: Optional[Callable[[Details], None]] = None,
+    ) -> None:
+        self.wait = wait
+        self.exception = exception
+        self.max_tries = max_tries
+        self.max_time = max_time
+        self.jitter = jitter
+        self.giveup_condition = giveup_condition
+        self.on_success = on_success
+        self.on_backoff = on_backoff
+        self.on_giveup = on_giveup
+
+    def invoke(
+        self,
+        func: Callable[[Any], Any],
+        *args: Tuple[Any, ...],
+        **kwargs: Dict[str, Any],
+    ) -> Any:
+        """Safely invoke the provided function with retry mechanisms.
+
+        This method attempts to call the given function, and in the event of
+        a specified exception, employs a retry mechanism that considers
+        wait times, jitter, maximum attempts, and maximum time. During the
+        retry process, various callbacks (`on_backoff`, `on_success`, and
+        `on_giveup`) can be triggered based on the outcome.
+
+        Parameters
+        ----------
+        func: Callable[[Any], Any]
+            The function to be invoked.
+        *args: Tuple[Any, ...]
+            Positional arguments to pass to `func`.
+        **kwargs: Dict[str, Any]
+            Keyword arguments to pass to `func`.
+
+        Returns
+        -------
+        Any
+            The result of the function invocation.
+
+        Raises
+        ------
+        Exception
+            If the number of tries exceeds `max_tries`, if the total time
+            exceeds `max_time`, if `wait` generator raises `StopInteration`,
+            or if the `giveup_condition` returns True for a raised exception.
+
+        Notes
+        -----
+        The time between retries is determined by the provided `wait` generator
+        and can optionally be jittered using the `jitter` function. The exact
+        exceptions that trigger a retry, as well as conditions to stop retries,
+        are also determined by the class's initialization parameters.
+        """
+
+        def try_call_event_handler(
+            handler: Optional[Callable[[Details], None]], details: Details
+        ) -> None:
+            if handler is not None:
+                handler(details)
+
+        tries = 0
+        start = datetime.now()
+
+        while True:
+            tries += 1
+            elapsed = timedelta.total_seconds(datetime.now() - start)
+            details = Details(
+                func=func, args=args, kwargs=kwargs, tries=tries, elapsed=elapsed
+            )
+
+            try:
+                ret = func(*args, **kwargs)
+            except self.exception as err:  # type: ignore
+                # Check if giveup event should be triggered
+                max_tries_exceeded = tries == self.max_tries
+                max_time_exceeded = (
+                    self.max_time is not None and elapsed >= self.max_time
+                )
+
+                def giveup_check(_exception: Exception) -> bool:
+                    if self.giveup_condition is None:
+                        return False
+                    return self.giveup_condition(_exception)
+
+                if giveup_check(err) or max_tries_exceeded or max_time_exceeded:
+                    # Trigger giveup event
+                    try_call_event_handler(self.on_giveup, details)
+                    raise
+
+                try:
+                    seconds = next(self.wait)
+                    if self.jitter is not None:
+                        seconds = self.jitter(seconds)
+                    if self.max_time is not None:
+                        seconds = min(seconds, self.max_time - elapsed)
+                    details.wait = seconds
+                except StopIteration:
+                    # Trigger giveup event
+                    try_call_event_handler(self.on_giveup, details)
+                    raise err from None
+
+                # Trigger backoff event
+                try_call_event_handler(self.on_backoff, details)
+
+                # Sleep
+                time.sleep(seconds)
+            else:
+                # Trigger success event
+                try_call_event_handler(self.on_success, details)
+                return ret
