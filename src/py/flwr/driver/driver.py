05454aeb51d4f2526fb432b99cf85e99d87e9f9a
Heng Pan <ac.panh99@gmail.com>
2023-10-25 20:01:43 +0100

    augment Driver constructor

--- src/py/flwr/driver/driver.py
+++ src/py/flwr/driver/driver.py
@@ -17,7 +17,7 @@
 
 from typing import Iterable, List, Optional, Tuple
 
-from flwr.driver.grpc_driver import GrpcDriver
+from flwr.driver.grpc_driver import DEFAULT_SERVER_ADDRESS_DRIVER, GrpcDriver
 from flwr.proto.driver_pb2 import (
     CreateWorkloadRequest,
     GetNodesRequest,
@@ -31,7 +31,13 @@ from flwr.proto.task_pb2 import TaskIns, TaskRes
 class Driver:
     """`Driver` class provides an interface to the Driver API."""
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        driver_service_address: str = DEFAULT_SERVER_ADDRESS_DRIVER,
+        certificates: Optional[bytes] = None,
+    ) -> None:
+        self.addr = driver_service_address
+        self.certificates = certificates
         self.grpc_driver: Optional[GrpcDriver] = None
         self.workload_id: Optional[int] = None
         self.node = Node(node_id=0, anonymous=True)
@@ -40,7 +46,9 @@ class Driver:
         # Check if the GrpcDriver is initialized
         if self.grpc_driver is None or self.workload_id is None:
             # Connect and create workload
-            self.grpc_driver = GrpcDriver()
+            self.grpc_driver = GrpcDriver(
+                driver_service_address=self.addr, certificates=self.certificates
+            )
             self.grpc_driver.connect()
             res = self.grpc_driver.create_workload(CreateWorkloadRequest())
             self.workload_id = res.workload_id
