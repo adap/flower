2dbc18842f6a448cf381f3466a2b4bab9df80754
Charles Beauville <charlesbeauville@gmail.com>
2023-11-06 18:16:11 +0100

    Add pylint-protobuf library

--- dev/test.sh
+++ dev/test.sh
@@ -35,7 +35,7 @@ python -m mypy src/py
 echo "- mypy: done"
 
 echo "- pylint: start"
-python -m pylint --ignore=src/py/flwr/proto src/py/flwr
+python -m pylint --ignore=src/py/flwr/proto --load-plugins=pylint_protobuf src/py/flwr
 echo "- pylint: done"
 
 echo "- flake8: start"
