cbefebf6ecf750097a3697fb49b1f1806f799234
yan-gao-GY <yan@flower.dev>
2023-11-19 18:02:54 +0000

    Change client class name to XgbClient

--- examples/xgboost-comprehensive/client.py
+++ examples/xgboost-comprehensive/client.py
@@ -77,7 +77,7 @@ params = BST_PARAMS
 
 
 # Define Flower client
-class FlowerClient(fl.client.Client):
+class XgbClient(fl.client.Client):
     def __init__(self):
         self.bst = None
         self.config = None
@@ -162,4 +162,4 @@ class FlowerClient(fl.client.Client):
 
 
 # Start Flower client
-fl.client.start_client(server_address="127.0.0.1:8080", client=FlowerClient())
+fl.client.start_client(server_address="127.0.0.1:8080", client=XgbClient())
