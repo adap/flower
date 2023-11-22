0a7202e1c4498f84caaa3b267ebc40bdf3f42429
Charles Beauville <charles@flower.dev>
2023-11-22 17:04:05 +0100

    Fix example docs script

--- dev/update-examples.sh
+++ dev/update-examples.sh
@@ -22,12 +22,12 @@ cd examples/
 for d in $(printf '%s\n' */ | sort -V); do
   example=${d%/}
   # For each example, copy the README into the source of the Example docs
-  [ $example = doc ] || cp $example/README.md $ROOT/examples/doc/source/$example.md 2>&1 >/dev/null
+  [[ $example = doc ]] || cp $example/README.md $ROOT/examples/doc/source/$example.md 2>&1 >/dev/null
   # For each example, copy all images of the _static folder into the examples
   # docs static folder
-  [ $example = doc ] || [ -d "$example/_static" ] && cp $example/_static/**/*.{jpg,png,jpeg} $ROOT/examples/doc/source/_static/ 2>&1 >/dev/null
+  [[ $example = doc ]] || [ -d "$example/_static" ] && cp $example/_static/**/*.{jpg,png,jpeg} $ROOT/examples/doc/source/_static/ 2>&1 >/dev/null
   # For each example, insert the name of the example into the index file
-  [ $example = doc ] || (echo $INSERT_LINE; echo a; echo $example; echo .; echo wq) | ed $INDEX 2>&1 >/dev/null
+  [[ $example = doc ]] || (echo $INSERT_LINE; echo a; echo $example; echo .; echo wq) | ed $INDEX 2>&1 >/dev/null
 done
 
 echo "\`\`\`" >> $INDEX
