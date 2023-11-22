95fe43f964a08361076dc68ae59e6265d58ef0e2
Charles Beauville <charles@flower.dev>
2023-11-22 17:06:36 +0100

    Fix script

--- dev/update-examples.sh
+++ dev/update-examples.sh
@@ -25,7 +25,7 @@ for d in $(printf '%s\n' */ | sort -V); do
   [[ $example != doc ]] && cp $example/README.md $ROOT/examples/doc/source/$example.md 2>&1 >/dev/null
   # For each example, copy all images of the _static folder into the examples
   # docs static folder
-  [[ $example != doc ]] && [ -d "$example/_static" ] && cp $example/_static/**/*.{jpg,png,jpeg} $ROOT/examples/doc/source/_static/ 2>&1 >/dev/null
+  [[ $example != doc ]] && [ -d "$example/_static" ] && cp $example/_static/**.{jpg,png,jpeg} $ROOT/examples/doc/source/_static/ 2>&1 >/dev/null
   # For each example, insert the name of the example into the index file
   [[ $example != doc ]] && (echo $INSERT_LINE; echo a; echo $example; echo .; echo wq) | ed $INDEX 2>&1 >/dev/null
 done
