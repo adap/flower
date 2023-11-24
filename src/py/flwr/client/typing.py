d0cc1f176d219b80cb66399f035cff05a2d20886
Heng Pan <ac.panh99@gmail.com>
2023-11-24 18:58:31 +0000

    fix conflicts

--- src/py/flwr/client/typing.py
+++ src/py/flwr/client/typing.py
@@ -14,29 +14,8 @@
 # ==============================================================================
 """Custom types for Flower clients."""
 
-from dataclasses import dataclass
 from typing import Callable
 
-from flwr.client.workload_state import WorkloadState
-from flwr.proto.task_pb2 import TaskIns, TaskRes
-
 from .client import Client as Client
-
-
-@dataclass
-class Fwd:
-    """."""
-
-    task_ins: TaskIns
-    state: WorkloadState
-
-
-@dataclass
-class Bwd:
-    """."""
-
-    task_res: TaskRes
-    state: WorkloadState
-
 
 ClientFn = Callable[[str], Client]
