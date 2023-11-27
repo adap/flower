1a57a37e6fa094281dd956e1b411abfe2c08b15a
Heng Pan <ac.panh99@gmail.com>
2023-11-27 14:02:33 +0000

    add timeout to flower-client

--- e2e/test_driver.sh
+++ e2e/test_driver.sh
@@ -16,10 +16,10 @@ esac
 timeout 2m flower-server $server_arg &
 sleep 3
 
-flower-client $client_arg --callable client:flower --server 127.0.0.1:9092 &
+timeout 2m flower-client $client_arg --callable client:flower --server 127.0.0.1:9092 &
 sleep 3
 
-flower-client $client_arg --callable client:flower --server 127.0.0.1:9092 &
+timeout 2m flower-client $client_arg --callable client:flower --server 127.0.0.1:9092 &
 sleep 3
 
 timeout 2m python driver.py &
