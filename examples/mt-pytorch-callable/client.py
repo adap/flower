b930bc883f75ba58937cae85b317faba8219fbd1
Charles Beauville <charles@flower.dev>
2023-11-28 16:44:54 +0100

    Add working wandb middleware example

--- examples/mt-pytorch-callable/client.py
+++ examples/mt-pytorch-callable/client.py
@@ -1,3 +1,5 @@
+import datetime
+import time
 import warnings
 from collections import OrderedDict
 
@@ -5,6 +7,9 @@ import flwr as fl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+import wandb
+from flwr.common.serde import client_message_from_proto, server_message_from_proto
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import Compose, Normalize, ToTensor
@@ -108,9 +113,74 @@ def client_fn(cid: str):
     return FlowerClient().to_client()
 
 
+def test_middleware(fwd, app):
+    print("before")
+
+    bwd = app(fwd)
+
+    print("after")
+
+    return bwd
+
+
+def get_middleware(name, id):
+    now = datetime.datetime.now().strftime("%b%d_%H_%M_%S")
+    wandb_group = f"exp_{now}"
+    wandb.init(project=name, group=wandb_group, name=f"client-{id}")
+
+    def wandb_middleware(fwd, app):
+        start_time = None
+        round = ""
+
+        server_message = server_message_from_proto(
+            fwd.task_ins.task.legacy_server_message
+        )
+        if server_message.fit_ins:
+            config = server_message.fit_ins.config
+            if "round" in config:
+                round = f"_rnd-{config['round']}"
+            start_time = time.time()
+        if server_message.evaluate_ins:
+            config = server_message.evaluate_ins.config
+            if "round" in config:
+                round = f"_rnd-{config['round']}"
+
+        bwd = app(fwd)
+
+        results_to_log = {}
+
+        if len(round) > 0:
+            results_to_log["round"] = round
+
+        client_message = client_message_from_proto(
+            bwd.task_res.task.legacy_client_message
+        )
+
+        if client_message.evaluate_res:
+            results_to_log["evaluate_loss"] = client_message.evaluate_res.loss
+            if "accuracy" in client_message.evaluate_res.metrics:
+                results_to_log["accuracy"] = client_message.evaluate_res.metrics[
+                    "accuracy"
+                ]
+
+        if client_message.fit_res:
+            if start_time is not None:
+                results_to_log["fit_time"] = time.time() - start_time
+            if "loss" in client_message.fit_res.metrics:
+                results_to_log["loss"] = client_message.fit_res.metrics["loss"]
+            if "accuracy" in client_message.fit_res.metrics:
+                results_to_log["accuracy"] = client_message.fit_res.metrics["accuracy"]
+
+        wandb.log(results_to_log)
+
+        return bwd
+
+    return wandb_middleware
+
+
 # To run this: `flower-client --callable client:flower`
 flower = fl.flower.Flower(
-    client_fn=client_fn,
+    client_fn=client_fn, middleware=[test_middleware, get_middleware("W&B Test 2", 2)]
 )
 
 
