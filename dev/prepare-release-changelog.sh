e5fea8ce58e4c2a942e19e50bc72791762d66c25
Charles Beauville <charles@flower.dev>
2023-11-29 13:41:05 +0100

    Improve script to prepare changelog for release

--- dev/prepare-release-changelog.sh
+++ dev/prepare-release-changelog.sh
@@ -2,6 +2,9 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
+# Get the current date in the format YYYY-MM-DD
+current_date=$(date +"%Y-%m-%d")
+
 tags=$(git tag --sort=-v:refname)
 new_version=$1
 old_version=$(echo "$tags" | sed -n '1p')
@@ -14,8 +17,8 @@ thanks="\n### Thanks to our contributors\n\nWe would like to give our special th
 # Check if the token exists in the markdown file
 if ! grep -q "$token" doc/source/ref-changelog.md; then
     # If the token does not exist in the markdown file, append the new content after the version
-    awk -v version="$new_version" -v text="$thanks" \
-        '{print} $0 ~ "## " version {print text}' doc/source/ref-changelog.md > temp.md && mv temp.md doc/source/ref-changelog.md
+    awk -v version="$new_version" -v date="$current_date" -v text="$thanks" \
+        '{ if ($0 ~ "## Unreleased") print "## " version " (" date ")\n" text; else print $0 }' doc/source/ref-changelog.md > temp.md && mv temp.md doc/source/ref-changelog.md
 else
     # If the token exists, replace the line containing the token with the new shortlog
     awk -v token="$token" -v newlog="$shortlog $token" '{ if ($0 ~ token) print newlog; else print $0 }' doc/source/ref-changelog.md > temp.md && mv temp.md doc/source/ref-changelog.md
