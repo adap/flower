15428a9d795c63f8b239fdb7740ef0b96c37d348
danielnugraha <daniel.nata@flower.dev>
2023-12-08 09:39:03 +0100

    Remove flwr from class names

--- src/kotlin/flwr/src/main/java/dev/flower/android/Worker.kt
+++ src/kotlin/flwr/src/main/java/dev/flower/android/Worker.kt
@@ -12,47 +12,43 @@ import androidx.work.WorkerParameters
 import java.util.UUID
 import java.util.concurrent.TimeUnit
 
-class FlwrWorker(private val context: Context,
+internal class BackgroundWorker(private val context: Context,
                  workerParams: WorkerParameters,
                  private val client: Client,
                  private val grpcRere: Boolean) : CoroutineWorker(context, workerParams) {
+
     override suspend fun doWork(): Result {
-        if (grpcRere) {
-            inputData.getString("serverAddress")?.let { startFlowerRere(it, false, flwrClient) }
-        } else {
-            inputData.getString("serverAddress")?.let { startClient(it, false, flwrClient) }
-        }
+        inputData.getString("serverAddress")?.let { startClient(it, false, client, grpcRere) }
         val workManager: WorkManager = WorkManager.getInstance(context)
         workManager.cancelWorkById(id)
         return Result.success()
     }
+}
 
-    /**
-     * Start a PeriodicWorker that runs Flower client in the background.
-     *
-     * @param interval The interval for the PeriodicWorker to resume its work periodically in minutes.
-     * @param serverAddress The IPv4 or IPv6 address of the server. If the Flower server runs on the
-     * same machine on port 8080, then server_address would be “[::]:8080”.
-     */
-    fun startFlwrPeriodicWorker(interval: Long, serverAddress: String) {
-        val constraints: Constraints = Constraints.Builder().build()
+/**
+* Start a PeriodicWorker that runs Flower client in the background.
+*
+* @param interval The interval for the PeriodicWorker to resume its work periodically in minutes.
+* @param serverAddress The IPv4 or IPv6 address of the server. If the Flower server runs on the
+* same machine on port 8080, then server_address would be “[::]:8080”.
+*/
+fun startPeriodicWorker(interval: Long, serverAddress: String) {
+    val constraints: Constraints = Constraints.Builder().build()
 
-        val workRequest: PeriodicWorkRequest = PeriodicWorkRequestBuilder<FlwrWorker>(
-             interval, TimeUnit.MINUTES
+    val workRequest: PeriodicWorkRequest = PeriodicWorkRequestBuilder<BackgroundWorker>(
+        interval, TimeUnit.MINUTES
+    )
+        .setInitialDelay(0, TimeUnit.MILLISECONDS)
+        .setInputData(
+            Data.Builder()
+                .putString("serverAddress", serverAddress)
+                .build()
         )
-            .setInitialDelay(0, TimeUnit.MILLISECONDS)
-            .setInputData(
-                Data.Builder()
-                    .putString("serverAddress", serverAddress)
-                    .build()
-            )
-            .setConstraints(constraints)
-            .build()
+        .setConstraints(constraints)
+        .build()
 
-        val uniqueWorkName = "FlwrWorker${UUID.randomUUID()}"
+    val uniqueWorkName = "Worker${UUID.randomUUID()}"
 
-        WorkManager.getInstance(context)
-            .enqueueUniquePeriodicWork(uniqueWorkName, ExistingPeriodicWorkPolicy.KEEP, workRequest)
-    }
-
+    WorkManager.getInstance(context)
+        .enqueueUniquePeriodicWork(uniqueWorkName, ExistingPeriodicWorkPolicy.KEEP, workRequest)
 }
