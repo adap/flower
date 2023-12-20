a24c85827581fe1b98d2400b7891115bcb420fc3
Adam Narozniak <adam@flower.dev>
2023-12-20 12:51:16 +0100

    Strip pycharm metadata in the format.sh script

--- dev/format.sh
+++ dev/format.sh
@@ -18,7 +18,7 @@ python -m docformatter -i -r examples
 
 # Notebooks
 python -m black --ipynb -q doc/source/*.ipynb
-KEYS="metadata.celltoolbar metadata.language_info metadata.toc metadata.notify_time metadata.varInspector metadata.accelerator metadata.vscode cell.metadata.id cell.metadata.heading_collapsed cell.metadata.hidden cell.metadata.code_folding cell.metadata.tags cell.metadata.init_cell cell.metadata.vscode"
+KEYS="metadata.celltoolbar metadata.language_info metadata.toc metadata.notify_time metadata.varInspector metadata.accelerator metadata.vscode cell.metadata.id cell.metadata.heading_collapsed cell.metadata.hidden cell.metadata.code_folding cell.metadata.tags cell.metadata.init_cell cell.metadata.vscode cell.metadata.pycharm"
 python -m nbstripout doc/source/*.ipynb --extra-keys "$KEYS"
 python -m nbstripout examples/*/*.ipynb --extra-keys "$KEYS"
 
