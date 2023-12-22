f6093c12916e74e2f48457d0c18d017f4ac0702f
edogab33 <edo.gab33@gmail.com>
2023-12-22 18:18:14 +0100

    fix mypy errors

--- baselines/flanders/flanders/strategy.py
+++ baselines/flanders/flanders/strategy.py
@@ -1,22 +1,26 @@
 """FLANDERS strategy."""
 
-from logging import INFO
+import typing
+from logging import INFO, WARNING
 from typing import Callable, Dict, List, Optional, Tuple, Union
 
 import numpy as np
 from flwr.common import (
+    EvaluateIns,
+    EvaluateRes,
     FitIns,
     FitRes,
     MetricsAggregationFn,
     NDArrays,
     Parameters,
     Scalar,
+    ndarrays_to_parameters,
     parameters_to_ndarrays,
 )
 from flwr.common.logger import log
 from flwr.server.client_manager import ClientManager
 from flwr.server.client_proxy import ClientProxy
-from flwr.server.strategy.fedavg import FedAvg
+from flwr.server.strategy.aggregate import aggregate, weighted_loss_avg
 
 from .utils import load_all_time_series
 
@@ -28,7 +32,7 @@ than or equal to the values of `min_fit_clients` and `min_evaluate_clients`.
 """
 
 
-class Flanders(FedAvg):
+class Flanders:
     """Aggregation function based on MAR.
 
     Take a look at the paper for more details about the parameters.
@@ -37,7 +41,6 @@ class Flanders(FedAvg):
     # pylint: disable=too-many-arguments,too-many-instance-attributes
     def __init__(
         self,
-        *,
         fraction_fit: float = 1.0,
         fraction_evaluate: float = 1.0,
         min_fit_clients: int = 2,
@@ -115,20 +118,18 @@ class Flanders(FedAvg):
             Distance function used to compute the distance between predicted
             params and real ones, by default None
         """
-        super().__init__(
-            fraction_fit=fraction_fit,
-            fraction_evaluate=fraction_evaluate,
-            min_fit_clients=min_fit_clients,
-            min_evaluate_clients=min_evaluate_clients,
-            min_available_clients=min_available_clients,
-            evaluate_fn=evaluate_fn,
-            on_fit_config_fn=on_fit_config_fn,
-            on_evaluate_config_fn=on_evaluate_config_fn,
-            accept_failures=accept_failures,
-            initial_parameters=initial_parameters,
-            fit_metrics_aggregation_fn=fit_metrics_aggregation_fn,
-            evaluate_metrics_aggregation_fn=evaluate_metrics_aggregation_fn,
-        )
+        self.fraction_fit = fraction_fit
+        self.fraction_evaluate = fraction_evaluate
+        self.min_fit_clients = min_fit_clients
+        self.min_evaluate_clients = min_evaluate_clients
+        self.min_available_clients = min_available_clients
+        self.evaluate_fn = evaluate_fn
+        self.on_fit_config_fn = on_fit_config_fn
+        self.on_evaluate_config_fn = on_evaluate_config_fn
+        self.accept_failures = accept_failures
+        self.initial_parameters = initial_parameters
+        self.fit_metrics_aggregation_fn = fit_metrics_aggregation_fn
+        self.evaluate_metrics_aggregation_fn = evaluate_metrics_aggregation_fn
         self.to_keep = to_keep
         self.window = window
         self.maxiter = maxiter
@@ -138,6 +139,33 @@ class Flanders(FedAvg):
         self.malicious_selected = False
         self.distance_function = distance_function
 
+    def __repr__(self) -> str:
+        """Compute a string representation of the strategy."""
+        rep = f"FLANDERS(accept_failures={self.accept_failures})"
+        return rep
+
+    @typing.no_type_check
+    def num_fit_clients(self, num_available_clients: int) -> Tuple[int, int]:
+        """Return the sample size and the required number of available clients."""
+        num_clients = int(num_available_clients * self.fraction_fit)
+        return max(num_clients, self.min_fit_clients), self.min_available_clients
+
+    @typing.no_type_check
+    def num_evaluation_clients(self, num_available_clients: int) -> Tuple[int, int]:
+        """Use a fraction of available clients for evaluation."""
+        num_clients = int(num_available_clients * self.fraction_evaluate)
+        return max(num_clients, self.min_evaluate_clients), self.min_available_clients
+
+    @typing.no_type_check
+    def initialize_parameters(
+        self, client_manager: ClientManager
+    ) -> Optional[Parameters]:
+        """Initialize global model parameters."""
+        initial_parameters = self.initial_parameters
+        self.initial_parameters = None  # Don't keep initial parameters in memory
+        return initial_parameters
+
+    @typing.no_type_check
     def configure_fit(
         self, server_round: int, parameters: Parameters, client_manager: ClientManager
     ) -> List[Tuple[ClientProxy, FitIns]]:
@@ -165,6 +193,7 @@ class Flanders(FedAvg):
         # Return client/config pairs
         return [(client, fit) for client, fit in zip(clients, fit_ins_list)]
 
+    @typing.no_type_check
     def aggregate_fit(
         self,
         server_round: int,
@@ -226,16 +255,21 @@ class Flanders(FedAvg):
             results = np.array(results)[good_clients_idx].tolist()
             log(INFO, f"Good clients: {good_clients_idx}")
 
-            # Apply FedAvg for the remaining clients
-            log(INFO, "Applying FedAvg for the remaining clients")
-            parameters_aggregated, metrics_aggregated = super().aggregate_fit(
-                server_round, results, failures
-            )
-        else:
-            # Apply FedAvg on every clients' params during the first round
-            parameters_aggregated, metrics_aggregated = super().aggregate_fit(
-                server_round, results, failures
-            )
+        log(INFO, "Applying FedAvg")
+        # Convert results
+        weights_results = [
+            (parameters_to_ndarrays(fit_res.parameters), fit_res.num_examples)
+            for _, fit_res in results
+        ]
+        parameters_aggregated = ndarrays_to_parameters(aggregate(weights_results))
+
+        # Aggregate custom metrics if aggregation fn was provided
+        metrics_aggregated = {}
+        if self.fit_metrics_aggregation_fn:
+            fit_metrics = [(res.num_examples, res.metrics) for _, res in results]
+            metrics_aggregated = self.fit_metrics_aggregation_f(fit_metrics)
+        elif server_round == 1:  # Only log this warning once
+            log(WARNING, "No fit_metrics_aggregation_fn provided")
 
         return (
             parameters_aggregated,
@@ -243,6 +277,33 @@ class Flanders(FedAvg):
             good_clients_idx,
             malicious_clients_idx,
         )
+
+    @typing.no_type_check
+    def configure_evaluate(
+        self, server_round: int, parameters: Parameters, client_manager: ClientManager
+    ) -> List[Tuple[ClientProxy, EvaluateIns]]:
+        """Configure the next round of evaluation."""
+        # Do not configure federated evaluation if fraction eval is 0.
+        if self.fraction_evaluate == 0.0:
+            return []
+
+        # Parameters and config
+        config = {}
+        if self.on_evaluate_config_fn is not None:
+            # Custom evaluation config function provided
+            config = self.on_evaluate_config_fn(server_round)
+        evaluate_ins = EvaluateIns(parameters, config)
+
+        # Sample clients
+        sample_size, min_num_clients = self.num_evaluation_clients(
+            client_manager.num_available()
+        )
+        clients = client_manager.sample(
+            num_clients=sample_size, min_num_clients=min_num_clients
+        )
+
+        # Return client/config pairs
+        return [(client, evaluate_ins) for client in clients]
 
     def evaluate(
         self,
@@ -261,11 +322,42 @@ class Flanders(FedAvg):
 
         eval_res = self.evaluate_fn(
             server_round, parameters_to_ndarrays(parameters), config, output_dir
-        )
+        )  # type: ignore [call-arg]
         if eval_res is None:
             return None
         loss, metrics = eval_res
         return loss, metrics
+
+    def aggregate_evaluate(
+        self,
+        server_round: int,
+        results: List[Tuple[ClientProxy, EvaluateRes]],
+        failures: List[Union[Tuple[ClientProxy, EvaluateRes], BaseException]],
+    ) -> Tuple[Optional[float], Dict[str, Scalar]]:
+        """Aggregate evaluation losses using weighted average."""
+        if not results:
+            return None, {}
+        # Do not aggregate if there are failures and failures are not accepted
+        if not self.accept_failures and failures:
+            return None, {}
+
+        # Aggregate loss
+        loss_aggregated = weighted_loss_avg(
+            [
+                (evaluate_res.num_examples, evaluate_res.loss)
+                for _, evaluate_res in results
+            ]
+        )
+
+        # Aggregate custom metrics if aggregation fn was provided
+        metrics_aggregated = {}
+        if self.evaluate_metrics_aggregation_fn:
+            eval_metrics = [(res.num_examples, res.metrics) for _, res in results]
+            metrics_aggregated = self.evaluate_metrics_aggregation_fn(eval_metrics)
+        elif server_round == 1:  # Only log this warning once
+            log(WARNING, "No evaluate_metrics_aggregation_fn provided")
+
+        return loss_aggregated, metrics_aggregated
 
 
 def mar(X, pred_step, alpha=1, beta=1, maxiter=100, window=0):
