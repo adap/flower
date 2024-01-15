a354966a6a7bac6f15eb9420aa5799f2a7e60ce3
Mohammad Naseri <mohammad@flower.dev>
2024-01-15 15:27:59 +0000

    Fix error

--- src/py/flwr/server/strategy/__init__.py
+++ src/py/flwr/server/strategy/__init__.py
@@ -16,7 +16,7 @@
 
 
 from .bulyan import Bulyan as Bulyan
-from .dp_strategy_wrapper import DPStrategyWrapperFixedClipping
+from .dp_strategy_wrapper_fixed_clipping import DPStrategyWrapperFixedClipping
 from .dpfedavg_adaptive import DPFedAvgAdaptive as DPFedAvgAdaptive
 from .dpfedavg_fixed import DPFedAvgFixed as DPFedAvgFixed
 from .fault_tolerant_fedavg import FaultTolerantFedAvg as FaultTolerantFedAvg
