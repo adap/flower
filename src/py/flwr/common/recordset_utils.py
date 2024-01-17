c35f590fcdcd6ef9f73ffd88260acd87f23ae740
Daniel J. Beutel <daniel@flower.dev>
2024-01-17 18:24:13 +0100

    Update src/py/flwr/common/recordset_utils.py

--- src/py/flwr/common/recordset_utils.py
+++ src/py/flwr/common/recordset_utils.py
@@ -14,6 +14,7 @@
 # ==============================================================================
 """RecordSet utilities."""
 
+
 from .parametersrecord import Array, ParametersRecord
 from .typing import Parameters
 
