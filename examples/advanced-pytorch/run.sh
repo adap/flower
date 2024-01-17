7479965a3eacc90e13469c941693488bf76c1e20
Adam Narozniak <adam@flower.dev>
2024-01-17 17:48:24 +0100

    Migrate advanced-pytorch example to use FDS

--- examples/advanced-pytorch/run.sh
+++ examples/advanced-pytorch/run.sh
@@ -2,15 +2,12 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 
-# Download the CIFAR-10 dataset
-python -c "from torchvision.datasets import CIFAR10; CIFAR10('./dataset', download=True)"
-
 # Download the EfficientNetB0 model
 python -c "import torch; torch.hub.load( \
         'NVIDIA/DeepLearningExamples:torchhub', \
         'nvidia_efficientnet_b0', pretrained=True)"
 
-python server.py &
+python server.py --toy True&
 sleep 3  # Sleep for 3s to give the server enough time to start
 
 for i in `seq 0 9`; do
