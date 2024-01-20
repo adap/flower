f5fb3f7cbb9f46316567df2a049f0db83b630bc2
jafermarq <javier@flower.dev>
2024-01-20 11:47:07 +0000

    w/ previous

--- examples/advanced-pytorch/server.py
+++ examples/advanced-pytorch/server.py
@@ -100,7 +100,7 @@ def main():
 
     # Create strategy
     strategy = fl.server.strategy.FedAvg(
-        fraction_fit=0.2,
+        fraction_fit=1.0,
         fraction_evaluate=1.0,
         min_fit_clients=2,
         min_evaluate_clients=2,
