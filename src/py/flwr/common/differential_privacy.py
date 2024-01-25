bade827c7682aa4aae8fefebd4ebc57e1d433515
Mohammad Naseri <mohammad@flower.dev>
2024-01-25 23:01:07 +0000

    Fix test error

--- src/py/flwr/common/differential_privacy.py
+++ src/py/flwr/common/differential_privacy.py
@@ -17,29 +17,29 @@ import numpy as np
 from flwr.common.typing import NDArrays
 
 
-def get_norm(input: NDArrays) -> float:
+def get_norm(input_array: NDArrays) -> float:
     """Compute the L2 norm of the flattened input."""
     flattened_input = np.concatenate(
-        [np.asarray(sub_input).flatten() for sub_input in input]
+        [np.asarray(sub_input).flatten() for sub_input in input_array]
     )
     return float(np.linalg.norm(flattened_input))
 
 
-def add_gaussian_noise(input: NDArrays, std_dev: float) -> NDArrays:
+def add_gaussian_noise(input_array: NDArrays, std_dev: float) -> NDArrays:
     """Add noise to each element of the provided input from Gaussian (Normal)
     distribution with respect to the passed standard deviation."""
     noised_input = [
-        layer + np.random.normal(0, std_dev**2, layer.shape) for layer in input
+        layer + np.random.normal(0, std_dev**2, layer.shape) for layer in input_array
     ]
     return noised_input
 
 
-def clip_inputs(input: NDArrays, clipping_norm: float) -> NDArrays:
+def clip_inputs(input_array: NDArrays, clipping_norm: float) -> NDArrays:
     """Clip model update based on the clipping norm.
 
     FlatClip method of the paper: https://arxiv.org/pdf/1710.06963.pdf
     """
-    input_norm = get_norm(input)
+    input_norm = get_norm(input_array)
     scaling_factor = min(1, clipping_norm / input_norm)
-    clipped_inputs: NDArrays = [layer * scaling_factor for layer in input]
+    clipped_inputs: NDArrays = [layer * scaling_factor for layer in input_array]
     return clipped_inputs
