b02f263769655aec0e919f4683865a12bb1e7851
Javier <jafermarq@users.noreply.github.com>
2024-01-25 10:37:41 +0000

    Make examples use `start_client()`. (#2718)

--- examples/quickstart-huggingface/client.py
+++ examples/quickstart-huggingface/client.py
@@ -108,7 +108,7 @@ def main(node_id):
             return float(loss), len(testloader), {"accuracy": float(accuracy)}
 
     # Start client
-    fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=IMDBClient())
+    fl.client.start_client(server_address="127.0.0.1:8080", client=IMDBClient().to_client())
 
 
 if __name__ == "__main__":
