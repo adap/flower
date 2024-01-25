55cc1bfa11e4046424058468bbf7dc5674b9237b
Raj Parekh <rajparekh144@gmail.com>
2024-01-24 16:06:18 -0800

    Added comments with tf repeat functionality

--- baselines/fedstar/fedstar/models.py
+++ baselines/fedstar/fedstar/models.py
@@ -306,6 +306,7 @@ class PslNetwork(tf.keras.Model):  # pylint: disable=abstract-method
                 tf.print(f"Epoch {epoch+1}/{epochs}")
             step = 0
             # Run train step on each batch
+            # tf.data.Dataset.repeat can also be used instead of itertools.cycle
             for step, ((l_batch_x, l_batch_y), (u_batch_x, u_batch_y)) in enumerate(
                 zip(itertools.cycle(l_dataset), u_dataset)
             ):
