89c735a9b086b1cb3f5ebca99eeedcb539e38dfb
Javier <jafermarq@users.noreply.github.com>
2024-01-29 10:54:09 +0000

    Replace `Fwd/Bwd` with `Message/Context` (#2842)
    Co-authored-by: jafermarq <javier@flower.dev>
    Co-authored-by: Heng Pan <pan@flower.dev>
    Co-authored-by: Daniel J. Beutel <daniel@flower.dev>

--- src/py/flwr/client/grpc_rere_client/connection.py
+++ src/py/flwr/client/grpc_rere_client/connection.py
@@ -148,9 +148,7 @@ def grpc_request_response(
         task_ins: Optional[TaskIns] = get_task_ins(response)
 
         # Discard the current TaskIns if not valid
-        if task_ins is not None and not validate_task_ins(
-            task_ins, discard_reconnect_ins=True
-        ):
+        if task_ins is not None and not validate_task_ins(task_ins):
             task_ins = None
 
         # Remember `task_ins` until `task_res` is available
--- src/py/flwr/client/middleware/utils_test.py
+++ src/py/flwr/client/middleware/utils_test.py
@@ -18,24 +18,41 @@
 import unittest
 from typing import List
 
-from flwr.client.typing import Bwd, FlowerCallable, Fwd, Layer
+from flwr.client.typing import FlowerCallable, Layer
+from flwr.common.configsrecord import ConfigsRecord
 from flwr.common.context import Context
+from flwr.common.message import Message, Metadata
+from flwr.common.metricsrecord import MetricsRecord
 from flwr.common.recordset import RecordSet
-from flwr.proto.task_pb2 import TaskIns, TaskRes  # pylint: disable=E0611
 
 from .utils import make_ffn
+
+METRIC = "context"
+COUNTER = "counter"
+
+
+def _increment_context_counter(context: Context) -> None:
+    # Read from context
+    current_counter: int = context.state.get_metrics(METRIC)[COUNTER]  # type: ignore
+    # update and override context
+    current_counter += 1
+    context.state.set_metrics(METRIC, record=MetricsRecord({COUNTER: current_counter}))
 
 
 def make_mock_middleware(name: str, footprint: List[str]) -> Layer:
     """Make a mock middleware layer."""
 
-    def middleware(fwd: Fwd, app: FlowerCallable) -> Bwd:
+    def middleware(message: Message, context: Context, app: FlowerCallable) -> Message:
         footprint.append(name)
-        fwd.task_ins.task_id += f"{name}"
-        bwd = app(fwd)
+        # add empty ConfigRecord to in_message for this middleware layer
+        message.message.set_configs(name=name, record=ConfigsRecord())
+        _increment_context_counter(context)
+        out_message: Message = app(message, context)
         footprint.append(name)
-        bwd.task_res.task_id += f"{name}"
-        return bwd
+        _increment_context_counter(context)
+        # add empty ConfigRegcord to out_message for this middleware layer
+        out_message.message.set_configs(name=name, record=ConfigsRecord())
+        return out_message
 
     return middleware
 
@@ -43,12 +60,22 @@ def make_mock_middleware(name: str, footprint: List[str]) -> Layer:
 def make_mock_app(name: str, footprint: List[str]) -> FlowerCallable:
     """Make a mock app."""
 
-    def app(fwd: Fwd) -> Bwd:
+    def app(message: Message, context: Context) -> Message:
         footprint.append(name)
-        fwd.task_ins.task_id += f"{name}"
-        return Bwd(task_res=TaskRes(task_id=name), context=Context(state=RecordSet()))
+        message.message.set_configs(name=name, record=ConfigsRecord())
+        out_message = Message(metadata=message.metadata, message=RecordSet())
+        out_message.message.set_configs(name=name, record=ConfigsRecord())
+        print(context)
+        return out_message
 
     return app
+
+
+def _get_dummy_flower_message() -> Message:
+    return Message(
+        message=RecordSet(),
+        metadata=Metadata(run_id=0, task_id="", group_id="", ttl="", task_type="mock"),
+    )
 
 
 class TestMakeApp(unittest.TestCase):
@@ -63,42 +90,54 @@ class TestMakeApp(unittest.TestCase):
         mock_middleware_layers = [
             make_mock_middleware(name, footprint) for name in mock_middleware_names
         ]
-        task_ins = TaskIns()
+
+        state = RecordSet()
+        state.set_metrics(METRIC, record=MetricsRecord({COUNTER: 0.0}))
+        context = Context(state=state)
+        message = _get_dummy_flower_message()
 
         # Execute
         wrapped_app = make_ffn(mock_app, mock_middleware_layers)
-        dummy_context = Context(state=RecordSet())
-        task_res = wrapped_app(Fwd(task_ins=task_ins, context=dummy_context)).task_res
+        out_message = wrapped_app(message, context)
 
         # Assert
         trace = mock_middleware_names + ["app"]
         self.assertEqual(footprint, trace + list(reversed(mock_middleware_names)))
         # pylint: disable-next=no-member
-        self.assertEqual(task_ins.task_id, "".join(trace))
-        self.assertEqual(task_res.task_id, "".join(reversed(trace)))
+        self.assertEqual("".join(message.message.configs.keys()), "".join(trace))
+        self.assertEqual(
+            "".join(out_message.message.configs.keys()), "".join(reversed(trace))
+        )
+        self.assertEqual(
+            state.get_metrics(METRIC)[COUNTER], 2 * len(mock_middleware_layers)
+        )
 
     def test_filter(self) -> None:
         """Test if a middleware can filter incoming TaskIns."""
         # Prepare
         footprint: List[str] = []
         mock_app = make_mock_app("app", footprint)
-        task_ins = TaskIns()
+        context = Context(state=RecordSet())
+        message = _get_dummy_flower_message()
 
-        def filter_layer(fwd: Fwd, _: FlowerCallable) -> Bwd:
+        def filter_layer(
+            message: Message,
+            _1: Context,
+            _2: FlowerCallable,
+        ) -> Message:
             footprint.append("filter")
-            fwd.task_ins.task_id += "filter"
+            message.message.set_configs(name="filter", record=ConfigsRecord())
+            out_message = Message(metadata=message.metadata, message=RecordSet())
+            out_message.message.set_configs(name="filter", record=ConfigsRecord())
             # Skip calling app
-            return Bwd(
-                task_res=TaskRes(task_id="filter"), context=Context(state=RecordSet())
-            )
+            return out_message
 
         # Execute
         wrapped_app = make_ffn(mock_app, [filter_layer])
-        dummy_context = Context(state=RecordSet())
-        task_res = wrapped_app(Fwd(task_ins=task_ins, context=dummy_context)).task_res
+        out_message = wrapped_app(message, context)
 
         # Assert
         self.assertEqual(footprint, ["filter"])
         # pylint: disable-next=no-member
-        self.assertEqual(task_ins.task_id, "filter")
-        self.assertEqual(task_res.task_id, "filter")
+        self.assertEqual(list(message.message.configs.keys())[0], "filter")
+        self.assertEqual(list(out_message.message.configs.keys())[0], "filter")
