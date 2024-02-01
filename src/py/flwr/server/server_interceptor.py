580bc141f92fc7121e18d815ff6756ed8f53a37e
Daniel Nata Nugraha <daniel.nata@flower.dev>
2024-02-01 19:57:10 +0100

    Add auth logic for server interceptor

--- src/py/flwr/server/server_interceptor.py
+++ src/py/flwr/server/server_interceptor.py
@@ -15,25 +15,49 @@
 """Flower server interceptor."""
 
 import grpc
+from common.secure_aggregation.crypto.symmetric_encryption import generate_shared_key
+from flwr.proto.fleet_pb2 import (
+    CreateNodeRequest,
+    CreateNodeResponse,
+)
+from flwr.server.fleet.message_handler import message_handler
+from flwr.server.state import StateFactory, State
 
-def _unary_unary_rpc_terminator(code, details):
+def _unary_unary_rpc_terminator():
 
-    def terminate(ignored_request, context):
-        context.abort(code, details)
+    def terminate(_, context):
+        context.abort(grpc.StatusCode.UNAUTHENTICATED, "Access denied!")
 
     return grpc.unary_unary_rpc_method_handler(terminate)
+
+def _create_node_with_public_key(state: State, server_public_key: bytes):
+
+    def send_public_key(request: CreateNodeRequest, context: grpc.ServicerContext) -> CreateNodeResponse:
+        context.set_trailing_metadata(
+            (
+                ("public-key", server_public_key),
+            )
+        )
+        return message_handler.create_node(request, state)
+
+    return grpc.unary_unary_rpc_method_handler(send_public_key)
+
+def _handle_authentication(public_key, private_key):
+    generate_shared_key(public_key, private_key)
 
 
 class AuthenticateClientInterceptor(grpc.ServerInterceptor):
 
-    def __init__(self, header, value, code, details):
-        self._header = header
-        self._value = value
-        self._terminator = _unary_unary_rpc_terminator(code, details)
+    def __init__(self, state_factory: StateFactory):
+        self._public_key_header = "public-key"
+        self._auth_token_header = "auth-token"
+        self._state_factory = state_factory
+        self._terminator = _unary_unary_rpc_terminator()
+        self._create_node_handler = _create_node_with_public_key()
 
-    def intercept_service(self, continuation, handler_call_details):
-        if (self._header,
-                self._value) in handler_call_details.invocation_metadata:
+    def intercept_service(self, continuation, handler_call_details: grpc.HandlerCallDetails):
+        if (self._header, self._value) in handler_call_details.invocation_metadata:
+            grpc.unary_unary_rpc_method_handler
             return continuation(handler_call_details)
         else:
             return self._terminator
