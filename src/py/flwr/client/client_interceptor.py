533a3aeebbdc9d5019ed4b94c3cb68eea3b7e9ff
Daniel Nata Nugraha <daniel.nata@flower.dev>
2024-02-08 10:23:24 +0100

    Add new ClientCallDetails for client interceptor

--- src/py/flwr/client/client_interceptor.py
+++ src/py/flwr/client/client_interceptor.py
@@ -14,3 +14,47 @@
 # ==============================================================================
 """Flower client interceptor."""
 
+import grpc
+import collections
+from typing import Callable, Union
+from flwr.proto.fleet_pb2 import (
+    CreateNodeRequest,
+    DeleteNodeRequest,
+    PullTaskInsRequest,
+    PushTaskResRequest,
+)
+
+Request = Union[CreateNodeRequest, DeleteNodeRequest, PullTaskInsRequest, PushTaskResRequest]
+
+class _ClientCallDetails(
+        collections.namedtuple(
+            '_ClientCallDetails',
+            ('method', 'timeout', 'metadata', 'credentials')),
+        grpc.ClientCallDetails):
+    pass
+
+class AuthenticateClientInterceptor(grpc.UnaryUnaryClientInterceptor):
+    def __init__(self, private_key: ec.EllipticCurvePrivateKey, public_key: ec.EllipticCurvePublicKey):
+        self.private_key = private_key
+        
+    def intercept_unary_unary(self, continuation: Callable, client_call_details: grpc.ClientCallDetails, request: Request):
+        """Flower client interceptor."""
+        metadata = []
+        if client_call_details.metadata is not None:
+            metadata = list(client_call_details.metadata)
+
+        if isinstance(request, CreateNodeRequest):
+            pass
+        elif isinstance(request, (DeleteNodeRequest, PullTaskInsRequest, PushTaskResRequest)):
+            pass
+        else:
+            pass
+
+        metadata.append(())
+        client_call_details = _ClientCallDetails(
+            client_call_details.method, client_call_details.timeout, metadata,
+            client_call_details.credentials)
+        
+        response = continuation(client_call_details, request)
+        return response
+            
