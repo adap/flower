8f04e25ca1977ee437776e7fe2ef41eb714c036e
Daniel Nata Nugraha <daniel.nata@flower.dev>
2024-02-10 14:21:32 +0100

    Fix line too long

--- src/py/flwr/server/state/authentication/in_memory_auth_state.py
+++ src/py/flwr/server/state/authentication/in_memory_auth_state.py
@@ -21,6 +21,7 @@ from in_memory_state import InMemoryState
 
 
 class InMemoryAuthState(AuthenticationState, InMemoryState):
+    """In-memory-based authentication state implementation."""
     def __init__(self) -> None:
         """Init InMemoryAuthState."""
         super().__init__()
