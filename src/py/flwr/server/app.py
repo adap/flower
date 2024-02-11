8d0438ba236002fecc0538f1db0f7ad91f3ca626
Daniel J. Beutel <daniel@flower.dev>
2024-02-11 21:39:05 +0100

    Add agrparse to flower-server-app (#2927)

--- src/py/flwr/server/app.py
+++ src/py/flwr/server/app.py
@@ -20,7 +20,7 @@ import importlib.util
 import sys
 import threading
 from dataclasses import dataclass
-from logging import ERROR, INFO, WARN
+from logging import DEBUG, ERROR, INFO, WARN
 from os.path import isfile
 from pathlib import Path
 from signal import SIGINT, SIGTERM, signal
@@ -78,7 +78,54 @@ class ServerConfig:
 def run_server_app() -> None:
     """Run Flower server app."""
     event(EventType.RUN_SERVER_APP_ENTER)
+
+    args = _parse_args_run_server_app().parse_args()
+
+    # Obtain certificates
+    if args.insecure:
+        if args.root_certificates is not None:
+            sys.exit(
+                "Conflicting options: The '--insecure' flag disables HTTPS, "
+                "but '--root-certificates' was also specified. Please remove "
+                "the '--root-certificates' option when running in insecure mode, "
+                "or omit '--insecure' to use HTTPS."
+            )
+        log(
+            WARN,
+            "Option `--insecure` was set. "
+            "Starting insecure HTTP client connected to %s.",
+            args.server,
+        )
+        root_certificates = None
+    else:
+        # Load the certificates if provided, or load the system certificates
+        cert_path = args.root_certificates
+        if cert_path is None:
+            root_certificates = None
+        else:
+            root_certificates = Path(cert_path).read_bytes()
+        log(
+            DEBUG,
+            "Starting secure HTTPS client connected to %s "
+            "with the following certificates: %s.",
+            args.server,
+            cert_path,
+        )
+
+    log(
+        DEBUG,
+        "Flower will load ServerApp `%s`",
+        getattr(args, "server-app"),
+    )
+
+    log(
+        DEBUG,
+        "root_certificates: `%s`",
+        root_certificates,
+    )
+
     log(WARN, "Not implemented: run_server_app")
+
     event(EventType.RUN_SERVER_APP_LEAVE)
 
 
@@ -767,3 +814,42 @@ def _add_args_fleet_api(parser: argparse.ArgumentParser) -> None:
         type=int,
         default=1,
     )
+
+
+def _parse_args_run_server_app() -> argparse.ArgumentParser:
+    """Parse flower-server-app command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Start a Flower server app",
+    )
+
+    parser.add_argument(
+        "server-app",
+        help="For example: `server:app` or `project.package.module:wrapper.app`",
+    )
+    parser.add_argument(
+        "--insecure",
+        action="store_true",
+        help="Run the server app without HTTPS. By default, the app runs with "
+        "HTTPS enabled. Use this flag only if you understand the risks.",
+    )
+    parser.add_argument(
+        "--root-certificates",
+        metavar="ROOT_CERT",
+        type=str,
+        help="Specifies the path to the PEM-encoded root certificate file for "
+        "establishing secure HTTPS connections.",
+    )
+    parser.add_argument(
+        "--server",
+        default="0.0.0.0:9092",
+        help="Server address",
+    )
+    parser.add_argument(
+        "--dir",
+        default="",
+        help="Add specified directory to the PYTHONPATH and load Flower "
+        "app from there."
+        " Default: current working directory.",
+    )
+
+    return parser
