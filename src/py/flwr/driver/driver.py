767c8f2d7612d26301185670ff34076cf89c9cdd
Heng Pan <ac.panh99@gmail.com>
2024-02-13 15:22:05 +0000

    update Driver class

--- src/py/flwr/driver/driver.py
+++ src/py/flwr/driver/driver.py
@@ -13,10 +13,11 @@
 # limitations under the License.
 # ==============================================================================
 """Flower driver service client."""
-
-
+import time
 from typing import Iterable, List, Optional, Tuple
 
+from flwr.common.message import Message
+from flwr.common.serde import message_from_taskres, message_to_taskins
 from flwr.driver.grpc_driver import DEFAULT_SERVER_ADDRESS_DRIVER, GrpcDriver
 from flwr.proto.driver_pb2 import (  # pylint: disable=E0611
     CreateRunRequest,
@@ -25,7 +26,7 @@ from flwr.proto.driver_pb2 import (  # pylint: disable=E0611
     PushTaskInsRequest,
 )
 from flwr.proto.node_pb2 import Node  # pylint: disable=E0611
-from flwr.proto.task_pb2 import TaskIns, TaskRes  # pylint: disable=E0611
+from flwr.proto.task_pb2 import TaskIns  # pylint: disable=E0611
 
 
 class Driver:
@@ -40,7 +41,6 @@ class Driver:
         Tuple containing root certificate, server certificate, and private key
         to start a secure SSL-enabled server. The tuple is expected to have
         three bytes elements in the following order:
-
             * CA certificate.
             * server certificate.
             * server private key.
@@ -68,44 +68,134 @@ class Driver:
             self.grpc_driver.connect()
             res = self.grpc_driver.create_run(CreateRunRequest())
             self.run_id = res.run_id
-
         return self.grpc_driver, self.run_id
 
-    def get_nodes(self) -> List[Node]:
+    def get_node_ids(self) -> List[int]:
         """Get node IDs."""
         grpc_driver, run_id = self._get_grpc_driver_and_run_id()
-
         # Call GrpcDriver method
         res = grpc_driver.get_nodes(GetNodesRequest(run_id=run_id))
-        return list(res.nodes)
+        return [node.node_id for node in res.nodes]
 
-    def push_task_ins(self, task_ins_list: List[TaskIns]) -> List[str]:
-        """Schedule tasks."""
+    def push_messages(self, messages: Iterable[Message]) -> List[str]:
+        """Push messages to specified node IDs.
+
+        This method takes an iterable of messages and sends each message
+        to the node it specifies in `node_id`.
+
+        Parameters
+        ----------
+        messages : Iterable[Message]
+            An iterable of messages to be sent.
+
+        Returns
+        -------
+        message_ids : List[str]
+            A list of IDs for the messages that were sent, which can be used
+            to pull responses.
+        """
         grpc_driver, run_id = self._get_grpc_driver_and_run_id()
-
-        # Set run_id
-        for task_ins in task_ins_list:
-            task_ins.run_id = run_id
-
+        # Construct TaskIns
+        task_ins_list: List[TaskIns] = []
+        for msg in messages:
+            # Check node_id
+            if msg.metadata.node_id == 0:
+                raise ValueError("Message has no node_id.")
+            # Convert Message to TaskIns
+            taskins = message_to_taskins(msg)
+            # Set producer
+            taskins.task.producer.node_id = self.node.node_id
+            taskins.task.producer.anonymous = self.node.anonymous
+            # Set run_id
+            taskins.run_id = run_id
+            # Add to list
+            task_ins_list.append(taskins)
         # Call GrpcDriver method
         res = grpc_driver.push_task_ins(PushTaskInsRequest(task_ins_list=task_ins_list))
         return list(res.task_ids)
 
-    def pull_task_res(self, task_ids: Iterable[str]) -> List[TaskRes]:
-        """Get task results."""
-        grpc_driver, _ = self._get_grpc_driver_and_run_id()
+    def pull_messages(self, message_ids: Iterable[str]) -> Iterable[Message]:
+        """Pull messages based on message IDs.
 
-        # Call GrpcDriver method
+        This method is used to collect responses or messages from the network
+        that correspond to a set of given task identifiers.
+
+        Parameters
+        ----------
+        message_ids : Iterable[str]
+            An iterable of task identifiers for which messages are to be retrieved.
+
+        Returns
+        -------
+        Iterable[Tuple[Message, int]]
+            An iterable of tuples, each containing a `Message` object and the ID of
+            the node from which the message was received.
+        """
+        grpc_driver, _ = self._get_grpc_driver_and_run_id()
+        # Pull TaskRes
         res = grpc_driver.pull_task_res(
-            PullTaskResRequest(node=self.node, task_ids=task_ids)
+            PullTaskResRequest(node=self.node, task_ids=message_ids)
         )
-        return list(res.task_res_list)
+        # Convert TaskRes to Message
+        msgs = [
+            (message_from_taskres(taskres), taskres.task.producer.node_id)
+            for taskres in res.task_res_list
+        ]
+        return msgs
+
+    def send_and_receive(
+        self,
+        messages: Iterable[Tuple[Message, Iterable[int]]],
+        *,
+        time_out: Optional[float] = None,
+    ) -> Iterable[Tuple[Message, int]]:
+        """Push messages to specified node IDs and pull the responses.
+
+        This method sends a list of messages to their target node IDs and then
+        waits for the responses. It continues to pull responses until either all
+        responses are received or the specified time out duration is exceeded.
+
+        Parameters
+        ----------
+        messages : Iterable[Tuple[Message, Iterable[int]]]
+            An iterable of tuples, each containing a `Message` object and an iterable
+            of target node IDs to which the message should be sent.
+        time_out : Optional[float], default=None
+            The time out duration in seconds. If specified, the method will wait for
+            responses for this duration. If None, there is no time limit and the method
+            will wait until responses for all messages are received.
+
+        Returns
+        -------
+        Iterable[Tuple[Message, int]]
+            An iterable of tuples, each containing a `Message` object and the ID of
+            the node from which the message was received.
+
+        Notes
+        -----
+        This method uses `push_messages` to send the messages and `pull_messages`
+        to collect the responses. If `time_out` is set, the method may not return
+        responses for all sent messages.
+        """
+        # Push messages
+        msg_ids = self.push_messages(messages)
+
+        # Pull messages
+        end_time = time.time() + time_out
+        ret: List[Tuple[Message, int]] = []
+        while time.time() < end_time:
+            res_msgs = self.pull_messages(msg_ids)
+            ret += res_msgs
+            if len(ret) == len(msg_ids):
+                break
+            # Sleep
+            time.sleep(3)
+        return ret
 
     def __del__(self) -> None:
         """Disconnect GrpcDriver if connected."""
         # Check if GrpcDriver is initialized
         if self.grpc_driver is None:
             return
-
         # Disconnect
         self.grpc_driver.disconnect()
