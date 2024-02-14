c9b9185fba2068361e5500191e1fc983d956c876
CarlosMari <97597629+CarlosMari@users.noreply.github.com>
2024-02-14 11:18:29 -0600

    Dropped the parent class tests

--- datasets/flwr_datasets/partitioner/grouped_natural_id_partitioner_test.py
+++ datasets/flwr_datasets/partitioner/grouped_natural_id_partitioner_test.py
@@ -4,7 +4,7 @@
 import itertools
 import unittest
 from typing import Tuple
-import numpy as n
+import numpy as np
 
 from parameterized import parameterized
 
@@ -35,7 +35,7 @@ def _create_dataset(num_rows: int, n_unique_natural_ids: int) -> Dataset:
     return dataset
 
 
-class TestNaturalIdPartitioner(unittest.TestCase):
+class TestGroupedNaturalIdPartitioner(unittest.TestCase):
     """Test IidPartitioner."""
 
     @parameterized.expand(  # type: ignore
@@ -43,52 +43,35 @@ class TestNaturalIdPartitioner(unittest.TestCase):
         list(itertools.product([10, 30, 100, 1000], [2, 3, 4, 5], [1,3,4,4]))
     )
     def test_load_partition_num_partitions(
-        self, num_rows: int, num_unique_natural_id: int, num_nodes: int
+        self, num_rows: int, num_unique_natural_id: int, num_groups: int
     ) -> None:
         """Test if the number of partitions match the number of nodes.
 
         """
-        _, partitioner = _dummy_setup(num_rows, num_unique_natural_id, num_nodes)
+        _, partitioner = _dummy_setup(num_rows, num_unique_natural_id, num_groups)
         # Simulate usage to start lazy node_id_to_natural_id creation
         _ = partitioner.load_partition(0)
-        self.assertEqual(len(partitioner.node_id_to_natural_id.keys()), num_nodes, f'part: {partitioner._num_groups}, real: {num_nodes}')
+        self.assertEqual(len(partitioner.node_id_to_natural_id.keys()), num_groups, f'part: {partitioner._num_groups}, real: {num_groups}')
 
     @parameterized.expand(  # type: ignore
         # num_rows, num_unique_natural_ids
         list(itertools.product([10, 30, 100, 1000], [2, 3, 4, 5], [1,3,4,4]))
     )
     def test_load_partition_correct_division(
-        self, num_rows: int, num_unique_natural_ids: int, num_nodes: int
+        self, num_rows: int, num_unique_natural_ids: int, num_groups: int
     ) -> None:
         """Test if the data in partitions is correct.
 
         Only the correct data is tested in this method.
         """
 
-        true_labels = np.array_split(np.array(range(num_unique_natural_ids)).astype(str), num_nodes)
+        true_labels = np.array_split(np.array(range(num_unique_natural_ids)).astype(str), num_groups)
 
-        _, partitioner = _dummy_setup(num_rows, num_unique_natural_ids, num_nodes)
+        _, partitioner = _dummy_setup(num_rows, num_unique_natural_ids, num_groups)
         dataset = partitioner.load_partition(0)
         labels = np.unique(dataset['natural_id'])
 
         self.assertTrue((labels == true_labels[0]).all(), f'labels: {labels}; true_labels: {true_labels[0]}')
-
-    def test_partitioner_with_non_existing_column_partition_by(self) -> None:
-        """Test error when the partition_by columns does not exist."""
-        dataset = _create_dataset(10, 2)
-        partitioner = GroupedNaturalIdPartitioner(partition_by="not-existing",num_groups=2)
-        partitioner.dataset = dataset
-        with self.assertRaises(ValueError):
-            partitioner.load_partition(0)
-
-
-
-    def test_cannot_set_node_id_to_natural_id(self) -> None:
-        """Test the lack of ability to set node_id_to_natural_id."""
-        _, partitioner = _dummy_setup(num_rows=10, n_unique_natural_ids=2, num_nodes=3)
-        with self.assertRaises(AttributeError):
-            partitioner.node_id_to_natural_id = {0: "0"}
-
 
 
 if __name__ == "__main__":
