21e590a3a4326eac6d453524fa2e47cad6e7b1e0
Daniel Nata Nugraha <daniel.nata@flower.dev>
2024-02-15 13:07:40 +0100

    Add symmetric encryption test

--- src/py/flwr/server/superlink/state/state_test.py
+++ src/py/flwr/server/superlink/state/state_test.py
@@ -28,6 +28,15 @@ from flwr.proto.task_pb2 import Task, TaskIns, TaskRes  # pylint: disable=E0611
 from flwr.server.superlink.state import InMemoryState, SqliteState, State
 
 
+from flwr.common.secure_aggregation.crypto.symmetric_encryption import (
+    compute_hmac,
+    generate_key_pairs,
+    generate_shared_key,
+    public_key_to_bytes,
+    verify_hmac,
+)
+
+
 class StateTest(unittest.TestCase):
     """Test all state implementations."""
 
@@ -398,6 +407,25 @@ class StateTest(unittest.TestCase):
         # Assert
         assert num == 2
 
+    def test_client_public_keys(self) -> None:
+        """Test client public keys store and get from state."""
+        state: State = self.state_factory()
+        key_pairs = [generate_key_pairs() for _ in range(3)]
+        public_keys = {public_key_to_bytes(pair[1]) for pair in key_pairs}
+
+        state.store_client_public_keys(public_keys)
+
+        assert state.get_client_public_keys() == public_keys
+
+    def test_in_memory_node_id_public_key_pair() -> None:
+        """Test store and get node_id public_key pair."""
+        state: State = self.state_factory()
+        node_id = state.create_node()
+        public_key = public_key_to_bytes(generate_key_pairs()[1])
+
+        state.store_node_id_public_key_pair(node_id, public_key)
+
+        assert state.get_public_key_from_node_id(node_id) == public_key
 
 def create_task_ins(
     consumer_node_id: int,
