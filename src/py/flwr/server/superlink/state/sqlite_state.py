77e5c3cb161d776544dbb2c97bb7667c47ad3e45
Daniel Nata Nugraha <daniel.nata@flower.dev>
2024-02-15 12:59:08 +0100

    Fix authentication state

--- src/py/flwr/server/superlink/state/sqlite_state.py
+++ src/py/flwr/server/superlink/state/sqlite_state.py
@@ -542,6 +542,63 @@ class SqliteState(State):
         log(ERROR, "Unexpected run creation failure.")
         return 0
 
+    def store_node_id_public_key_pair(self, node_id: int, public_key: bytes) -> None:
+        """Store `node_id` and `public_key` as key-value pair in state."""
+        query = (
+            "INSERT OR REPLACE INTO node_key (node_id, public_key) "
+            "VALUES (:node_id, :public_key)"
+        )
+        self.query(query, {"node_id": node_id, "public_key": public_key})
+
+    def get_public_key_from_node_id(self, node_id: int) -> bytes:
+        """Get client's public key in urlsafe bytes for `node_id`."""
+        query = "SELECT public_key FROM node_key WHERE node_id = :node_id"
+        rows = self.query(query, {"node_id": node_id})
+        public_key: bytes = rows[0]["public_key"]
+        return public_key
+
+    def store_server_public_private_key(
+        self, public_key: bytes, private_key: bytes
+    ) -> None:
+        """Store server's `public_key` and `private_key` in state."""
+        query = (
+            "INSERT OR REPLACE INTO credential (public_key, private_key) "
+            "VALUES (:public_key, :private_key)"
+        )
+        self.query(query, {"public_key": public_key, "private_key": private_key})
+
+    def get_server_private_key(self) -> bytes:
+        """Get server private key in urlsafe bytes."""
+        query = "SELECT private_key FROM credential"
+        rows = self.query(query)
+        private_key: bytes = rows[0]["private_key"]
+        return private_key
+
+    def get_server_public_key(self) -> bytes:
+        """Get server public key in urlsafe bytes."""
+        query = "SELECT public_key FROM credential"
+        rows = self.query(query)
+        public_key: bytes = rows[0]["public_key"]
+        return public_key
+
+    def store_client_public_keys(self, public_keys: Set[bytes]) -> None:
+        """Store a set of client public keys in state."""
+        query = "INSERT INTO public_key (public_key) VALUES (:public_key)"
+        for public_key in public_keys:
+            self.query(query, {"public_key": public_key})
+
+    def store_client_public_key(self, public_key: bytes) -> None:
+        """Retrieve a client public key in state."""
+        query = "INSERT INTO public_key (public_key) VALUES (:public_key)"
+        self.query(query, {"public_key": public_key})
+
+    def get_client_public_keys(self) -> Set[bytes]:
+        """Retrieve all currently stored client public keys as a set."""
+        query = "SELECT public_key FROM public_key"
+        rows = self.query(query)
+        result: Set[bytes] = {row["public_key"] for row in rows}
+        return result
+
 
 def dict_factory(
     cursor: sqlite3.Cursor,
--- src/py/flwr/server/superlink/state/state.py
+++ src/py/flwr/server/superlink/state/state.py
@@ -152,3 +152,37 @@ class State(abc.ABC):
     @abc.abstractmethod
     def create_run(self) -> int:
         """Create one run."""
+
+    @abc.abstractmethod
+    def store_node_id_public_key_pair(self, node_id: int, public_key: bytes) -> None:
+        """Store `node_id` and `public_key` as key-value pair in state."""
+
+    @abc.abstractmethod
+    def get_public_key_from_node_id(self, node_id: int) -> bytes:
+        """Get client's public key in urlsafe bytes for `node_id`."""
+
+    @abc.abstractmethod
+    def store_server_public_private_key(
+        self, public_key: bytes, private_key: bytes
+    ) -> None:
+        """Store server's `public_key` and `private_key` in state."""
+
+    @abc.abstractmethod
+    def get_server_private_key(self) -> bytes:
+        """Get server private key in urlsafe bytes."""
+
+    @abc.abstractmethod
+    def get_server_public_key(self) -> bytes:
+        """Get server public key in urlsafe bytes."""
+
+    @abc.abstractmethod
+    def store_client_public_keys(self, public_keys: Set[bytes]) -> None:
+        """Store a set of client public keys in state."""
+
+    @abc.abstractmethod
+    def store_client_public_key(self, public_key: bytes) -> None:
+        """Retrieve a client public key in state."""
+
+    @abc.abstractmethod
+    def get_client_public_keys(self) -> Set[bytes]:
+        """Retrieve all currently stored client public keys as a set."""
