3733e7e9273ad2021708291e5dcf1d44b02b814d
Carlos Mari <carlosmarinoguera@gmail.com>
2024-02-14 20:11:14 -0600

    Autoformat

--- datasets/flwr_datasets/partitioner/group_natural_id_partitioner.py
+++ datasets/flwr_datasets/partitioner/group_natural_id_partitioner.py
@@ -1,37 +1,37 @@
-from typing import Dict
-import datasets
-from flwr_datasets.partitioner.natural_id_partitioner import NaturalIdPartitioner
 import numpy as np
 
+import datasets
+from flwr_datasets.partitioner.natural_id_partitioner import NaturalIdPartitioner
+
+
 class GroupNaturalIdPartitioner(NaturalIdPartitioner):
-    """Partitioner for a dataset in which the final partition results in different 
-        non overlapping datasets for a given number of groups/nodes
-        
-        Parameters
-        ----------
-        partition_by : str
-            The label in the dataset that divided the data.
-        num_groups : int
-            The number of groups in which the data will be divided.
-        """
+    """Partitioner for a dataset in which the final partition results in different non
+    overlapping datasets for a given number of groups/nodes.
+
+    Parameters
+    ----------
+    partition_by : str
+        The label in the dataset that divided the data.
+    num_groups : int
+        The number of groups in which the data will be divided.
+    """
 
     def __init__(self, partition_by, num_groups) -> None:
         super().__init__(partition_by)
         self._num_groups = num_groups
-
 
     def _create_int_node_id_to_natural_id(self) -> None:
         """Create a mapping from int indices to unique client or group ids from dataset.
 
         Natural ids come from the column specified in `partition_by`.
         """
-
         num_labels = len(self.dataset.unique(self._partition_by))
         if self._num_groups > num_labels:
-            raise ValueError("The number of groups cannot be greater than the number of labels in the dataset.")
+            raise ValueError(
+                "The number of groups cannot be greater than the number of labels in the dataset."
+            )
         unique_natural_ids = self.dataset.unique(self._partition_by)
 
-        
         # Divides the labels between the number of nodes/ groups
         split_natural_ids = np.array_split(unique_natural_ids, self._num_groups)
 
@@ -42,8 +42,8 @@ class GroupNaturalIdPartitioner(NaturalIdPartitioner):
     def load_partition(self, node_id: int) -> datasets.Dataset:
         """Load a single partition corresponding to a single `node_id`.
 
-        The choice of the partition is based on the parameter node_id, 
-        and the mapping computed in the function 
+        The choice of the partition is based on the parameter node_id,
+        and the mapping computed in the function
         _create_int_node_id_to_natural_id()
 
         Parameters
@@ -62,5 +62,3 @@ class GroupNaturalIdPartitioner(NaturalIdPartitioner):
         return self.dataset.filter(
             lambda row: row[self._partition_by] in self._node_id_to_natural_id[node_id]
         )
-    
-    
--- datasets/flwr_datasets/partitioner/group_natural_id_partitioner_test.py
+++ datasets/flwr_datasets/partitioner/group_natural_id_partitioner_test.py
@@ -4,12 +4,15 @@
 import itertools
 import unittest
 from typing import Tuple
-import numpy as np
 
+import numpy as np
 from parameterized import parameterized
 
 from datasets import Dataset
-from flwr_datasets.partitioner.group_natural_id_partitioner import GroupNaturalIdPartitioner
+from flwr_datasets.partitioner.group_natural_id_partitioner import (
+    GroupNaturalIdPartitioner,
+)
+
 
 def _dummy_setup(
     num_rows: int, n_unique_natural_ids: int, num_nodes: int
@@ -19,14 +22,15 @@ def _dummy_setup(
     The partitioner has automatically the dataset assigned to it.
     """
     dataset = _create_dataset(num_rows, n_unique_natural_ids)
-    partitioner = GroupNaturalIdPartitioner(partition_by="natural_id",num_groups=num_nodes)
+    partitioner = GroupNaturalIdPartitioner(
+        partition_by="natural_id", num_groups=num_nodes
+    )
     partitioner.dataset = dataset
     return dataset, partitioner
 
 
 def _create_dataset(num_rows: int, n_unique_natural_ids: int) -> Dataset:
     """Create dataset based on the number of rows and unique natural ids."""
-
     data = {
         "features": list(range(num_rows)),
         "natural_id": [f"{i % n_unique_natural_ids}" for i in range(num_rows)],
@@ -40,22 +44,24 @@ class TestGroupNaturalIdPartitioner(unittest.TestCase):
 
     @parameterized.expand(  # type: ignore
         # num_rows, num_unique_natural_ids
-        list(itertools.product([10, 30, 100, 1000], [2, 3, 4, 5], [1,3,4,4]))
+        list(itertools.product([10, 30, 100, 1000], [2, 3, 4, 5], [1, 3, 4, 4]))
     )
     def test_load_partition_num_partitions(
         self, num_rows: int, num_unique_natural_id: int, num_groups: int
     ) -> None:
-        """Test if the number of partitions match the number of nodes.
-
-        """
+        """Test if the number of partitions match the number of nodes."""
         _, partitioner = _dummy_setup(num_rows, num_unique_natural_id, num_groups)
         # Simulate usage to start lazy node_id_to_natural_id creation
         _ = partitioner.load_partition(0)
-        self.assertEqual(len(partitioner.node_id_to_natural_id.keys()), num_groups, f'part: {partitioner._group_size}, real: {num_groups}')
+        self.assertEqual(
+            len(partitioner.node_id_to_natural_id.keys()),
+            num_groups,
+            f"part: {partitioner._group_size}, real: {num_groups}",
+        )
 
     @parameterized.expand(  # type: ignore
         # num_rows, num_unique_natural_ids
-        list(itertools.product([10, 30, 100, 1000], [2, 3, 4, 5], [1,3,4,4]))
+        list(itertools.product([10, 30, 100, 1000], [2, 3, 4, 5], [1, 3, 4, 4]))
     )
     def test_load_partition_correct_division(
         self, num_rows: int, num_unique_natural_ids: int, num_groups: int
@@ -64,14 +70,18 @@ class TestGroupNaturalIdPartitioner(unittest.TestCase):
 
         Only the correct data is tested in this method.
         """
-
-        true_labels = np.array_split(np.array(range(num_unique_natural_ids)).astype(str), num_groups)
+        true_labels = np.array_split(
+            np.array(range(num_unique_natural_ids)).astype(str), num_groups
+        )
 
         _, partitioner = _dummy_setup(num_rows, num_unique_natural_ids, num_groups)
         dataset = partitioner.load_partition(0)
-        labels = np.unique(dataset['natural_id'])
+        labels = np.unique(dataset["natural_id"])
 
-        self.assertTrue((labels == true_labels[0]).all(), f'labels: {labels}; true_labels: {true_labels[0]}')
+        self.assertTrue(
+            (labels == true_labels[0]).all(),
+            f"labels: {labels}; true_labels: {true_labels[0]}",
+        )
 
 
 if __name__ == "__main__":
