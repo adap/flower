8ceabe0d8adfdb87ffeb3f8076fcdb4fd65f2e3c
Charles Beauville <charles@flower.dev>
2024-02-18 17:19:01 +0100

    Fix name error

--- src/py/flwr/client/app.py
+++ src/py/flwr/client/app.py
@@ -28,11 +28,7 @@ from requests.exceptions import ConnectionError as RequestsConnectionError
 from flwr.client.client import Client
 from flwr.client.clientapp import ClientApp
 from flwr.client.typing import ClientFn
-from flwr.common import (
-    GRPC_MAX_MESSAGE_LENGTH,
-    EventType,
-    event as RequestsConnectionError,
-)
+from flwr.common import GRPC_MAX_MESSAGE_LENGTH, EventType, event
 from flwr.common.address import parse_address
 from flwr.common.constant import (
     MISSING_EXTRA_REST,
