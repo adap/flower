a654ff63379d5863465e8e87116eb1c480620cae
Taner Topal <taner@flower.dev>
2024-02-18 18:55:18 +0100

    Update src/py/flwr/cli/__init__.py
    Co-authored-by: Daniel J. Beutel <daniel@flower.ai>

--- src/py/flwr/cli/__init__.py
+++ src/py/flwr/cli/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Flower command line interface package."""
+"""Flower command line interface."""
