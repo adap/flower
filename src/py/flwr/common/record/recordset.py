789d777178c03d8b4c5a386b755941c0e6aef93f
Heng Pan <ac.panh99@gmail.com>
2024-02-19 17:33:10 +0000

    update recordset

--- src/py/flwr/common/record/recordset.py
+++ src/py/flwr/common/record/recordset.py
@@ -24,15 +24,18 @@ from .parametersrecord import ParametersRecord
 from .typeddict import TypedDict
 
 T = TypeVar("T")
+DEFAULT_PARAMETERS = "parameters:builtin:default"
+DEFAULT_METRICS = "metrics:builtin:default"
+DEFAULT_CONFIGS = "configs:builtin:default"
 
 
 @dataclass
 class RecordSet:
     """Enhanced RecordSet with a unified and Pythonic interface."""
 
-    _parameters: TypedDict[str, ParametersRecord]
-    _metrics: TypedDict[str, MetricsRecord]
-    _configs: TypedDict[str, ConfigsRecord]
+    _parameters_dict: TypedDict[str, ParametersRecord]
+    _metrics_dict: TypedDict[str, MetricsRecord]
+    _configs_dict: TypedDict[str, ConfigsRecord]
 
     def __init__(
         self,
@@ -47,33 +50,69 @@ class RecordSet:
 
             return _check_fn
 
-        self._parameters = TypedDict[str, ParametersRecord](
+        self._parameters_dict = TypedDict[str, ParametersRecord](
             _get_check_fn(str), _get_check_fn(ParametersRecord)
         )
-        self._metrics = TypedDict[str, MetricsRecord](
+        self._metrics_dict = TypedDict[str, MetricsRecord](
             _get_check_fn(str), _get_check_fn(MetricsRecord)
         )
-        self._configs = TypedDict[str, ConfigsRecord](
+        self._configs_dict = TypedDict[str, ConfigsRecord](
             _get_check_fn(str), _get_check_fn(ConfigsRecord)
         )
         if parameters_dict is not None:
-            self._parameters.update(parameters_dict)
+            self._parameters_dict.update(parameters_dict)
         if metrics_dict is not None:
-            self._metrics.update(metrics_dict)
+            self._metrics_dict.update(metrics_dict)
         if configs_dict is not None:
-            self._configs.update(configs_dict)
+            self._configs_dict.update(configs_dict)
 
     @property
     def parameters_dict(self) -> TypedDict[str, ParametersRecord]:
         """Dictionary of ParametersRecord."""
-        return self._parameters
+        return self._parameters_dict
 
     @property
     def metrics_dict(self) -> TypedDict[str, MetricsRecord]:
         """Dictionary of MetricsRecord."""
-        return self._metrics
+        return self._metrics_dict
 
     @property
     def configs_dict(self) -> TypedDict[str, ConfigsRecord]:
         """Dictionary of ConfigsRecord."""
-        return self._configs
+        return self._configs_dict
+
+    @property
+    def default_parameters(self) -> ParametersRecord:
+        """Default ParametersRecord."""
+        if DEFAULT_PARAMETERS not in self._parameters_dict:
+            raise KeyError("No default ParametersRecord.")
+        return self._parameters_dict[DEFAULT_PARAMETERS]
+
+    @default_parameters.setter
+    def default_parameters(self, value: ParametersRecord) -> None:
+        """Set default_parameters."""
+        self._parameters_dict[DEFAULT_PARAMETERS] = value
+
+    @property
+    def default_metrics(self) -> MetricsRecord:
+        """Default MetricsRecord."""
+        if DEFAULT_METRICS not in self._metrics_dict:
+            raise KeyError("No default MetricsRecord.")
+        return self._metrics_dict[DEFAULT_METRICS]
+
+    @default_metrics.setter
+    def default_metrics(self, value: MetricsRecord) -> None:
+        """Set default_metrics."""
+        self._metrics_dict[DEFAULT_METRICS] = value
+
+    @property
+    def default_configs(self) -> ConfigsRecord:
+        """Default ConfigsRecord."""
+        if DEFAULT_CONFIGS not in self._configs_dict:
+            raise KeyError("No default ConfigsRecord.")
+        return self._configs_dict[DEFAULT_CONFIGS]
+
+    @default_configs.setter
+    def default_configs(self, value: ConfigsRecord) -> None:
+        """Set default_configs."""
+        self._configs_dict[DEFAULT_CONFIGS] = value
