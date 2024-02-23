c4a659bb4cfc1c383ad9e0b3b64bb77275d9a6ad
Daniel J. Beutel <daniel@flower.ai>
2024-02-23 15:00:07 +0100

    Update src/py/flwr/server/superlink/fleet/vce/backend/backend.py

--- src/py/flwr/server/superlink/fleet/vce/backend/backend.py
+++ src/py/flwr/server/superlink/fleet/vce/backend/backend.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Generic Backend class for Fleet API using the VCE."""
+"""Generic Backend class for Fleet API using the Simulation Engine."""
 
 
 from abc import ABC, abstractmethod
