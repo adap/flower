0e4ab143ac76c5340404ae22162e5d1f3c79408f
jafermarq <javier@flower.ai>
2024-02-25 22:31:49 +0000

    terminate method for backend; asyncio event to trigger stop

--- src/py/flwr/server/superlink/fleet/vce/backend/raybackend.py
+++ src/py/flwr/server/superlink/fleet/vce/backend/raybackend.py
@@ -28,6 +28,7 @@ from flwr.simulation.ray_transport.ray_actor import (
     ClientAppActor,
     init_ray,
 )
+from flwr.simulation.ray_transport.utils import enable_tf_gpu_growth
 
 from .backend import Backend, BackendConfig
 
@@ -56,7 +57,9 @@ class RayBackend(Backend):
         self.client_resources_key = "client_resources"
 
         # Create actor pool
-        actor_kwargs = backend_config.get("actor_kwargs", {})
+        use_tf = backend_config.get("tensorflow", False)
+        actor_kwargs = {"on_actor_init_fn": enable_tf_gpu_growth} if use_tf else {}
+
         client_resources = self._validate_client_resources(config=backend_config)
         self.pool = BasicActorPool(
             actor_type=ClientAppActor,
@@ -151,3 +154,7 @@ class RayBackend(Backend):
         ) = await self.pool.fetch_result_and_return_actor_to_pool(future)
 
         return out_mssg, updated_context
+
+    async def terminate(self) -> None:
+        """Terminate all actors in actor pool."""
+        await self.pool.terminate_all_actors()
