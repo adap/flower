74176ecbba75d8940bd6f2b07a1e0b88e05a646f
Daniel J. Beutel <daniel@flower.ai>
2024-02-26 14:19:40 +0100

    Update src/py/flwr/client/mod/centraldp_mods.py

--- src/py/flwr/client/mod/centraldp_mods.py
+++ src/py/flwr/client/mod/centraldp_mods.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Clipping modifiers for central DP with client side clipping."""
+"""Clipping modifiers for central DP with client-side clipping."""
 
 
 from flwr.client.typing import ClientAppCallable
