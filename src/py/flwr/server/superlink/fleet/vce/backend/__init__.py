98fb4b458e7d9cda53b44c327b381e340c3b18f2
jafermarq <javier@flower.ai>
2024-02-26 17:54:20 +0000

    .

--- src/py/flwr/server/superlink/fleet/vce/backend/__init__.py
+++ src/py/flwr/server/superlink/fleet/vce/backend/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""VirtualClientEngine Backends."""
+"""Simulation Engine Backends."""
 
 import importlib
 from typing import Dict, Type
