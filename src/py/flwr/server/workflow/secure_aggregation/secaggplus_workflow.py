60d12111170d6441d68ecfd8ae6b923231ebde78
Heng Pan <pan@flower.ai>
2024-03-04 18:40:51 +0000

    add check init params func

--- src/py/flwr/server/workflow/secure_aggregation/secaggplus_workflow.py
+++ src/py/flwr/server/workflow/secure_aggregation/secaggplus_workflow.py
@@ -17,6 +17,12 @@
 
 from __future__ import annotations
 
+from flwr.server.driver import Driver
+from flwr.common import Context, log
+from flwr.common.secure_aggregation.secaggplus_constants import Key, Stage, RECORD_KEY_CONFIGS, RECORD_KEY_STATE
+
+from logging import WARN, INFO
+
 
 class SecAggPlusWorkflow:
     """The workflow for the SecAgg+ protocol.
@@ -36,15 +42,19 @@ class SecAggPlusWorkflow:
         of shares needed for reconstruction. This threshold ensures privacy by allowing
         for the recovery of contributions from dropped clients during aggregation,
         without compromising individual client data.
+    max_weight : Optional[float] (default: 1000.0)
+        The maximum value of the weight that can be assigned to any single client's 
+        update during the weighted average calculation on the server side, e.g., in the 
+        FedAvg algorithm.
     clipping_range : float, optional (default: 8.0)
         The range within which model parameters are clipped before quantization.
         This parameter ensures each model parameter is bounded within
         [-clipping_range, clipping_range], facilitating quantization.
-    quantization_range : int, optional (default: 1 << 20)
+    quantization_range : int, optional (default: 1048576, this equals 2**20)
         The size of the range into which floating-point model parameters are quantized,
         mapping each parameter to an integer in [0, quantization_range-1]. This
         facilitates cryptographic operations on the model updates.
-    modulus_range : int, optional (default: 1 << 30)
+    modulus_range : int, optional (default: 2147483648, this equals 2**30)
         The range of values from which random mask entries are uniformly sampled
         ([0, modulus_range-1]).
 
@@ -53,6 +63,7 @@ class SecAggPlusWorkflow:
     - Generally, higher `num_shares` means more robust to dropouts while increasing the
     computational costs; higher `reconstruction_threshold` means better privacy 
     guarantees but less tolerance to dropouts.
+    - Too large `max_weight` may compromise the precision of the quantization.
     - `modulus_range` must be larger than `quantization_range`.
     - When `num_shares` is a float, it is interpreted as the proportion of all selected
     clients, and hence the number of shares will be determined in the runtime. This 
@@ -71,7 +82,78 @@ class SecAggPlusWorkflow:
         num_shares: int | float,
         reconstruction_threshold: int | float,
         *,
+        max_weight: float = 1000.0,
         quantization_range: float = 8.0,
-        target_range: int = 1 << 20,
-        mod_range: int = 1 << 30,
-    ) -> None: ...
+        target_range: int = 1048576,
+        mod_range: int = 2147483648,
+    ) -> None:
+        self.num_shares = num_shares
+        self.reconstruction_threshold = reconstruction_threshold
+        self.max_weight = max_weight
+        self.quantization_range = quantization_range
+        self.target_range = target_range
+        self.mod_range = mod_range
+        self.sampled_node_ids: List[int] = []
+        
+        self._check_init_params()
+    
+    def __call__(self, driver: Driver, context: Context) -> None:
+        ...
+
+    def _check_init_params(self) -> None:
+        # Check `num_shares`
+        if not isinstance(self.num_shares, (int, float)):
+            raise TypeError("`num_shares` must be of type int or float.")
+        if isinstance(self.num_shares, int):
+            if self.num_shares <= 2:
+                raise ValueError("`num_shares` as an integer must be greater than 2.")
+            if self.num_shares > self.mod_range / self.target_range:
+                log(
+                    WARN, 
+                    "A `num_shares` larger than `mod_range / target_range` will potentially "
+                    "cause overflow when computing the aggregated model parameters."
+                )
+            if self.num_shares == 1:
+                self.num_shares = 1.0
+        elif self.num_shares <= 0:
+            raise ValueError("`num_shares` as a float must be greater than 0.")
+        
+        # Check `reconstruction_threshold`
+        if not isinstance(self.reconstruction_threshold, (int, float)):
+            raise TypeError("`reconstruction_threshold` must be of type int or float.")
+        if isinstance(self.reconstruction_threshold, int):
+            if isinstance(self.num_shares, int):
+                if self.reconstruction_threshold >= self.num_shares:
+                    raise ValueError(
+                        "`reconstruction_threshold` must be less than `num_shares`."
+                    )
+            if self.reconstruction_threshold == 1:
+                self.reconstruction_threshold = 1.0
+        else:
+            if not (0 < self.reconstruction_threshold <= 1):
+                raise ValueError(
+                    "If `reconstruction_threshold` is a float, "
+                    "it must be greater than 0 and less than or equal to 1."
+                )
+        
+        # Check `max_weight`
+        if self.max_weight <= 0:
+            raise ValueError("`max_weight` must be greater than 0.")
+        
+        # Check `quantization_range`
+        if self.quantization_range <= 0:
+            raise ValueError("`quantization_range` must be greater than 0.")
+        
+        # Check `target_range`
+        if not isinstance(self.target_range, int) or self.target_range <= 0:
+            raise ValueError("`target_range` must be an integer and greater than 0.")
+        
+        # Check `mod_range`
+        if not isinstance(self.mod_range, int) or self.mod_range <= self.target_range:
+            raise ValueError("`mod_range` must be an integer and greater than `target_range`.")
+
+    def _setup(self, driver: Driver, context: Context) -> None:
+        # Protocol config
+        self.sampled_node_ids = driver.get_node_ids()
+        cfg = context.state.configs_records[RECORD_KEY_STATE]
+        
