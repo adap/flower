204ebbf8671b4bdabf73be5b8d9e47c3b870899f
Taner Topal <taner@flower.dev>
2024-03-05 21:32:10 +0100

    Update src/py/flwr/cli/flower_toml.py
    Co-authored-by: Daniel J. Beutel <daniel@flower.ai>

--- src/py/flwr/cli/flower_toml.py
+++ src/py/flwr/cli/flower_toml.py
@@ -12,10 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Utility to validate the flower.toml file.
-
-This module will be improved over time.
-"""
+"""Utility to validate the `flower.toml` file."""
 
 import importlib
 import os
