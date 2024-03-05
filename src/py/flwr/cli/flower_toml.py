cb702134f202c3dff6938c48c0e246e8a80d139d
Taner Topal <taner@flower.dev>
2024-03-05 21:44:31 +0100

    Update src/py/flwr/cli/flower_toml.py
    Co-authored-by: Daniel J. Beutel <daniel@flower.ai>

--- src/py/flwr/cli/flower_toml.py
+++ src/py/flwr/cli/flower_toml.py
@@ -77,7 +77,8 @@ def validate_object_reference(ref: str) -> Tuple[bool, Optional[str]]:
 
     Returns
     -------
-    tuple(bool, Optional[str]): is_valid, reason why it might not be
+    Tuple[bool, Optional[str]]
+        A boolean indicating whether an object reference is valid and the reason why it might not be.
     """
     module_str, _, attributes_str = ref.partition(":")
     if not module_str:
