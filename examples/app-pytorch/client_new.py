bbbeb956ad52b7984c2a6ef438453de97be1bd58
Daniel J. Beutel <daniel@flower.ai>
2024-03-06 21:39:55 +0100

    Add app-pytorch example for ClientApp functions

---
+++ examples/app-pytorch/client_new.py
@@ -0,0 +1,24 @@
+import flwr
+from flwr.common import Message, Context
+
+
+# Run via `flower-client-app client:app`
+app = flwr.client.ClientApp()
+
+
+@app.train()
+def train(msg: Message, ctx: Context):
+    print("`train` is not implemented, echoing original message")
+    return msg.create_reply(msg.content, ttl=msg.metadata.ttl)
+
+
+@app.evaluate()
+def eval(msg: Message, ctx: Context):
+    print("`evaluate` is not implemented, echoing original message")
+    return msg.create_reply(msg.content, ttl=msg.metadata.ttl)
+
+
+@app.query()
+def q(msg: Message, ctx: Context):
+    print("`query` is not implemented, echoing original message")
+    return msg.create_reply(msg.content, ttl=msg.metadata.ttl)
