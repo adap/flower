ea7f2a98245ae7fcbaaaf217cd3e9cfe15fa6033
Mohammad Naseri <mohammad@flower.ai>
2024-03-07 11:28:50 +0000

    Check if msg has error

--- src/py/flwr/client/mod/localdp_mod.py
+++ src/py/flwr/client/mod/localdp_mod.py
@@ -107,6 +107,11 @@ class LocalDpMod:
 
         # Call inner app
         out_msg = call_next(msg, ctxt)
+
+        # Check if the msg has error
+        if out_msg.has_error():
+            return out_msg
+
         fit_res = compat.recordset_to_fitres(out_msg.content, keep_input=True)
 
         client_to_server_params = parameters_to_ndarrays(fit_res.parameters)
