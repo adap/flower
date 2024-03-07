d13caf7083e329ae3d13a0f6b3f0e1539cf25824
Daniel Nata Nugraha <daniel.nata@flower.ai>
2024-03-07 09:12:34 +0100

    Fix lint generate.sh

--- e2e/client-auth/generate.sh
+++ e2e/client-auth/generate.sh
@@ -5,8 +5,6 @@ set -e
 # Change directory to the script's directory
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-CA_PASSWORD=notsafe
-
 CERT_DIR=certificates
 
 # Generate directories if not exists
@@ -54,26 +52,27 @@ openssl x509 \
     -extfile certificate.conf \
     -extensions req_ext
 
+KEY_DIR=keys
 
-mkdir -p keys
+mkdir -p $KEY_DIR
 
-rm -f keys/*
+rm -f $KEY_DIR/*
 
-ssh-keygen -t ecdsa -b 384 -N "" -f "keys/server_credentials"
+ssh-keygen -t ecdsa -b 384 -N "" -f "${KEY_DIR}/server_credentials" -C ""
 
 generate_client_credentials() {
     local num_clients=${1:-2} 
     for ((i=1; i<=num_clients; i++))
     do
-        ssh-keygen -t ecdsa -b 384 -N "" -f "keys/client_credentials_$i" -C ""
+        ssh-keygen -t ecdsa -b 384 -N "" -f "${KEY_DIR}/client_credentials_$i" -C ""
     done
 }
 
-generate_client_credentials $1
+generate_client_credentials "$1"
 
-printf "%s" "$(cat "keys/client_credentials_1.pub" | sed 's/.$//')" > keys/client_public_keys.csv
+printf "%s" "$(cat "${KEY_DIR}/client_credentials_1.pub" | sed 's/.$//')" > $KEY_DIR/client_public_keys.csv
 for ((i=2; i<=${1:-2}; i++))
 do
-    printf ",%s" "$(cat "keys/client_credentials_$i.pub" | sed 's/.$//')" >> keys/client_public_keys.csv
+    printf ",%s" "$(cat "${KEY_DIR}/client_credentials_$i.pub" | sed 's/.$//')" >> $KEY_DIR/client_public_keys.csv
 done
-printf "\n" >> keys/client_public_keys.csv
+printf "\n" >> $KEY_DIR/client_public_keys.csv
