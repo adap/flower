ad93b48ecd8f5b64115e3d7faaf3214d404122e2
Daniel J. Beutel <daniel@flower.ai>
2024-03-09 10:53:41 +0100

    Rename utils to task

--- src/py/flwr/cli/new/new.py
+++ src/py/flwr/cli/new/new.py
@@ -116,10 +116,10 @@ def new(
         f"{pnl}/client.py": {"template": f"app/code/client.{framework_str}.py.tpl"},
     }
 
-    # In case framework is MlFramework.PYTORCH generate additionally the utils.py file
+    # In case framework is MlFramework.PYTORCH generate additionally the task.py file
     if framework_str == MlFramework.PYTORCH.value.lower():
-        files[f"{pnl}/utils.py"] = {
-            "template": f"app/code/utils.{framework_str}.py.tpl"
+        files[f"{pnl}/task.py"] = {
+            "template": f"app/code/task.{framework_str}.py.tpl"
         }
 
     context = {"project_name": project_name}
