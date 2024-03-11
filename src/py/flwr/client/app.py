daf6390a3d39b507dd3e9a9b9ac057f3b3c9a051
Charles Beauville <charles@flower.ai>
2024-03-11 14:43:32 +0100

    Improve format

--- src/py/flwr/client/app.py
+++ src/py/flwr/client/app.py
@@ -458,11 +458,15 @@ def _start_client_internal(
 
                 log(
                     INFO,
-                    "Received: %s message %s for group %s of run %s",
+                    "[RUN %s, ROUND %s]",
+                    message.metadata.run_id,
+                    message.metadata.group_id,
+                )
+                log(
+                    INFO,
+                    "Received: %s message %s",
                     message.metadata.message_type,
                     message.metadata.message_id,
-                    message.metadata.group_id,
-                    message.metadata.run_id,
                 )
 
                 # Handle control message
@@ -493,11 +497,15 @@ def _start_client_internal(
                 send(out_message)
                 log(
                     INFO,
-                    "Sent: %s reply %s for group %s of run %s",
-                    out_message.metadata.message_type,
-                    out_message.metadata.message_id,
-                    out_message.metadata.group_id,
+                    "[RUN %s, ROUND %s]",
                     out_message.metadata.run_id,
+                    out_message.metadata.group_id,
+                )
+                log(
+                    INFO,
+                    "Sent: %s reply to message %s",
+                    out_message.metadata.message_type,
+                    message.metadata.message_id,
                 )
 
             # Unregister node
