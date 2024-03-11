f9f3e2f9034d08a3239e7ec3b61fce1ec1b5745d
Heng Pan <pan@flower.ai>
2024-03-11 14:19:54 +0000

    fix description

--- src/py/flwr/client/mod/secure_aggregation/secaggplus_mod_test.py
+++ src/py/flwr/client/mod/secure_aggregation/secaggplus_mod_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""The SecAgg+ protocol handler tests."""
+"""The SecAgg+ protocol modifier tests."""
 
 import unittest
 from itertools import product
