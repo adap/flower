5866311f8d20ce8c7e113b5486cf476dd9be09e3
Heng Pan <pan@flower.ai>
2024-03-12 21:00:00 +0000

    Fix the module doc string of `secaggplus_mod` (#3106)

--- src/py/flwr/client/mod/secure_aggregation/secaggplus_mod.py
+++ src/py/flwr/client/mod/secure_aggregation/secaggplus_mod.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Message handler for the SecAgg+ protocol."""
+"""Modifier for the SecAgg+ protocol."""
 
 
 import os
