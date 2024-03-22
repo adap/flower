3541bb34fd29630c6e88982db1c8baf6adbffbdd
Adam Narozniak <adam@flower.dev>
2024-03-22 10:04:01 +0100

    Fix metric utils

--- datasets/flwr_datasets/metrics/utils.py
+++ datasets/flwr_datasets/metrics/utils.py
@@ -22,29 +22,13 @@ from functools import singledispatch
 
 from flwr_datasets.partitioner import Partitioner
 
-class Foo:
 
-    @overload
-    def bar(x: int) -> int:
-        pass
-
-    @overload
-    def bar(x: str) -> str:
-        pass
-
-    def bar(x):
-        """Documentation..."""
-        return x
-
-
-
-@overload
 def compute_counts(
         labels: list,
         unique_labels: Union[List[int], List[str]]
 ) -> pd.Series:
     """
-    BBBBBCompute the count of labels when taking into account all possible labels.
+    Compute the count of labels when taking into account all possible labels.
     Also known as absolute frequency.
 
     Parameters
@@ -60,53 +44,14 @@ def compute_counts(
     label_counts: pd.Series
         The pd.Series with label as indices and counts as values.
     """
-    ...
-    # labels_series = pd.Series(labels)
-    # label_counts = labels_series.value_counts()
-    # label_counts_with_zeros = pd.Series(index=unique_labels, data=0)
-    # label_counts_with_zeros = label_counts_with_zeros.add(label_counts,
-    #                                                       fill_value=0).astype(int)
-    # return label_counts_with_zeros
+    labels_series = pd.Series(labels)
+    label_counts = labels_series.value_counts()
+    label_counts_with_zeros = pd.Series(index=unique_labels, data=0)
+    label_counts_with_zeros = label_counts_with_zeros.add(label_counts,
+                                                          fill_value=0).astype(int)
+    return label_counts_with_zeros
 
-@overload
-def compute_counts(
-        partitioner: Partitioner
-) -> pd.Series:
-    """
-    CCCCCSome string.
 
-    Parameters
-    ----------
-    partitioner: Partitioner
-        Something
-
-    Returns
-    -------
-        couunts:
-        jlksdfjalskjfd
-    """
-    ...
-def compute_counts(
-       data
-) -> pd.Series:
-    """
-    AAAAACompute the count of labels when taking into account all possible labels.
-    Also known as absolute frequency.
-
-    Parameters
-    ----------
-    labels: Union[List[int], List[str]]
-        The labels from the datasets.
-    unique_labels: Union[List[int], List[str]]
-        The reference all unique label. Needed to avoid missing any label, instead
-        having the value equal to zero for them.
-
-    Returns
-    -------
-    label_counts: pd.Series
-        The pd.Series with label as indices and counts as values.
-    """
-    pass
 def compute_distribution(
         labels: Union[List[int], List[str]],
         unique_labels: Union[List[int], List[str]]
