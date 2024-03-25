29b1d64618c5d38d28e342a46a03eed87cd871bb
Daniel Nata Nugraha <daniel.nata@flower.ai>
2024-03-25 11:04:54 +0100

    Attempt to change package name

--- src/kotlin/build.gradle.kts
+++ src/kotlin/build.gradle.kts
@@ -5,12 +5,12 @@ buildscript {
     }
     dependencies {
         // Not 8.0.2 because on macOS `Could not find protoc-3.11.0-osx-aarch_64.exe (com.google.protobuf:protoc:3.11.0)`
-        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.android.tools.build:gradle:8.3.1")
         // Not 0.9.2 because of https://github.com/grpc/grpc-kotlin/issues/380
-        classpath("com.google.protobuf:protobuf-gradle-plugin:0.9.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
 
-        classpath("com.vanniktech:gradle-maven-publish-plugin:0.25.3") // NEW
+        classpath("com.vanniktech:gradle-maven-publish-plugin:0.28.0") // NEW
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
--- src/kotlin/flwr/build.gradle.kts
+++ src/kotlin/flwr/build.gradle.kts
@@ -9,13 +9,13 @@ plugins {
 
 android {
     namespace = "dev.flower"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         aarMetadata {
-            minCompileSdk = 28
+            minCompileSdk = 34
         }
-        minSdk = 28
+        minSdk = 34
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -36,7 +36,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packagingOptions {
         resources {
@@ -73,17 +73,17 @@ protobuf {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.compose.runtime:runtime:1.5.0")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-    implementation("io.grpc:grpc-okhttp:$grpcVersion")
-    implementation("io.grpc:grpc-protobuf-lite:$grpcVersion")
-    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.compose.runtime:runtime:1.6.4")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.23"))
+    implementation("io.grpc:grpc-okhttp:1.62.2")
+    implementation("io.grpc:grpc-protobuf-lite:1.62.2")
+    implementation("io.grpc:grpc-stub:1.62.2")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
 
     protobuf(files("../../proto"))
-    dokkaPlugin("org.jetbrains.dokka:android-documentation-plugin:1.8.20")
+    dokkaPlugin("org.jetbrains.dokka:android-documentation-plugin:1.9.20")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
