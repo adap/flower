927ac3369a6489a916c3966a193e2b4fae04469d
Javier <jafermarq@users.noreply.github.com>
2024-03-28 11:15:55 +0000

    Apply suggestions from code review
    Co-authored-by: Daniel J. Beutel <daniel@flower.ai>

--- src/py/flwr/client/app.py
+++ src/py/flwr/client/app.py
@@ -485,7 +485,7 @@ def _start_client_internal(
                 # Create an error reply message that will never be used to prevent
                 # the used-before-assignment linting error
                 reply_message = message.create_error_reply(
-                    error=Error(code=0, reason="Unknown"), ttl=message.metadata.ttl
+                    error=Error(code=0, reason="Unknown")
                 )
 
                 # Handle app loading and task message
@@ -504,7 +504,7 @@ def _start_client_internal(
 
                     # Legacy grpc-bidi
                     if transport in ["grpc-bidi", None]:
-                        # Rasie exception, crashes process
+                        # Raise exception, crash process
                         raise ex
 
                     # Don't update/change NodeState
@@ -513,7 +513,7 @@ def _start_client_internal(
                     # Reason example: "<class 'ZeroDivisionError'>:<'division by zero'>"
                     reason = str(type(ex)) + ":<'" + str(ex) + "'>"
                     reply_message = message.create_error_reply(
-                        error=Error(code=0, reason=reason), ttl=message.metadata.ttl
+                        error=Error(code=0, reason=reason)
                     )
 
                 # Send
