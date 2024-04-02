955ddfee0cd7c1dae47d2b2999637aa61de4fb15
Heng Pan <pan@flower.ai>
2024-04-02 16:20:44 +0100

    fix module docstring

--- src/py/flwr/server/superlink/state/utils.py
+++ src/py/flwr/server/superlink/state/utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""In-memory State implementation."""
+"""Utility functions for State."""
 
 
 import time
