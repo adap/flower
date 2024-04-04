491fdc89942940471c5ab9611c70380bfb40b457
Charles Beauville <charles@flower.ai>
2024-04-04 11:30:02 +0200

    Fix script and add button image

--- dev/update-examples.sh
+++ dev/update-examples.sh
@@ -21,30 +21,35 @@ rm -f "examples/doc/source/*.md"
 cd examples/
 for d in $(printf '%s\n' */ | sort -V); do
   example=${d%/}
-  # For each example, copy the README into the source of the Example docs
-  [[ $example != doc ]] && cp $example/README.md $ROOT/examples/doc/source/$example.md 2>&1 >/dev/null
 
-  gh_text="[<img src=\"_static/view-gh.png\" alt=\"View on GitHub\" width=\"200\"/>](https://github.com/adap/flower/blob/main/$example)"
-  readme_file="$ROOT/examples/doc/source/$example.md"
+  if [[ $example != doc ]]; then
 
-  if ! grep -Fq "$gh_text" "$readme_file"; then
-    awk -v text="$gh_text" '
-    /^# / && !found {
-      print $0 "\n" text;
-      found=1;
-      next;
+    # For each example, copy the README into the source of the Example docs
+    cp $example/README.md $ROOT/examples/doc/source/$example.md 2>&1 >/dev/null
+
+    gh_text="[<img src=\"_static/view-gh.png\" alt=\"View on GitHub\" width=\"200\"/>](https://github.com/adap/flower/blob/main/examples/$example)"
+    readme_file="$ROOT/examples/doc/source/$example.md"
+
+    if ! grep -Fq "$gh_text" "$readme_file"; then
+      awk -v text="$gh_text" '
+      /^# / && !found {
+        print $0 "\n" text;
+        found=1;
+        next;
+      }
+      { print }
+      ' "$readme_file" > tmpfile && mv tmpfile "$readme_file"
+    fi
+    
+    # For each example, copy all images of the _static folder into the examples
+    # docs static folder
+    [ -d "$example/_static" ] && {
+      cp $example/_static/**.{jpg,png,jpeg} $ROOT/examples/doc/source/_static/ 2>/dev/null || true
     }
-    { print }
-    ' "$readme_file" > tmpfile && mv tmpfile "$readme_file"
+    # For each example, insert the name of the example into the index file
+    (echo $INSERT_LINE; echo a; echo $example; echo .; echo wq) | ed $INDEX 2>&1 >/dev/null
+
   fi
-  
-  # For each example, copy all images of the _static folder into the examples
-  # docs static folder
-  [[ $example != doc ]] && [ -d "$example/_static" ] && {
-    cp $example/_static/**.{jpg,png,jpeg} $ROOT/examples/doc/source/_static/ 2>/dev/null || true
-  }
-  # For each example, insert the name of the example into the index file
-  [[ $example != doc ]] && (echo $INSERT_LINE; echo a; echo $example; echo .; echo wq) | ed $INDEX 2>&1 >/dev/null
 done
 
 echo "\`\`\`" >> $INDEX
