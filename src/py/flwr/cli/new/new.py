16f83547496bd5d9259672920cd1fee225c597cc
Charles Beauville <charles@flower.ai>
2024-04-12 18:32:23 +0200

    Add forgotten extension

--- src/py/flwr/cli/new/new.py
+++ src/py/flwr/cli/new/new.py
@@ -127,7 +127,7 @@ def new(
 
     # List of files to render
     files = {
-        ".gitignore": {"template": "app/.gitignore"},
+        ".gitignore": {"template": "app/.gitignore.tpl"},
         "README.md": {"template": "app/README.md.tpl"},
         "flower.toml": {"template": "app/flower.toml.tpl"},
         "pyproject.toml": {"template": f"app/pyproject.{framework_str}.toml.tpl"},
