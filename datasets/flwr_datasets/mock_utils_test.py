be79f8f373146e136067b3f8b35724af7738788d
Adam Narozniak <adam@flower.dev>
2024-04-22 10:09:41 +0200

    Fix types

--- datasets/flwr_datasets/mock_utils_test.py
+++ datasets/flwr_datasets/mock_utils_test.py
@@ -190,7 +190,7 @@ def _generate_random_image_column(
     pil_imgs = []
     for np_image in np_images:
         # Convert the NumPy array to a PIL image
-        pil_img_beg = Image.fromarray(np_image)
+        pil_img_beg = Image.fromarray(np_image)  # type: ignore
 
         # Save the image to an in-memory bytes buffer
         in_memory_file = io.BytesIO()
