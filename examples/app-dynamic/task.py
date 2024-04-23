3289463a41525fa9d63f015ac716e400b0c57fb9
Adam Narozniak <adam@flower.dev>
2024-04-23 07:45:26 +0200

    Move weighted avg def

--- examples/app-dynamic/task.py
+++ examples/app-dynamic/task.py
@@ -1,9 +1,12 @@
 from collections import OrderedDict
 from logging import INFO
+from typing import List, Tuple
+from flwr.common import Metrics
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 from flwr.common.logger import log
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -80,3 +83,24 @@ def set_weights(net, parameters):
     params_dict = zip(net.state_dict().keys(), parameters)
     state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
     net.load_state_dict(state_dict, strict=True)
+
+
+# Define metric aggregation function
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    examples = [num_examples for num_examples, _ in metrics]
+
+    # Multiply accuracy of each client by number of examples used
+    train_losses = [num_examples * m["train_loss"] for num_examples, m in metrics]
+    train_accuracies = [
+        num_examples * m["train_accuracy"] for num_examples, m in metrics
+    ]
+    val_losses = [num_examples * m["val_loss"] for num_examples, m in metrics]
+    val_accuracies = [num_examples * m["val_accuracy"] for num_examples, m in metrics]
+
+    # Aggregate and return custom metric (weighted average)
+    return {
+        "train_loss": sum(train_losses) / sum(examples),
+        "train_accuracy": sum(train_accuracies) / sum(examples),
+        "val_loss": sum(val_losses) / sum(examples),
+        "val_accuracy": sum(val_accuracies) / sum(examples),
+    }
