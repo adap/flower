3ae8a85e32fefa1527242ed6ac8162e0462a4ae6
Adam Narozniak <adam@flower.dev>
2024-04-23 07:43:43 +0200

    Modify models

--- examples/app-dynamic/models.py
+++ examples/app-dynamic/models.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Net(nn.Module):
+class CIFARNet(nn.Module):
 
     def __init__(self) -> None:
-        super(Net, self).__init__()
+        super(CIFARNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
@@ -26,15 +26,12 @@ class Net(nn.Module):
 class MNISTNet(nn.Module):
     def __init__(self) -> None:
         super(MNISTNet, self).__init__()
-        # Input size: [1, 28, 28]
-        self.conv1 = nn.Conv2d(1, 6, 5)  # Output size: [6, 24, 24]
-        self.pool = nn.MaxPool2d(2, 2)  # Output size: [6, 12, 12]
-        self.conv2 = nn.Conv2d(6, 16, 5)  # Output size: [16, 8, 8]
-        # After another pooling step: [16, 4, 4]
-        # Adjusted for the flattened size of the pool(conv2) output
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)  # Output size: 10, for 10 digit classes
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
