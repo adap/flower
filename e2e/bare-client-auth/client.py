ae12cb64d6ed868abcb5e65d6aad1fabf6ebe662
Daniel Nata Nugraha <daniel.nata@flower.ai>
2024-04-29 15:18:21 +0200

    Remove start client legacy

--- e2e/bare-client-auth/client.py
+++ e2e/bare-client-auth/client.py
@@ -28,12 +28,3 @@ def client_fn(cid):
 app = fl.client.ClientApp(
     client_fn=client_fn,
 )
-
-if __name__ == "__main__":
-    # Start Flower client
-    fl.client.start_client(
-        server_address="127.0.0.1:8080", 
-        client=FlowerClient().to_client(),
-        root_certificates=Path("certificates/ca.crt").read_bytes(),
-        insecure=False,
-    )
