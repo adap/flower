cc61c1cf2def08df040b4d48ef1b4149bd69dfa7
Daniel Nata Nugraha <daniel.nata@flower.ai>
2024-04-29 13:09:31 +0200

    Add context to message handler as comment

--- src/py/flwr/server/superlink/fleet/grpc_rere/server_interceptor.py
+++ src/py/flwr/server/superlink/fleet/grpc_rere/server_interceptor.py
@@ -109,6 +109,8 @@ class AuthenticateServerInterceptor(grpc.ServerInterceptor):  # type: ignore
         auth metadata sent by the client. Continue RPC call if client is authenticated,
         else, terminate RPC call by setting context to abort.
         """
+        
+        # The default message handler in flwr.server.superlink.fleet.message_handler
         message_handler: grpc.RpcMethodHandler = continuation(handler_call_details)
         return self._generic_auth_unary_method_handler(message_handler)
 
