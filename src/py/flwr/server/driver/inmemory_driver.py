b2dcc86f18b1d5dec277bbc217d26f4efed6fd7e
jafermarq <javier@flower.ai>
2024-04-30 15:06:18 +0100

    updated /w fab

--- src/py/flwr/server/driver/inmemory_driver.py
+++ src/py/flwr/server/driver/inmemory_driver.py
@@ -35,13 +35,21 @@ class InMemoryDriver(Driver):
     ----------
     state_factory : StateFactory
         A StateFactory embedding a state designed for in-memory communication.
+    fab_id : str (default: None)
+        The identifier of the FAB used in the run.
+    fab_version : str (default: None)
+        The version of the FAB used in the run.
     """
 
     def __init__(
         self,
         state_factory: StateFactory,
+        fab_id: Optional[str] = None,
+        fab_version: Optional[str] = None,
     ) -> None:
         self.run_id: Optional[int] = None
+        self.fab_id = fab_id if fab_id is not None else ""
+        self.fab_version = fab_version if fab_version is not None else ""
         self.node = Node(node_id=0, anonymous=True)
         state = state_factory.state()
 
@@ -70,7 +78,9 @@ class InMemoryDriver(Driver):
         If unset, create a new run.
         """
         if self.run_id is None:
-            self.run_id = self.state.create_run("None/None", "None")
+            self.run_id = self.state.create_run(
+                fab_id=self.fab_id, fab_version=self.fab_version
+            )
         return self.run_id
 
     def create_message(  # pylint: disable=too-many-arguments
