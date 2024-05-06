1cfb090d65bbf7402eb44229d7760aefb3940b83
Charles Beauville <charles@flower.ai>
2024-05-06 14:17:38 +0200

    Add test

---
+++ e2e/test_reconnection.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -e
+
+case "$1" in
+  rest)
+    rest_arg="--rest"
+    server_address="http://localhost:9093"
+    db_arg="--database :flwr-in-memory-state:"
+    ;;
+  sqlite)
+    rest_arg=""
+    server_address="127.0.0.1:9092"
+    db_arg="--database $(date +%s).db"
+    ;;
+  *)
+    rest_arg=""
+    server_address="127.0.0.1:9092"
+    db_arg="--database :flwr-in-memory-state:"
+    ;;
+esac
+
+dir_arg="--dir ./.."
+
+timeout 2m flower-superlink --insecure $db_arg $rest_arg &
+sl_pid=$!
+sleep 3
+
+timeout 2m flower-client-app client:app --insecure $rest_arg --server $server_address &
+cl1_pid=$!
+sleep 3
+
+timeout 2m flower-client-app client:app --insecure $rest_arg --server $server_address &
+cl2_pid=$!
+sleep 3
+
+# Kill superlink
+kill $sl_pid
+sleep 5
+
+# Restart superlink
+timeout 2m flower-superlink --insecure $db_arg $rest_arg &
+sl_pid=$!
+sleep 3
+
+# Kill first client
+kill $cl1_pid
+sleep 5
+
+timeout 2m flower-client-app client:app --insecure $rest_arg --server $server_address &
+cl1_pid=$!
+sleep 3
+
+timeout 2m flower-server-app server:app --insecure $dir_arg $rest_arg --server $server_address &
+pid=$!
+sleep 1
+
+kill $cl1_pid
+sleep 1
+
+timeout 2m flower-client-app client:app --insecure $rest_arg --server $server_address &
+cl1_pid=$!
+
+wait $pid
+res=$?
+
+if [[ "$res" = "0" ]];
+  then echo "Training worked correctly"; kill $cl1_pid; kill $cl2_pid; kill $sl_pid;
+  else echo "Training had an issue" && exit 1;
+fi
+
