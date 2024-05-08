b69455a8ad1245fedb333dcc50ccc278effac94f
Charles Beauville <charles@flower.ai>
2024-05-08 21:44:42 +0200

    feat(framework:skip) Install project on flwr new

--- src/py/flwr/cli/new/new.py
+++ src/py/flwr/cli/new/new.py
@@ -16,6 +16,7 @@
 
 import os
 import re
+import subprocess
 from enum import Enum
 from string import Template
 from typing import Dict, Optional
@@ -178,6 +179,16 @@ def new(
             context=context,
         )
 
+    if (
+        subprocess.run(
+            ["pip", "install", "-e", project_dir],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        ).returncode
+        != 0
+    ):
+        raise typer.Exit(code=1)
+
     print(
         typer.style(
             "🎊 Project creation successful.\n\n"
@@ -188,7 +199,7 @@ def new(
     )
     print(
         typer.style(
-            f"	cd {project_name}\n" + "	pip install -e .\n	flwr run\n",
+            f"	cd {project_name}\n" + "	flwr run\n",
             fg=typer.colors.BRIGHT_CYAN,
             bold=True,
         )
