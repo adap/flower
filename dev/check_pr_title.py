60daaa85cea5b93b7ebf4503ccccfe73d6894600
Charles Beauville <charles@flower.ai>
2024-05-15 18:03:51 +0200

    Add docstring and improve print statement

--- dev/check_pr_title.py
+++ dev/check_pr_title.py
@@ -32,13 +32,13 @@ if __name__ == "__main__":
         config = yaml.safe_load(file)
 
     # Extract types, project, and scope from the config
-    types = "|".join(config["types"])
-    project = "|".join(config["project"]) + "|\\*"
+    types = "|".join(config["type"])
+    projects = "|".join(config["project"]) + "|\\*"
     scope = config["scope"]
 
     # Construct the pattern
     pattern_template = config["pattern_template"]
-    pattern = pattern_template.format(types=types, project=project, scope=scope)
+    pattern = pattern_template.format(types=types, projects=projects, scope=scope)
 
     # Check for the pattern in the first argument given to the script
     if re.search(pattern, pr_title):
@@ -46,11 +46,11 @@ if __name__ == "__main__":
         sys.exit(0)
     else:
         print(
-            f"PR title `{pr_title}` is invalid, it should be of the form: <PR_TYPE>(<PR_SCOPE>) "
-            f"<PR_SUBJECT> with <PR_TYPE> in {types}, and "
-            f"<PR_PROJECT> in {project} (where '*' is used when modifying multiple projects),"
+            f"PR title `{pr_title}` is invalid, it should be of the form:\n\n\t<PR_TYPE>(<PR_PROJECT>) "
+            f"<PR_SUBJECT>\n\n with <PR_TYPE> in [{types}], and "
+            f"<PR_PROJECT> in [{'|'.join(config['project']) + '|*'}] (where '*' is used when modifying multiple projects), "
             "and <PR_SUBJECT> starting with "
-            "a capitalized verb in the imperative mood and without any punctuation at the end.\n"
-            "A valid example is: `feat(framework) Add flwr build CLI command`"
+            "a capitalized verb in the imperative mood and without any punctuation at the end.\n\n"
+            "A valid example is:\n\n\t`feat(framework) Add flwr build CLI command`\n"
         )
         sys.exit(1)
