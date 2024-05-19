90630c1ed8460268b3a85e3bd311c4855c6172d5
Heng Pan <pan@flower.ai>
2024-05-19 11:17:55 +0100

    add LightSecAggState and implement _setup

--- src/py/flwr/client/mod/secure_aggregation/lightsecagg_mod.py
+++ src/py/flwr/client/mod/secure_aggregation/lightsecagg_mod.py
@@ -15,70 +15,142 @@
 """Modifier for the LightSecAgg protocol."""
 
 
-from logging import ERROR, INFO, WARNING
-from typing import Dict, List
+from __future__ import annotations
+from logging import ERROR, INFO, WARNING, DEBUG
+from typing import Dict, List, cast
 
 import galois
 import numpy as np
 
 from flwr.common.logger import log
-from flwr.common.parameter import ndarrays_to_parameters, parameters_to_ndarrays
 from flwr.common.secure_aggregation.crypto.symmetric_encryption import (
     bytes_to_public_key,
     generate_key_pairs,
     generate_shared_key,
+    private_key_to_bytes,
     public_key_to_bytes,
 )
 from flwr.common.secure_aggregation.lightsecagg_utils import (
     compute_aggregated_encoded_mask,
     encode_mask,
+    encrypt_sub_mask,
+    decrypt_sub_mask,
+    padding,
+    ndarray_to_bytes,
+    ndarray_from_bytes,
     mask_weights,
 )
+from flwr.common.secure_aggregation.lightsecagg_constants import Key, Stage, RECORD_KEY_STATE, RECORD_KEY_CONFIGS
 from flwr.common.secure_aggregation.ndarrays_arithmetic import (
     factor_combine,
     get_zero_parameters,
     parameters_multiply,
 )
 from flwr.common.secure_aggregation.quantization import quantize
-from flwr.common.typing import Scalar
+from flwr.common import RecordSet, ConfigsRecord
+from flwr.common.typing import NDArrayInt
+from dataclasses import dataclass, field
+
+
+@dataclass
+class LightSecAggState:
+    """State of the LightSecAgg protocol."""
+    
+    current_stage: str = Stage.SETUP
+    nid: int = 0
+    
+    # Protocol settings
+    N: int = 0  # The number of all clients
+    T: int = 0  # The maximum number of corrupted clients
+    U: int = 0  # The minimum number of surviving clients
+    p: int = 0  # The prime number for the finite field
+    d: int = 0  # The model size after padding
+    
+    # Quantization settings
+    clipping_range: float = 0.
+    target_range: int = 0
+    max_weight: float = 0.  # The maximum weight factor allowed
+    
+    # Runtime variables
+    pk: bytes = b""  # ECDH public key
+    sk: bytes = b""  # ECDH secret key
+    # The dict key is the node ID of another client (int)
+    # The dict value is the shared secret with that client from ECDH agreement
+    ss_dict: dict[int, bytes] = field(default_factory=dict)
+    # The dict key is the node ID of another client (int)
+    # The dict value is the encoded sub-mask from that client
+    encoded_sub_mask_dict: dict[int, NDArrayInt] = field(default_factory=dict)
+    mask: NDArrayInt = field(default=np.ndarray([], dtype=np.int_))
+    
+    
+    def to_recordset(self, rs: RecordSet) -> None:
+        """Save the instance to the given RecordSet."""
+        # Init the ConfigsRecord
+        cfg = ConfigsRecord()
+        flds = set(self.__dict__.keys())
+        
+        # Save unsupported types
+        flds.difference_update({"ss_dict", "encoded_sub_mask_dict", "mask"})
+        cfg["ss_dict:K"] = list(self.ss_dict.keys())
+        cfg["ss_dict:V"] = list(self.ss_dict.values())
+        cfg["encoded_sub_mask_dict:K"] = list(self.encoded_sub_mask_dict.keys())
+        cfg["encoded_sub_mask_dict:V"] = [
+            ndarray_to_bytes(v) for v in self.encoded_sub_mask_dict.values()
+        ]
+        cfg["mask"] = ndarray_to_bytes(self.mask)
+        
+        # Save supported types
+        for fld in flds:
+            cfg[fld] = self.__dict__[fld]  # type: ignore
+            
+        # Save to the RecordSet
+        rs.configs_records[RECORD_KEY_STATE] = cfg
+        
+    @classmethod
+    def from_recordset(cls, rs: RecordSet) -> LightSecAggState:
+        """Construct an instance from the given RecordSet."""
+        # Retrieve the ConfigsRecord
+        state = LightSecAggState()
+        if RECORD_KEY_STATE not in rs.configs_records:
+            return state
+        cfg = rs.configs_records[RECORD_KEY_STATE]
+        
+        # Retrieve unsupported types
+        keys = cast(list[str], cfg.pop("ss_dict:K"))
+        values = cast(list[bytes], cfg.pop("ss_dict:V"))
+        state.ss_dict = dict(zip(keys, values))
+        keys = cast(list[str], cfg.pop("encoded_sub_mask_dict:K"))
+        values = cast(list[bytes], cfg.pop("encoded_sub_mask_dict:V"))
+        state.encoded_sub_mask_dict = dict(zip(keys, [ndarray_from_bytes(v) for v in values]))
+        state.mask = ndarray_from_bytes(cfg["mask"])
+        
+        # Retrieve supported types
+        for k, v in cfg.items():
+            state.__dict__[k] = v  # type: ignore
+        return state
+        
 
 
 # set up configurations and return the public key
-def setup_config(client: SAClientWrapper, config_dict: Dict[str, Scalar]) -> bytes:
+def setup_config(state: LightSecAggState, configs: ConfigsRecord) -> ConfigsRecord:
+    log(DEBUG, "Node %d: starting stage 0...", state.nid)
     # Assigning parameter values to object fields
-    cfg = config_dict
-    client.sec_id = cfg["id"]
-    client.N = cfg["sample_num"]
-    client.id = cfg["id"]
-    client.T = cfg["privacy_guarantee"]
-    client.U = cfg["min_clients"]
-    client.p = cfg["prime_number"]
-    client.clipping_range = cfg["clipping_range"]
-    client.target_range = cfg["target_range"]
-    client.max_weights_factor = cfg["max_weights_factor"]
+    state.N = cast(int, configs[Key.SAMPLE_NUMBER])
+    state.T = cast(int, configs[Key.PRIVACY_GUARANTEE])
+    state.U = cast(int, configs[Key.MIN_ACTIVE_CLIENTS])
+    state.p = cast(int, configs[Key.PRIME_NUMBER])
+    state.clipping_range = cast(float, configs[Key.CLIPPING_RANGE])
+    state.target_range = cast(int, configs[Key.TARGET_RANGE])
+    state.max_weight = cast(float, configs[Key.MAX_WEIGHT])
 
-    # Testing , to be removed================================================
-    client.test = 0
-    if "test" in cfg and cfg["test"] == 1:
-        client.test = 1
-        client.test_vector_shape = [(cfg["test_vector_dimension"],)]
-        client.test_dropout_value = cfg["test_dropout_value"]
-        client.vector_length = client.test_vector_shape[0][0]
-        client.d = padding(client.vector_length + 1, client.U, client.T)
-    else:
-        # End =================================================================
-        weights = parameters_to_ndarrays(client.get_parameters({}))
-        client.vector_length = sum([o.size for o in weights])
-        client.d = padding(client.vector_length + 1, client.U, client.T)
-    # dict key is the ID of another client (int)
-    # dict value is the shared key, generated from the secret key and a public key of another client
-    client.shared_key_dict = {}
-    # dict key is the ID of another client (int)
-    # dict value is the encoded sub-mask z_j,i where j is the key, i is the id of current client.
-    client.encoded_mask_dict = {}
-    client.sk, client.pk = generate_key_pairs()
-    log(INFO, "LightSecAgg Stage 0 Completed: Config Set Up")
-    return public_key_to_bytes(client.pk)
+    state.d = cast(int, configs[Key.MODEL_SIZE])
+    state.d = padding(state.d + 1, state.U, state.T)
+    
+    # Generate ECDH keys
+    sk, pk = generate_key_pairs()
+    state.sk, state.pk = private_key_to_bytes(sk), public_key_to_bytes(pk)
+    log(DEBUG, "Node %d: stage 0 completes. uploading public keys...", state.nid)
+    return ConfigsRecord({Key.PUBLIC_KEY: state.pk})
 
 
 def ask_encrypted_encoded_masks(client, public_keys_dict: Dict[int, bytes]):
