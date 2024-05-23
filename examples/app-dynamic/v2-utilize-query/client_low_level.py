57e2c2f813e56e9c3a9c6e5b8d63a29c70692a9d
Adam Narozniak <adam@flower.dev>
2024-05-23 11:43:07 +0200

    Fix sending error message

--- examples/app-dynamic/v2-utilize-query/client_low_level.py
+++ examples/app-dynamic/v2-utilize-query/client_low_level.py
@@ -3,18 +3,20 @@ from flwr.common import Context
 from flwr.common import Message
 from flwr.common.constant import ErrorCode
 from flwr.common.message import Error
-
+import random
 # Flower ClientApp
 app = ClientApp()
 
 @app.query()
 def query(msg: Message, ctx: Context):
     try:
-        raise ValueError()
+        if random.choice([True, False]):
+            print("Error in client. Sending the Error reply.")
+            raise ValueError()
         # load the model/dataset
         print(f"In Client msg.content.configs_records: {msg.content.configs_records}")
         return msg.create_reply(msg.content)
     except:
-        msg.create_error_reply(
+        return msg.create_error_reply(
             error=Error(code=ErrorCode.UNKNOWN, reason="Unknown")
         )
