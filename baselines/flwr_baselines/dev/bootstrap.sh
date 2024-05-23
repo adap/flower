c8463d72d7c04fcd5aa1ab7e461145959567249d
Chong Shen Ng <chong.shen@flower.ai>
2024-05-23 11:19:41 +0100

    Revert versions in bootstrap.sh

--- baselines/flwr_baselines/dev/bootstrap.sh
+++ baselines/flwr_baselines/dev/bootstrap.sh
@@ -6,8 +6,8 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 ./dev/rm-caches.sh
 
 # Upgrade/install spcific versions of `pip`, `setuptools`, and `poetry`
-python -m pip install -U pip==24.0.0
-python -m pip install -U setuptools==69.5.1
+python -m pip install -U pip==23.3.1
+python -m pip install -U setuptools==68.2.2
 python -m pip install -U poetry==1.7.1
 
 # Use `poetry` to install project dependencies
