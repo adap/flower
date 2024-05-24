878ecee9355e53b434cfc1dda98c213c4312c201
Daniel J. Beutel <daniel@flower.ai>
2024-05-24 18:39:37 +0200

    Update src/py/flwr/client/supernode/app.py

--- src/py/flwr/client/supernode/app.py
+++ src/py/flwr/client/supernode/app.py
@@ -290,7 +290,7 @@ def _try_setup_client_authentication(
             "Error: Unable to parse the public key file in "
             "'--auth-supernode-public-key'. Authentication requires elliptic "
             "curve private and public key pair. Please ensure that the file "
-            "path contains valid public key file and try again."
+            "path contains a valid public key file and try again."
         )
 
     return (
