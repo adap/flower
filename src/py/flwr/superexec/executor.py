fd003ba9c283b9776886698fb67010a14a9597bf
Charles Beauville <charles@flower.ai>
2024-06-06 16:08:38 +0200

    Fix imports

--- src/py/flwr/superexec/executor.py
+++ src/py/flwr/superexec/executor.py
@@ -15,6 +15,7 @@
 """Executes and monitor a Flower run."""
 
 from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from subprocess import Popen
