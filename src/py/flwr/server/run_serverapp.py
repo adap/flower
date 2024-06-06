a7e990f1080b6e2cc3176877cccc149718eaf891
Charles Beauville <charles@flower.ai>
2024-06-06 18:37:22 +0200

    Revert driver and server_app changes

--- src/py/flwr/server/run_serverapp.py
+++ src/py/flwr/server/run_serverapp.py
@@ -130,9 +130,10 @@ def run_server_app() -> None:
 
     # Initialize GrpcDriver
     driver = GrpcDriver(
-        run_id=args.run_id,
         driver_service_address=args.server,
         root_certificates=root_certificates,
+        fab_id=args.fab_id,
+        fab_version=args.fab_version,
     )
 
     # Run the ServerApp with the Driver
