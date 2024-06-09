5d415a40461d4aeea72b6a5327e7655446fa8b4b
Charles Beauville <charles@flower.ai>
2024-06-09 18:35:37 +0200

    Rename and remove create_node

--- src/py/flwr/client/app.py
+++ src/py/flwr/client/app.py
@@ -265,8 +265,7 @@ def _start_client_internal(
         transport, server_address
     )
 
-    run_tracker = _RunTracker()
-    _ = run_tracker
+    app_state_tracker = _AppStateTracker()
 
     def _on_sucess(retry_state: RetryState) -> None:
         if retry_state.tries > 1:
@@ -308,7 +307,7 @@ def _start_client_internal(
 
     node_state = NodeState()
 
-    while not run_tracker.interrupt:
+    while not app_state_tracker.interrupt:
         sleep_duration: int = 0
         with connection(
             address,
@@ -322,12 +321,11 @@ def _start_client_internal(
             receive, send, create_node, delete_node, get_run = conn
 
             # Register node
-            run_tracker.create_node = create_node
             if create_node is not None:
                 create_node()  # pylint: disable=not-callable
 
-            run_tracker.register_signal_handler()
-            while not run_tracker.interrupt:
+            app_state_tracker.register_signal_handler()
+            while not app_state_tracker.interrupt:
                 try:
                     # Receive
                     message = receive()
@@ -399,7 +397,7 @@ def _start_client_internal(
                             e_code = ErrorCode.LOAD_CLIENT_APP_EXCEPTION
                             exc_entity = "SuperNode"
 
-                        if not run_tracker.interrupt:
+                        if not app_state_tracker.interrupt:
                             log(
                                 ERROR, "%s raised an exception", exc_entity, exc_info=ex
                             )
@@ -425,7 +423,7 @@ def _start_client_internal(
 
         if sleep_duration == 0:
             log(INFO, "Disconnect and shut down")
-            del run_tracker
+            del app_state_tracker
             break
 
         # Sleep and reconnect afterwards
@@ -599,7 +597,7 @@ def _init_connection(transport: Optional[str], server_address: str) -> Tuple[
 
 
 @dataclass
-class _RunTracker:
+class _AppStateTracker:
     interrupt: bool = False
 
     def register_signal_handler(self) -> None:
