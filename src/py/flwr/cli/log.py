3fe5fdc793c6a56dbb86c5e47f981f00014790a5
Chong Shen Ng <chong.shen@flower.ai>
2024-06-11 15:06:32 +0100

    Add type

--- src/py/flwr/cli/log.py
+++ src/py/flwr/cli/log.py
@@ -41,10 +41,10 @@ def log(
         """Log channel connectivity."""
         log(DEBUG, channel_connectivity)
 
-    def stream_logs(run_id, channel: grpc.Channel, duration: int) -> None:
+    def stream_logs(run_id: int, channel: grpc.Channel, duration: int) -> None:
         """Stream logs with connection refresh."""
 
-    def print_logs(run_id, channel: grpc.Channel, timeout: int) -> None:
+    def print_logs(run_id: int, channel: grpc.Channel, timeout: int) -> None:
         """Print logs."""
 
     channel = create_channel(
