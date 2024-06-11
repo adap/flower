14e8b35ba370034007960d10dec6f10e2dde70c7
Charles Beauville <charles@flower.ai>
2024-06-11 17:04:53 +0200

    order imports

--- src/py/flwr/superexec/exec_servicer.py
+++ src/py/flwr/superexec/exec_servicer.py
@@ -20,7 +20,7 @@ import threading
 import time
 from logging import INFO
 from subprocess import Popen
-from typing import Any, Dict, List, Generator
+from typing import Any, Dict, Generator, List
 
 import grpc
 
