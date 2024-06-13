e7b39505a804b5aa1046dcfa2ba4227500a91381
Daniel J. Beutel <daniel@flower.ai>
2024-06-13 22:37:34 +0200

    Update src/py/flwr/superexec/app.py

--- src/py/flwr/superexec/app.py
+++ src/py/flwr/superexec/app.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Flower superexec app."""
+"""Flower SuperExec app."""
 
 import argparse
 import sys
