64f6f3da48951c56cc45b4ae1251216824d63df8
Charles Beauville <charles@flower.ai>
2024-06-13 23:14:56 +0200

    feat(framework) Add SuperExec servicer

---
+++ src/py/flwr/superexec/executor.py
@@ -0,0 +1,59 @@
+# Copyright 2024 Flower Labs GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Execute and monitor a Flower run."""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from subprocess import Popen
+from typing import Optional
+
+
+@dataclass
+class Run:
+    """Represents a Flower run (composed of a run_id and the associated process)."""
+
+    run_id: int
+    proc: Popen  # type: ignore
+
+
+class Executor(ABC):
+    """Execute and monitor a Flower run."""
+
+    @abstractmethod
+    def start_run(
+        self,
+        fab_file: bytes,
+        ttl: Optional[float] = None,
+    ) -> Run:
+        """Start a run using the given Flower App ID and version.
+
+        This method creates a new run on the SuperLink, returns its run_id
+        and also starts the run execution.
+
+        Parameters
+        ----------
+        fab_file : bytes
+            The Flower App Bundle file bytes.
+        ttl : Optional[float] (default: None)
+            Time-to-live for the round trip of this message, i.e., the time from sending
+            this message to receiving a reply. It specifies in seconds the duration for
+            which the message and its potential reply are considered valid. If unset,
+            the default TTL (i.e., `common.DEFAULT_TTL`) will be used.
+
+        Returns
+        -------
+        run_id : int
+            The run_id of the run created by the SuperLink.
+        """
