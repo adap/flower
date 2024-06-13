a3ffe02e58afc5d03b1b6cea95f9b87609fa832e
Chong Shen Ng <chong.shen@flower.ai>
2024-06-13 11:20:35 +0100

    Update flwr log

--- src/py/flwr/cli/log.py
+++ src/py/flwr/cli/log.py
@@ -14,22 +14,61 @@
 # ==============================================================================
 """Flower command line interface `log` command."""
 
-from logging import DEBUG
+import time
+from logging import DEBUG, INFO, ERROR
 
+import grpc
 import typer
 from typing_extensions import Annotated
 
 from flwr.common.grpc import GRPC_MAX_MESSAGE_LENGTH, create_channel
-from flwr.common.logger import log as flwr_log
-from flwr.proto.exec_pb2 import FetchLogsRequest  # pylint: disable=E0611
+from flwr.common.logger import log as logger
+from flwr.proto.exec_pb2 import StreamLogsRequest
 from flwr.proto.exec_pb2_grpc import ExecStub
 
+
+def stream_logs(run_id: int, channel: grpc.Channel, duration: int) -> None:
+    """Stream logs from the beginning of a run with connection refresh."""
+    start_time = time.time()
+    stub = ExecStub(channel)
+    req = StreamLogsRequest(run_id=run_id)
+
+    for res in stub.StreamLogs(req):
+        print(res.log_output)
+        if time.time() - start_time > duration:
+            break
+
+def print_logs(run_id: int, channel: grpc.Channel, timeout: int) -> None:
+    """Print logs from the beginning of a run."""
+    stub = ExecStub(channel)
+    req = StreamLogsRequest(run_id=run_id)
+
+    while True:
+        try:
+            # Enforce timeout for graceful exit
+            for res in stub.StreamLogs(req, timeout=timeout):
+                print(res.log_output)
+        except grpc.RpcError as e:
+            if e.code() == grpc.StatusCode.DEADLINE_EXCEEDED:
+                logger(INFO, "No new logs, exiting")
+            if e.code() == grpc.StatusCode.NOT_FOUND:
+                logger(ERROR, "`run_id` is invalid, exiting")
+        finally:
+            channel.close()
+            break
 
 def log(
     run_id: Annotated[
         int,
         typer.Option(case_sensitive=False, help="The Flower run ID to query"),
     ],
+    period: Annotated[
+        int,
+        typer.Option(
+            case_sensitive=False,
+            help="Use this to set connection refresh time period (in seconds)",
+        ),
+    ] = 60,
     follow: Annotated[
         bool,
         typer.Option(case_sensitive=False, help="Use this flag to follow logstream"),
@@ -39,7 +78,8 @@ def log(
 
     def on_channel_state_change(channel_connectivity: str) -> None:
         """Log channel connectivity."""
-        flwr_log(DEBUG, channel_connectivity)
+        logger(DEBUG, channel_connectivity)
+
 
     channel = create_channel(
         server_address="127.0.0.1:9093",
@@ -49,12 +89,21 @@ def log(
         interceptors=None,
     )
     channel.subscribe(on_channel_state_change)
-    stub = ExecStub(channel)
 
-    req = FetchLogsRequest(run_id=run_id)
-
-    for res in stub.FetchLogs(req):
-        print(res.log_output)
-        if follow:
-            continue
-        break
+    if follow:
+        try:
+            while True:
+                logger(INFO, "Starting logstream")
+                stream_logs(run_id, channel, period)
+                time.sleep(2)
+                logger(INFO, "Reconnecting to logstream")
+        except KeyboardInterrupt:
+            logger(INFO, "Exiting logstream")
+        except grpc.RpcError as e:
+            if e.code() == grpc.StatusCode.NOT_FOUND:
+                logger(ERROR, "`run_id` is invalid, exiting")
+        finally:
+            channel.close()
+    else:
+        logger(INFO, "Printing logstream")
+        print_logs(run_id, channel, timeout=1)
