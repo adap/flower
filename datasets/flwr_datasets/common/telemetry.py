0a1da1b911bda1ba60788437f9c48557b4e23546
Adam Narozniak <51029327+adam-narozniak@users.noreply.github.com>
2024-06-17 10:07:35 +0200

    Update datasets/flwr_datasets/common/telemetry.py
    Co-authored-by: Daniel J. Beutel <daniel@flower.ai>

--- datasets/flwr_datasets/common/telemetry.py
+++ datasets/flwr_datasets/common/telemetry.py
@@ -169,7 +169,7 @@ def create_event(event_type: EventType, event_details: Optional[Dict[str, Any]])
         "source": state["source"],
         "cluster": state["cluster"],
         "date": date,
-        "flower": {
+        "package": {
             "package_name": package_name,
             "package_version": package_version,
         },
