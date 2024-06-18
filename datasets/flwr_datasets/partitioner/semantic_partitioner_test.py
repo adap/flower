ea2da1888477efd54b7423ff41906ef2588a35c5
KarhouTam <karhoutam@qq.com>
2024-06-18 21:57:57 +0800

    Optimize test cases

--- datasets/flwr_datasets/partitioner/semantic_partitioner_test.py
+++ datasets/flwr_datasets/partitioner/semantic_partitioner_test.py
@@ -16,6 +16,7 @@
 
 
 # pylint: disable=W0212
+import string
 import unittest
 from typing import Tuple
 
@@ -28,18 +29,19 @@ from flwr_datasets.partitioner.semantic_partitioner import SemanticPartitioner
 
 
 def _dummy_setup(
+    data_shape: tuple = (28, 28, 1),
     num_partitions: int = 3,
     num_rows: int = 10,
     partition_by: str = "label",
     efficient_net_type: int = 0,
-    pca_components: int = 128,
+    pca_components: int = 6,
     gmm_max_iter: int = 2,
     gmm_init_params: str = "random",
 ) -> Tuple[Dataset, SemanticPartitioner]:
     """Create a dummy dataset and partitioner for testing."""
     data = {
-        "labels": [i % 3 for i in range(num_rows)],
-        "features": [np.random.randn(1, 28, 28) for _ in range(num_rows)],
+        "image": [np.random.randn(*data_shape) for _ in range(num_rows)],
+        "label": [i % 3 for i in range(num_rows)],
     }
     dataset = Dataset.from_dict(data)
     partitioner = SemanticPartitioner(
@@ -57,32 +59,35 @@ def _dummy_setup(
 class TestSemanticPartitionerSuccess(unittest.TestCase):
     """Test SemanticPartitioner used with no exceptions."""
 
-    @parameterized.expand(  # type: ignore
+    @parameterized.expand(
         [
-            # num_partitions, num_rows, partition_by, efficient_net_type, pca_components, gmm_max_iter, gmm_init_way
-            (3, 50, "labels", 0, 128, 2, "kmeans"),
-            (5, 100, "labels", 0, 256, 1, "random"),
+            # data_shape, num_partitions, num_rows, partition_by, efficient_net_type, pca_components, gmm_max_iter, gmm_init_params
+            ((28, 28, 1), 3, 50, "label", 0, 128, 2, "kmeans"),
+            ((1, 28, 28), 5, 100, "label", 2, 256, 1, "random"),
+            ((32, 32, 3), 5, 100, "label", 7, 256, 1, "k-means++"),
         ]
     )
     def test_valid_initialization(
         self,
+        data_shape: tuple,
         num_partitions: int,
         num_rows: int,
         partition_by: str,
         efficient_net_type: int,
         pca_components: int,
         gmm_max_iter: int,
-        gmm_init_way: str,
+        gmm_init_params: str,
     ) -> None:
         """Test if alpha is correct scaled based on the given num_partitions."""
         _, partitioner = _dummy_setup(
-            num_partitions,
-            num_rows,
-            partition_by,
-            efficient_net_type,
-            pca_components,
-            gmm_max_iter,
-            gmm_init_way,
+            data_shape=data_shape,
+            num_partitions=num_partitions,
+            num_rows=num_rows,
+            partition_by=partition_by,
+            efficient_net_type=efficient_net_type,
+            pca_components=pca_components,
+            gmm_max_iter=gmm_max_iter,
+            gmm_init_params=gmm_init_params,
         )
         self.assertEqual(
             (
@@ -99,14 +104,19 @@ class TestSemanticPartitionerSuccess(unittest.TestCase):
                 efficient_net_type,
                 pca_components,
                 gmm_max_iter,
-                gmm_init_way,
+                gmm_init_params,
             ),
         )
+
+    @parameterized.expand([((28, 28, 1),), ((3, 32, 32),), ((28, 28),)])
+    def test_data_shape(self, data_shape: tuple):
+        """Test if data_shape is correct."""
+        _, partitioner = _dummy_setup(data_shape=data_shape)
+        partitioner.load_partition(0)
 
     @parameterized.expand([(0,), (1,), (2,), (3,)])
     def test_efficient_net_config(self, efficient_net_type: int):
         """Test if efficient_net_backbone and efficient_net_pretrained_weight are correct."""
-        num_partitions, num_rows, partition_by = 3, 100, "labels"
         efficient_nets_dict = [
             (models.efficientnet_b0, models.EfficientNet_B0_Weights.DEFAULT),
             (models.efficientnet_b1, models.EfficientNet_B1_Weights.DEFAULT),
@@ -117,15 +127,7 @@ class TestSemanticPartitionerSuccess(unittest.TestCase):
             (models.efficientnet_b6, models.EfficientNet_B6_Weights.DEFAULT),
             (models.efficientnet_b7, models.EfficientNet_B7_Weights.DEFAULT),
         ]
-        _, partitioner = _dummy_setup(
-            num_partitions,
-            num_rows,
-            partition_by,
-            efficient_net_type=efficient_net_type,
-            pca_components=128,
-            gmm_max_iter=2,
-            gmm_init_params="random",
-        )
+        _, partitioner = _dummy_setup(efficient_net_type=efficient_net_type)
         self.assertEqual(
             (
                 partitioner._efficient_net_backbone,
@@ -137,35 +139,19 @@ class TestSemanticPartitionerSuccess(unittest.TestCase):
             ),
         )
 
-    @parameterized.expand([(64,), (128,), (256,)])
+    @parameterized.expand([(64,), (96,), (32,)])
     def test_pca_components(self, pca_components: int) -> None:
         """Test if pca_components is correct scaled based on the given num_partitions."""
-        num_partitions, num_rows, partition_by = 3, 300, "labels"
-        _, partitioner = _dummy_setup(
-            num_partitions,
-            num_rows,
-            partition_by,
-            efficient_net_type=0,
-            pca_components=pca_components,
-            gmm_max_iter=2,
-            gmm_init_params="random",
-        )
+        _, partitioner = _dummy_setup(num_rows=100, pca_components=pca_components)
         self.assertEqual(partitioner._pca_components, pca_components)
 
-    @parameterized.expand([(2, "random"), (2, "kmeans"), (2, "k-means++")])
+    @parameterized.expand([(1, "random"), (2, "kmeans"), (2, "k-means++")])
     def test_gaussian_mixture_model(
         self, gmm_max_iter: int, gmm_init_params: str
     ) -> None:
-        """Test if gmm_max_iter and gmm_init_way are correct."""
-        num_partitions, num_rows, partition_by = 3, 300, "labels"
+        """Test if gmm_max_iter and gmm_init_params are correct."""
         _, partitioner = _dummy_setup(
-            num_partitions,
-            num_rows,
-            partition_by,
-            efficient_net_type=0,
-            pca_components=128,
-            gmm_max_iter=gmm_max_iter,
-            gmm_init_params=gmm_init_params,
+            gmm_max_iter=gmm_max_iter, gmm_init_params=gmm_init_params
         )
         self.assertEqual(
             (partitioner._gmm_max_iter, partitioner._gmm_init_params),
@@ -174,29 +160,42 @@ class TestSemanticPartitionerSuccess(unittest.TestCase):
 
     def test_determine_partition_id_to_indices(self) -> None:
         """Test the determine_nod_id_to_indices matches the flag after the call."""
-        num_partitions, num_rows, partition_by = 3, 300, "labels"
-        _, partitioner = _dummy_setup(num_partitions, num_rows, partition_by)
+
+        _, partitioner = _dummy_setup()
         partitioner._determine_partition_id_to_indices_if_needed()
         self.assertTrue(
             partitioner._partition_id_to_indices_determined
-            and len(partitioner._partition_id_to_indices) == num_partitions
+            and len(partitioner._partition_id_to_indices) == 3
         )
 
 
 class TestSemanticPartitionerFailure(unittest.TestCase):
     """Test SemanticPartitioner failures (exceptions) by incorrect usage."""
 
-    @parameterized.expand([(-2,), (-1,), (3,), (4,), (100,)])  # type: ignore
+    def test_invalid_dataset_type(self):
+        """Test if raises when the dataset is not an image dataset."""
+        alphabets = list(string.ascii_uppercase)
+        data = {
+            "letters": [alphabets[i % len(alphabets)] for i in range(300)],
+            "label": [i for i in range(300)],
+        }
+        dataset = Dataset.from_dict(data)
+        partitioner = SemanticPartitioner(num_partitions=3, partition_by="label")
+        partitioner.dataset = dataset
+        with self.assertRaises(TypeError):
+            partitioner.load_partition(0)
+
+    @parameterized.expand([((28, 1, 28),), ((3, 3, 32, 32),), ((28,),)])
+    def test_invalid_data_shape(self, data_shape: tuple):
+        """Test if raises when the data_shape is not a tuple of length 2."""
+        with self.assertRaises(ValueError):
+            _, partitioner = _dummy_setup(data_shape=data_shape)
+            partitioner.load_partition(0)
+
+    @parameterized.expand([(-2,), (-1,), (3,), (4,)])  # type: ignore
     def test_load_invalid_partition_index(self, partition_id):
         """Test if raises when the load_partition is above the num_partitions."""
-        _, partitioner = _dummy_setup(
-            num_partitions=3,
-            num_rows=300,
-            partition_by="labels",
-            efficient_net_type=0,
-            gmm_max_iter=2,
-            gmm_init_params="random",
-        )
+        _, partitioner = _dummy_setup(num_partitions=3)
         with self.assertRaises(KeyError):
             partitioner.load_partition(partition_id)
 
@@ -205,12 +204,7 @@ class TestSemanticPartitionerFailure(unittest.TestCase):
         """
         Test if efficient_net_type is not an integer or not in range [0, 7]."""
         with self.assertRaises((ValueError, TypeError)):
-            SemanticPartitioner(
-                num_partitions=2,
-                efficient_net_type=efficient_net_type,
-                partition_by="labels",
-                gmm_max_iter=2,
-            )
+            SemanticPartitioner(efficient_net_type=efficient_net_type)
 
     @parameterized.expand(  # type: ignore
         [(0,), (-1,), (11,), (100,)]
@@ -218,21 +212,14 @@ class TestSemanticPartitionerFailure(unittest.TestCase):
     def test_invalid_num_partitions(self, num_partitions):
         """Test if 0 is invalid num_partitions."""
         with self.assertRaises(ValueError):
-            _, partitioner = _dummy_setup(
-                num_partitions=num_partitions, num_rows=10, partition_by="labels"
-            )
+            _, partitioner = _dummy_setup(num_partitions=num_partitions, num_rows=10)
             partitioner.load_partition(0)
 
     @parameterized.expand([(0,), (-1,), (2.0,), (11,)])
     def test_invalid_pca_components(self, pca_components):
         """Test if pca_components is not a positive integer."""
         with self.assertRaises((ValueError, TypeError)):
-            _, partitioner = _dummy_setup(
-                num_partitions=2,
-                num_rows=10,
-                partition_by="labels",
-                pca_components=pca_components,
-            )
+            _, partitioner = _dummy_setup(pca_components=pca_components)
             partitioner.load_partition(0)
 
     @parameterized.expand(
@@ -251,11 +238,7 @@ class TestSemanticPartitionerFailure(unittest.TestCase):
         """
         with self.assertRaises(ValueError):
             _, partitioner = _dummy_setup(
-                num_partitions=2,
-                num_rows=10,
-                partition_by="labels",
-                gmm_max_iter=gmm_max_iter,
-                gmm_init_params=gmm_init_params,
+                gmm_max_iter=gmm_max_iter, gmm_init_params=gmm_init_params
             )
             partitioner.load_partition(0)
 
