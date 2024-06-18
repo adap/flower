deab241f17e0845ba6f0cf1e1923621d630cabbb
mohammadnaseri <mohammad@flower.ai>
2024-06-18 18:24:16 +0100

    Compute accuracy

--- examples/fl-tabular/server.py
+++ examples/fl-tabular/server.py
@@ -5,10 +5,19 @@ from flwr.common import ndarrays_to_parameters
 
 net = IncomeClassifier()
 params = ndarrays_to_parameters(get_weights(net))
+
+
+def weighted_average(metrics):
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+
+    return {"accuracy": sum(accuracies) / sum(examples)}
+
+
 strategy = FedAvg(
     initial_parameters=params,
+    evaluate_metrics_aggregation_fn=weighted_average,
 )
-
 app = ServerApp(
     strategy=strategy,
     config=ServerConfig(num_rounds=5),
