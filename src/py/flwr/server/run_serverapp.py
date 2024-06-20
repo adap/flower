62e85a0d12cd081030c88603d95c4705322441e2
Daniel J. Beutel <daniel@flower.ai>
2024-06-20 16:34:54 +0200

    Update src/py/flwr/server/run_serverapp.py

--- src/py/flwr/server/run_serverapp.py
+++ src/py/flwr/server/run_serverapp.py
@@ -259,7 +259,7 @@ def _parse_args_run_server_app() -> argparse.ArgumentParser:
         "--flwr-dir",
         default=None,
         help="""The path containing installed Flower Apps.
-    By default, this value isequal to:
+    By default, this value is equal to:
 
         - `$FLWR_HOME/` if `$FLWR_HOME` is defined
         - `$XDG_DATA_HOME/.flwr/` if `$XDG_DATA_HOME` is defined
