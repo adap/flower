c9f6048d3fc9f7d965f0767086041c0846441b92
Adam Narozniak <adam@flower.dev>
2024-06-21 11:47:43 +0200

    Create utils for extraction of private attributes

--- datasets/flwr_datasets/partitioner/utils.py
+++ datasets/flwr_datasets/partitioner/utils.py
@@ -15,8 +15,9 @@
 """Utils for partitioners."""
 
 
+import inspect
 import json
-from typing import Dict, List
+from typing import Any, Dict, List
 
 
 def load_partition_id_to_indices(path: str) -> Dict[int, List[int]]:
@@ -40,3 +41,49 @@ def load_partition_id_to_indices(path: str) -> Dict[int, List[int]]:
             for partition_id, indices_list in str_partition_id_to_indices.items()
         }
     return partition_id_to_indices
+
+
+def _extract_private_attributes_from_object(instance: object) -> Dict[str, Any]:
+    """Extract all the private attributes based on the constructor parameters.
+
+    It is assumed that all parameters are stored as attributes.
+    e.g. in IidPartitioner(num_partitions) it is assumed that there exist
+    _num_partitions. This method extracts all the attributes that should exist.
+    In this way for IidPartitioner the following dict is returned
+    {"num_partitions": instance._num_partitions}.
+    It is a utility function for to_config saving.
+
+    Parameters
+    ----------
+    instance: object
+        object from which the private attributes will be extracted
+
+    Returns
+    -------
+    name_in_constructor_to_private_attribute: Dict[str, Any]
+        Dictionary from that maps names in constructor to saved private attribute values
+    """
+    constructor_parameters = [
+        p for p in inspect.signature(instance.__init__).parameters if p != "self"
+    ]
+
+    name_in_constructor_to_private_attribute = {}
+    for constructor_parameter in constructor_parameters:
+        # add the leading underscore
+        private_attr = f"_{constructor_parameter}"
+        if hasattr(instance, private_attr):
+            name_in_constructor_to_private_attribute[constructor_parameter] = getattr(
+                instance, private_attr
+            )
+    return name_in_constructor_to_private_attribute
+
+def _remove_leading_underscores_from_config(config: Dict[str, Any]) -> Dict[str, Any]:
+    """
+    Remove all the keys from the dictionary that start with underscore (_).
+    """
+    new_config = {}
+    for key, value in config.items():
+        if not key.startswith("_"):
+            new_config[key] = value
+
+    return new_config
