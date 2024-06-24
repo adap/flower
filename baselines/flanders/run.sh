d6b60daec29dcb4b0185fdf76c88318d7637cf05
edogab33 <edo.gab33@gmail.com>
2024-06-24 12:29:52 +0200

    Update .sh script

--- baselines/flanders/run.sh
+++ baselines/flanders/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-python -m flanders.main --multirun server.num_rounds=50 dataset=mnist,cifar strategy=flanders aggregate_fn=fedavg,trimmedmean,fedmedian,bulyan,dnc server.pool_size=100 server.num_malicious=2 server.attack_fn=gaussian,lie,fang,minmax server.warmup_rounds=2 &> flanders.log
+python -m flanders.main --multirun server.num_rounds=50 dataset=mnist strategy=flanders aggregate_fn=fedavg,trimmedmean,fedmedian,krum,bulyan server.pool_size=100 server.num_malicious=0,20,60,80 server.attack_fn=gaussian,lie,fang,minmax server.warmup_rounds=2 client_resources.num_cpus=0.1 client_resources.num_gpus=0.1
+
+python -m flanders.main --multirun server.num_rounds=50 dataset=mnist strategy=fedavg,trimmedmean,fedmedian,krum,bulyan server.pool_size=100 server.num_malicious=0,20,60,80 server.attack_fn=gaussian,lie,fang,minmax server.warmup_rounds=2 client_resources.num_cpus=0.1 client_resources.num_gpus=0.1
+
+python -m flanders.main --multirun server.num_rounds=50 dataset=fmnist strategy=flanders aggregate_fn=fedavg,trimmedmean,fedmedian,krum,bulyan server.pool_size=100 server.num_malicious=0,20,60,80 server.attack_fn=gaussian,lie,fang,minmax server.warmup_rounds=2 client_resources.num_cpus=0.1 client_resources.num_gpus=0.1
+
+python -m flanders.main --multirun server.num_rounds=50 dataset=fmnist strategy=fedavg,trimmedmean,fedmedian,krum,bulyan server.pool_size=100 server.num_malicious=0,20,60,80 server.attack_fn=gaussian,lie,fang,minmax server.warmup_rounds=2 client_resources.num_cpus=0.1 client_resources.num_gpus=0.1
