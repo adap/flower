974df349b1e50c6f6bf54faf452bcd5ef1bd480f
Charles Beauville <charlesbeauville@gmail.com>
2024-06-29 13:36:54 +0200

    Update pip and setuptools as well

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -9,8 +9,8 @@ cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"/../
 ./dev/rm-caches.sh
 
 # Upgrade/install spcific versions of `pip`, `setuptools`, and `poetry`
-python -m pip install -U pip==24.0.0
-python -m pip install -U setuptools==69.5.1
+python -m pip install -U pip==24.1.1
+python -m pip install -U setuptools==70.1.1
 python -m pip install -U poetry==1.8.3
 
 # Use `poetry` to install project dependencies
