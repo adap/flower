f086ee9a3a7806e2971fd0d93b5ab2aa9456db7c
Charles Beauville <charles@flower.ai>
2024-06-29 13:12:58 +0200

    ci(*:skip) Update poetry to latest version

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -11,7 +11,7 @@ cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"/../
 # Upgrade/install spcific versions of `pip`, `setuptools`, and `poetry`
 python -m pip install -U pip==24.0.0
 python -m pip install -U setuptools==69.5.1
-python -m pip install -U poetry==1.7.1
+python -m pip install -U poetry==1.8.3
 
 # Use `poetry` to install project dependencies
 python -m poetry install --all-extras
