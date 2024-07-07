3f8c7bdec1c17a8cdc228fb1900e5a4a4ec69fe5
Chong Shen Ng <chong.shen@flower.ai>
2024-07-07 11:15:44 +0100

    Update serverapp

--- examples/quickstart-huggingface/huggingface_example/server_app.py
+++ examples/quickstart-huggingface/huggingface_example/server_app.py
@@ -1,17 +1,25 @@
 """huggingface_example: A Flower / Hugging Face app."""
 
-import flwr as fl
+from flwr.server import ServerApp, ServerConfig
+from flwr.server.strategy import FedAvg
 
-if __name__ == "__main__":
-    # Define strategy
-    strategy = fl.server.strategy.FedAvg(
-        fraction_fit=1.0,
-        fraction_evaluate=1.0,
-    )
+# if __name__ == "__main__":
+# Define strategy
+strategy = FedAvg(
+    fraction_fit=1.0,
+    fraction_evaluate=1.0,
+)
 
-    # Start server
-    fl.server.start_server(
-        server_address="0.0.0.0:8080",
-        config=fl.server.ServerConfig(num_rounds=3),
-        strategy=strategy,
-    )
+#    # Start server
+#    fl.server.start_server(
+#        server_address="0.0.0.0:8080",
+#        config=fl.server.ServerConfig(num_rounds=3),
+#        strategy=strategy,
+#    )
+
+config = ServerConfig(num_rounds=3)
+
+app = ServerApp(
+    config=config,
+    strategy=strategy,
+)
