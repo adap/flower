c801ce76df6675e003541a3a8cf933d516ba475c
Adam Narozniak <adam@flower.dev>
2024-07-09 12:52:57 +0200

    Rename the partitioner to pathological

--- datasets/flwr_datasets/partitioner/pathological_partitioner.py
+++ datasets/flwr_datasets/partitioner/pathological_partitioner.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Class constrained partitioner class that works with Hugging Face Datasets."""
+"""Pathological partitioner class that works with Hugging Face Datasets."""
 
 
 import warnings
