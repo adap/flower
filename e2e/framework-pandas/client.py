3d1b66bb422d222f9ec052b6ea0fa86eb1ade738
Daniel Nata Nugraha <daniel.nata@flower.ai>
2024-07-11 16:40:29 +0200

    omit pandas

--- e2e/framework-pandas/client.py
+++ e2e/framework-pandas/client.py
@@ -6,10 +6,7 @@ import pandas as pd
 
 import flwr as fl
 
-contents = os.listdir("./data")
-raise ValueError((f"Directory contents: {', '.join(contents)}"))
-
-df = pd.read_csv("./framework-pandas/data/client.csv")
+df = pd.read_csv("./data/client.csv")
 
 column_names = ["sepal length (cm)", "sepal width (cm)"]
 
