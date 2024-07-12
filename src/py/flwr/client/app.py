da1820532f138fe02fe862b1447d863e38cabefe
Charles Beauville <charles@flower.ai>
2024-07-12 15:00:12 +0200

    Remove unused import

--- src/py/flwr/client/app.py
+++ src/py/flwr/client/app.py
@@ -18,7 +18,7 @@ import signal
 import sys
 import time
 from dataclasses import dataclass
-from logging import DEBUG, ERROR, INFO, WARN
+from logging import ERROR, INFO, WARN
 from pathlib import Path
 from typing import Callable, ContextManager, Dict, Optional, Tuple, Type, Union
 
