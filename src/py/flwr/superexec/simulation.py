e5c989921edb738cdff035f367489320bde50e36
jafermarq <javier@flower.ai>
2024-07-13 11:25:36 +0200

    update & mirroing flow in deployment executor

--- src/py/flwr/superexec/simulation.py
+++ src/py/flwr/superexec/simulation.py
@@ -15,18 +15,19 @@
 """Simulation engine executor."""
 
 
-import os
 import subprocess
-from logging import ERROR, INFO
 import sys
+from logging import ERROR, INFO
 from pathlib import Path
-from flwr.common.logger import log
-from typing import Optional, Dict
+from typing import Dict, Optional
 
 from typing_extensions import override
 
 from flwr.cli.config_utils import get_fab_metadata
 from flwr.cli.install import install_from_fab
+from flwr.common.constant import RUN_ID_NUM_BYTES
+from flwr.common.logger import log
+from flwr.server.superlink.state.utils import generate_rand_int_from_bytes
 
 from .executor import Executor, RunTracker
 
@@ -39,46 +40,64 @@ class SimulationEngine(Executor):
         self, fab_file: bytes, override_config: Dict[str, str]
     ) -> Optional[RunTracker]:
         """Start run using the Flower Simulation Engine."""
+        try:
+            num_supernodes = override_config.get("num-supernodes")
+            if num_supernodes is None:
+                log(
+                    ERROR,
+                    "To start a run with the simulation plugin, please specify "
+                    "the number of supernodes. You can do this by using the "
+                    "`--config` argument of `flwr run`.",
+                )
+                return None
 
-        num_supernodes = override_config.get('num-supernodes')
-        if num_supernodes is None:
-            log(ERROR, "To start a run with the simulation plugin, please specify the number of supernodes. You can do this by using the `--config` argumet of `flwr run`.")
+            # Install FAB to flwr dir
+            fab_version, fab_id = get_fab_metadata(fab_file)
+            fab_path = install_from_fab(fab_file, None, True)
+
+            # Install FAB Python package
+            subprocess.check_call(
+                [sys.executable, "-m", "pip", "install", str(fab_path)],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
+
+            # In Simulation there is no SuperLink, still we create a run_id
+            run_id = generate_rand_int_from_bytes(RUN_ID_NUM_BYTES)
+            log(INFO, "Created run %s", str(run_id))
+
+            fab_name = Path(fab_id).name
+
+            # Start Simulation
+            proc = (
+                subprocess.Popen(
+                    [
+                        "flower-simulation",
+                        "--client-app",
+                        f"{fab_name}.client:app",
+                        "--server-app",
+                        f"{fab_name}.server:app",
+                        "--num-supernodes",
+                        f"{num_supernodes}",
+                        "--run-id",
+                        str(run_id),
+                    ],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    text=True,
+                ),
+            )
+            log(INFO, "Started run %s", str(run_id))
+
+            return RunTracker(
+                run_id=run_id,
+                proc=proc,
+            )
+
+        # pylint: disable-next=broad-except
+        except Exception as e:
+            log(ERROR, "Could not start run: %s", str(e))
             return None
-
-        _, fab_id = get_fab_metadata(fab_file)
-
-        run_id = int.from_bytes(os.urandom(8), "little", signed=True)
-        log(INFO, "Created run %s", str(run_id))
-
-        fab_path = install_from_fab(fab_file, None, True)
-
-        subprocess.check_call(
-            [sys.executable, "-m", "pip", "install", str(fab_path)],
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-
-        fab_name = Path(fab_id).name
-
-        return RunTracker(
-            run_id=run_id,
-            proc=subprocess.Popen(
-                [
-                    "flower-simulation",
-                    "--client-app",
-                    f"{fab_name}.client:app",
-                    "--server-app",
-                    f"{fab_name}.server:app",
-                    "--num-supernodes",
-                    f"{num_supernodes}",
-                    "--run-id",
-                    str(run_id),
-                ],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True,
-            ),
-        )
 
 
 executor = SimulationEngine()
