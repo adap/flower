b1450bb440a895e57136ee1f6649bb5f7341469b
Chong Shen Ng <chong.shen@flower.ai>
2024-07-15 09:46:14 +0100

    Update top level docstring

--- datasets/flwr_datasets/partitioner/distribution_partitioner.py
+++ datasets/flwr_datasets/partitioner/distribution_partitioner.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Distribution partitioner."""
+"""Distribution partitioner class that works with Hugging Face Datasets."""
 
 
 from collections import Counter
