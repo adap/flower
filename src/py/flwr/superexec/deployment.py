6000b54704f590036d90544cd35b9f311c39a2fc
Daniel J. Beutel <daniel@flower.ai>
2024-07-22 19:33:13 +0200

    Update src/py/flwr/superexec/deployment.py

--- src/py/flwr/superexec/deployment.py
+++ src/py/flwr/superexec/deployment.py
@@ -91,7 +91,7 @@ class DeploymentEngine(Executor):
             self.superlink = superlink_address
         if root_certificates := config.get("root-certificates"):
             if not isinstance(root_certificates, str):
-                raise ValueError("The `root-certificates` value should be a string.")
+                raise ValueError("The `root-certificates` value should be of type `str`.")
             self.root_certificates = root_certificates
             self.root_certificates_bytes = Path(str(root_certificates)).read_bytes()
         if flwr_dir := config.get("flwr-dir"):
