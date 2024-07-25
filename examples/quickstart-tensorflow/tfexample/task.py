8f77b44698bfc634a34ceffd4d43bef2481ec0ca
jafermarq <javier@flower.ai>
2024-07-25 22:27:14 +0100

    init

---
+++ examples/quickstart-tensorflow/tfexample/task.py
@@ -0,0 +1,46 @@
+"""tfexample: A Flower / TensorFlow app."""
+
+import os
+
+import tensorflow as tf
+import numpy as np
+from flwr_datasets import FederatedDataset
+from flwr_datasets.partitioner import IidPartitioner
+
+
+# Make TensorFlow log less verbose
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+
+def load_model(learning_rate: float = 0.001):
+    # Load model and data (MobileNetV2, CIFAR-10)
+    model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+    optimizer = tf.keras.optimizers.Adam(learning_rate)
+    model.compile(
+        optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+    return model
+
+
+fds = None  # Cache FederatedDataset
+
+
+def load_data(partition_id, num_partitions):
+    # Download and partition dataset
+    # Only initialize `FederatedDataset` once
+    global fds
+    if fds is None:
+        partitioner = IidPartitioner(num_partitions=num_partitions)
+        fds = FederatedDataset(
+            dataset="uoft-cs/cifar10",
+            partitioners={"train": partitioner},
+        )
+    partition = fds.load_partition(partition_id, "train")
+    partition.set_format("numpy")
+
+    # Divide data on each node: 80% train, 20% test
+    partition = partition.train_test_split(test_size=0.2)
+    x_train, y_train = partition["train"]["img"] / 255.0, partition["train"]["label"]
+    x_test, y_test = partition["test"]["img"] / 255.0, partition["test"]["label"]
+
+    return x_train, y_train, x_test, y_test
