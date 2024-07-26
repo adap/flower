b401a7199d18b30a264ae0dfb362d9a6ac116841
jafermarq <javier@flower.ai>
2024-07-26 16:10:21 +0100

    updates

--- examples/quickstart-pytorch/pytorchexample/client_app.py
+++ examples/quickstart-pytorch/pytorchexample/client_app.py
@@ -45,12 +45,7 @@ class FlowerClient(NumPyClient):
 
 
 def client_fn(context: Context):
-    """Construct a Client that will be run in a ClientApp.
-
-    You can use settings in `context.run_config` to parameterize the
-    construction of your Client. You could use the `context.node_config` to, for
-    example, indicate which dataset to load (e.g accesing the partition-id).
-    """
+    """Construct a Client that will be run in a ClientApp."""
 
     # Read the node_config to fetch data partition associated to this node
     partition_id = context.node_config["partition-id"]
