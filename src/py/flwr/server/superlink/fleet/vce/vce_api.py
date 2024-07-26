c96964f07df14fd5d1a291f090f83ab3db431802
Charles Beauville <charles@flower.ai>
2024-07-26 11:44:47 +0200

    Fix vce_api

--- src/py/flwr/server/superlink/fleet/vce/vce_api.py
+++ src/py/flwr/server/superlink/fleet/vce/vce_api.py
@@ -26,7 +26,6 @@ from queue import Empty, Queue
 from time import sleep
 from typing import Callable, Dict, Optional
 
-from flwr.cli.config_utils import get_fab_metadata_from_hash
 from flwr.client.client_app import ClientApp, ClientAppException, LoadClientAppError
 from flwr.client.node_state import NodeState
 from flwr.client.supernode.app import _get_load_client_app_fn
@@ -351,7 +350,7 @@ def start_vce(
                 project_dir=app_dir,
                 flwr_dir=flwr_dir,
                 multi_app=True,
-            )(*get_fab_metadata_from_hash(run.fab_hash))
+            )(run.fab_id, run.fab_version)
 
         if client_app:
             app = client_app
