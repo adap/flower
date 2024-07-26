daa905e5923c0052364e5ca09135456dd2a33a26
jafermarq <javier@flower.ai>
2024-07-26 14:46:30 +0100

    server-side init; no get_paramters() method

--- examples/quickstart-mlx/mlxexample/client_app.py
+++ examples/quickstart-mlx/mlxexample/client_app.py
@@ -29,10 +29,6 @@ class FlowerClient(NumPyClient):
         self.num_epochs = 1
         self.batch_size = batch_size
 
-    def get_parameters(self, config):
-        """Return the parameters of the model of this client."""
-        return get_params(self.model)
-
     def fit(self, parameters, config):
         """Train the model with data of this client."""
         set_params(self.model, parameters)
@@ -54,12 +50,7 @@ class FlowerClient(NumPyClient):
 
 
 def client_fn(context: Context) -> Client:
-    """Construct a Client that will be run in a ClientApp.
-
-    You can use settings in `context.run_config` to parameterize the
-    construction of your Client. You could use the `context.node_config` to, for
-    example, indicate which dataset to load (e.g accesing the partition-id).
-    """
+    """Construct a Client that will be run in a ClientApp."""
 
     # Read the node_config to fetch data partition associated to this node
     partition_id = context.node_config["partition-id"]
--- examples/quickstart-mlx/mlxexample/server_app.py
+++ examples/quickstart-mlx/mlxexample/server_app.py
@@ -2,9 +2,11 @@
 
 from typing import List, Tuple
 
-from flwr.common import Context, Metrics
+from flwr.common import Context, Metrics, ndarrays_to_parameters
 from flwr.server import ServerApp, ServerAppComponents, ServerConfig
 from flwr.server.strategy import FedAvg
+
+from mlxexample.task import MLP, get_params
 
 
 def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
@@ -15,22 +17,25 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
 
 
 def server_fn(context: Context) -> ServerAppComponents:
-    """Construct components that set the ServerApp behaviour.
+    """Construct components that set the ServerApp behaviour."""
 
-    You can use settings in `context.run_config` to parameterize the
-    construction of all elements (e.g the strategy or the number of rounds)
-    wrapped in the returned ServerAppComponents object.
-    """
+    # Init model
+    model = MLP(
+        num_layers=context.run_config["num-layers"],
+        input_dim=context.run_config["img-size"] ** 2,
+        hidden_dim=context.run_config["hidden-dim"],
+    )
+
+    # Convert model parameters to flwr.common.Parameters
+    ndarrays = get_params(model)
+    global_model_init = ndarrays_to_parameters(ndarrays)
 
     # Define the strategy
-    # We pass a callback to process the metrics returned by a client's
-    # evaluate() method. Similarly, we set the fraction of clients to
-    # sample for federated evaluation at run time based on the value
-    # defined in the pyproject.toml (or overrided when calling `flwr run`.)
     fraction_eval = context.run_config["fraction-evaluate"]
     strategy = FedAvg(
         fraction_evaluate=fraction_eval,
         evaluate_metrics_aggregation_fn=weighted_average,
+        initial_parameters=global_model_init,
     )
 
     # Construct ServerConfig
