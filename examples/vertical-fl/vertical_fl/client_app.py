179b16f39f81f20634b7ab950cd3b502f838dbee
Charles Beauville <charles@flower.ai>
2024-07-27 15:37:43 +0200

    Add tutorial

--- examples/vertical-fl/vertical_fl/client_app.py
+++ examples/vertical-fl/vertical_fl/client_app.py
@@ -1,16 +1,17 @@
 from flwr.client import ClientApp, NumPyClient
-import torch
+from flwr.common import Context
 from sklearn.preprocessing import StandardScaler
+import torch
 
 from vertical_fl.task import ClientModel, get_partitions_and_label
 
 
 class FlowerClient(NumPyClient):
-    def __init__(self, cid, data):
+    def __init__(self, cid, data, lr):
         self.cid = cid
         self.train = torch.tensor(StandardScaler().fit_transform(data)).float()
         self.model = ClientModel(input_size=self.train.shape[1])
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.01)
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr)
         self.embedding = self.model(self.train)
 
     def get_parameters(self, config):
@@ -30,8 +31,10 @@ class FlowerClient(NumPyClient):
 partitions, label = get_partitions_and_label()
 
 
-def client_fn(node_id, partition_id):
-    return FlowerClient(partition_id, partitions[partition_id]).to_client()
+def client_fn(context: Context):
+    partition_id = context.node_config["partition-id"]
+    lr = context.run_config["learning-rate"]
+    return FlowerClient(partition_id, partitions[partition_id], lr).to_client()
 
 
 app = ClientApp(
