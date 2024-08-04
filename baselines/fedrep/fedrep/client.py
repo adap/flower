e1b13003d087efca5877fecf69281c5b1bb4a501
KarhouTam <karhoutam@qq.com>
2024-08-04 13:50:42 +0800

    fix bug

--- baselines/fedrep/fedrep/client.py
+++ baselines/fedrep/fedrep/client.py
@@ -56,7 +56,7 @@ class BaseClient(NumPyClient):
             else None
         )
         self.hist: Dict[str, Dict[str, Any]] = defaultdict(dict)
-        self.num_epochs: int = config["num_epochs"]
+        self.num_epochs: int = config.num_local_epochs
         self.model_manager = model_manager_class(
             client_id=self.client_id,
             config=config,
