8c57e74d591198ad440808eb7a0968f05bd883f3
jafermarq <javier@flower.ai>
2024-08-06 12:22:39 +0100

    v1

--- src/py/flwr/server/superlink/fleet/vce/backend/backendconfig.py
+++ src/py/flwr/server/superlink/fleet/vce/backend/backendconfig.py
@@ -15,33 +15,57 @@
 """Backend config."""
 
 
-from typing import Optional
 from dataclasses import dataclass
-from flwr.common.typing import UserConfig # = Dict[str, UserConfigValue]
+from typing import Optional
+
+from flwr.common.typing import UserConfig
 
 
 @dataclass
 class ClientAppResources:
-    """Resources for a `ClientApp`"""
-    num_cpus: float
-    num_gpus: float
+    """Resources for a `ClientApp`.
+
+    These resources are used by a `Backend` to control the degree of parallelism
+    of a simulation. Lower `num_cpus` and `num_gpus` allow for running more
+    `ClientApp` objects in parallel. Note system resources are shared among all
+    `ClientApp`s running. In other words, per-`ClientApp` resource limits are not
+    enforced and are used exclusively as a guide to control parallelism.
+
+    Parameters
+    ----------
+    num_cpus : float (default: 1.0)
+        Indicates the number of CPUs that a `ClientApp` needs to be executed.
+    num_gpus : float (default: 0.0)
+        Indicates the number of GPUs that a `ClientApp` needs to be executed. This
+        value would normally be set based on the amount of VRAM a single `ClientApp`
+        needs. It can be a decimal point value. For example, if `num_gpus=0.25` at
+        most 4 `ClientApp` object could be run in parallel per GPU available and
+        asuming 4x`num_cpus` are available in your system.
+    """
+
+    num_cpus: float = 1.0
+    num_gpus: float = 0.0
 
 
 @dataclass
 class BackendConfig:
+    """A config for a Simulation Engine backend."""
 
     name: str
     clientapp_resources: ClientAppResources
     config: Optional[UserConfig]
 
-    def __init__(self,
-                 name: Optional[str] = "ray", 
-                 clientapp_resources: Optional[ClientAppResources] = ClientAppResources(2.0, 0.0),
-                config: Optional[UserConfig] = {},
-                )
+    def __init__(
+        self,
+        name: str = "ray",
+        clientapp_resources: Optional[ClientAppResources] = None,
+        config: Optional[UserConfig] = None,
+    ):
         self.name = name
+        if clientapp_resources is None:
+            # If unset, set default resources
+            clientapp_resources = ClientAppResources()
+
         self.clientapp_resources = clientapp_resources
-        self.config = config
 
-
-
+        self.config = {} if config is None else config
