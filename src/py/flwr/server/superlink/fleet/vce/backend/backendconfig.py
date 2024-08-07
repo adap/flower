9e055c959ae4d929b669e82230af8e8e4336f4d0
jafermarq <javier@flower.ai>
2024-08-07 11:18:23 +0100

    handle None values to `ClientAppResources`

--- src/py/flwr/server/superlink/fleet/vce/backend/backendconfig.py
+++ src/py/flwr/server/superlink/fleet/vce/backend/backendconfig.py
@@ -22,6 +22,9 @@ from typing import Dict, Optional
 from flwr.common.logger import log
 from flwr.common.typing import ConfigsRecordValues
 
+DEFAULT_CPUS = 2
+DEFAULT_GPUS = 0.0
+
 
 @dataclass
 class ClientAppResources:
@@ -35,9 +38,9 @@ class ClientAppResources:
 
     Parameters
     ----------
-    num_cpus : int (default: 2)
+    num_cpus : Optional[int] (default: 2)
         Indicates the number of CPUs that a `ClientApp` needs when running.
-    num_gpus : float (default: 0.0)
+    num_gpus : Optional[float] (default: 0.0)
         Indicates the number of GPUs that a `ClientApp` needs when running. This
         value would normally be set based on the amount of VRAM a single `ClientApp`
         needs. It can be a decimal point value. For example, if `num_gpus=0.25` at
@@ -45,14 +48,20 @@ class ClientAppResources:
         assuming 4x`num_cpus` are available in your system.
     """
 
-    num_cpus: int = 2
-    num_gpus: float = 0.0
+    num_cpus: Optional[int] = DEFAULT_CPUS
+    num_gpus: Optional[float] = DEFAULT_CPUS
 
     def __post_init__(self) -> None:
         """Validate resources after initialization."""
         self._validate()
 
     def _validate(self) -> None:
+
+        if self.num_cpus is None:
+            self.num_cpus = DEFAULT_CPUS
+
+        if self.num_gpus is None:
+            self.num_gpus = DEFAULT_GPUS
 
         if isinstance(self.num_cpus, float):
             num_cpus_int = int(self.num_cpus)
