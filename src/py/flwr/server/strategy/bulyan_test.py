aa69c2e878d8032e13bd37917253c77ea2352628
Charles Beauville <charles@flower.ai>
2024-08-11 21:31:32 +0200

    Fix tests

--- src/py/flwr/server/strategy/bulyan_test.py
+++ src/py/flwr/server/strategy/bulyan_test.py
@@ -129,4 +129,5 @@ def test_aggregate_fit() -> None:
         actual_list = parameters_to_ndarrays(actual_aggregated)
         actual = actual_list[0]
         assert (actual == expected[0]).all()
-    raise ValueError("aggregate_fit function returned None")
+    else:
+        raise ValueError("aggregate_fit function returned None")
