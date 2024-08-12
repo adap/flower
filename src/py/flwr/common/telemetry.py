0d9a6115969b08204fedaeaada7f33adc51ad7ef
Chong Shen Ng <chong.shen@flower.ai>
2024-08-12 11:30:27 +0100

    Add flwr-clientapp telemetry

--- src/py/flwr/common/telemetry.py
+++ src/py/flwr/common/telemetry.py
@@ -180,6 +180,10 @@ class EventType(str, Enum):
     RUN_SUPEREXEC_ENTER = auto()
     RUN_SUPEREXEC_LEAVE = auto()
 
+    # flwr-clientapp
+    FLWR_CLIENTAPP_ENTER = auto()
+    FLWR_CLIENTAPP_LEAVE = auto()
+
 
 # Use the ThreadPoolExecutor with max_workers=1 to have a queue
 # and also ensure that telemetry calls are not blocking.
