9aea5f281db2436b04b7fc7fc3ae18934ff4a367
yan-gao-GY <yan@flower.ai>
2024-08-22 17:35:58 +0100

    Update license

--- benchmarks/flowertune-llm/evaluation/general-nlp/gen_judgement.py
+++ benchmarks/flowertune-llm/evaluation/general-nlp/gen_judgement.py
@@ -1,4 +1,18 @@
-# This python file is adapted from https://github.com/lm-sys/FastChat/blob/main/fastchat/llm_judge/gen_judgment.py
+"""
+This python file is adapted from https://github.com/lm-sys/FastChat/blob/main/fastchat/llm_judge/gen_judgment.py
+
+FastChat (https://github.com/lm-sys/FastChat) is Licensed under the Apache License, Version 2.0.
+
+Citation:
+@misc{zheng2023judging,
+      title={Judging LLM-as-a-judge with MT-Bench and Chatbot Arena},
+      author={Lianmin Zheng and Wei-Lin Chiang and Ying Sheng and Siyuan Zhuang and Zhanghao Wu and Yonghao Zhuang and Zi Lin and Zhuohan Li and Dacheng Li and Eric. P Xing and Hao Zhang and Joseph E. Gonzalez and Ion Stoica},
+      year={2023},
+      eprint={2306.05685},
+      archivePrefix={arXiv},
+      primaryClass={cs.CL}
+}
+"""
 
 import argparse
 import json
