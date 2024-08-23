cfa7fde5c57b5be618baad57f4b6c191c403a621
yan-gao-GY <yan@flower.ai>
2024-08-23 16:54:07 +0100

    Formatting

--- benchmarks/flowertune-llm/evaluation/general-nlp/show_result.py
+++ benchmarks/flowertune-llm/evaluation/general-nlp/show_result.py
@@ -6,7 +6,9 @@ FastChat (https://github.com/lm-sys/FastChat) is Licensed under the Apache Licen
 Citation:
 @misc{zheng2023judging,
       title={Judging LLM-as-a-judge with MT-Bench and Chatbot Arena},
-      author={Lianmin Zheng and Wei-Lin Chiang and Ying Sheng and Siyuan Zhuang and Zhanghao Wu and Yonghao Zhuang and Zi Lin and Zhuohan Li and Dacheng Li and Eric. P Xing and Hao Zhang and Joseph E. Gonzalez and Ion Stoica},
+      author={Lianmin Zheng and Wei-Lin Chiang and Ying Sheng and Siyuan Zhuang and Zhanghao Wu
+      and Yonghao Zhuang and Zi Lin and Zhuohan Li and Dacheng Li and Eric. P Xing and Hao Zhang
+      and Joseph E. Gonzalez and Ion Stoica},
       year={2023},
       eprint={2306.05685},
       archivePrefix={arXiv},
