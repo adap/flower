7524626d9685210b42a23330c71249e44a6a66ed
jafermarq <javier@flower.ai>
2024-09-02 19:53:28 +0000

    basic run-dir creation and saving of results

--- baselines/moon/moon/server_app.py
+++ baselines/moon/moon/server_app.py
@@ -1,6 +1,10 @@
 """moon: A Flower Baseline."""
 
+import csv
+import json
 from collections import OrderedDict
+from datetime import datetime
+from pathlib import Path
 from typing import Callable, Dict, Optional, Tuple
 
 import torch
@@ -8,11 +12,13 @@ from datasets import load_dataset
 from torch.utils.data import DataLoader
 
 from flwr.common import Context
-from flwr.common.typing import NDArrays, Scalar
+from flwr.common.typing import NDArrays, Scalar, UserConfig
 from flwr.server import ServerApp, ServerAppComponents, ServerConfig
 from flwr.server.strategy import FedAvg
 from moon.dataset_preparation import get_data_transforms, get_transforms_apply_fn
 from moon.models import init_net, test
+
+RESULTS_FILE = "results.csv"
 
 
 def gen_evaluate_fn(
@@ -21,6 +27,7 @@ def gen_evaluate_fn(
     dataset_name: str,
     model_name: str,
     model_output_dim: int,
+    run_dir: Path,
 ) -> Callable[
     [int, NDArrays, Dict[str, Scalar]], Optional[Tuple[float, Dict[str, Scalar]]]
 ]:
@@ -36,13 +43,42 @@ def gen_evaluate_fn(
         net.load_state_dict(state_dict, strict=True)
         net.to(device)
         accuracy, loss = test(net, testloader, device=device)
+
+        # Append results
+        with open(f"{run_dir}/{RESULTS_FILE}", "a") as f:
+            writer = csv.writer(f)
+            writer.writerow([server_round, loss, accuracy])
+
         return loss, {"accuracy": accuracy}
 
     return evaluate
 
 
+def create_run_dir(config: UserConfig) -> Path:
+    """Create a directory where to save results from this run."""
+    # Create output directory given current timestamp
+    current_time = datetime.now()
+    run_dir = current_time.strftime("%Y-%m-%d/%H-%M-%S")
+    save_path = Path.cwd() / f"results/{run_dir}"
+    save_path.mkdir(parents=True, exist_ok=False)
+
+    # Save run config as json
+    with open(f"{save_path}/run_config.json", "w") as fp:
+        json.dump(config, fp)
+
+    # Prepare results.csv
+    fields = ["round", "loss", "acc"]
+    with open(f"{save_path}/{RESULTS_FILE}", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(fields)
+
+    return save_path
+
+
 def server_fn(context: Context) -> ServerAppComponents:
     """Construct ServerAppComponents object to create a ServerApp."""
+    run_dir = create_run_dir(context.run_config)
+
     dataset_name = str(context.run_config["dataset-name"])
     partition_by = context.run_config["dataset-partition-by"]
     # This is the exact same dataset as the one donwloaded by the clients via
@@ -65,6 +101,7 @@ def server_fn(context: Context) -> ServerAppComponents:
         dataset_name=dataset_name,
         model_name=str(context.run_config["model-name"]),
         model_output_dim=int(context.run_config["model-output-dim"]),
+        run_dir=run_dir,
     )
 
     strategy = FedAvg(
