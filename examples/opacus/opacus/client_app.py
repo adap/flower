2d91d3cf99a1305dc3dc46fe5137b73b775b09a6
mohammadnaseri <mohammad@flower.ai>
2024-09-26 16:58:49 +0100

    Init

---
+++ examples/opacus/opacus/client_app.py
@@ -0,0 +1,95 @@
+"""opacus: Training with Sample-Level Differential Privacy using Opacus Privacy Engine."""
+
+import argparse
+import warnings
+from collections import OrderedDict
+
+import torch
+from flwr.client import ClientApp, NumPyClient
+from flwr_datasets import FederatedDataset
+from opacus import PrivacyEngine
+from .task import train, test, Net, load_data, DEVICE, get_weights, set_weights
+from flwr.common import Context
+
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+class FlowerClient(NumPyClient):
+    def __init__(
+        self,
+        train_loader,
+        test_loader,
+        target_delta,
+        noise_multiplier,
+        max_grad_norm,
+    ) -> None:
+        super().__init__()
+        self.model = Net()
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.target_delta = target_delta
+        self.noise_multiplier = noise_multiplier
+        self.max_grad_norm = max_grad_norm
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    def fit(self, parameters, config):
+        model = self.model
+        set_weights(self.model, parameters)
+
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+        privacy_engine = PrivacyEngine(secure_mode=False)
+        (
+            model,
+            optimizer,
+            self.train_loader,
+        ) = privacy_engine.make_private(
+            module=model,
+            optimizer=optimizer,
+            data_loader=self.train_loader,
+            noise_multiplier=self.noise_multiplier,
+            max_grad_norm=self.max_grad_norm,
+        )
+
+        epsilon = train(
+            model,
+            self.train_loader,
+            privacy_engine,
+            optimizer,
+            self.target_delta,
+        )
+
+        if epsilon is not None:
+            print(f"Epsilon value for delta={self.target_delta} is {epsilon:.2f}")
+        else:
+            print("Epsilon value not available.")
+        return (self.get_parameters(config={}), len(self.train_loader), {})
+
+    def evaluate(self, parameters, config):
+        set_weights(self.model, parameters)
+        loss, accuracy = test(self.model, self.test_loader)
+        return loss, len(self.test_loader.dataset), {"accuracy": accuracy}
+
+
+def client_fn(context: Context):
+    partition_id = context.node_config["partition-id"]
+    # run_config = context.run_config
+    net = Net().to(DEVICE)
+    noise_multiplier = 1.0 if partition_id % 2 == 0 else 1.5
+
+    train_loader, test_loader = load_data(
+        partition_id=partition_id, num_partitions=context.node_config["num-partitions"]
+    )
+    return FlowerClient(
+        net,
+        train_loader,
+        test_loader,
+        context.run_config["target-delta"],
+        noise_multiplier,
+        context.run_config["max-grad-norm"],
+    ).to_client()
+
+
+app = ClientApp(client_fn=client_fn)
---
+++ examples/opacus/opacus/server_app.py
@@ -0,0 +1,32 @@
+"""opacus: Training with Sample-Level Differential Privacy using Opacus Privacy Engine."""
+
+from typing import List, Tuple
+
+from flwr.common import Metrics, Context, ndarrays_to_parameters
+from flwr.server import ServerApp, ServerConfig, ServerAppComponents
+from flwr.server.strategy import FedAvg
+from opacus.task import Net, get_weights
+
+
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+    return {"accuracy": sum(accuracies) / sum(examples)}
+
+
+def server_fn(context: Context) -> ServerAppComponents:
+    num_rounds = context.run_config["num-server-rounds"]
+
+    ndarrays = get_weights(Net())
+    parameters = ndarrays_to_parameters(ndarrays)
+
+    strategy = FedAvg(
+        evaluate_metrics_aggregation_fn=weighted_average,
+        initial_parameters=parameters,
+    )
+    config = ServerConfig(num_rounds=num_rounds)
+
+    return ServerAppComponents(config=config, strategy=strategy)
+
+
+app = ServerApp(server_fn=server_fn)
