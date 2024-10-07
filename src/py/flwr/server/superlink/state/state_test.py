c457b4544bdb01e791626d456faee9c8735f5418
mohammadnaseri <mohammad@flower.ai>
2024-10-07 20:14:20 +0100

    Edit

--- src/py/flwr/server/superlink/state/state_test.py
+++ src/py/flwr/server/superlink/state/state_test.py
@@ -785,6 +785,36 @@ class StateTest(unittest.TestCase):
             task_ins_list = state.get_task_ins(node_id=1, limit=None)
             assert len(task_ins_list) == 0
 
+    def test_get_task_res_not_return_expired(self) -> None:
+        """Test get_task_res not to return TaskRes if its TaskIns is expired."""
+        # Prepare
+        consumer_node_id = 1
+        state = self.state_factory()
+        run_id = state.create_run(None, None, "9f86d08", {})
+        task_ins = create_task_ins(
+            consumer_node_id=consumer_node_id, anonymous=False, run_id=run_id
+        )
+        task_ins.task.created_at = time.time() - 5
+        task_ins.task.ttl = 5.1
+
+        task_id = state.store_task_ins(task_ins=task_ins)
+
+        task_res = create_task_res(
+            producer_node_id=100,
+            anonymous=False,
+            ancestry=[str(task_id)],
+            run_id=run_id,
+        )
+        _ = state.store_task_res(task_res=task_res)
+
+        with patch("time.time", side_effect=lambda: task_ins.task.created_at + 6.1):
+            # Execute
+            if task_id is not None:
+                task_res_list = state.get_task_res(task_ids={task_id}, limit=None)
+
+            # Assert
+            assert len(task_res_list) == 0
+
 
 def create_task_ins(
     consumer_node_id: int,
