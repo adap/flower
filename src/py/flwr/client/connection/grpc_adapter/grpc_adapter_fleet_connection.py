e16a07285d49da7692e624c2230e88a0dc35b810
Heng Pan <pan@flower.ai>
2024-10-09 14:24:35 +0100

    update docstring

--- src/py/flwr/client/connection/grpc_adapter/grpc_adapter_fleet_connection.py
+++ src/py/flwr/client/connection/grpc_adapter/grpc_adapter_fleet_connection.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Connection for a gRPC request-response channel to the SuperLink."""
+"""Connection for a grpc-adapter request-response channel to the SuperLink."""
 
 
 from __future__ import annotations
