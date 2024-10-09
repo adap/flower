1e7d1d56c7a318542060d591a5eda2e85858d897
mohammadnaseri <mohammad@flower.ai>
2024-10-09 20:14:07 +0100

    Edit

--- src/py/flwr/server/superlink/state/state_test.py
+++ src/py/flwr/server/superlink/state/state_test.py
@@ -871,6 +871,32 @@ class StateTest(unittest.TestCase):
             # Assert
             assert len(task_res_list) != 0
 
+    def test_store_task_res_fail_if_consumer_producer_id_mismatch(self) -> None:
+        "Test store_task_res to fail if there is a mismatch between"
+        "the consumer_node_id of taskIns and the producer_node_id of taskRes."
+        # Prepare
+        consumer_node_id = 1
+        state = self.state_factory()
+        run_id = state.create_run(None, None, "9f86d08", {})
+        task_ins = create_task_ins(
+            consumer_node_id=consumer_node_id, anonymous=False, run_id=run_id
+        )
+
+        task_id = state.store_task_ins(task_ins=task_ins)
+
+        task_res = create_task_res(
+            producer_node_id=100,  # different than consumer_node_id
+            anonymous=False,
+            ancestry=[str(task_id)],
+            run_id=run_id,
+        )
+
+        # Execute
+        task_res_uuid = state.store_task_res(task_res=task_res)
+
+        # Assert
+        assert task_res_uuid == None
+
 
 def create_task_ins(
     consumer_node_id: int,
