1237ed2a4596596e3bf97ba4a319a0708b0e11df
Heng Pan <pan@flower.ai>
2024-10-21 09:34:25 +0100

    add args

--- src/py/flwr/server/app.py
+++ src/py/flwr/server/app.py
@@ -35,7 +35,7 @@ from cryptography.hazmat.primitives.serialization import (
 
 from flwr.common import GRPC_MAX_MESSAGE_LENGTH, EventType, event
 from flwr.common.address import parse_address
-from flwr.common.config import get_flwr_dir
+from flwr.common.config import get_flwr_dir, parse_config_args
 from flwr.common.constant import (
     DRIVER_API_DEFAULT_ADDRESS,
     EXEC_API_DEFAULT_ADDRESS,
@@ -49,6 +49,7 @@ from flwr.common.constant import (
 )
 from flwr.common.exit_handlers import register_exit_handlers
 from flwr.common.logger import log
+from flwr.common.object_ref import load_app, validate
 from flwr.common.secure_aggregation.crypto.symmetric_encryption import (
     private_key_to_bytes,
     public_key_to_bytes,
@@ -57,8 +58,8 @@ from flwr.proto.fleet_pb2_grpc import (  # pylint: disable=E0611
     add_FleetServicer_to_server,
 )
 from flwr.proto.grpcadapter_pb2_grpc import add_GrpcAdapterServicer_to_server
-from flwr.superexec.deployment import executor
 from flwr.superexec.exec_grpc import run_superexec_api_grpc
+from flwr.superexec.executor import Executor
 
 from .client_manager import ClientManager
 from .history import History
@@ -208,8 +209,9 @@ def run_superlink() -> None:
 
     event(EventType.RUN_SUPERLINK_ENTER)
 
-    # Parse IP address
+    # Parse IP addresses
     driver_address, _, _ = _format_address(args.driver_api_address)
+    exec_address, _, _ = _format_address(args.exec_api_address)
 
     # Obtain certificates
     certificates = _try_obtain_certificates(args)
@@ -227,16 +229,9 @@ def run_superlink() -> None:
         ffs_factory=ffs_factory,
         certificates=certificates,
     )
+    grpc_servers = [driver_server]
 
-    # Start Exec API
-    exec_server: grpc.Server = run_superexec_api_grpc(
-        address=EXEC_API_DEFAULT_ADDRESS,
-        executor=executor,
-        certificates=certificates,
-        config={},
-    )
-
-    grpc_servers = [exec_server, driver_server]
+    # Start Fleet API
     bckg_threads = []
     if not args.fleet_api_address:
         if args.fleet_api_type in [
@@ -261,7 +256,6 @@ def run_superlink() -> None:
         )
         num_workers = 1
 
-    # Start Fleet API
     if args.fleet_api_type == TRANSPORT_TYPE_REST:
         if (
             importlib.util.find_spec("requests")
@@ -328,6 +322,17 @@ def run_superlink() -> None:
         grpc_servers.append(fleet_server)
     else:
         raise ValueError(f"Unknown fleet_api_type: {args.fleet_api_type}")
+
+    # Start Exec API
+    exec_server: grpc.Server = run_superexec_api_grpc(
+        address=exec_address,
+        executor=_load_executor(args),
+        certificates=certificates,
+        config=parse_config_args(
+            [args.executor_config] if args.executor_config else args.executor_config
+        ),
+    )
+    grpc_servers.append(exec_server)
 
     # Graceful shutdown
     register_exit_handlers(
@@ -598,6 +603,7 @@ def _parse_args_run_superlink() -> argparse.ArgumentParser:
     _add_args_common(parser=parser)
     _add_args_driver_api(parser=parser)
     _add_args_fleet_api(parser=parser)
+    _add_args_exec_api(parser=parser)
 
     return parser
 
@@ -692,3 +698,52 @@ def _add_args_fleet_api(parser: argparse.ArgumentParser) -> None:
         type=int,
         help="Set the number of concurrent workers for the Fleet API server.",
     )
+
+
+def _add_args_exec_api(parser: argparse.ArgumentParser) -> None:
+    """Add command line arguments for Exec API."""
+    parser.add_argument(
+        "--executor-api-address",
+        help="Exec API server address (IPv4, IPv6, or a domain name)",
+        default=EXEC_API_DEFAULT_ADDRESS,
+    )
+    parser.add_argument(
+        "--executor",
+        help="For example: `deployment:exec` or `project.package.module:wrapper.exec`. "
+        "The default is `flwr.superexec.deployment:executor`",
+        default="flwr.superexec.deployment:executor",
+    )
+    parser.add_argument(
+        "--executor-dir",
+        help="The directory for the executor.",
+        default=".",
+    )
+    parser.add_argument(
+        "--executor-config",
+        help="Key-value pairs for the executor config, separated by spaces. "
+        "For example:\n\n`--executor-config 'verbose=true "
+        'root-certificates="certificates/superlink-ca.crt"\'`',
+    )
+
+
+def _load_executor(
+    args: argparse.Namespace,
+) -> Executor:
+    """Get the executor plugin."""
+    executor_ref: str = args.executor
+    valid, error_msg = validate(executor_ref, project_dir=args.executor_dir)
+    if not valid and error_msg:
+        raise LoadExecutorError(error_msg) from None
+
+    executor = load_app(executor_ref, LoadExecutorError, args.executor_dir)
+
+    if not isinstance(executor, Executor):
+        raise LoadExecutorError(
+            f"Attribute {executor_ref} is not of type {Executor}",
+        ) from None
+
+    return executor
+
+
+class LoadExecutorError(Exception):
+    """Error when trying to load `Executor`."""
