bfff378585ba09c5567d3addc460cd92019d65ea
jafermarq <javier@flower.ai>
2024-10-23 18:02:34 +0100

    space

--- src/py/flwr/server/app.py
+++ src/py/flwr/server/app.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 """Flower server app."""
+
 import argparse
 import csv
 import importlib.util
