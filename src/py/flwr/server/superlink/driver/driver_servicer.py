3484551b02af489be193c3697b45d1977462d75c
Heng Pan <pan@flower.ai>
2024-10-25 11:44:52 +0100

    rm empty lines

--- src/py/flwr/server/superlink/driver/driver_servicer.py
+++ src/py/flwr/server/superlink/driver/driver_servicer.py
@@ -227,12 +227,10 @@ class DriverServicer(driver_pb2_grpc.DriverServicer):
             if run and run.fab_hash:
                 if result := ffs.get(run.fab_hash):
                     fab = Fab(run.fab_hash, result[0])
-
             if run and fab:
                 # Update run status to STARTING
                 if state.update_run_status(run_id, RunStatus(Status.STARTING, "", "")):
                     log(INFO, "Starting run %d", run_id)
-
                     return PullServerAppInputsResponse(
                         context=(
                             context_to_proto(serverapp_ctxt) if serverapp_ctxt else None
