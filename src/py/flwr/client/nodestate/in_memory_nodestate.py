69053eccf3a42d40331d29b912ad88d291b1bc06
Daniel J. Beutel <daniel@flower.ai>
2024-11-07 15:57:45 +0100

    Update src/py/flwr/client/nodestate/in_memory_nodestate.py

--- src/py/flwr/client/nodestate/in_memory_nodestate.py
+++ src/py/flwr/client/nodestate/in_memory_nodestate.py
@@ -24,7 +24,6 @@ class InMemoryNodeState(NodeState):
     """In-memory NodeState implementation."""
 
     def __init__(self) -> None:
-
         # Store node_id
         self.node_id: Optional[int]
 
