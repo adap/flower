886ed62cb0b576f582162ff9292203ae9871a8ac
chancejohnstone <chancellor.johnstone@gmail.com>
2024-11-16 10:05:28 -0500

    feat(baselines) clean up aggregate

--- baselines/fedht/fedht/aggregate.py
+++ baselines/fedht/fedht/aggregate.py
@@ -1,17 +1,3 @@
-# Copyright 2020 Flower Labs GmbH. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
 """Aggregation functions for strategy implementations."""
 # mypy: disallow_untyped_calls=False
 
@@ -20,12 +6,6 @@ from typing import List, Tuple
 
 import numpy as np
 from flwr.common import NDArrays
-
-def weighted_loss_avg(results: List[Tuple[int, float]]) -> float:
-    """Aggregate evaluation results obtained from multiple clients."""
-    num_total_evaluation_examples = sum(num_examples for (num_examples, _) in results)
-    weighted_losses = [num_examples * loss for num_examples, loss in results]
-    return sum(weighted_losses) / num_total_evaluation_examples
 
 # calls hardthreshold function for each list element in weights_all
 def hardthreshold_list(weights_all, num_keep: int) -> NDArrays:
