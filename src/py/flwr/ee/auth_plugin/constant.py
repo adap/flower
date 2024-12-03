bb57e65bfb78fae1d7875781bed9bbe7b6f82e6a
Daniel Nata Nugraha <daniel.nata@flower.ai>
2024-12-03 11:53:28 +0100

    Add keycloak auth plugin

---
+++ src/py/flwr/ee/auth_plugin/constant.py
@@ -0,0 +1,30 @@
+# Copyright 2024 Flower Labs GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Flower EE auth plugin constants."""
+
+_ACCESS_TOKEN = "access_token"
+_REFRESH_TOKEN = "refresh_token"
+_AUTH_URL = "auth_url"
+_TOKEN_URL = "token_url"
+_VALIDATE_URL = "validate_url"
+_CLIENT_ID = "client_id"
+_CLIENT_SECRET = "client_secret"
+_DEVICE_CODE = "device_code"
+_VERIFICATION_URI_COMPLETE = "verification_uri_complete"
+_EXPIRES_IN = "expires_in"
+_INTERVAL = "interval"
+_AUTH_TYPE = "auth_type"
+_GRANT_TYPE = "grant_type"
+_DEVICE_FLOW_GRANT_TYPE = "urn:ietf:params:oauth:grant-type:device_code"
