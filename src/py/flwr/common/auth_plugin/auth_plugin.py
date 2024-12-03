a176c39e1ac30e90b57a8033ebe05f4dcb9b00f8
Daniel Nata Nugraha <daniel.nata@flower.ai>
2024-12-03 11:05:36 +0100

    Add abstract user auth plugin

---
+++ src/py/flwr/common/auth_plugin/auth_plugin.py
@@ -0,0 +1,82 @@
+# Copyright 2024 Flower Labs GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Abstract classes for Flower User Auth Plugin."""
+
+from abc import ABC, abstractmethod
+from collections.abc import Sequence
+from pathlib import Path
+from typing import Any, Union
+
+import grpc
+
+from flwr.proto.exec_pb2 import (  # pylint: disable=E0611
+    GetAuthTokenRequest,
+    GetAuthTokenResponse,
+    LoginResponse,
+)
+from flwr.proto.exec_pb2_grpc import ExecStub
+
+class ExecAuthPlugin(ABC):
+    """Abstract Flower Exec API Auth Plugin class."""
+
+    @abstractmethod
+    def __init__(self, config: dict[str, Any]):
+        """Abstract constructor (init)."""
+
+    @abstractmethod
+    def get_login_response(self) -> LoginResponse:
+        """Send relevant login details as a LoginResponse."""
+
+    @abstractmethod
+    def validate_token_in_metadata(self, metadata: Sequence[tuple[str, Union[str, bytes]]]) -> bool:
+        """Authenticate auth tokens in the provided metadata."""
+
+    @abstractmethod
+    def get_auth_token_response(
+        self, request: GetAuthTokenRequest
+    ) -> GetAuthTokenResponse:
+        """Send relevant tokens as a GetAuthTokenResponse."""
+
+    @abstractmethod
+    def refresh_token(self, context: grpc.ServicerContext) -> bool:
+        """Refresh auth tokens in the provided metadata."""
+
+
+class CliAuthPlugin(ABC):
+    """Abstract Flower CLI Auth Plugin class."""
+
+    @staticmethod
+    @abstractmethod
+    def login(
+        login_details: dict[str, str],
+        config: dict[str, Any],
+        federation: str,
+        exec_stub: ExecStub,
+    ) -> dict[str, Any]:
+        """Login logic to log in user to the SuperLink."""
+
+    @abstractmethod
+    def __init__(self, config: dict[str, Any], config_path: Path):
+        """Abstract constructor (init)."""
+
+    @abstractmethod
+    def write_token_to_metadata(self, metadata: Sequence[tuple[str, Union[str, bytes]]]) -> None:
+        """Provide relevant auth tokens in the metadata."""
+
+    @abstractmethod
+    def store_refresh_token(
+        self, metadata: Sequence[tuple[str, Union[str, bytes]]]
+    ) -> None:
+        """Store refresh tokens to .credentials."""
