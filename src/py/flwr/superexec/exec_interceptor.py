c0774c6fe0e826aec772baff1549a1c42996a50f
Heng Pan <pan@flower.ai>
2024-12-07 22:19:14 +0800

    fix bugs

--- src/py/flwr/superexec/exec_interceptor.py
+++ src/py/flwr/superexec/exec_interceptor.py
@@ -75,11 +75,7 @@ class ExecInterceptor(grpc.ServerInterceptor):  # type: ignore
             request: Request,
             context: grpc.ServicerContext,
         ) -> Response:
-            if isinstance(
-                request, (GetAuthTokensRequest, GetAuthTokensRequest)
-            ) or self.auth_plugin.validate_tokens_in_metadata(
-                context.invocation_metadata()
-            ):
+            if isinstance(request, (GetLoginDetailsRequest, GetAuthTokensRequest)):
                 return method_handler.unary_unary(request, context)  # type: ignore
 
             tokens = self.auth_plugin.refresh_tokens(context.invocation_metadata())
