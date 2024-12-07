f03e4eb9c0de4ec3c5d0f036679c80ae13146012
jafermarq <javier@flower.ai>
2024-12-07 09:14:39 +0000

    init

--- src/py/flwr/client/message_handler/message_handler.py
+++ src/py/flwr/client/message_handler/message_handler.py
@@ -104,8 +104,6 @@ def handle_legacy_message_from_msgtype(
             "Please use `NumPyClient.to_client()` method to convert it to `Client`.",
         )
 
-    client.set_context(context)
-
     message_type = message.metadata.message_type
 
     # Handle GetPropertiesIns
