91b07b3907402ee48b637af1510425a5df5b4dbc
Charles Beauville <charles@flower.ai>
2024-12-11 09:38:38 -0800

    ci(framework) Fix multi version docs (#4679)

--- framework/docs/build-versioned-docs.sh
+++ framework/docs/build-versioned-docs.sh
@@ -95,14 +95,16 @@ END
     # Actually building the docs for a given language and version
     sphinx-build -b html source/ build/html/${current_version}/${current_language} -A lang=True -D language=${current_language}
 
-    cd $(git rev-parse --show-toplevel)
-
     # Clean the history of the checked-out branch to remove conflicts
     git clean -fd
 
   done
+
+  cd $(git rev-parse --show-toplevel)
 done
-  
+
+cd framework/docs
+
 # Build the main version (main for GH CI, local branch for local) 
 if [ $GITHUB_ACTIONS ]
 then
