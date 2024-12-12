a395dbeada472f9a1342a7be271998efc5031889
Chong Shen Ng <chong.shen@flower.ai>
2024-12-12 09:55:11 +0000

    Update docstring

--- src/py/flwr/server/superlink/utils.py
+++ src/py/flwr/server/superlink/utils.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""SuperLink abort."""
+"""SuperLink utilities."""
 
 
 from typing import Union
