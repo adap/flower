2e86bcf2b1a7c9a0b3c1a387eed7b44c5215130c
Adam Narozniak <adam@flower.dev>
2024-12-16 16:13:51 +0100

    Update scripts

--- datasets/dev/format.sh
+++ datasets/dev/format.sh
@@ -20,15 +20,15 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
 # Python
 echo "Formatting started: Python"
-python -m isort flwr_datasets/
-python -m black -q flwr_datasets/
-python -m docformatter -i -r flwr_datasets/
-python -m ruff check --fix flwr_datasets/
+python -m isort flwr_datasets/ tests/
+python -m black -q flwr_datasets/ tests/
+python -m docformatter -i -r flwr_datasets/ tests/
+python -m ruff check --fix flwr_datasets/ tests/
 echo "Formatting done: Python"
 
 # Notebooks
 echo "Formatting started: Notebooks"
-python -m black --ipynb -q doc/source/*.ipynb
+python -m black --ipynb -q docs/source/*.ipynb
 KEYS="metadata.celltoolbar metadata.language_info metadata.toc metadata.notify_time metadata.varInspector metadata.accelerator metadata.vscode cell.metadata.id cell.metadata.heading_collapsed cell.metadata.hidden cell.metadata.code_folding cell.metadata.tags cell.metadata.init_cell cell.metadata.vscode cell.metadata.pycharm"
-python -m nbstripout --keep-output doc/source/*.ipynb --extra-keys "$KEYS"
+python -m nbstripout --keep-output docs/source/*.ipynb --extra-keys "$KEYS"
 echo "Formatting done: Notebooks"
--- datasets/dev/test.sh
+++ datasets/dev/test.sh
@@ -11,39 +11,40 @@ echo "=== test.sh ==="
 echo "- Start Python checks"
 
 echo "- isort: start"
-python -m isort --check-only flwr_datasets/
+python -m isort --check-only flwr_datasets/ tests/
 echo "- isort: done"
 
 echo "- black: start"
-python -m black --check flwr_datasets/
+python -m black --check flwr_datasets/ tests/
 echo "- black: done"
 
 echo "- init_py_check: start"
-python -m flwr_tool.init_py_check flwr_datasets/
+python -m flwr_tool.init_py_check flwr_datasets/ tests/
 echo "- init_py_check: done"
 
 echo "- docformatter: start"
-python -m docformatter -c -r flwr_datasets/
+python -m docformatter -c -r flwr_datasets/ tests/
 echo "- docformatter:  done"
 
 echo "- ruff: start"
-python -m ruff check flwr_datasets/
+python -m ruff check flwr_datasets/ tests/
 echo "- ruff: done"
 
 echo "- mypy: start"
-python -m mypy flwr_datasets/
+python -m mypy flwr_datasets/ tests/
 echo "- mypy: done"
 
 echo "- pylint: start"
-python -m pylint flwr_datasets/
+python -m pylint flwr_datasets/ tests/
 echo "- pylint: done"
 
 echo "- flake8: start"
-python -m flake8 flwr_datasets/
+python -m flake8 flwr_datasets/ tests/
 echo "- flake8: done"
 
 echo "- pytest: start"
-python -m pytest flwr_datasets/
+cd tests/
+python -m pytest.
 echo "- pytest: done"
 
 echo "- All Python checks passed"
