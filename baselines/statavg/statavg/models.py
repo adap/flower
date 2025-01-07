7506d12f61b1a4dc6ba39a29b0a06092fe6b359a
Pavlos Bouzinis <82902650+paulBooz@users.noreply.github.com>
2025-01-07 13:23:46 +0200

    feat(baselines) Add new baseline statavg (#3921)
    Co-authored-by: jafermarq <javier@flower.ai>

---
+++ baselines/statavg/statavg/models.py
@@ -0,0 +1,23 @@
+"""Model used for FL training."""
+
+import tensorflow as tf
+
+
+def get_model(input_shape: int, num_classes: int) -> tf.keras.Sequential:
+    """Return the model."""
+    # creates a model with the given input shape and output: num_classes
+    model = tf.keras.Sequential(
+        [
+            tf.keras.layers.Dense(128, activation="relu", input_dim=input_shape),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(num_classes, activation="softmax"),
+        ]
+    )
+    opt = tf.keras.optimizers.Adam(
+        learning_rate=0.002, beta_1=0.99, beta_2=0.999, epsilon=1e-08
+    )
+    model.compile(
+        loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"]
+    )
+    return model
