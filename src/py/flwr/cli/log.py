c170200553ae42f0f51381a06615fbf03786d0fd
Heng Pan <pan@flower.ai>
2025-01-21 09:35:10 +0000

    Update src/py/flwr/cli/log.py
    Co-authored-by: Chong Shen Ng <chong.shen@flower.ai>

--- src/py/flwr/cli/log.py
+++ src/py/flwr/cli/log.py
@@ -154,8 +154,8 @@ def log(
             help="Override federation configuration values in the format:\n\n"
             "`--federation-config 'key1=value1 key2=value2' --federation-config "
             "'key3=value3'`\n\nValues can be of any type supported in TOML, such as "
-            "bool, int, float, or string. Ensure the keys (`key1`, `key2`, etc.) exist "
-            "in the federation configuration.",
+            "bool, int, float, or string. Ensure that the keys (`key1`, `key2`, `key3` in this example) exist "
+            "in the federation configuration under the `[tool.flwr.federations.<YOUR_FEDERATION>]` table of the `pyproject.toml` in order to be properly overridden.",
         ),
     ] = None,
     stream: Annotated[
