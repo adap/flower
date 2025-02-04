441fb51d5338118f35c6d812f8730cfb6fcc6669
Heng Pan <pan@flower.ai>
2025-02-04 20:41:10 +0800

    patch release versions

--- framework/docs/source/conf.py
+++ framework/docs/source/conf.py
@@ -92,7 +92,7 @@ author = "The Flower Authors"
 release = "1.16.0"
 # The current released version
 rst_prolog = """
-.. |stable_flwr_version| replace:: 1.15.0
+.. |stable_flwr_version| replace:: 1.15.1
 .. |stable_flwr_superlink_docker_digest| replace:: 4b317d5b6030710b476f4dbfab2c3a33021ad40a0fcfa54d7edd45e0c51d889c
 .. |ubuntu_version| replace:: 24.04
 .. |setuptools_version| replace:: 70.3.0
