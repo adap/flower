c209779a57065bb82219b7b01d0ed3343c2687fa
Heng Pan <pan@flower.ai>
2025-02-04 23:27:33 +0800

    fix length

--- src/py/flwr/common/exit_handlers.py
+++ src/py/flwr/common/exit_handlers.py
@@ -85,4 +85,5 @@ def register_exit_handlers(
 
     # Register signal handlers
     for sig in SIGNAL_TO_EXIT_CODE:
-        default_handlers[sig] = signal.signal(sig, graceful_exit_handler)  # type: ignore
+        default_handler = signal.signal(sig, graceful_exit_handler)  # type: ignore
+        default_handlers[sig] = default_handler
