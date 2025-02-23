ee644158429e0922cfa1121397159dc639bfe058
Chong Shen Ng <chong.shen@flower.ai>
2025-02-23 19:36:37 +0000

    Add event log plugin arg to SuperLink

--- src/py/flwr/server/app.py
+++ src/py/flwr/server/app.py
@@ -55,6 +55,7 @@ from flwr.common.constant import (
     TRANSPORT_TYPE_GRPC_RERE,
     TRANSPORT_TYPE_REST,
 )
+from flwr.common.event_log_plugin import EventLogWriterPlugin
 from flwr.common.exit import ExitCode, flwr_exit
 from flwr.common.exit_handlers import register_exit_handlers
 from flwr.common.grpc import generic_create_grpc_server
@@ -90,7 +91,11 @@ BASE_DIR = get_flwr_dir() / "superlink" / "ffs"
 
 
 try:
-    from flwr.ee import add_ee_args_superlink, get_exec_auth_plugins
+    from flwr.ee import (
+        add_ee_args_superlink,
+        get_exec_auth_plugins,
+        get_exec_event_log_writer_plugins,
+    )
 except ImportError:
 
     # pylint: disable-next=unused-argument
@@ -100,6 +105,12 @@ except ImportError:
     def get_exec_auth_plugins() -> dict[str, type[ExecAuthPlugin]]:
         """Return all Exec API authentication plugins."""
         raise NotImplementedError("No authentication plugins are currently supported.")
+
+    def get_exec_event_log_writer_plugins() -> dict[str, type[EventLogWriterPlugin]]:
+        """Return all Exec API event log writer plugins."""
+        raise NotImplementedError(
+            "No event log writer plugins are currently supported."
+        )
 
 
 def start_server(  # pylint: disable=too-many-arguments,too-many-locals
@@ -272,9 +283,13 @@ def run_superlink() -> None:
     verify_tls_cert = not getattr(args, "disable_oidc_tls_cert_verification", None)
 
     auth_plugin: Optional[ExecAuthPlugin] = None
+    event_log_plugin: Optional[EventLogWriterPlugin] = None
     # Load the auth plugin if the args.user_auth_config is provided
     if cfg_path := getattr(args, "user_auth_config", None):
         auth_plugin = _try_obtain_exec_auth_plugin(Path(cfg_path), verify_tls_cert)
+        # Enable event logging if the args.enable_event_log is True
+        if args.enable_event_log:
+            event_log_plugin = _try_obtain_exec_event_log_writer_plugin()
 
     # Initialize StateFactory
     state_factory = LinkStateFactory(args.database)
@@ -294,6 +309,7 @@ def run_superlink() -> None:
             [args.executor_config] if args.executor_config else args.executor_config
         ),
         auth_plugin=auth_plugin,
+        event_log_plugin=event_log_plugin,
     )
     grpc_servers = [exec_server]
 
@@ -596,6 +612,20 @@ def _try_obtain_exec_auth_plugin(
         sys.exit("No authentication type is provided in the configuration.")
     except NotImplementedError:
         sys.exit("No authentication plugins are currently supported.")
+
+
+def _try_obtain_exec_event_log_writer_plugin() -> Optional[EventLogWriterPlugin]:
+    """Return an instance of the event log writer plugin."""
+    try:
+        all_plugins: dict[str, type[EventLogWriterPlugin]] = (
+            get_exec_event_log_writer_plugins()
+        )
+        plugin_class = all_plugins["default"]
+        return plugin_class()
+    except KeyError:
+        sys.exit("No event log writer plugin is provided.")
+    except NotImplementedError:
+        sys.exit("No event log writer plugins are currently supported.")
 
 
 def _run_fleet_api_grpc_rere(
