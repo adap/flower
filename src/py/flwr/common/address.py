e7a0e456ad01450a3ddfb6582adf6f7a4ef0c19c
Chong Shen Ng <chong.shen@flower.ai>
2025-02-23 11:43:40 +0000

    Fix docsig

--- src/py/flwr/common/address.py
+++ src/py/flwr/common/address.py
@@ -113,6 +113,7 @@ def get_ip_address_from_servicer_context(context: grpc.ServicerContext) -> str:
     ----------
     context : grpc.ServicerContext
         The gRPC ServicerContext object. The context.peer() returns a string like:
+
           - "ipv4:127.0.0.1:56789" for IPv4
           - "ipv6:[2001:db8::1]:54321" for IPv6
 
