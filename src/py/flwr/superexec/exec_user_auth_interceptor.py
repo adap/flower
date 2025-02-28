9714e91e4f72eb4bcccb5a59169d14331dd508f4
Chong Shen Ng <chong.shen@flower.ai>
2025-02-28 16:31:42 +0000

    fix(framework) Split validate tokens logic for auth plugin (#4972)

--- src/py/flwr/superexec/exec_user_auth_interceptor.py
+++ src/py/flwr/superexec/exec_user_auth_interceptor.py
@@ -77,15 +77,17 @@ class ExecUserAuthInterceptor(grpc.ServerInterceptor):  # type: ignore
         ) -> Response:
             call = method_handler.unary_unary or method_handler.unary_stream
             metadata = context.invocation_metadata()
-            if isinstance(
-                request, (GetLoginDetailsRequest, GetAuthTokensRequest)
-            ) or self.auth_plugin.validate_tokens_in_metadata(metadata):
+            if isinstance(request, (GetLoginDetailsRequest, GetAuthTokensRequest)):
+                return call(request, context)  # type: ignore
+
+            valid_tokens, _ = self.auth_plugin.validate_tokens_in_metadata(metadata)
+            if valid_tokens:
                 return call(request, context)  # type: ignore
 
             tokens = self.auth_plugin.refresh_tokens(context.invocation_metadata())
             if tokens is not None:
                 context.send_initial_metadata(tokens)
-                return call(request, context)
+                return call(request, context)  # type: ignore
 
             context.abort(grpc.StatusCode.UNAUTHENTICATED, "Access denied")
             raise grpc.RpcError()  # This line is unreachable
