0a8a2219007e2bbfc1082df3392f666e281d1516
Charles Beauville <charles@flower.ai>
2025-03-04 19:08:30 +0100

    Update script

--- intelligence/dev/build_docs.sh
+++ intelligence/dev/build_docs.sh
@@ -2,6 +2,10 @@
 set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/../
 
+cd ts && \
+  pnpm build:docs --readme none --name "TypeScript API" && \
+  cd ..
+
 cd docs
 
 make html
