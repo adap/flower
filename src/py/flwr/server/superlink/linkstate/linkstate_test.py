70cee40e249080521cc24adfcd35ec148d49862f
Heng Pan <pan@flower.ai>
2025-03-06 20:57:39 +0000

    refactor(framework) Remove tasks

--- src/py/flwr/server/superlink/linkstate/linkstate_test.py
+++ src/py/flwr/server/superlink/linkstate/linkstate_test.py
@@ -875,7 +875,7 @@ class StateTest(unittest.TestCase):
                 assert res is None
 
     def test_get_message_ins_not_return_expired(self) -> None:
-        """Test get_message_ins not to return expired tasks."""
+        """Test get_message_ins not to return expired replies."""
         # Prepare
         state = self.state_factory()
         node_id = state.create_node(1e3)
