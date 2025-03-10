87d68624356186d94b68d29cd19462f056fd4b6e
Chong Shen Ng <chong.shen@flower.ai>
2025-03-10 17:30:34 +0000

    refactor(framework) Raise warning if errors are encountered in the `Driver` API's `push_message` (#5001)
    Co-authored-by: Daniel J. Beutel <daniel@flower.ai>
    Co-authored-by: Javier <jafermarq@users.noreply.github.com>

--- src/py/flwr/server/driver/grpc_driver.py
+++ src/py/flwr/server/driver/grpc_driver.py
@@ -212,6 +212,15 @@ class GrpcDriver(Driver):
                 messages_list=message_proto_list, run_id=cast(Run, self._run).run_id
             )
         )
+        if len([msg_id for msg_id in res.message_ids if msg_id]) != len(
+            list(message_proto_list)
+        ):
+            log(
+                WARNING,
+                "Not all messages could be pushed to the SuperLink. The returned "
+                "list has `None` for those messages (the order is preserved as passed "
+                "to `push_messages`). This could be due to a malformed message.",
+            )
         return list(res.message_ids)
 
     def pull_messages(self, message_ids: Iterable[str]) -> Iterable[Message]:
