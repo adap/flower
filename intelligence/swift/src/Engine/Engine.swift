0b19d78a09188f8fc58ce833935c5a41e858bee8
Daniel Nata Nugraha <daniel.nata@flower.ai>
2025-03-10 13:51:45 +0100

    Add Package.swift in root

---
+++ intelligence/swift/src/Engine/Engine.swift
@@ -0,0 +1,43 @@
+// Copyright 2024 Flower Labs GmbH. All Rights Reserved.
+
+import Foundation
+import MLX
+
+protocol Engine {
+    func chat(
+        _ messages: [Message],
+        model: String?,
+        temperature: Float?,
+        maxCompletionTokens: Int?,
+        stream: Bool,
+        onStreamEvent: ((StreamEvent) -> Void)?,
+        tools: [Tool]?
+    ) async throws -> Message
+    
+    func fetchModel(model: String, callback: @escaping (Progress) -> Void) async throws -> Void
+}
+
+extension Engine {
+    func chat(
+        _ messages: [Message],
+        model: String?,
+        temperature: Float? = nil,
+        maxCompletionTokens: Int? = nil,
+        stream: Bool = false,
+        onStreamEvent: ((StreamEvent) -> Void)? = nil,
+        tools: [Tool]? = nil
+    ) async throws -> Message {
+        throw Failure(
+            code: .notImplementedError,
+            message: "Chat function is not implemented yet"
+        )
+    }
+    
+    func fetchModel(model: String, callback: @escaping (Progress) -> Void) async throws -> Void {
+        throw Failure(
+            code: .notImplementedError,
+            message: "FetchModel function is not implemented yet"
+        )
+    }
+        
+}
---
+++ intelligence/swift/src/Engine/MlxEngine.swift
@@ -0,0 +1,114 @@
+// Copyright 2024 Flower Labs GmbH. All Rights Reserved.
+
+import Foundation
+import MLX
+import MLXLLM
+import MLXRandom
+import MLXLMCommon
+
+class MlxEngine: Engine {
+    private let modelMapping = MODEL_MAPPING
+    enum LoadState {
+        case idle
+        case loaded(ModelContainer)
+    }
+
+    var loadState = LoadState.idle
+    var modelConfiguration = ModelRegistry.llama3_2_1B_4bit {
+        didSet {
+            loadState = .idle
+        }
+    }
+    
+    func load(_ progressHandler: ((Progress) -> Void)? = nil) async throws -> ModelContainer {
+        switch loadState {
+        case .idle:
+            MLX.GPU.set(cacheLimit: 20 * 1024 * 1024)
+
+            let modelContainer = try await LLMModelFactory.shared.loadContainer(
+                configuration: modelConfiguration
+            ) { [modelConfiguration] progress in
+                if let progressHandler = progressHandler {
+                    let ret = Progress(totalBytes: Int(progress.totalUnitCount), loadedBytes: Int(progress.completedUnitCount), percentage: progress.fractionCompleted, description: modelConfiguration.name)
+                    progressHandler(ret)
+                }
+            }
+
+            loadState = .loaded(modelContainer)
+            return modelContainer
+
+        case .loaded(let modelContainer):
+            return modelContainer
+        }
+    }
+    
+    private func convertToolsToDictionaries(_ tools: [Tool]?) throws -> [[String: Any]] {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        if let tools = tools {
+            let jsonData = try encoder.encode(tools)
+            let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
+            if let dictionaryArray = jsonObject as? [[String: Any]] {
+                return dictionaryArray
+            }
+        }
+        return []
+    }
+    
+    func chat(
+        _ messages: [Message],
+        model: String? = nil,
+        temperature: Float? = nil,
+        maxCompletionTokens: Int? = nil,
+        stream: Bool = false,
+        onStreamEvent: ((StreamEvent) -> Void)? = nil,
+        tools: [Tool]? = nil
+    ) async throws -> [String : Any] {
+        if let model = model {
+            modelConfiguration = modelMapping[model] ?? ModelRegistry.llama3_2_1B_4bit
+        }
+        
+        let generateParameters = GenerateParameters(temperature: temperature ?? 0.6)
+        let displayEveryNTokens = 4
+        
+        let modelContainer = try await load()
+        MLXRandom.seed(UInt64(Date.timeIntervalSinceReferenceDate * 1000))
+
+        let result = try await modelContainer.perform { context in
+            let input = try await context.processor.prepare(
+                input: .init(
+                    messages: messages.map {
+                        Dictionary(uniqueKeysWithValues: [($0.role, $0.content)])
+                    },
+                    tools: convertToolsToDictionaries(tools))
+            )
+            return try MLXLMCommon.generate(
+                input: input, parameters: generateParameters, context: context
+            ) { tokens in
+                if tokens.count % displayEveryNTokens == 0 {
+                    let text = context.tokenizer.decode(tokens: tokens)
+                    if stream, let onStreamEvent = onStreamEvent {
+                        onStreamEvent(StreamEvent(chunk: text))
+                    }
+                }
+                if tokens.count >= maxCompletionTokens ?? 240 {
+                    return .stop
+                } else {
+                    return .more
+                }
+            }
+        }
+        return ["content" : result.output]
+    }
+    
+    func fetchModel(model: String, callback: @escaping (Progress) -> Void) async throws -> Void {
+        modelConfiguration = modelMapping[model] ?? ModelRegistry.llama3_2_1B_4bit
+        let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let base = documents.appending(component: "huggingface")
+        if case let .id(id) = modelConfiguration.id {
+            let modelUrl = base.appending(component: "models").appending(component: id)
+            if FileManager.default.fileExists(atPath: modelUrl.path) { return }
+        }
+        _ = try await load()
+    }
+}
