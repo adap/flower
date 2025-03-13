490335e60fb151f43dcfe59366d97633b9afb55d
Daniel Nata Nugraha <daniel.nata@flower.ai>
2025-03-13 13:31:22 +0100

    Add project.yml

---
+++ intelligence/swift/examples/hello-world-ios/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  FI-Swift-Examples
+//
+//  Created by Daniel Nugraha on 13.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
