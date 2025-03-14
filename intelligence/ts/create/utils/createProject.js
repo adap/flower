f5e1bc1f42b8840f94d28000abc8e1b77e7f8827
Charles Beauville <charles@flower.ai>
2025-03-14 23:51:53 +0100

    Fix formatting

--- intelligence/ts/create/utils/createProject.js
+++ intelligence/ts/create/utils/createProject.js
@@ -1,26 +1,18 @@
-import fs from "fs/promises";
-import path from "node:path";
-import chalk from "chalk";
-import { execSync } from "node:child_process";
+import fs from 'fs/promises';
+import path from 'node:path';
+import chalk from 'chalk';
+import { execSync } from 'node:child_process';
 
-const updateFile = async (
-  filename,
-  projectPath,
-  requestedPackage,
-  projectName,
-) => {
+const updateFile = async (filename, projectPath, requestedPackage, projectName) => {
   console.log(chalk.gray(`Updating ${filename}...`));
 
   const packagePath = path.join(projectPath, filename);
   const stat = await fs.lstat(packagePath);
 
   if (stat.isFile()) {
-    const currentPackageJson = await fs.readFile(packagePath, "utf8");
-    const newFileContent = currentPackageJson.replace(
-      requestedPackage,
-      projectName,
-    );
-    await fs.writeFile(packagePath, newFileContent, "utf8");
+    const currentPackageJson = await fs.readFile(packagePath, 'utf8');
+    const newFileContent = currentPackageJson.replace(requestedPackage, projectName);
+    await fs.writeFile(packagePath, newFileContent, 'utf8');
   }
 };
 
@@ -31,23 +23,15 @@ export const init = async (projectName, requestedPackage) => {
     const tempDir = path.join(currentDir, `temp-${projectName}`);
     const fullURL = `https://github.com/adap/flower.git`;
 
-    console.log(
-      chalk.gray("Cloning template repository into a temporary folder..."),
-    );
+    console.log(chalk.gray('Cloning template repository into a temporary folder...'));
     execSync(`git clone --depth 1 ${fullURL} ${tempDir}`, {
-      stdio: "inherit",
+      stdio: 'inherit',
     });
 
     // Define the path to the template inside the cloned repo
-    const templatePath = path.join(
-      tempDir,
-      "intelligence",
-      "ts",
-      "templates",
-      requestedPackage,
-    );
+    const templatePath = path.join(tempDir, 'intelligence', 'ts', 'templates', requestedPackage);
 
-    console.log(chalk.gray("Copying template files to project destination..."));
+    console.log(chalk.gray('Copying template files to project destination...'));
     // Create destination folder and copy the template files into it
     await fs.mkdir(destination, { recursive: true });
     await fs.cp(templatePath, destination, { recursive: true });
@@ -55,15 +39,10 @@ export const init = async (projectName, requestedPackage) => {
     // Remove the temporary cloned repository
     await fs.rm(tempDir, { recursive: true, force: true });
 
-    console.log(chalk.gray("📑  Files copied..."));
+    console.log(chalk.gray('📑  Files copied...'));
 
-    await updateFile(
-      "package.json",
-      destination,
-      requestedPackage,
-      projectName,
-    );
-    await updateFile("README.md", destination, requestedPackage, projectName);
+    await updateFile('package.json', destination, requestedPackage, projectName);
+    await updateFile('README.md', destination, requestedPackage, projectName);
 
     console.log(chalk.green(`\ncd ${projectName}\npnpm start`));
   } catch (error) {
