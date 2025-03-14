ad5068383184aa840c693e564bab749d936ba170
Charles Beauville <charles@flower.ai>
2025-03-14 09:21:13 +0100

    fix(intelligence) Fix for nextjs example (#5103)

--- intelligence/ts/examples/nextjs-web-chats/lib/chat.ts
+++ intelligence/ts/examples/nextjs-web-chats/lib/chat.ts
@@ -1,21 +1,21 @@
-import { ChatResponseResult, FlowerIntelligence, Message, ChatOptions } from '@flwr/flwr';
+import { ChatResponseResult, FlowerIntelligence, Message } from '@flwr/flwr';
 
 const fi: FlowerIntelligence = FlowerIntelligence.instance;
 
 // Global chat history with an initial system message.
 export const history: Message[] = [
-    { role: "system", content: "You are a friendly assistant that loves using emojis." }
+  { role: 'system', content: 'You are a friendly assistant that loves using emojis.' },
 ];
 
 export async function chatWithHistory(question: string): Promise<string> {
-    history.push({ role: "user", content: question });
-    const response: ChatResponseResult = await fi.chat(question, {
-        messages: history,
-    } as ChatOptions);
-    if (response.ok) {
-        history.push(response.message);
-        return response.message.content;
-    } else {
-        throw new Error('Failed to get a valid response.');
-    }
+  history.push({ role: 'user', content: question });
+  const response: ChatResponseResult = await fi.chat({
+    messages: history,
+  });
+  if (response.ok) {
+    history.push(response.message);
+    return response.message.content;
+  } else {
+    throw new Error('Failed to get a valid response.');
+  }
 }
