2fac14102f099bc051f7989666b4d833dd60650c
Taner Topal <taner@flower.ai>
2025-03-17 17:35:56 +0100

    Fix build issue with Vercel (#5124)

--- intelligence/ts/examples/chat/next.config.ts
+++ intelligence/ts/examples/chat/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Guide on how to configure for huggingface/transformers
+  // https://huggingface.co/docs/transformers.js/tutorials/next#step-2-install-and-configure-transformersjs
+  // (Optional) Export as a static site
+  // See https://nextjs.org/docs/pages/building-your-application/deploying/static-exports#configuration
+  output: 'export', // Feel free to modify/remove this option
+
+  // Override the default webpack configuration
+  webpack: (config) => {
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      'onnxruntime-node$': false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
