b6d95c937edab45a2e9a2fd9b2916dcc14a90512
Daniel Nata Nugraha <daniel.nata@flower.ai>
2025-04-24 15:41:23 +0200

    Add unit tests

--- intelligence/kt/build.gradle.kts
+++ intelligence/kt/build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ktfmt)
 }
