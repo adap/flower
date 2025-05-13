53b416c453844a9bc94615c571ff829938fe1c96
Heng Pan <pan@flower.ai>
2025-05-13 17:00:28 +0100

    update failure message

--- framework/py/flwr/server/serverapp/app.py
+++ framework/py/flwr/server/serverapp/app.py
@@ -186,7 +186,10 @@ def run_serverapp(  # pylint: disable=R0914, disable=W0212, disable=R0915
 
             # Set up heartbeat sender
             heartbeat_fn = get_grpc_app_heartbeat_fn(
-                grid._stub, run.run_id, failure_message="Heartbeat failed unexpectedly."
+                grid._stub,
+                run.run_id,
+                failure_message="Heartbeat failed unexpectedly. The SuperLink could not "
+                "find the provided run ID, or the run status is invalid.",
             )
             heartbeat_sender = HeartbeatSender(heartbeat_fn)
             heartbeat_sender.start()
