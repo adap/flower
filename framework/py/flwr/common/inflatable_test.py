b8453c22b36228366e4fa70dcc1a1f94a47336b5
Heng Pan <pan@flower.ai>
2025-05-16 08:34:26 +0100

    feat(framework) Include child object IDs in the object head (#5380)

--- framework/py/flwr/common/inflatable_test.py
+++ framework/py/flwr/common/inflatable_test.py
@@ -22,7 +22,7 @@ from dataclasses import dataclass
 
 import pytest
 
-from .constant import HEAD_BODY_DIVIDER, TYPE_BODY_LEN_DIVIDER
+from .constant import HEAD_BODY_DIVIDER, HEAD_VALUE_DIVIDER
 from .inflatable import (
     InflatableObject,
     _get_object_body,
@@ -31,6 +31,7 @@ from .inflatable import (
     check_body_len_consistency,
     get_object_body,
     get_object_body_len_from_object_content,
+    get_object_head_values_from_object_content,
     get_object_id,
     get_object_type_from_object_content,
 )
@@ -41,10 +42,11 @@ class CustomDataClass(InflatableObject):
     """A dummy dataclass to test Inflatable features."""
 
     data: bytes
+    _children = None
 
     def deflate(self) -> bytes:  # noqa: D102
         obj_body = self.data
-        return add_header_to_object_body(object_body=obj_body, cls=self)
+        return add_header_to_object_body(object_body=obj_body, obj=self)
 
     @classmethod
     def inflate(  # noqa: D102
@@ -54,6 +56,16 @@ class CustomDataClass(InflatableObject):
             raise ValueError("`CustomDataClass` does not have children.")
         object_body = get_object_body(object_content, cls)
         return cls(data=object_body)
+
+    @property
+    def children(self) -> dict[str, InflatableObject] | None:
+        """Get children."""
+        return self._children
+
+    @children.setter
+    def children(self, children: dict[str, InflatableObject]) -> None:
+        """Set children only for testing purposes."""
+        self._children = children
 
 
 def test_deflate_and_inflate() -> None:
@@ -102,20 +114,58 @@ def test_get_object_body() -> None:
     assert get_object_body(obj_b, CustomDataClass) == data
 
 
-def test_add_header_to_object_body() -> None:
-    """Test helper function that adds the header to the object body and returns the
-    object content."""
+@pytest.mark.parametrize(
+    "children",
+    [
+        [],
+        [CustomDataClass(b"child1 data")],
+        [CustomDataClass(b"child1 data"), CustomDataClass(b"child2 data")],
+    ],
+)
+def test_add_header_to_object_body(children: list[InflatableObject]) -> None:
+    """Test helper function that adds the header to the object body."""
     data = b"this is a test"
     obj = CustomDataClass(data)
+    obj.children = {child.object_id: child for child in children}
     obj_b = obj.deflate()
 
     # Expected object head
-    exp_obj_head = f"CustomDataClass{TYPE_BODY_LEN_DIVIDER}{len(data)}".encode()
+    head_str = f"%s{HEAD_VALUE_DIVIDER}%s{HEAD_VALUE_DIVIDER}%d" % (
+        "CustomDataClass",
+        ",".join(child.object_id for child in children),
+        len(data),
+    )
+    exp_obj_head = head_str.encode()
     assert _get_object_head(obj_b) == exp_obj_head
 
     # Expected object content
-    exp_obj_conten = exp_obj_head + HEAD_BODY_DIVIDER + data
-    assert add_header_to_object_body(data, obj) == exp_obj_conten
+    exp_obj_content = exp_obj_head + HEAD_BODY_DIVIDER + data
+    assert add_header_to_object_body(data, obj) == exp_obj_content
+
+
+@pytest.mark.parametrize(
+    "children",
+    [
+        [],
+        [CustomDataClass(b"child1 data")],
+        [CustomDataClass(b"child1 data"), CustomDataClass(b"child2 data")],
+    ],
+)
+def test_get_head_values_from_object_content(children: list[InflatableObject]) -> None:
+    """Test helper function that extracts the values of the object head."""
+    # Prepare
+    data = b"this is a test"
+    obj = CustomDataClass(data)
+    obj.children = {child.object_id: child for child in children}
+    obj_b = obj.deflate()
+
+    # Execute
+    obj_type, children_ids, body_len = get_object_head_values_from_object_content(obj_b)
+
+    # Assert
+    assert obj_type == "CustomDataClass"
+    assert children_ids == [child.object_id for child in children]
+    assert body_len == len(data)
 
 
 def test_get_object_type_from_object_content() -> None:
