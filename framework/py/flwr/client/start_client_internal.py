a6517d5fc23bbb6be3424924264025b961df3fe2
Daniel J. Beutel <daniel@flower.ai>
2025-05-20 22:38:47 +0200

    refactor(framework) Move  into

--- framework/py/flwr/client/start_client_internal.py
+++ framework/py/flwr/client/start_client_internal.py
@@ -36,9 +36,18 @@ from flwr.cli.install import install_from_fab
 from flwr.client.client import Client
 from flwr.client.client_app import ClientApp, LoadClientAppError
 from flwr.client.clientapp.app import flwr_clientapp
+from flwr.client.clientapp.clientappio_servicer import (
+    ClientAppInputs,
+    ClientAppIoServicer,
+)
+from flwr.client.grpc_adapter_client.connection import grpc_adapter
+from flwr.client.grpc_client.connection import grpc_connection
+from flwr.client.grpc_rere_client.connection import grpc_request_response
+from flwr.client.message_handler.message_handler import handle_control_message
 from flwr.client.nodestate.nodestate_factory import NodeStateFactory
+from flwr.client.run_info_store import DeprecatedRunInfoStore
 from flwr.client.typing import ClientFnExt
-from flwr.common import GRPC_MAX_MESSAGE_LENGTH, Context, EventType, Message, event
+from flwr.common import GRPC_MAX_MESSAGE_LENGTH, Context, Message
 from flwr.common.address import parse_address
 from flwr.common.constant import (
     CLIENT_OCTET,
@@ -57,18 +66,10 @@ from flwr.common.constant import (
 )
 from flwr.common.exit import ExitCode, flwr_exit
 from flwr.common.grpc import generic_create_grpc_server
-from flwr.common.logger import log, warn_deprecated_feature
+from flwr.common.logger import log
 from flwr.common.retry_invoker import RetryInvoker, RetryState, exponential
 from flwr.common.typing import Fab, Run, RunNotRunningException, UserConfig
 from flwr.proto.clientappio_pb2_grpc import add_ClientAppIoServicer_to_server
-
-from .clientapp.clientappio_servicer import ClientAppInputs, ClientAppIoServicer
-from .grpc_adapter_client.connection import grpc_adapter
-from .grpc_client.connection import grpc_connection
-from .grpc_rere_client.connection import grpc_request_response
-from .message_handler.message_handler import handle_control_message
-from .numpy_client import NumPyClient
-from .run_info_store import DeprecatedRunInfoStore
 
 
 def _check_actionable_client(
@@ -83,136 +84,6 @@ def _check_actionable_client(
         raise ValueError(
             "Both `client_fn` and `client` are provided, but only one is allowed"
         )
-
-
-# pylint: disable=import-outside-toplevel
-# pylint: disable=too-many-branches
-# pylint: disable=too-many-locals
-# pylint: disable=too-many-statements
-# pylint: disable=too-many-arguments
-def start_client(
-    *,
-    server_address: str,
-    client_fn: Optional[ClientFnExt] = None,
-    client: Optional[Client] = None,
-    grpc_max_message_length: int = GRPC_MAX_MESSAGE_LENGTH,
-    root_certificates: Optional[Union[bytes, str]] = None,
-    insecure: Optional[bool] = None,
-    transport: Optional[str] = None,
-    authentication_keys: Optional[
-        tuple[ec.EllipticCurvePrivateKey, ec.EllipticCurvePublicKey]
-    ] = None,
-    max_retries: Optional[int] = None,
-    max_wait_time: Optional[float] = None,
-) -> None:
-    """Start a Flower client node which connects to a Flower server.
-
-    Warning
-    -------
-    This function is deprecated since 1.13.0. Use :code:`flower-supernode` command
-    instead to start a SuperNode.
-
-    Parameters
-    ----------
-    server_address : str
-        The IPv4 or IPv6 address of the server. If the Flower
-        server runs on the same machine on port 8080, then `server_address`
-        would be `"[::]:8080"`.
-    client_fn : Optional[ClientFnExt]
-        A callable that instantiates a Client. (default: None)
-    client : Optional[flwr.client.Client]
-        An implementation of the abstract base
-        class `flwr.client.Client` (default: None)
-    grpc_max_message_length : int (default: 536_870_912, this equals 512MB)
-        The maximum length of gRPC messages that can be exchanged with the
-        Flower server. The default should be sufficient for most models.
-        Users who train very large models might need to increase this
-        value. Note that the Flower server needs to be started with the
-        same value (see `flwr.server.start_server`), otherwise it will not
-        know about the increased limit and block larger messages.
-    root_certificates : Optional[Union[bytes, str]] (default: None)
-        The PEM-encoded root certificates as a byte string or a path string.
-        If provided, a secure connection using the certificates will be
-        established to an SSL-enabled Flower server.
-    insecure : bool (default: True)
-        Starts an insecure gRPC connection when True. Enables HTTPS connection
-        when False, using system certificates if `root_certificates` is None.
-    transport : Optional[str] (default: None)
-        Configure the transport layer. Allowed values:
-        - 'grpc-bidi': gRPC, bidirectional streaming
-        - 'grpc-rere': gRPC, request-response (experimental)
-        - 'rest': HTTP (experimental)
-    authentication_keys : Optional[Tuple[PrivateKey, PublicKey]] (default: None)
-        Tuple containing the elliptic curve private key and public key for
-        authentication from the cryptography library.
-        Source: https://cryptography.io/en/latest/hazmat/primitives/asymmetric/ec/
-        Used to establish an authenticated connection with the server.
-    max_retries: Optional[int] (default: None)
-        The maximum number of times the client will try to connect to the
-        server before giving up in case of a connection error. If set to None,
-        there is no limit to the number of tries.
-    max_wait_time: Optional[float] (default: None)
-        The maximum duration before the client stops trying to
-        connect to the server in case of connection error.
-        If set to None, there is no limit to the total time.
-
-    Examples
-    --------
-    Starting a gRPC client with an insecure server connection::
-
-        start_client(
-            server_address=localhost:8080,
-            client_fn=client_fn,
-        )
-
-    Starting a TLS-enabled gRPC client using system certificates::
-
-        def client_fn(context: Context):
-            return FlowerClient().to_client()
-
-        start_client(
-            server_address=localhost:8080,
-            client_fn=client_fn,
-            insecure=False,
-        )
-
-    Starting a TLS-enabled gRPC client using provided certificates::
-
-        from pathlib import Path
-
-        start_client(
-            server_address=localhost:8080,
-            client_fn=client_fn,
-            root_certificates=Path("/crts/root.pem").read_bytes(),
-        )
-    """
-    msg = (
-        "flwr.client.start_client() is deprecated."
-        "\n\tInstead, use the `flower-supernode` CLI command to start a SuperNode "
-        "as shown below:"
-        "\n\n\t\t$ flower-supernode --insecure --superlink='<IP>:<PORT>'"
-        "\n\n\tTo view all available options, run:"
-        "\n\n\t\t$ flower-supernode --help"
-        "\n\n\tUsing `start_client()` is deprecated."
-    )
-    warn_deprecated_feature(name=msg)
-
-    event(EventType.START_CLIENT_ENTER)
-    start_client_internal(
-        server_address=server_address,
-        node_config={},
-        load_client_app_fn=None,
-        client_fn=client_fn,
-        client=client,
-        grpc_max_message_length=grpc_max_message_length,
-        root_certificates=root_certificates,
-        insecure=insecure,
-        transport=transport,
-        authentication_keys=authentication_keys,
-        max_retries=max_retries,
-        max_wait_time=max_wait_time,
-    )
-    event(EventType.START_CLIENT_LEAVE)
 
 
 # pylint: disable=import-outside-toplevel
@@ -637,107 +508,6 @@ def start_client_internal(
         time.sleep(sleep_duration)
 
 
-def start_numpy_client(
-    *,
-    server_address: str,
-    client: NumPyClient,
-    grpc_max_message_length: int = GRPC_MAX_MESSAGE_LENGTH,
-    root_certificates: Optional[bytes] = None,
-    insecure: Optional[bool] = None,
-    transport: Optional[str] = None,
-) -> None:
-    """Start a Flower NumPyClient which connects to a gRPC server.
-
-    Warning
-    -------
-    This function is deprecated since 1.7.0. Use :code:`flwr.client.start_client`
-    instead and first convert your :code:`NumPyClient` to type
-    :code:`flwr.client.Client` by executing its :code:`to_client()` method.
-
-    Parameters
-    ----------
-    server_address : str
-        The IPv4 or IPv6 address of the server. If the Flower server runs on
-        the same machine on port 8080, then `server_address` would be
-        `"[::]:8080"`.
-    client : flwr.client.NumPyClient
-        An implementation of the abstract base class `flwr.client.NumPyClient`.
-    grpc_max_message_length : int (default: 536_870_912, this equals 512MB)
-        The maximum length of gRPC messages that can be exchanged with the
-        Flower server. The default should be sufficient for most models.
-        Users who train very large models might need to increase this
-        value. Note that the Flower server needs to be started with the
-        same value (see `flwr.server.start_server`), otherwise it will not
-        know about the increased limit and block larger messages.
-    root_certificates : bytes (default: None)
-        The PEM-encoded root certificates as a byte string or a path string.
-        If provided, a secure connection using the certificates will be
-        established to an SSL-enabled Flower server.
-    insecure : Optional[bool] (default: None)
-        Starts an insecure gRPC connection when True. Enables HTTPS connection
-        when False, using system certificates if `root_certificates` is None.
-    transport : Optional[str] (default: None)
-        Configure the transport layer. Allowed values:
-        - 'grpc-bidi': gRPC, bidirectional streaming
-        - 'grpc-rere': gRPC, request-response (experimental)
-        - 'rest': HTTP (experimental)
-
-    Examples
-    --------
-    Starting a gRPC client with an insecure server connection::
-
-        start_numpy_client(
-            server_address=localhost:8080,
-            client=FlowerClient(),
-        )
-
-    Starting a TLS-enabled gRPC client using system certificates::
-
-        start_numpy_client(
-            server_address=localhost:8080,
-            client=FlowerClient(),
-            insecure=False,
-        )
-
-    Starting a TLS-enabled gRPC client using provided certificates::
-
-        from pathlib import Path
-
-        start_numpy_client(
-            server_address=localhost:8080,
-            client=FlowerClient(),
-            root_certificates=Path("/crts/root.pem").read_bytes(),
-        )
-    """
-    mssg = (
-        "flwr.client.start_numpy_client() is deprecated. \n\tInstead, use "
-        "`flwr.client.start_client()` by ensuring you first call "
-        "the `.to_client()` method as shown below: \n"
-        "\tflwr.client.start_client(\n"
-        "\t\tserver_address='<IP>:<PORT>',\n"
-        "\t\tclient=FlowerClient().to_client(),"
-        " # <-- where FlowerClient is of type flwr.client.NumPyClient object\n"
-        "\t)\n"
-        "\tUsing `start_numpy_client()` is deprecated."
-    )
-
-    warn_deprecated_feature(name=mssg)
-
-    # Calling this function is deprecated. A warning is thrown.
-    # We first need to convert the supplied client to `Client.`
-
-    wrp_client = client.to_client()
-
-    start_client(
-        server_address=server_address,
-        client=wrp_client,
-        grpc_max_message_length=grpc_max_message_length,
-        root_certificates=root_certificates,
-        insecure=insecure,
-        transport=transport,
-    )
-
-
 def _init_connection(transport: Optional[str], server_address: str) -> tuple[
     Callable[
         [
@@ -781,7 +551,7 @@ def _init_connection(transport: Optional[str], server_address: str) -> tuple[
         try:
             from requests.exceptions import ConnectionError as RequestsConnectionError
 
-            from .rest_client.connection import http_request_response
+            from flwr.client.rest_client.connection import http_request_response
         except ModuleNotFoundError:
             flwr_exit(ExitCode.COMMON_MISSING_EXTRA_REST)
         if server_address[:4] != "http":
