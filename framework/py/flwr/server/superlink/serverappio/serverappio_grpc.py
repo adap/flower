bab92080990ad4f44d0238d21000e6b5051e5f5a
Javier <jafermarq@users.noreply.github.com>
2025-05-22 13:51:40 +0100

    refactor(framework) Add `ObjectStore` factory to `ServerAppIoServicer` and `FleetServicer` (#5384)
    Co-authored-by: Heng Pan <pan@flower.ai>

--- framework/py/flwr/server/superlink/serverappio/serverappio_grpc.py
+++ framework/py/flwr/server/superlink/serverappio/serverappio_grpc.py
@@ -28,6 +28,7 @@ from flwr.proto.serverappio_pb2_grpc import (  # pylint: disable=E0611
 )
 from flwr.server.superlink.ffs.ffs_factory import FfsFactory
 from flwr.server.superlink.linkstate import LinkStateFactory
+from flwr.supercore.object_store import ObjectStoreFactory
 
 from .serverappio_servicer import ServerAppIoServicer
 
@@ -36,6 +37,7 @@ def run_serverappio_api_grpc(
     address: str,
     state_factory: LinkStateFactory,
     ffs_factory: FfsFactory,
+    objectstore_factory: ObjectStoreFactory,
     certificates: Optional[tuple[bytes, bytes, bytes]],
 ) -> grpc.Server:
     """Run ServerAppIo API (gRPC, request-response)."""
@@ -43,6 +45,7 @@ def run_serverappio_api_grpc(
     serverappio_servicer: grpc.Server = ServerAppIoServicer(
         state_factory=state_factory,
         ffs_factory=ffs_factory,
+        objectstore_factory=objectstore_factory,
     )
     serverappio_add_servicer_to_server_fn = add_ServerAppIoServicer_to_server
     serverappio_grpc_server = generic_create_grpc_server(
