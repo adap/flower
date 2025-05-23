af6a0b457a537777e1e18111b49dbe9abfffb2cd
Heng Pan <pan@flower.ai>
2025-05-23 17:31:01 +0100

    delete CreateRun RPC

--- framework/py/flwr/server/superlink/serverappio/serverappio_servicer.py
+++ framework/py/flwr/server/superlink/serverappio/serverappio_servicer.py
@@ -57,8 +57,6 @@ from flwr.proto.message_pb2 import (  # pylint: disable=E0611
 )
 from flwr.proto.node_pb2 import Node  # pylint: disable=E0611
 from flwr.proto.run_pb2 import (  # pylint: disable=E0611
-    CreateRunRequest,
-    CreateRunResponse,
     GetRunRequest,
     GetRunResponse,
     GetRunStatusRequest,
@@ -120,32 +118,6 @@ class ServerAppIoServicer(serverappio_pb2_grpc.ServerAppIoServicer):
         all_ids: set[int] = state.get_nodes(request.run_id)
         nodes: list[Node] = [Node(node_id=node_id) for node_id in all_ids]
         return GetNodesResponse(nodes=nodes)
-
-    def CreateRun(
-        self, request: CreateRunRequest, context: grpc.ServicerContext
-    ) -> CreateRunResponse:
-        """Create run ID."""
-        log(DEBUG, "ServerAppIoServicer.CreateRun")
-        state: LinkState = self.state_factory.state()
-        if request.HasField("fab"):
-            fab = fab_from_proto(request.fab)
-            ffs: Ffs = self.ffs_factory.ffs()
-            fab_hash = ffs.put(fab.content, {})
-            _raise_if(
-                validation_error=fab_hash != fab.hash_str,
-                request_name="CreateRun",
-                detail=f"FAB ({fab.hash_str}) hash from request doesn't match contents",
-            )
-        else:
-            fab_hash = ""
-        run_id = state.create_run(
-            request.fab_id,
-            request.fab_version,
-            fab_hash,
-            user_config_from_proto(request.override_config),
-            ConfigRecord(),
-        )
-        return CreateRunResponse(run_id=run_id)
 
     def PushMessages(
         self, request: PushInsMessagesRequest, context: grpc.ServicerContext
