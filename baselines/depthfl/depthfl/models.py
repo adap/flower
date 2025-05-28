5bc47889bfb6f2a44b279124c78be6fea7da83d1
Ragnar <rodiondenmark@gmail.com>
2025-05-28 16:32:34 +0200

    Update models.py

--- baselines/depthfl/depthfl/models.py
+++ baselines/depthfl/depthfl/models.py
@@ -210,7 +210,7 @@ def test(  # pylint: disable=too-many-locals
             images, labels = images.to(device), labels.to(device)
             output_lst = net(images)
 
-            # ensemble classfiers' output
+            # ensemble classifiers' output
             ensemble_output = torch.stack(output_lst, dim=2)
             ensemble_output = torch.sum(ensemble_output, dim=2) / len(output_lst)
 
