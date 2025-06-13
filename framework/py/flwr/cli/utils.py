ad7cb86d1ceaf73f37b4637f7056400bb60d7c82
Heng Pan <pan@flower.ai>
2025-06-13 11:35:26 +0100

    address comment

--- framework/py/flwr/cli/utils.py
+++ framework/py/flwr/cli/utils.py
@@ -28,7 +28,12 @@ import typer
 
 from flwr.cli.cli_user_auth_interceptor import CliUserAuthInterceptor
 from flwr.common.auth_plugin import CliAuthPlugin
-from flwr.common.constant import AUTH_TYPE_JSON_KEY, CREDENTIALS_DIR, FLWR_DIR
+from flwr.common.constant import (
+    AUTH_TYPE_JSON_KEY,
+    CREDENTIALS_DIR,
+    FLWR_DIR,
+    RUN_ID_NOT_FOUND_MESSAGE,
+)
 from flwr.common.grpc import (
     GRPC_MAX_MESSAGE_LENGTH,
     create_channel,
@@ -323,7 +328,10 @@ def flwr_cli_grpc_exc_handler() -> Iterator[None]:
             # pylint: disable=E1101
             typer.secho(e.details(), fg=typer.colors.RED, bold=True)
             raise typer.Exit(code=1) from None
-        if e.code() == grpc.StatusCode.NOT_FOUND and e.details() == "Run ID not found":
+        if (
+            e.code() == grpc.StatusCode.NOT_FOUND
+            and e.details() == RUN_ID_NOT_FOUND_MESSAGE
+        ):
             typer.secho(
                 "❌ Run ID not found.",
                 fg=typer.colors.RED,
