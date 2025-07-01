94a2867de93e61d0a504042979faacc84238720e
Chong Shen Ng <chong.shen@flower.ai>
2025-07-01 17:45:57 +0100

    Update framework/py/flwr/superexec/exec_grpc.py
    Co-authored-by: Daniel J. Beutel <daniel@flower.ai>

--- framework/py/flwr/superexec/exec_grpc.py
+++ framework/py/flwr/superexec/exec_grpc.py
@@ -63,7 +63,8 @@ def run_exec_api_grpc(
     """Run Exec API (gRPC, request-response)."""
     executor.set_config(config)
 
-    license_checker = get_license_checker()() if get_license_checker() else None
+    license_plugin: LicensePlugin = get_license_checker()
+    license_checker = license_plugin() if license_plugin else None
     if license_checker:
         license_checker.get_license_info()
         if not license_checker.check_license():
