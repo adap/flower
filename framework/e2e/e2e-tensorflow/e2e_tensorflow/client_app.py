ebdb2496539de71c7122c2bffa1f6028cf809af7
Heng Pan <pan@flower.ai>
2025-07-11 10:45:31 +0100

    use tf datasets

--- framework/e2e/e2e-tensorflow/e2e_tensorflow/client_app.py
+++ framework/e2e/e2e-tensorflow/e2e_tensorflow/client_app.py
@@ -1,35 +1,38 @@
 import os
 
 import tensorflow as tf
-from datasets import load_dataset
+import tensorflow_datasets as tfds
 
 from flwr.client import ClientApp, NumPyClient, start_client
 from flwr.common import Context
 
-SUBSET_SIZE = 1000
+# Set subset sizes
+TRAIN_SUBSET_SIZE = 100
+TEST_SUBSET_SIZE = 10
 
 # Make TensorFlow log less verbose
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-# Load CIFAR-10 from Hugging Face
-dataset = load_dataset("uoft-cs/cifar10")
-dataset.set_format(type="tensorflow", columns=["img", "label"])
+
+# Load CIFAR-10 as supervised (returns (img, label))
+ds_train, ds_test = tfds.load("cifar10", split=["train", "test"], as_supervised=True)
 
 
-# Define train and test conversions
-def convert_to_tf_dataset(split):
-    return tf.data.Dataset.from_generator(
-        lambda: dataset[split],
-        output_signature={
-            "img": tf.TensorSpec(shape=(32, 32, 3), dtype=tf.uint8),
-            "label": tf.TensorSpec(shape=(), dtype=tf.int64),
-        },
-    ).map(lambda x: (tf.cast(x["img"], tf.float32) / 255.0, x["label"]))
+# Define one fast preprocessing function
+@tf.function
+def preprocess_sample(image, label):
+    image = tf.cast(image, tf.float32) / 255.0
+    return image, label
 
 
-# Apply and batch/prefetch
-ds_train = convert_to_tf_dataset("train").batch(32).prefetch(tf.data.AUTOTUNE)
-ds_test = convert_to_tf_dataset("test").batch(32).prefetch(tf.data.AUTOTUNE)
+def preprocess(split, subset_size):
+    return (
+        split.take(subset_size)
+        .map(preprocess_sample, num_parallel_calls=tf.data.AUTOTUNE)
+        .batch(32)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+
 
 # Load model (MobileNetV2, CIFAR-10)
 model = tf.keras.applications.MobileNetV2(
@@ -45,12 +48,12 @@ class FlowerClient(NumPyClient):
 
     def fit(self, parameters, config):
         model.set_weights(parameters)
-        model.fit(ds_train, epochs=1, batch_size=32)
+        model.fit(preprocess(ds_train, TRAIN_SUBSET_SIZE), epochs=1, batch_size=32)
         return model.get_weights(), len(ds_train), {}
 
     def evaluate(self, parameters, config):
         model.set_weights(parameters)
-        loss, accuracy = model.evaluate(ds_test)
+        loss, accuracy = model.evaluate(preprocess(ds_test, TEST_SUBSET_SIZE))
         return loss, len(ds_test), {"accuracy": accuracy}
 
 
