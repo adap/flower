18448485658f1af7ab0622e35c22ad199ebddf2a
Charles Beauville <charles@flower.ai>
2025-07-29 18:10:55 +0200

    ci(intelligence): Bump to 0.2.0 (post 0.1.13 release)

--- intelligence/ts/src/constants.ts
+++ intelligence/ts/src/constants.ts
@@ -15,6 +15,6 @@
 
 export const DEFAULT_MODEL = 'meta/llama3.2-1b/instruct-fp16';
 export const REMOTE_URL = 'https://api.flower.ai';
-export const VERSION = '0.1.13';
+export const VERSION = '0.2.0';
 export const SDK = 'TS';
 export const ALLOWED_ROLES = ['user', 'system', 'assistant'] as const;
