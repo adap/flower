78f3551c6a78ec5bcdf86d95195e188a6c14da74
yan-gao-GY <yan@flower.ai>
2025-09-08 17:33:45 +0100

    Init migration

--- examples/xgboost-quickstart/xgboost_quickstart/client_app.py
+++ examples/xgboost-quickstart/xgboost_quickstart/client_app.py
@@ -1,117 +1,37 @@
 """xgboost_quickstart: A Flower / XGBoost app."""
 
 import warnings
-
-from flwr.common.context import Context
-
 import xgboost as xgb
-from flwr.client import Client, ClientApp
+
+from flwr.client import ClientApp
+from flwr.common import Context, Message, ConfigRecord, RecordDict, MetricRecord
 from flwr.common.config import unflatten_dict
-from flwr.common import (
-    Code,
-    EvaluateIns,
-    EvaluateRes,
-    FitIns,
-    FitRes,
-    Parameters,
-    Status,
-)
 
 from xgboost_quickstart.task import load_data, replace_keys
+
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
 
-# Define Flower Client and client_fn
-class FlowerClient(Client):
-    def __init__(
-        self,
-        train_dmatrix,
-        valid_dmatrix,
-        num_train,
-        num_val,
-        num_local_round,
-        params,
-    ):
-        self.train_dmatrix = train_dmatrix
-        self.valid_dmatrix = valid_dmatrix
-        self.num_train = num_train
-        self.num_val = num_val
-        self.num_local_round = num_local_round
-        self.params = params
-
-    def _local_boost(self, bst_input):
-        # Update trees based on local training data.
-        for i in range(self.num_local_round):
-            bst_input.update(self.train_dmatrix, bst_input.num_boosted_rounds())
-
-        # Bagging: extract the last N=num_local_round trees for sever aggregation
-        bst = bst_input[
-            bst_input.num_boosted_rounds()
-            - self.num_local_round : bst_input.num_boosted_rounds()
-        ]
-
-        return bst
-
-    def fit(self, ins: FitIns) -> FitRes:
-        global_round = int(ins.config["global_round"])
-        if global_round == 1:
-            # First round local training
-            bst = xgb.train(
-                self.params,
-                self.train_dmatrix,
-                num_boost_round=self.num_local_round,
-                evals=[(self.valid_dmatrix, "validate"), (self.train_dmatrix, "train")],
-            )
-        else:
-            bst = xgb.Booster(params=self.params)
-            global_model = bytearray(ins.parameters.tensors[0])
-
-            # Load global model into booster
-            bst.load_model(global_model)
-
-            # Local training
-            bst = self._local_boost(bst)
-
-        # Save model
-        local_model = bst.save_raw("json")
-        local_model_bytes = bytes(local_model)
-
-        return FitRes(
-            status=Status(
-                code=Code.OK,
-                message="OK",
-            ),
-            parameters=Parameters(tensor_type="", tensors=[local_model_bytes]),
-            num_examples=self.num_train,
-            metrics={},
-        )
-
-    def evaluate(self, ins: EvaluateIns) -> EvaluateRes:
-        # Load global model
-        bst = xgb.Booster(params=self.params)
-        para_b = bytearray(ins.parameters.tensors[0])
-        bst.load_model(para_b)
-
-        # Run evaluation
-        eval_results = bst.eval_set(
-            evals=[(self.valid_dmatrix, "valid")],
-            iteration=bst.num_boosted_rounds() - 1,
-        )
-        auc = round(float(eval_results.split("\t")[1].split(":")[1]), 4)
-
-        return EvaluateRes(
-            status=Status(
-                code=Code.OK,
-                message="OK",
-            ),
-            loss=0.0,
-            num_examples=self.num_val,
-            metrics={"AUC": auc},
-        )
+# Flower ClientApp
+app = ClientApp()
 
 
-def client_fn(context: Context):
+def _local_boost(bst_input, num_local_round, train_dmatrix):
+    # Update trees based on local training data.
+    for i in range(num_local_round):
+        bst_input.update(train_dmatrix, bst_input.num_boosted_rounds())
+
+    # Bagging: extract the last N=num_local_round trees for sever aggregation
+    bst = bst_input[
+          bst_input.num_boosted_rounds()
+          - num_local_round: bst_input.num_boosted_rounds()
+          ]
+    return bst
+
+
+@app.train()
+def train(msg: Message, context: Context) -> Message:
     # Load model and data
     partition_id = context.node_config["partition-id"]
     num_partitions = context.node_config["num-partitions"]
@@ -119,21 +39,70 @@ def client_fn(context: Context):
         partition_id, num_partitions
     )
 
+    # Load config
     cfg = replace_keys(unflatten_dict(context.run_config))
     num_local_round = cfg["local_epochs"]
+    params = cfg["params"]
 
-    # Return Client instance
-    return FlowerClient(
-        train_dmatrix,
-        valid_dmatrix,
-        num_train,
-        num_val,
-        num_local_round,
-        cfg["params"],
+    global_round = msg.content["model_config"]["server-round"]
+    if global_round == 1:
+        # First round local training
+        bst = xgb.train(
+            params,
+            train_dmatrix,
+            num_boost_round=num_local_round,
+            evals=[(valid_dmatrix, "validate"), (train_dmatrix, "train")],
+        )
+    else:
+        bst = xgb.Booster(params=params)
+        global_model = bytearray(msg.content["model_config"]["model"])
+
+        # Load global model into booster
+        bst.load_model(global_model)
+
+        # Local training
+        bst = _local_boost(bst, num_local_round, train_dmatrix)
+
+    # Save model
+    local_model = bst.save_raw("json")
+    local_model_bytes = bytes(local_model)
+
+    # Construct reply message
+    model_record = ConfigRecord({"model": local_model_bytes})
+    content = RecordDict({"local_model": model_record})
+    return Message(content=content, reply_to=msg)
+
+
+@app.evaluate()
+def evaluate(msg: Message, context: Context) -> Message:
+    # Load model and data
+    partition_id = context.node_config["partition-id"]
+    num_partitions = context.node_config["num-partitions"]
+    _, valid_dmatrix, _, num_val = load_data(
+        partition_id, num_partitions
     )
 
+    # Load config
+    cfg = replace_keys(unflatten_dict(context.run_config))
+    params = cfg["params"]
 
-# Flower ClientApp
-app = ClientApp(
-    client_fn,
-)
+    # Load global model
+    bst = xgb.Booster(params=params)
+    para_b = bytearray(msg.content["model_config"]["model"])
+    bst.load_model(para_b)
+
+    # Run evaluation
+    eval_results = bst.eval_set(
+        evals=[(valid_dmatrix, "valid")],
+        iteration=bst.num_boosted_rounds() - 1,
+    )
+    auc = round(float(eval_results.split("\t")[1].split(":")[1]), 4)
+
+    # Construct and return reply Message
+    metrics = {
+        "auc": auc,
+        "num-examples": num_val,
+    }
+    metric_record = MetricRecord(metrics)
+    content = RecordDict({"metrics": metric_record})
+    return Message(content=content, reply_to=msg)
--- examples/xgboost-quickstart/xgboost_quickstart/server_app.py
+++ examples/xgboost-quickstart/xgboost_quickstart/server_app.py
@@ -1,54 +1,187 @@
 """xgboost_quickstart: A Flower / XGBoost app."""
 
-from typing import Dict
+import json
+from logging import INFO, WARN
 
-from flwr.common import Context, Parameters
-from flwr.server import ServerApp, ServerAppComponents, ServerConfig
-from flwr.server.strategy import FedXgbBagging
-
-
-def evaluate_metrics_aggregation(eval_metrics):
-    """Return an aggregated metric (AUC) for evaluation."""
-    total_num = sum([num for num, _ in eval_metrics])
-    auc_aggregated = (
-        sum([metrics["AUC"] * num for num, metrics in eval_metrics]) / total_num
-    )
-    metrics_aggregated = {"AUC": auc_aggregated}
-    return metrics_aggregated
-
-
-def config_func(rnd: int) -> Dict[str, str]:
-    """Return a configuration with global epochs."""
-    config = {
-        "global_round": str(rnd),
-    }
-    return config
-
-
-def server_fn(context: Context):
-    # Read from config
-    num_rounds = context.run_config["num-server-rounds"]
-    fraction_fit = context.run_config["fraction-fit"]
-    fraction_evaluate = context.run_config["fraction-evaluate"]
-
-    # Init an empty Parameter
-    parameters = Parameters(tensor_type="", tensors=[])
-
-    # Define strategy
-    strategy = FedXgbBagging(
-        fraction_fit=fraction_fit,
-        fraction_evaluate=fraction_evaluate,
-        evaluate_metrics_aggregation_fn=evaluate_metrics_aggregation,
-        on_evaluate_config_fn=config_func,
-        on_fit_config_fn=config_func,
-        initial_parameters=parameters,
-    )
-    config = ServerConfig(num_rounds=num_rounds)
-
-    return ServerAppComponents(strategy=strategy, config=config)
-
+from flwr.common import ConfigRecord, Context, Message, MessageType, RecordDict
+from flwr.common.logger import log
+from flwr.server import Grid, ServerApp
+from flwr.serverapp.strategy.strategy_utils import (
+    aggregate_metricrecords,
+    sample_nodes,
+    validate_message_reply_consistency,
+)
 
 # Create ServerApp
-app = ServerApp(
-    server_fn=server_fn,
-)
+app = ServerApp()
+
+
+def aggregate_bagging(
+    bst_prev_org: bytes | None,
+    bst_curr_org: bytes,
+) -> bytes:
+    """Conduct bagging aggregation for given trees."""
+    if not bst_prev_org:
+        return bst_curr_org
+
+    # Get the tree numbers
+    tree_num_prev, _ = _get_tree_nums(bst_prev_org)
+    _, paral_tree_num_curr = _get_tree_nums(bst_curr_org)
+
+    bst_prev = json.loads(bytearray(bst_prev_org))
+    bst_curr = json.loads(bytearray(bst_curr_org))
+
+    bst_prev["learner"]["gradient_booster"]["model"]["gbtree_model_param"][
+        "num_trees"
+    ] = str(tree_num_prev + paral_tree_num_curr)
+    iteration_indptr = bst_prev["learner"]["gradient_booster"]["model"][
+        "iteration_indptr"
+    ]
+    bst_prev["learner"]["gradient_booster"]["model"]["iteration_indptr"].append(
+        iteration_indptr[-1] + paral_tree_num_curr
+    )
+
+    # Aggregate new trees
+    trees_curr = bst_curr["learner"]["gradient_booster"]["model"]["trees"]
+    for tree_count in range(paral_tree_num_curr):
+        trees_curr[tree_count]["id"] = tree_num_prev + tree_count
+        bst_prev["learner"]["gradient_booster"]["model"]["trees"].append(
+            trees_curr[tree_count]
+        )
+        bst_prev["learner"]["gradient_booster"]["model"]["tree_info"].append(0)
+
+    bst_prev_bytes = bytes(json.dumps(bst_prev), "utf-8")
+
+    return bst_prev_bytes
+
+
+def _get_tree_nums(xgb_model_org: bytes) -> tuple[int, int]:
+    xgb_model = json.loads(bytearray(xgb_model_org))
+    # Get the number of trees
+    tree_num = int(
+        xgb_model["learner"]["gradient_booster"]["model"]["gbtree_model_param"][
+            "num_trees"
+        ]
+    )
+    # Get the number of parallel trees
+    paral_tree_num = int(
+        xgb_model["learner"]["gradient_booster"]["model"]["gbtree_model_param"][
+            "num_parallel_tree"
+        ]
+    )
+    return tree_num, paral_tree_num
+
+
+def _construct_messages(
+    node_ids: list[int],
+    record: RecordDict,
+    message_type: MessageType,
+) -> list[Message]:
+
+    messages = []
+    for node_id in node_ids:  # one message for each node
+        message = Message(
+            content=record,
+            message_type=message_type,  # target method in ClientApp
+            dst_node_id=node_id,
+        )
+        messages.append(message)
+    return messages
+
+def configure_nodes(
+    grid: Grid,
+    fraction: float,
+    min_available_nodes: int,
+    global_model: bytes | None,
+    server_round: int,
+    message_type: str
+) -> list[Message]:
+    """Configure nodes for training or evaluation."""
+    # Sample nodes
+    sample_size = int(len(list(grid.get_node_ids())) * fraction)
+    node_ids, num_total = sample_nodes(grid, min_available_nodes, sample_size)
+    log(
+        INFO,
+        "configure_train: Sampled %s nodes (out of %s)",
+        len(node_ids),
+        len(num_total),
+    )
+
+    # Create messages
+    gmodel_config_record = ConfigRecord({
+        "model": global_model,
+        "server-round": server_round,
+    })
+    recorddict = RecordDict({"model_config": gmodel_config_record})
+
+    return _construct_messages(node_ids, recorddict, message_type)
+
+
+@app.main()
+def main(grid: Grid, context: Context) -> None:
+    # Load config
+    num_rounds = context.run_config["num-server-rounds"]
+    fraction_train = context.run_config["fraction-train"]
+    fraction_evaluate = context.run_config["fraction-evaluate"]
+    min_available_nodes = context.run_config["min-available-nodes"]
+
+    # Init global model
+    global_model = b""  # Init with empty list
+
+    for server_round in range(num_rounds):
+        log(INFO, "")  # Add newline for log readability
+        log(INFO, "Starting round %s/%s", server_round + 1, num_rounds)
+
+        # --- TRAIN (CLIENTAPP-SIDE) ---------------------------------
+        # Construct train messages
+        messages = configure_nodes(
+            grid,
+            fraction_train,
+            min_available_nodes,
+            global_model,
+            server_round,
+            MessageType.TRAIN,
+        )
+
+        # Send messages and wait for all results
+        replies = grid.send_and_receive(messages)
+        log(INFO, "Received %s/%s results", len(replies), len(messages))
+
+        # Aggregate received models with bagging strategy
+        for msg in replies:
+            if msg.has_content():
+                bst = msg.content["local_model"]["model"]
+                global_model = aggregate_bagging(global_model, bst)
+            else:
+                log(WARN, f"message {msg.metadata.message_id} as an error.")
+
+        # --- EVALUATION (CLIENTAPP-SIDE) ---------------------------------
+        # Construct evaluate messages
+        messages = configure_nodes(
+            grid,
+            fraction_evaluate,
+            min_available_nodes,
+            global_model,
+            server_round,
+            MessageType.EVALUATE,
+        )
+
+        # Send messages and wait for all results
+        replies = grid.send_and_receive(messages)
+        log(INFO, "Received %s/%s results", len(replies), len(messages))
+
+        # Aggregate received metrics
+        replies_with_content = []
+        for msg in replies:
+            if msg.has_content():
+                replies_with_content.append(msg.content)
+            else:
+                log(WARN, f"message {msg.metadata.message_id} as an error.")
+
+        metrics =aggregate_metricrecords(
+            replies_with_content,
+            weighting_metric_name="num-examples",
+        )
+
+        # Log average eval AUC
+        log(INFO, f"Avg eval AUC: {metrics['auc']:.3f}")
