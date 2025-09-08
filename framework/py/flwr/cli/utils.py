7b85ab539abfffe40672cf13aac6fd48cda076ef
Heng Pan <pan@flower.ai>
2025-09-08 09:56:34 +0100

    Update framework/py/flwr/cli/utils.py
    Co-authored-by: Chong Shen Ng <chong.shen@flower.ai>

--- framework/py/flwr/cli/utils.py
+++ framework/py/flwr/cli/utils.py
@@ -322,8 +322,8 @@ def flwr_cli_grpc_exc_handler() -> Iterator[None]:
             else:
                 typer.secho(
                     "❌ The SuperLink cannot process this request. Please verify that "
-                    "you set the address to its Control API endpoint correctly, and "
-                    "ensure you are using a compatible Flower version.",
+                    "you set the address to its Control API endpoint correctly in `pyproject.toml`, and "
+                    "ensure that the Flower versions used by the CLI and SuperLink are compatible.",
                     fg=typer.colors.RED,
                     bold=True,
                 )
