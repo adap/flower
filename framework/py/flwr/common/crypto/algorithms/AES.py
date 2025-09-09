43c943408fb8157e66cddd111a5599283e4f0c8e
sarahfalco <sarah.falco.s@gmail.com>
2025-09-09 09:19:35 +0200

    fic

--- framework/py/flwr/common/crypto/algorithms/AES.py
+++ framework/py/flwr/common/crypto/algorithms/AES.py
@@ -1,11 +1,9 @@
-import inspect
-import time
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
 
-KEY_AES = b"0123456789abcdef0123456789abcdef"  # 32 byte -> AES-256
-BLOCK_SIZE = 16  # dimensione blocco AES (in byte)
+KEY_AES = b"0123456789abcdef0123456789abcdef"
+BLOCK_SIZE = 16
 
 def pad(data: bytes) -> bytes:
     pad_len = BLOCK_SIZE - len(data) % BLOCK_SIZE
@@ -18,21 +16,16 @@ def unpad(data: bytes) -> bytes:
     return data[:-pad_len]
 def encrypt(data: bytes) -> bytes:
 
-    # caller_frame = inspect.stack()[1]
-    # caller_name = caller_frame.function  # Nome della funzione chiamante
-    # start_time = time.perf_counter()
     iv = os.urandom(BLOCK_SIZE)
     cipher = Cipher(algorithms.AES(KEY_AES), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor()
     padded_data = pad(data)
     ciphertext = encryptor.update(padded_data) + encryptor.finalize()
-    end_time = time.perf_counter()
-    #print(f"Encryption time AES: {end_time - start_time:.4f} seconds (Called by: {caller_name})")
 
     return iv + ciphertext
 
 def decrypt(encrypted_data: bytes) -> bytes:
-    start_time = time.perf_counter()
+
     if len(encrypted_data) < BLOCK_SIZE:
         raise ValueError("Dati cifrati troppo corti")
     iv = encrypted_data[:BLOCK_SIZE]
@@ -41,6 +34,5 @@ def decrypt(encrypted_data: bytes) -> bytes:
     decryptor = cipher.decryptor()
     padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
     plaintext = unpad(padded_plaintext)
-    end_time = time.perf_counter()
-    #print(f"Decryption time AES: {end_time - start_time:.4f} seconds")
+
     return plaintext
