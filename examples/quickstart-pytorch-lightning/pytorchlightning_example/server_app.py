a0052c400a3fabecf182dbf951b098633792fda1
jafermarq <javier@flower.ai>
2025-09-09 17:32:00 +0100

    wip

--- examples/quickstart-pytorch-lightning/pytorchlightning_example/server_app.py
+++ examples/quickstart-pytorch-lightning/pytorchlightning_example/server_app.py
@@ -1,30 +1,39 @@
 """pytorchlightning_example: A Flower / PyTorch Lightning app."""
 
-from flwr.common import Context, ndarrays_to_parameters
-from flwr.server import ServerApp, ServerAppComponents, ServerConfig
-from flwr.server.strategy import FedAvg
+import torch
+from flwr.app import ArrayRecord, ConfigRecord, Context
+from flwr.serverapp import Grid, ServerApp
+from flwr.serverapp.strategy import FedAvg
 from pytorchlightning_example.task import LitAutoEncoder, get_parameters
 
+# Create ServerApp
+app = ServerApp()
 
-def server_fn(context: Context) -> ServerAppComponents:
-    """Construct components for ServerApp."""
 
-    # Convert model parameters to flwr.common.Parameters
-    ndarrays = get_parameters(LitAutoEncoder())
-    global_model_init = ndarrays_to_parameters(ndarrays)
+@app.main()
+def main(grid: Grid, context: Context) -> None:
+    """Main entry point for the ServerApp."""
 
-    # Define strategy
-    strategy = FedAvg(
-        fraction_fit=0.5,
-        fraction_evaluate=0.5,
-        initial_parameters=global_model_init,
+    # Read run config
+    num_rounds: int = context.run_config["num-server-rounds"]
+    lr: float = context.run_config["learning-rate"]
+
+    # Load global model
+    global_model = LitAutoEncoder()
+    arrays = ArrayRecord(global_model.state_dict())
+
+    # Initialize FedAvg strategy
+    strategy = FedAvg(fraction_train=0.5, fraction_evaluate=0.5)
+
+    # Start strategy, run FedAvg for `num_rounds`
+    result = strategy.start(
+        grid=grid,
+        initial_arrays=arrays,
+        train_config=ConfigRecord({"lr": lr}),
+        num_rounds=num_rounds,
     )
 
-    # Construct ServerConfig
-    num_rounds = context.run_config["num-server-rounds"]
-    config = ServerConfig(num_rounds=num_rounds)
-
-    return ServerAppComponents(strategy=strategy, config=config)
-
-
-app = ServerApp(server_fn=server_fn)
+    # Save final model to disk
+    print("\nSaving final model to disk...")
+    state_dict = result.arrays.to_torch_state_dict()
+    torch.save(state_dict, "final_model.pt")
