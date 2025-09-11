d0747104428a0c8da1e43509e2f248816217960a
yan-gao-GY <yan@flower.ai>
2025-09-11 11:02:38 +0100

    Update model saving method

--- examples/xgboost-quickstart/xgboost_quickstart/server_app.py
+++ examples/xgboost-quickstart/xgboost_quickstart/server_app.py
@@ -1,10 +1,14 @@
 """xgboost_quickstart: A Flower / XGBoost app."""
 
 import numpy as np
+import xgboost as xgb
 
 from flwr.common import ArrayRecord, Context
+from flwr.common.config import unflatten_dict
 from flwr.server import Grid, ServerApp
 from flwr.serverapp.strategy import FedXgbBagging
+
+from xgboost_quickstart.task import replace_keys
 
 
 # Create ServerApp
@@ -17,9 +21,12 @@ def main(grid: Grid, context: Context) -> None:
     num_rounds = context.run_config["num-server-rounds"]
     fraction_train = context.run_config["fraction-train"]
     fraction_evaluate = context.run_config["fraction-evaluate"]
+    # Flatted config dict and replace "-" with "_"
+    cfg = replace_keys(unflatten_dict(context.run_config))
+    params = cfg["params"]
 
     # Init global model
-    global_model = b""  # Init with empty object
+    global_model = b""  # Init with an empty object; the XGBooster will be created and trained on the client side.
     arrays = ArrayRecord([np.frombuffer(global_model, dtype=np.uint8)])
 
     # Initialize FedXgbBagging strategy
@@ -36,7 +43,12 @@ def main(grid: Grid, context: Context) -> None:
     )
 
     # Save final model to disk
+    bst = xgb.Booster(params=params)
+    global_model = bytearray(result.arrays["0"].numpy().tobytes())
+
+    # Load global model into booster
+    bst.load_model(global_model)
+
+    # Save model
     print("\nSaving final model to disk...")
-    model_arr = bytearray(result.arrays["0"].numpy().tobytes())
-    with open("final_model.json", "wb") as f:
-        f.write(model_arr)
+    bst.save_model("model.json")
