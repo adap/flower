96f307e4e3b597f9d86089f01d19d11e386d85f6
yan-gao-GY <yan@flower.ai>
2025-09-12 09:58:38 +0100

    Init

--- examples/xgboost-comprehensive/xgboost_comprehensive/client_app.py
+++ examples/xgboost-comprehensive/xgboost_comprehensive/client_app.py
@@ -163,3 +163,167 @@ def client_fn(context: Context):
 app = ClientApp(
     client_fn,
 )
+
+
+
+import numpy as np
+import warnings
+import xgboost as xgb
+
+from flwr.client import ClientApp
+from flwr.common import (
+    ArrayRecord,
+    ConfigRecord,
+    Context,
+    Message,
+    RecordDict,
+    MetricRecord,
+)
+from flwr.common.config import unflatten_dict
+
+from xgboost_quickstart.task import load_data, replace_keys
+
+
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+# Flower ClientApp
+app = ClientApp()
+
+
+def _local_boost(bst_input, num_local_round, train_dmatrix):
+    # Update trees based on local training data.
+    for i in range(num_local_round):
+        bst_input.update(train_dmatrix, bst_input.num_boosted_rounds())
+
+    # Bagging: extract the last N=num_local_round trees for sever aggregation
+    bst = bst_input[
+        bst_input.num_boosted_rounds()
+        - num_local_round : bst_input.num_boosted_rounds()
+    ]
+    return bst
+
+
+@app.train()
+def train(msg: Message, context: Context) -> Message:
+    # Load model and data
+    partition_id = context.node_config["partition-id"]
+    num_partitions = context.node_config["num-partitions"]
+
+    # Parse configs
+    # Flatted config dict and replace "-" with "_"
+    cfg = replace_keys(unflatten_dict(context.run_config))
+    num_local_round = cfg["local_epochs"]
+    train_method = cfg["train_method"]
+    params = cfg["params"]
+    partitioner_type = cfg["partitioner_type"]
+    seed = cfg["seed"]
+    test_fraction = cfg["test_fraction"]
+    centralised_eval_client = cfg["centralised_eval_client"]
+
+    # Load training and validation data
+    train_dmatrix, valid_dmatrix, num_train, num_val = load_data(
+        partitioner_type,
+        partition_id,
+        num_partitions,
+        centralised_eval_client,
+        test_fraction,
+        seed,
+    )
+
+    # Setup learning rate
+    if cfg["scaled_lr"]:
+        new_lr = cfg["params"]["eta"] / num_partitions
+        cfg["params"].update({"eta": new_lr})
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # Load model and data
+    partition_id = context.node_config["partition-id"]
+    num_partitions = context.node_config["num-partitions"]
+    train_dmatrix, valid_dmatrix, num_train, num_val = load_data(
+        partition_id, num_partitions
+    )
+
+    # Read from run config
+    num_local_round = context.run_config["local-epochs"]
+    # Flatted config dict and replace "-" with "_"
+    cfg = replace_keys(unflatten_dict(context.run_config))
+    params = cfg["params"]
+
+    global_round = msg.content["config"]["server-round"]
+    if global_round == 1:
+        # First round local training
+        bst = xgb.train(
+            params,
+            train_dmatrix,
+            num_boost_round=num_local_round,
+            evals=[(valid_dmatrix, "validate"), (train_dmatrix, "train")],
+        )
+    else:
+        bst = xgb.Booster(params=params)
+        global_model = bytearray(msg.content["arrays"]["0"].numpy().tobytes())
+
+        # Load global model into booster
+        bst.load_model(global_model)
+
+        # Local training
+        bst = _local_boost(bst, num_local_round, train_dmatrix)
+
+    # Save model
+    local_model = bst.save_raw("json")
+    model_np = np.frombuffer(local_model, dtype=np.uint8)
+
+    # Construct reply message
+    # Note: we store the model as the first item in a list into ArrayRecord,
+    # which can be accessed using index ["0"].
+    model_record = ArrayRecord([model_np])
+    metrics = {
+        "num-examples": num_train,
+    }
+    metric_record = MetricRecord(metrics)
+    content = RecordDict({"arrays": model_record, "metrics": metric_record})
+    return Message(content=content, reply_to=msg)
+
+
+@app.evaluate()
+def evaluate(msg: Message, context: Context) -> Message:
+    # Load model and data
+    partition_id = context.node_config["partition-id"]
+    num_partitions = context.node_config["num-partitions"]
+    _, valid_dmatrix, _, num_val = load_data(partition_id, num_partitions)
+
+    # Load config
+    cfg = replace_keys(unflatten_dict(context.run_config))
+    params = cfg["params"]
+
+    # Load global model
+    bst = xgb.Booster(params=params)
+    global_model = bytearray(msg.content["arrays"]["0"].numpy().tobytes())
+    bst.load_model(global_model)
+
+    # Run evaluation
+    eval_results = bst.eval_set(
+        evals=[(valid_dmatrix, "valid")],
+        iteration=bst.num_boosted_rounds() - 1,
+    )
+    auc = round(float(eval_results.split("\t")[1].split(":")[1]), 4)
+
+    # Construct and return reply Message
+    metrics = {
+        "auc": auc,
+        "num-examples": num_val,
+    }
+    metric_record = MetricRecord(metrics)
+    content = RecordDict({"metrics": metric_record})
+    return Message(content=content, reply_to=msg)
