c60b4ed112e4e9fe1ac2d8843e7849015f393214
Yan Gao <yan@flower.ai>
2025-09-12 21:28:00 +0800

    Update framework/py/flwr/serverapp/strategy/strategy_utils.py
    Co-authored-by: Javier <jafermarq@users.noreply.github.com>

--- framework/py/flwr/serverapp/strategy/strategy_utils.py
+++ framework/py/flwr/serverapp/strategy/strategy_utils.py
@@ -267,13 +267,12 @@ def aggregate_bagging(
     bst_prev = json.loads(bytearray(bst_prev_org))
     bst_curr = json.loads(bytearray(bst_curr_org))
 
-    bst_prev["learner"]["gradient_booster"]["model"]["gbtree_model_param"][
-        "num_trees"
-    ] = str(tree_num_prev + paral_tree_num_curr)
-    iteration_indptr = bst_prev["learner"]["gradient_booster"]["model"][
-        "iteration_indptr"
-    ]
-    bst_prev["learner"]["gradient_booster"]["model"]["iteration_indptr"].append(
+    previous_model = bst_prev["learner"]["gradient_booster"]["model"]
+    previous_model["gbtree_model_param"]["num_trees"] = str(
+        tree_num_prev + paral_tree_num_curr
+    )
+    iteration_indptr = previous_model["iteration_indptr"]
+    previous_model["iteration_indptr"].append(
         iteration_indptr[-1] + paral_tree_num_curr
     )
 
@@ -281,10 +280,8 @@ def aggregate_bagging(
     trees_curr = bst_curr["learner"]["gradient_booster"]["model"]["trees"]
     for tree_count in range(paral_tree_num_curr):
         trees_curr[tree_count]["id"] = tree_num_prev + tree_count
-        bst_prev["learner"]["gradient_booster"]["model"]["trees"].append(
-            trees_curr[tree_count]
-        )
-        bst_prev["learner"]["gradient_booster"]["model"]["tree_info"].append(0)
+        previous_model["trees"].append(trees_curr[tree_count])
+        previous_model["tree_info"].append(0)
 
     bst_prev_bytes = bytes(json.dumps(bst_prev), "utf-8")
 
