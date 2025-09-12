e99eef0e3dd87912cf2c41a672dba6ce0d832de3
yan-gao-GY <yan@flower.ai>
2025-09-12 14:33:50 +0100

    Init

--- examples/xgboost-comprehensive/xgboost_comprehensive/server_app.py
+++ examples/xgboost-comprehensive/xgboost_comprehensive/server_app.py
@@ -166,3 +166,87 @@ def server_fn(context: Context):
 app = ServerApp(
     server_fn=server_fn,
 )
+
+
+
+
+
+
+
+
+import numpy as np
+import xgboost as xgb
+
+from flwr.common import ArrayRecord, Context
+from flwr.common.config import unflatten_dict
+from flwr.server import Grid, ServerApp
+from flwr.serverapp.strategy import FedXgbBagging
+
+from xgboost_quickstart.task import replace_keys
+
+
+# Create ServerApp
+app = ServerApp()
+
+
+@app.main()
+def main(grid: Grid, context: Context) -> None:
+    # Read from config
+    # Flatted config dict and replace "-" with "_"
+    cfg = replace_keys(unflatten_dict(context.run_config))
+    num_rounds = cfg["num_server_rounds"]
+    fraction_fit = cfg["fraction_fit"]
+    fraction_evaluate = cfg["fraction_evaluate"]
+    train_method = cfg["train_method"]
+    params = cfg["params"]
+    centralised_eval = cfg["centralised_eval"]
+
+    if centralised_eval:
+        # This is the exact same dataset as the one downloaded by the clients via
+        # FlowerDatasets. However, we don't use FlowerDatasets for the server since
+        # partitioning is not needed.
+        # We make use of the "test" split only
+        test_set = load_dataset("jxie/higgs")["test"]
+        test_set.set_format("numpy")
+        test_dmatrix = transform_dataset_to_dmatrix(test_set)
+
+
+
+
+    # Read run config
+    num_rounds = context.run_config["num-server-rounds"]
+    fraction_train = context.run_config["fraction-train"]
+    fraction_evaluate = context.run_config["fraction-evaluate"]
+    # Flatted config dict and replace "-" with "_"
+    cfg = replace_keys(unflatten_dict(context.run_config))
+    params = cfg["params"]
+
+    # Init global model
+    global_model = b""  # Init with an empty object; the XGBooster will be created and trained on the client side.
+    # Note: we store the model as the first item in a list into ArrayRecord,
+    # which can be accessed using index ["0"].
+    arrays = ArrayRecord([np.frombuffer(global_model, dtype=np.uint8)])
+
+    # Initialize FedXgbBagging strategy
+    strategy = FedXgbBagging(
+        fraction_train=fraction_train,
+        fraction_evaluate=fraction_evaluate,
+    )
+
+    # Start strategy, run FedXgbBagging for `num_rounds`
+    result = strategy.start(
+        grid=grid,
+        initial_arrays=arrays,
+        num_rounds=num_rounds,
+    )
+
+    # Save final model to disk
+    bst = xgb.Booster(params=params)
+    global_model = bytearray(result.arrays["0"].numpy().tobytes())
+
+    # Load global model into booster
+    bst.load_model(global_model)
+
+    # Save model
+    print("\nSaving final model to disk...")
+    bst.save_model("model.json")
