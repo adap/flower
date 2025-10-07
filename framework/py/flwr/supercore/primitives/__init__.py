c831f9322eff24c3f50600514e8dfb0431428b7c
Javier <jafermarq@users.noreply.github.com>
2025-10-07 10:56:13 +0100

    Apply suggestions from code review
    Co-authored-by: Heng Pan <pan@flower.ai>

--- framework/py/flwr/supercore/primitives/__init__.py
+++ framework/py/flwr/supercore/primitives/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""SuperCore primitives."""
+"""Cryptographic primitives for the Flower infrastructure."""
