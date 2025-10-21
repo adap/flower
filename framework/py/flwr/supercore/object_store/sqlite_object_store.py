eab4fa9af2205e43d336b496d875bc4859586014
Heng Pan <pan@flower.ai>
2025-10-21 18:12:18 +0100

    Apply suggestion from @jafermarq
    Co-authored-by: Javier <jafermarq@users.noreply.github.com>

--- framework/py/flwr/supercore/object_store/sqlite_object_store.py
+++ framework/py/flwr/supercore/object_store/sqlite_object_store.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Flower abstract ObjectStore definition."""
+"""Flower SQLite ObjectStore implementation."""
 
 
 from typing import Optional, cast
