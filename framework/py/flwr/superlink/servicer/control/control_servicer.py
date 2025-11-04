8ceb2bc4571305ce236b655db32ef6d15f0ae165
yan-gao-GY <yan@flower.ai>
2025-11-04 17:25:12 +0000

    Update verification_dict

--- framework/py/flwr/superlink/servicer/control/control_servicer.py
+++ framework/py/flwr/superlink/servicer/control/control_servicer.py
@@ -128,9 +128,9 @@ class ControlServicer(control_pb2_grpc.ControlServicer):
             # Convert verification to dict[str, str] type
             verification_dict = {
                 item["public_key_id"]: {
-                    k: v for k, v in item.items() if k != "public_key_id"
+                    k: str(v) for k, v in item.items() if k != "public_key_id"
                 }
-                for item in verification
+                for item in verification_dict
             }
 
             # Download FAB from Hub
