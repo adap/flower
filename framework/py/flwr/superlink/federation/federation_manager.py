50adf9f8f1dd719a0d11605961547ad0d9a19afd
Javier <jafermarq@users.noreply.github.com>
2025-11-05 13:35:47 +0100

    refactor(framework): Change signature in `FederationManager` (#6088)
    Co-authored-by: Heng Pan <pan@flower.ai>

--- framework/py/flwr/superlink/federation/federation_manager.py
+++ framework/py/flwr/superlink/federation/federation_manager.py
@@ -26,8 +26,8 @@ class FederationManager(ABC):
         """Check if a federation exists."""
 
     @abstractmethod
-    def is_member(self, federation: str, flwr_aid: str) -> bool:
-        """Check if a member of the federation."""
+    def has_member(self, flwr_aid: str, federation: str) -> bool:
+        """Check if the given account is a member of the federation."""
 
     @abstractmethod
     def filter_nodes(self, node_ids: set[int], federation: str) -> set[int]:
