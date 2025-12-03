a279e614436081cb5b5005adc4bc4fd96fa6a8ae
sarahfalco <sarah.falco.s@gmail.com>
2025-12-03 18:58:49 +0100

    TLS

--- examples/customCriptography/start_client.sh
+++ examples/customCriptography/start_client.sh
@@ -1,64 +1,47 @@
 #!/bin/bash
 
-# ==========================
-#  Configurazioni da Python
-# ==========================
+NUM_CLIENTS=$(python3 -c "from flwr.common.crypto.config_cripto  import NUM_CLIENTS; print(NUM_CLIENTS)")
+# Recupero variabili da Python
 TLS=$(python3 -c "from flwr.common.crypto.config_cripto import TLS; print(TLS)")
 NUM_CLIENTS=$(python3 -c "from flwr.common.crypto.config_cripto import NUM_CLIENTS; print(NUM_CLIENTS)")
 
 LOG_DIR="logs"
 mkdir -p "$LOG_DIR"
 
-# ==========================
-#  Avvio del superlink
-# ==========================
+mkdir -p $LOG_DIR
+# Avvio del superlink
 if [ "$TLS" = "True" ]; then
-    echo "[*] Avvio superlink in modalità TLS + AUTH"
+    echo "[*] Avvio superlink in modalità TLS"
     flower-superlink \
         --ssl-ca-certfile certificates/ca.crt \
         --ssl-certfile certificates/server.pem \
         --ssl-keyfile certificates/server.key \
-        | tee "$LOG_DIR/superlink.log" &
+        >"$LOG_DIR/superlink.log" 2>&1 &
     TLS_FLAG="--root-certificates certificates/ca.crt"
-    AUTH_ENABLED=true
-
 else
-    echo "[*] Avvio SuperLink in modalità INSECURE"
-    flower-superlink --insecure > "$LOG_DIR/superlink.log" 2>&1 &
-
+    echo "[*] Avvio superlink in modalità INSECURE"
+    flower-superlink --insecure >"$LOG_DIR/superlink.log" 2>&1 &
     TLS_FLAG="--insecure"
-    AUTH_ENABLED=false
 fi
 
 sleep 2
 
-# ==========================
-#  Avvio SuperNode
-# ==========================
-CPU_THREADS=6  # Numero massimo thread CPU per client
-
+# Avvio dei supernode
 for ((i=1; i<=NUM_CLIENTS; i++)); do
-
     PORT=$((9093 + i))
     DATASET="datasets/cifar10_part_$i"
     LOG_FILE="$LOG_DIR/client_$i.log"
 
-    echo "[*] Avvio client $i con max $CPU_THREADS thread CPU"
-
-    OMP_NUM_THREADS=$CPU_THREADS \
-    MKL_NUM_THREADS=$CPU_THREADS \
-    OPENBLAS_NUM_THREADS=$CPU_THREADS \
-    NUMEXPR_NUM_THREADS=$CPU_THREADS \
-    VECLIB_MAXIMUM_THREADS=$CPU_THREADS \
-    torch_num_threads=$CPU_THREADS \
     flower-supernode \
-        --superlink 127.0.0.1:9092 \
-        --clientappio-api-address 0.0.0.0:${PORT} \
-        $TLS_FLAG \
-        --node-config "dataset-path=\"$DATASET\"" \
-        | tee "$LOG_FILE" &
+      --superlink 127.0.0.1:9092 \
+      --clientappio-api-address 0.0.0.0:${PORT} \
+      --insecure \
+      $TLS_FLAG \
+      --node-config "dataset-path=\"$DATASET\"" \
+      >"$LOG_FILE" 2>&1 &
 
     echo "[✓] Avviato client $i su porta $PORT con dataset $DATASET (log: $LOG_FILE)"
 done
 
 wait
+
