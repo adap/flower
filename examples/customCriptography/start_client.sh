d4cc12f7b85c8e6f58d289784b6cdc0855c345f1
sarahfalco <sarah.falco.s@gmail.com>
2025-12-04 17:19:55 +0100

    com

--- examples/customCriptography/start_client.sh
+++ examples/customCriptography/start_client.sh
@@ -49,7 +49,6 @@ for ((i=1; i<=NUM_CLIENTS; i++)); do
       | tee "$LOG_FILE" &
 
     echo "[✓] Avviato client $i su porta $PORT con dataset $DATASET (log: $LOG_FILE)"
-    sleep 0.3
 done
 
 wait
