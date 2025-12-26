7f6f32c6719dd64acf9f7a2a9cfdc03046932d65
Daniel J. Beutel <daniel@flower.ai>
2025-12-26 23:33:03 +0100

    Improve docstring

--- framework/py/flwr/app/message_type.py
+++ framework/py/flwr/app/message_type.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Flower constants."""
+"""MessageType constants."""
 
 from __future__ import annotations
 
