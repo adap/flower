00b9f2ee5bd49ed45d04ddd31dd50ea5ce7e98db
Javier <jafermarq@users.noreply.github.com>
2026-01-15 12:23:04 +0000

    Update framework/py/flwr/cli/config/ls.py
    Co-authored-by: Heng Pan <pan@flower.ai>

--- framework/py/flwr/cli/config/ls.py
+++ framework/py/flwr/cli/config/ls.py
@@ -14,6 +14,7 @@
 # ==============================================================================
 """Flower command line interface `config list` command."""
 
+
 import io
 import json
 from typing import Annotated
