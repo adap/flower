50f867b22fcac1ca3d80a765b66674e6837a5b12
Chong Shen Ng <chong.shen@flower.ai>
2026-01-16 15:19:37 +0000

    Remove unused file

--- framework/py/flwr/supercore/state/schema/base.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import declarative_base
-
-from flwr.supercore.state.schema.corestate_tables import (  # noqa: F401
-    corestate_metadata,
-)
-
-Base = declarative_base(metadata=corestate_metadata)
