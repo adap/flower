2b9c15aea77ad23e8afd1c14f7070db32cdb4c37
jafermarq <javier@flower.ai>
2026-01-21 17:48:02 +0000

    simplify

--- framework/e2e/test_serverapp_heartbeat.py
+++ framework/e2e/test_serverapp_heartbeat.py
@@ -6,9 +6,6 @@ import subprocess
 import sys
 import time
 
-import tomli
-import tomli_w
-
 from flwr.common.constant import (
     SERVERAPPIO_API_DEFAULT_CLIENT_ADDRESS,
     SIMULATIONIO_API_DEFAULT_CLIENT_ADDRESS,
@@ -24,27 +21,6 @@ address_arg = (
     else SERVERAPPIO_API_DEFAULT_CLIENT_ADDRESS
 )
 app_cmd = "flwr-simulation" if use_sim else "flwr-serverapp"
-
-
-def add_e2e_federation() -> None:
-    """Add e2e federation to pyproject.toml."""
-    # Load the pyproject.toml file
-    with open("pyproject.toml", "rb") as f:
-        pyproject = tomli.load(f)
-
-    # Remove e2e federation if exists
-    pyproject["tool"]["flwr"]["federations"].pop("e2e", None)
-
-    # Add e2e federation
-    pyproject["tool"]["flwr"]["federations"]["e2e"] = {
-        "address": "127.0.0.1:9093",
-        "insecure": True,
-        "options": {"num-supernodes": 0},
-    }
-
-    # Write the updated pyproject.toml file
-    with open("pyproject.toml", "wb") as f:
-        tomli_w.dump(pyproject, f)
 
 
 def run_superlink() -> subprocess.Popen:
@@ -128,9 +104,6 @@ def main() -> None:
 
     # Determine if the test is running in simulation mode
     print(f"Running in {'simulation' if use_sim else 'deployment'} mode.")
-
-    # Add e2e federation to pyproject.toml
-    add_e2e_federation()
 
     # Start the SuperLink
     print("Starting SuperLink...")
