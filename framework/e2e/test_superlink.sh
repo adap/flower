7b9b0f499c2407a2707741b47e4eb7aad4b429e7
Heng Pan <pan@flower.ai>
2026-01-21 19:30:47 +0000

    reset

--- framework/e2e/test_superlink.sh
+++ framework/e2e/test_superlink.sh
@@ -61,14 +61,6 @@ esac
 # Install Flower app
 pip install -e . --no-deps
 
-# revert changes if any in pyproject.toml
-# This is needed for multi-stage CI tests that 
-# perform migrations more than once. A conflict
-# arise when toml-federations are migrated and then 
-# re-injected with commands below. It's safer to
-# start from a clean slate.
-git checkout pyproject.toml
-
 # Remove any duplicates
 sed -i '/^\[tool\.flwr\.federations\.e2e\]/,/^$/d' pyproject.toml
 
@@ -119,13 +111,10 @@ cleanup_and_exit() {
     exit $1
 }
 
-# Run `flwr ls` in deprecated mode to trigger migration
-flwr ls . e2e
-
-# Check for "finished:completed" status in a loop with a gtimeout
-while [ "$found_success" = false ] && [ $elapsed -lt $gtimeout ]; do
+# Check for "finished:completed" status in a loop with a timeout
+while [ "$found_success" = false ] && [ $elapsed -lt $timeout ]; do
     # Run the command and capture output
-    output=$(flwr ls e2e --format=json)
+    output=$(flwr ls . e2e --format=json)
 
     # Extract status from the first run (or loop over all if needed)
     status=$(echo "$output" | jq -r '.runs[0].status')
