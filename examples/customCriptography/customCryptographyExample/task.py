dc3caf753e341208dff8592116a2657fb68e2d12
sarahfalco <sarah.falco.s@gmail.com>
2026-01-21 21:16:41 +0100

    com

--- examples/customCriptography/customCryptographyExample/task.py
+++ examples/customCriptography/customCryptographyExample/task.py
@@ -117,7 +117,7 @@ def get_model(model_name: str, num_classes=10, pretrained=True):
         model.fc = nn.Linear(model.fc.in_features, num_classes)
         return model
     elif model_name == "resnet34":
-        model = resnet34(pretrained=pretrained)
+        model = resnet34(pretrained=True)
         # 🔧 adattamento per CIFAR (32x32)
         model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         model.maxpool = nn.Identity()  # rimuove maxpool iniziale
