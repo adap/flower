5ef8c10721669a5276f5eefbef7f5735f8bff72a
jafermarq <javier@flower.ai>
2026-02-16 14:52:56 +0000

    init

--- framework/py/flwr/cli/app.py
+++ framework/py/flwr/cli/app.py
@@ -26,6 +26,7 @@ from .app_cmd import publish as app_publish
 from .app_cmd import review as app_review
 from .build import build
 from .config import ls as config_list
+from .federation import create as federation_create
 from .federation import ls as federation_list
 from .install import install
 from .log import log
@@ -91,6 +92,7 @@ federation_app = typer.Typer(help="Manage Federations")
 federation_app.command("list", **ALLOW_EXTRAS)(federation_list)
 # Hide "ls" command (left as alias)
 federation_app.command(hidden=True, **ALLOW_EXTRAS)(federation_list)
+federation_app.command()(federation_create)
 app.add_typer(federation_app, name="federation")
 
 # Create config command group
