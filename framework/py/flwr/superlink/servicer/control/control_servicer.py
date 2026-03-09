ad511ef42bf189baac17d81a5283ae8f189dd929
Chong Shen Ng <chong.shen@flower.ai>
2026-03-09 15:21:22 +0000

    refactor(framework): Align log text with `ListRuns` method (#6724)

--- framework/py/flwr/superlink/servicer/control/control_servicer.py
+++ framework/py/flwr/superlink/servicer/control/control_servicer.py
@@ -285,7 +285,7 @@ class ControlServicer(control_pb2_grpc.ControlServicer):
         self, request: ListRunsRequest, context: grpc.ServicerContext
     ) -> ListRunsResponse:
         """Handle `flwr ls` command."""
-        log(INFO, "ControlServicer.List")
+        log(INFO, "ControlServicer.ListRuns")
         state = self.linkstate_factory.state()
 
         flwr_aid = _check_flwr_aid_exists(get_current_account_info().flwr_aid, context)
