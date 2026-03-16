ca44777d7c7f0187885f412aed588db890ba74b7
Daniel J. Beutel <daniel@flower.ai>
2026-03-16 17:54:08 +0100

    ci(devtool): Add mypy and pytest to devtool (#6770)

--- dev/test-devtool.sh
+++ dev/test-devtool.sh
@@ -12,8 +12,16 @@ echo "- black: start"
 python -m black --check devtool
 echo "- black: done"
 
+echo "- mypy: start"
+python -m mypy devtool
+echo "- mypy: done"
+
 echo "- pylint: start"
 PYLINTHOME=.pylint.d python -m pylint devtool
 echo "- pylint: done"
+
+echo "- pytest: start"
+python -m pytest devtool
+echo "- pytest: done"
 
 echo "- All devtool Python checks passed"
