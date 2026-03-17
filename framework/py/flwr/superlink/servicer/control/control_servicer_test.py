e04acd794bc8bdc0860ccaeb0299e3d7848d907a
jafermarq <javier@flower.ai>
2026-03-17 09:34:51 +0000

    w/ prev

--- framework/py/flwr/superlink/servicer/control/control_servicer_test.py
+++ framework/py/flwr/superlink/servicer/control/control_servicer_test.py
@@ -14,6 +14,7 @@
 # ==============================================================================
 """Test the Control API servicer."""
 
+# pylint: disable=too-many-lines
 
 import hashlib
 import json
