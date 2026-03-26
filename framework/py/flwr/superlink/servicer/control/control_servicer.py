b7518c25834b38a65be0bb2fa559ce6522f53e8a
Heng Pan <pan@flower.ai>
2026-03-26 12:46:39 +0000

    feat(framework): Derive `run_type` from federation (#6848)

--- framework/py/flwr/superlink/servicer/control/control_servicer.py
+++ framework/py/flwr/superlink/servicer/control/control_servicer.py
@@ -171,7 +171,6 @@ class ControlServicer(control_pb2_grpc.ControlServicer):
         flwr_aid = _get_flwr_aid(context)
         override_config = user_config_from_proto(request.override_config)
         federation_options = config_record_from_proto(request.federation_options)
-        run_type = RunType.SIMULATION if federation_options else RunType.SERVER_APP
 
         with rpc_error_translator(context, rpc_name):
             # Check (1) federation exists and (2) the flwr_aid is a member
@@ -192,6 +191,11 @@ class ControlServicer(control_pb2_grpc.ControlServicer):
                     f"Account with ID '{flwr_aid}' is not a member of the "
                     f"federation '{federation}'.",
                 )
+
+            # Derive run type based on the presence of simulation config
+            run_type = RunType.SERVER_APP
+            if state.federation_manager.get_simulation_config(federation) is not None:
+                run_type = RunType.SIMULATION
 
         try:
             # Validate user config overrides matches keys in run config in FAB
